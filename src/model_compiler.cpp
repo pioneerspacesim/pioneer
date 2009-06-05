70a3c577e911acf6c930d8184ba2e936f4e38628
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2009-06-05 19:04:28 +0000

    more sbre object types implemented in model_compiler
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@294 e632f14b-6550-0410-b89e-a82653faca30

--- src/model_compiler.cpp
+++ src/model_compiler.cpp
@@ -45,6 +45,23 @@ public:
 		OPENBRACE, CLOSEBRACE, ASSIGN, COMMA, COLON, FLOAT,
 		INTEGER, IDENTIFIER
 	} type;
+	static std::string ToString(token_type_t type) {
+		switch (type) {
+			case Token::OPENBRACKET: return "(";
+			case Token::CLOSEBRACKET: return ")";
+			case Token::OPENBRACE: return "{";
+			case Token::CLOSEBRACE: return "}";
+			case Token::ASSIGN: return "=";
+			case Token::COMMA: return ",";
+			case Token::FLOAT: return "float";
+			case Token::INTEGER: return "integer";
+			case Token::IDENTIFIER: return "identifier";
+			case Token::COLON: return ":";
+			case Token::END: return "End of file";
+			default:
+					 return "??unknown??";
+		}
+	}
 	int line;
 	union value {
 		int i;
@@ -64,7 +81,7 @@ public:
 		abort();
 	}
 	void Check(enum token_type_t type) {
-		if (this->type != type) Error("Unexpected token type %d", type);
+		if (this->type != type) Error("Unexpected token %s, expected %s", ToString(this->type).c_str(), ToString(type).c_str());
 	}
 	float GetFloat() {
 		if (type == INTEGER) {
@@ -75,8 +92,14 @@ public:
 		Error("Expected float");
 		return 0;
 	}
-	bool MatchIdentifier(const char *str) {
+	bool IsIdentifier(const char *str) {
 		return (type == IDENTIFIER) && (strcmp(str, val.s)==0);
+	}
+	void MatchIdentifier(const char *str) {
+		if (!IsIdentifier(str)) {
+			if (type == IDENTIFIER) Error("Expected identifier '%s', got '%s'", str, val.s);
+			else Error("Expected identifer '%s', got token '%s'", str, ToString(type).c_str());
+		}
 	}
 };
 
@@ -163,10 +186,13 @@ void lex(const char *crud, std::vector<Token> &tokens)
 
 typedef std::vector<Token>::iterator tokenIter_t;
 
+int modelNum;
 static std::map<std::string, Model*> models;
+static std::map<std::string, int> models_idx;
 static std::vector<vector3f> vertices;
 static std::map<std::string, int> vertex_identifiers;
-static Model *cur_model;
+static std::vector<Uint16> instrs;
+int sbre_cache_size;
 
 static bool VertexIdentifierExists(const char *vid)
 {
@@ -181,9 +207,9 @@ static int parsePlainVtx(tokenIter_t &t)
 {
 	vector3f v;
 	bool normalize = false;
-	if ((*t).MatchIdentifier("v")) {
+	if ((*t).IsIdentifier("v")) {
 
-	} else if ((*t).MatchIdentifier("n")) {
+	} else if ((*t).IsIdentifier("n")) {
 		normalize = true;
 	} else {
 		(*t).Error("Expected vertex");
@@ -210,8 +236,8 @@ static int parsePlainVtxOrVtxRef(tokenIter_t &t)
 {
 	//int numPVtx;				// number of plain vertices + 6
 	(*t).Check(Token::IDENTIFIER);
-	if ((*t).MatchIdentifier("v") ||
-	    (*t).MatchIdentifier("n")) {
+	if ((*t).IsIdentifier("v") ||
+	    (*t).IsIdentifier("n")) {
 		return parsePlainVtx(t);
 	} else {
 		printf("Turd\n");
@@ -224,16 +250,92 @@ static int parsePlainVtxOrVtxRef(tokenIter_t &t)
 	}
 }
 
+static void comp_get_line_bits(tokenIter_t &t)
+{
+	int num = 0;
+	for (;;) {
+		printf("aub: %s\n", (*t).val.s);
+		if ((*t).IsIdentifier("hermite")) {
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			Uint16 v1 = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 v2 = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 v3 = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(LTYPE_HERMITE);
+			instrs.push_back(v1);
+			instrs.push_back(v2);
+			instrs.push_back(v3);
+			num++;
+		} else if ((*t).IsIdentifier("line")) {
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			Uint16 v1 = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(LTYPE_LINE);
+			instrs.push_back(v1);
+			num++;
+		} else {
+			(*t).Error("Expected a hermite or line segment");
+		}
+		if ((*t).type == Token::CLOSEBRACKET) {
+			break;
+		} else if ((*t).type == Token::COMMA) {
+			t++;
+		} else {
+			break;
+		}
+	}
+	if (num<2) (*t).Error("Expected at least 2 hermite or line segments");
+	else if (num>4) (*t).Error("Expected at most 4 hermite or line segments");
+}
+
+static int get_model_reference(tokenIter_t &t)
+{
+	(*t).Check(Token::IDENTIFIER);
+
+	std::map<std::string, int>::iterator i = models_idx.find((*t).val.s);
+	if (i == models_idx.end()) {
+		(*t).Error("Unknown model '%s'", (*t).val.s);
+		return -1;
+	} else {
+		int model = (*i).second;
+		++t;
+		return model;
+	}
+}
+
+static Uint16 get_anim(tokenIter_t &t)
+{
+	(*t++).MatchIdentifier("anim");
+	(*t++).Check(Token::ASSIGN);
+	if ((*t).type != Token::INTEGER) {
+		(*t).Error("Integer anim expected");
+	} else {
+		Uint16 anim = (*t).val.i;
+		if (anim > 9) {
+			(*t).Error("Anim must be in range 0-9");
+		}
+		++t;
+		return anim;
+	}
+}
+
 void parseModel(tokenIter_t &t)
 {
-	std::vector<Uint16> instrs;
+	const char *modelName;
+	sbre_cache_size = 0;
+	vertices.clear();
+	vertex_identifiers.clear();
+	instrs.clear();
 	Model *m = new Model;
 	memset(m, 0, sizeof(Model));
-
 	(*t++).Check(Token::OPENBRACKET);
 	(*t).Check(Token::IDENTIFIER);
 	printf("Model %s:\n", (*t).val.s);
-	models[(*t).val.s] = m;
+	modelName = (*t).val.s;
 	t++;
 	(*t++).Check(Token::CLOSEBRACKET);
 	(*t++).Check(Token::OPENBRACE);
@@ -246,24 +348,29 @@ void parseModel(tokenIter_t &t)
 		bool flag_ycenter = false;
 		bool flag_xref = false;
 		bool flag_invisible = false;
+		bool flag_thrust = false;
 		// first the flags
 		bool got_flag;
 		do {
 			got_flag = false;
 
-			if ((*t).MatchIdentifier("xref")) {
+			if ((*t).IsIdentifier("xref")) {
 				flag_xref = true;
 				got_flag = true;
 			}
-			else if ((*t).MatchIdentifier("ycenter")) {
+			else if ((*t).IsIdentifier("thrust")) {
+				flag_thrust = true;
+				got_flag = true;
+			}
+			else if ((*t).IsIdentifier("ycenter")) {
 				flag_ycenter = true;
 				got_flag = true;
 			}
-			else if ((*t).MatchIdentifier("xcenter")) {
+			else if ((*t).IsIdentifier("xcenter")) {
 				flag_xcenter = true;
 				got_flag = true;
 			}
-			else if ((*t).MatchIdentifier("invisible")) {
+			else if ((*t).IsIdentifier("invisible")) {
 				flag_invisible = true;
 				got_flag = true;
 			}
@@ -278,8 +385,8 @@ static const int TFLAG_YCENTER = 0x4000;
 static const int RFLAG_XREF = 0x8000;
 static const int RFLAG_INVISIBLE = 0x4000;*/
 	
-		if ((*t).MatchIdentifier("tri")) {
-			if (flag_xcenter || flag_ycenter) (*t).Error("Invalid flag");
+		if ((*t).IsIdentifier("tri")) {
+			if (flag_xcenter || flag_ycenter || flag_thrust) (*t).Error("Invalid flag");
 			++t;
 			(*t++).Check(Token::OPENBRACKET);
 			Uint16 v1 = parsePlainVtxOrVtxRef(t);
@@ -295,8 +402,8 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 			instrs.push_back(v2);
 			instrs.push_back(v3);
 	
-		} else if ((*t).MatchIdentifier("quad")) {
-			if (flag_xcenter || flag_ycenter) (*t).Error("Invalid flag");
+		} else if ((*t).IsIdentifier("quad")) {
+			if (flag_xcenter || flag_ycenter || flag_thrust) (*t).Error("Invalid flag");
 			++t;
 			(*t++).Check(Token::OPENBRACKET);
 			Uint16 v1 = parsePlainVtxOrVtxRef(t);
@@ -314,6 +421,167 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 			instrs.push_back(v2);
 			instrs.push_back(v3);
 			instrs.push_back(v4);
+
+		} else if ((*t).IsIdentifier("circle")) {
+			
+			if (flag_xcenter || flag_ycenter || flag_invisible || flag_thrust) (*t).Error("Invalid flag");
+
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			(*t).Check(Token::INTEGER);
+			Uint16 steps = (*t++).val.i;
+			(*t++).Check(Token::COMMA);
+			Uint16 vtx = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 norm = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 up = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			(*t++).MatchIdentifier("radius");
+			(*t++).Check(Token::ASSIGN);
+			int rad = (int)((*t).GetFloat() * 100.0);
+			if ((rad <= 0) || (rad > 65535))
+				(*t).Error("Circle radius must be in range 0.01 to 655");
+			++t;
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(PTYPE_CIRCLE |
+					(flag_xref ? RFLAG_XREF : 0));
+			instrs.push_back(0x8000); // not cached
+			instrs.push_back(steps);
+			instrs.push_back(vtx);
+			instrs.push_back(norm);
+			instrs.push_back(up);
+			instrs.push_back(rad);
+
+		} else if ((*t).IsIdentifier("cylinder")) {
+
+			if (flag_xcenter || flag_ycenter || flag_invisible || flag_thrust) (*t).Error("Invalid flag");
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			(*t).Check(Token::INTEGER);
+			Uint16 steps = (*t++).val.i;
+			(*t++).Check(Token::COMMA);
+			Uint16 startvtx = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 endvtx = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 updir = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			(*t++).MatchIdentifier("radius");
+			(*t++).Check(Token::ASSIGN);
+			int rad = (int)((*t).GetFloat() * 100.0);
+			if ((rad <= 0) || (rad > 65535))
+				(*t).Error("Cylinder radius must be in range 0.01 to 655");
+			++t;
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(PTYPE_CYLINDER |
+					(flag_xref ? RFLAG_XREF : 0));
+			instrs.push_back(sbre_cache_size++);
+			instrs.push_back(steps);
+			instrs.push_back(startvtx);
+			instrs.push_back(endvtx);
+			instrs.push_back(updir);
+			instrs.push_back(rad);
+
+		} else if ((*t).IsIdentifier("tube")) {
+
+			if (flag_xcenter || flag_ycenter || flag_invisible || flag_thrust) (*t).Error("Invalid flag");
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			(*t).Check(Token::INTEGER);
+			Uint16 steps = (*t++).val.i;
+			(*t++).Check(Token::COMMA);
+			Uint16 startvtx = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 endvtx = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 updir = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			(*t++).MatchIdentifier("innerrad");
+			(*t++).Check(Token::ASSIGN);
+			int innerrad = (int)((*t).GetFloat() * 100.0);
+			if ((innerrad <= 0) || (innerrad > 65535))
+				(*t).Error("Tube innerrad must be in range 0.01 to 655");
+			++t;
+			(*t++).Check(Token::COMMA);
+			(*t++).MatchIdentifier("outerrad");
+			(*t++).Check(Token::ASSIGN);
+			int outerrad = (int)((*t).GetFloat() * 100.0);
+			if ((outerrad <= 0) || (outerrad > 65535))
+				(*t).Error("Tube innerrad must be in range 0.01 to 655");
+			++t;
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(PTYPE_TUBE |
+					(flag_xref ? RFLAG_XREF : 0));
+			instrs.push_back(sbre_cache_size++);
+			instrs.push_back(steps);
+			instrs.push_back(startvtx);
+			instrs.push_back(endvtx);
+			instrs.push_back(updir);
+			instrs.push_back(outerrad);
+			instrs.push_back(innerrad);
+
+		} else if ((*t).IsIdentifier("smooth")) {
+			if (flag_xcenter || flag_ycenter || flag_invisible || flag_thrust) (*t).Error("Invalid flag");
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			(*t).Check(Token::INTEGER);
+			int steps = (*t).val.i;
+			if ((steps<1) || (steps>99)) (*t).Error("Too many steps for smooth(). Must be 1-99.");
+			
+			instrs.push_back(PTYPE_COMPSMOOTH2 |
+					(flag_xref ? RFLAG_XREF : 0) | 
+					(flag_invisible ? RFLAG_INVISIBLE : 0));
+			instrs.push_back(sbre_cache_size++);
+			instrs.push_back(steps);
+
+			++t;
+			(*t++).Check(Token::COMMA);
+			Uint16 v1 = parsePlainVtxOrVtxRef(t);
+			instrs.push_back(v1);
+			(*t++).Check(Token::COMMA);
+
+			comp_get_line_bits(t);
+			(*t++).Check(Token::CLOSEBRACKET);
+			instrs.push_back(LTYPE_END);
+
+		} else if ((*t).IsIdentifier("subobject")) {
+			
+			if (flag_xcenter || flag_ycenter || flag_invisible || flag_xref) (*t).Error("Invalid flag");
+			++t;
+			(*t++).Check(Token::OPENBRACKET);
+			Uint16 model = get_model_reference(t);
+			(*t++).Check(Token::COMMA);
+
+			Uint16 offset = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 norm = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			Uint16 zaxis = parsePlainVtxOrVtxRef(t);
+			(*t++).Check(Token::COMMA);
+			
+			(*t++).MatchIdentifier("scale");
+			(*t++).Check(Token::ASSIGN);
+			int scale = (int)((*t).GetFloat() * 100.0);
+			if ((scale <= 0) || (scale > 65535))
+				(*t).Error("subobject scale must be in range 0.01 to 655");
+			++t;
+			Uint16 anim = 0x8000;
+			if ((*t).type == Token::COMMA) {
+				// get anim
+				++t;
+				anim = get_anim(t);
+			}
+			(*t++).Check(Token::CLOSEBRACKET);
+
+			instrs.push_back(PTYPE_SUBOBJECT | (flag_thrust ? SUBOBJ_THRUST : 0));
+			instrs.push_back(anim);
+			instrs.push_back(model);
+			instrs.push_back(offset);
+			instrs.push_back(norm);
+			instrs.push_back(zaxis);
+			instrs.push_back(scale);
+
 		} else {
 			printf("assignment\n");
 			// assignment
@@ -333,7 +601,7 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 	printf("%d plain vertices\n", vertices.size());
 	m->numPVtx = 6+vertices.size();
 	m->pPVtx = new PlainVertex[vertices.size()];
-	for (int i=0; i<vertices.size(); i++) {
+	for (int i=0; i<(signed)vertices.size(); i++) {
 		m->pPVtx[i].type = VTYPE_PLAIN;
 		m->pPVtx[i].pos.x = vertices[i].x;
 		m->pPVtx[i].pos.y = vertices[i].y;
@@ -342,7 +610,7 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 	m->scale = 1.0;
 	m->radius = 1.0;
 
-	m->numCache = 0;
+	m->numCache = sbre_cache_size;
 	
 	m->pLOD[0].pixrad = 0;
 	m->pLOD[0].pData1 = new Uint16[instrs.size()];
@@ -351,9 +619,11 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 	m->pLOD[0].pThruster = 0;
 
 	Uint16 *data = m->pLOD[0].pData1;
-	for (int i=0; i<instrs.size(); i++) {
+	for (int i=0; i<(signed)instrs.size(); i++) {
+		printf("%d ", instrs[i]);
 		*(data++) = instrs[i];
 	}
+	printf("\n");
 
 	// dummy complex vertex
 	m->cvStart = m->numPVtx;
@@ -365,7 +635,9 @@ static const int RFLAG_INVISIBLE = 0x4000;*/
 	m->pCVtx[0].pParam[3] = -1;
 	m->pCVtx[0].pParam[4] = -1;
 
-	ppModel[0] = m;
+	ppModel[modelNum] = m;
+	models_idx[modelName] = modelNum++;
+	models[modelName] = m;
 }
 
 void parse(std::vector<Token> &tokens)
@@ -373,10 +645,10 @@ void parse(std::vector<Token> &tokens)
 	std::vector<Token>::iterator i = tokens.begin();
 
 	while (i != tokens.end()) {
-		if ((*i).MatchIdentifier("model")) {
+		if ((*i).IsIdentifier("model")) {
 			++i;
 			parseModel(i);
-			return;
+			(*i++).Check(Token::CLOSEBRACE);
 		} else if ((*i).type == Token::END) {
 			break;
 		} else {
