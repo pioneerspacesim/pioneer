bcf8d51cff6c85d71958d3ec015b8296f5a250d6
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2009-10-12 13:19:16 +0000

    even even more betterer kepler doing!
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@435 e632f14b-6550-0410-b89e-a82653faca30

--- src/StarSystem.cpp
+++ src/StarSystem.cpp
@@ -296,17 +296,17 @@ vector3d Orbit::OrbitalPosAtTime(double t)
 	const double M = 2*M_PI*t / period;
 	// eccentric anomaly
 	// NR method to solve for E: M = E-sin(E)
-	double E = M + e*0.5;
-	for (int iter=40; iter > 0; --iter) {
+	double E = M;
+	for (int iter=5; iter > 0; --iter) {
 		E = E - (E-e*(sin(E))-M) / (1.0 - e*cos(E));
 	}
-	// true anomaly (angle of orbit position)
-	double v = 2*atan(sqrt((1+e)/(1-e)) * tan(E/2.0));
-//	double v = acos( (cos(E) - e) / (1.0 - e*cos(E)) );
 	// heliocentric distance
-	double r = semiMajorAxis * (1 - e*e) / (1 + e*cos(v));
+	double r = semiMajorAxis * (1.0 - e*cos(E));
+	// true anomaly (angle of orbit position)
+	double cos_v = (cos(E) - e) / (1.0 - e*cos(E));
+	double sin_v = (sqrt(1.0-e*e)*sin(E))/ (1.0 - e*cos(E));
 
-	vector3d pos = vector3d(cos(v)*r, sin(v)*r, 0);
+	vector3d pos = vector3d(cos_v*r, sin_v*r, 0);
 	pos = rotMatrix * pos;
 	return pos;
 }
