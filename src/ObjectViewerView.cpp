3e7211fd6dd12b8bebb89ba8e0b9a8aa65140880
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2010-01-03 17:40:14 +0000

    fix object viewer
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@518 e632f14b-6550-0410-b89e-a82653faca30

--- src/ObjectViewerView.cpp
+++ src/ObjectViewerView.cpp
@@ -12,7 +12,7 @@ ObjectViewerView::ObjectViewerView(): View()
 	m_camRot = matrix4x4d::Identity();
 	
 	m_infoLabel = new Gui::Label("");
-	Add(m_infoLabel, 2, 2);
+	Add(m_infoLabel, 2, Gui::Screen::GetHeight()-66-Gui::Screen::GetFontHeight());
 }
 
 void ObjectViewerView::Draw3D()
@@ -34,21 +34,16 @@ void ObjectViewerView::Draw3D()
 	if (Pi::MouseButtonState(3)) {
 		int m[2];
 		Pi::GetMouseMotion(m);
-		m_camRot = m_camRot * matrix4x4d::RotateXMatrix(0.002*m[1]) *
-				matrix4x4d::RotateYMatrix(0.002*m[0]);
+		m_camRot = matrix4x4d::RotateXMatrix(-0.002*m[1]) *
+				matrix4x4d::RotateYMatrix(-0.002*m[0]) * m_camRot;
 	}
-	vector3d pos = m_camRot * vector3d(0,0,viewingDist);
 		
 	float lightPos[4] = { .577, .577, .577, 0 };
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 	
 	Body *body = Pi::player->GetNavTarget();
 	if (body) {
-		Frame cam_frame(body->GetFrame(), "", Frame::TEMP_VIEWING);
-		cam_frame.SetOrientation(m_camRot);
-		cam_frame.SetPosition(body->GetPosition()+pos);
-		body->Render(vector3d(0.0), matrix4x4d::Identity());
-		body->GetFrame()->RemoveChild(&cam_frame);
+		body->Render(vector3d(0,0,-viewingDist), m_camRot);
 	}
 }
 
@@ -65,12 +60,12 @@ void ObjectViewerView::Update()
 
 	char buf[128];
 	Body *body = Pi::player->GetNavTarget();
-	if(body && body != lastTarget) {
+	if(body && (body != lastTarget)) {
 		// Reset view distance for new target.
 		viewingDist = body->GetBoundingRadius() * 2.0f;
 		lastTarget = body;
 	}
-	snprintf(buf, sizeof(buf), "View dist: %.2f     Object: %s", viewingDist, (body ? body->GetLabel().c_str() : "<none>"));
+	snprintf(buf, sizeof(buf), "View dist: %s     Object: %s", format_distance(viewingDist).c_str(), (body ? body->GetLabel().c_str() : "<none>"));
 	m_infoLabel->SetText(buf);
 }
 
