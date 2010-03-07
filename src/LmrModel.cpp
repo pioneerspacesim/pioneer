d8e9db5c84f8ed4b618d67798872da5528cc1fec
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2010-03-07 21:21:17 +0000

    fix delete/free mismatch
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@551 e632f14b-6550-0410-b89e-a82653faca30

--- src/LmrModel.cpp
+++ src/LmrModel.cpp
@@ -858,10 +858,11 @@ int LmrCollMesh::GetTrisWithGeomflag(unsigned int flags, int num, vector3d *outV
 
 LmrCollMesh::~LmrCollMesh()
 {
+	// nice. mixed allocation. for the love of realloc...
 	delete geomTree;
-	delete pVertex;
-	delete pIndex;
-	delete pFlag;
+	free(pVertex);
+	free(pIndex);
+	free(pFlag);
 }
 
 LmrModel *LmrLookupModelByName(const char *name) throw (LmrModelNotFoundException)
