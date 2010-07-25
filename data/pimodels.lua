14788cef347c97f5f149c3dff48fcceda04a2c89
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2010-07-25 12:12:20 +0000

    oops. this needed for gernot models
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@634 e632f14b-6550-0410-b89e-a82653faca30

--- data/pimodels.lua
+++ data/pimodels.lua
@@ -68,6 +68,8 @@ function lerp_materials(a, m1, m2)
 end
 
 --dofile "data/models/adverts.lua"
+load_lua(CurrentDirectory .. "/model_functions")
+load_lua(CurrentDirectory .. "/sub_models")
 load_lua(CurrentDirectory .. "/models")
 
 poo = 0
