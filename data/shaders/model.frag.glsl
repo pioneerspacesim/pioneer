51939b78e317d777bc2ff7776d6904e9430845d9
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2010-09-05 15:09:52 +0000

    removed r300g glsl hack since it is almost no longer needed
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@665 e632f14b-6550-0410-b89e-a82653faca30

--- data/shaders/model.frag.glsl
+++ data/shaders/model.frag.glsl
@@ -8,24 +8,9 @@ void main(void)
 	vec4 amb = vec4(0.0);
 	vec4 diff = vec4(0.0);
 	vec4 spec = vec4(0.0);
-#if defined(VENDOR_R300_GALLIUM)
-	{ /* for the love of a buggy alpha-stage driver :) */
-		float nDotVP; 
-		float nDotHV;         
-		nDotVP = max(0.0, dot(tnorm, normalize(vec3(gl_LightSource[0].position))));
-		nDotHV = max(0.0, dot(tnorm, vec3(gl_LightSource[0].halfVector)));
-		if (nDotVP != 0.0) {
-			float pf = pow(nDotHV, gl_FrontMaterial.shininess);
-			spec += gl_LightSource[0].specular * pf;
-		}
-		amb += gl_LightSource[0].ambient;
-		diff += gl_LightSource[0].diffuse * nDotVP;
-	}
-#else
 	for (int i=0; i<NUM_LIGHTS; ++i) {
 		DirectionalLight(i, tnorm, amb, diff, spec);
 	}
-#endif
 	gl_FragColor = gl_FrontLightModelProduct.sceneColor +
 		amb * gl_FrontMaterial.ambient +
 		diff * gl_FrontMaterial.diffuse +
