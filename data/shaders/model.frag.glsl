7bff95cee95d40762c7d01bd5d1a83dc6e60a7c6
tompox <tompox@e632f14b-6550-0410-b89e-a82653faca30>
2010-09-24 14:36:24 +0000

    make the model fragment shader more closely match fixed function (like actually applying the material emission...). still there is a cockup on textured, lots of difference between the fixed function and shadered rendering of the church model
    git-svn-id: https://pioneer.svn.sourceforge.net/svnroot/pioneer/trunk@703 e632f14b-6550-0410-b89e-a82653faca30

--- data/shaders/model.frag.glsl
+++ data/shaders/model.frag.glsl
@@ -11,10 +11,12 @@ void main(void)
 	for (int i=0; i<NUM_LIGHTS; ++i) {
 		DirectionalLight(i, tnorm, amb, diff, spec);
 	}
-	gl_FragColor = gl_FrontLightModelProduct.sceneColor +
+	gl_FragColor = 
+		gl_LightModel.ambient * gl_FrontMaterial.ambient +
 		amb * gl_FrontMaterial.ambient +
 		diff * gl_FrontMaterial.diffuse +
-		spec * gl_FrontMaterial.specular;
+		spec * gl_FrontMaterial.specular +
+		gl_FrontMaterial.emission;
 
 	if ( usetex )
 		gl_FragColor *= texture2D(tex, gl_TexCoord[0].st);
