e6ca64d927ffe5e31eb5552ffbb8785c341d06b1
Robert Norris <rob@eatenbyagrue.org>
2011-03-17 18:05:26 +1100

    spawn bulk ships everywhere :)

--- data/modules/BulkShips.lua
+++ data/modules/BulkShips.lua
@@ -7,13 +7,40 @@ Module:new {
 
 	onEnterSystem = function(self)
 		local sys = Pi.GetCurrentSystem()
-		local starport = sys:GetRandomStarport(Pi.rand)
+		local population = sys:GetSystemPopulation()
 
-		print(string.format("spawning static ship near %s in system %s", starport:GetBodyName(), sys:GetSystemName()))
+		if population == 0 then
+			return
+		end
 
-		local ship, e = Pi.SpawnRandomStaticShip(Pi.FindBodyForSBody(starport))
-		if e then
-			print("BulkShips: "..e)
+		--[[
+		assuming these are huge supply ships and not your run-of-the-mill
+		traders and not warships or whatever, we'll do it like this:
+
+		- first one is free
+		- one ship per billion up to 4 billion
+		- one ship per 5 billion after that
+		]]
+
+		local num_bulk_ships = 1;
+		while population > 1 do
+			if num_bulk_ships < 4 then
+				population = population-1
+				num_bulk_ships = num_bulk_ships+1
+			elseif population > 5 then
+				population = population-5
+				num_bulk_ships = num_bulk_ships+1
+			else
+				break
+			end
+		end
+
+		for i=1, num_bulk_ships do
+			local starport = sys:GetRandomStarport(Pi.rand)
+			local ship, e = Pi.SpawnRandomStaticShip(Pi.FindBodyForSBody(starport))
+			if e then
+				print("BulkShips: "..e)
+			end
 		end
 	end,
 }
