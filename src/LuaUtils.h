0c04cfba3b663bd983de759464a172026c6ae7f1
Robert Norris <rob@eatenbyagrue.org>
2011-03-22 21:35:09 +1100

    integer key table helpers

--- src/LuaUtils.h
+++ src/LuaUtils.h
@@ -10,9 +10,23 @@ inline void pi_lua_settable(lua_State *l, const char *key, int value)
 	lua_settable(l, -3);
 }
 
-inline void pi_lua_settable(lua_State *l, const char *key, float value)
+inline void pi_lua_settable(lua_State *l, int key, int value)
+{
+	lua_pushinteger(l, key);
+	lua_pushinteger(l, value);
+	lua_settable(l, -3);
+}
+
+inline void pi_lua_settable(lua_State *l, const char *key, double value)
 {
 	lua_pushstring(l, key);
+	lua_pushnumber(l, value);
+	lua_settable(l, -3);
+}
+
+inline void pi_lua_settable(lua_State *l, int key, double value)
+{
+	lua_pushinteger(l, key);
 	lua_pushnumber(l, value);
 	lua_settable(l, -3);
 }
