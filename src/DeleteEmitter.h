4fcf92efb917085f6bce876591ffa429f58ea719
Robert Norris <rob@eatenbyagrue.org>
2011-03-23 16:44:04 +1100

    document DeleteEmitter, its just a little bit spooky

--- src/DeleteEmitter.h
+++ src/DeleteEmitter.h
@@ -3,6 +3,10 @@
 
 #include "libs.h"
 
+// inherit from this class to be able to get notifications when an object is
+// destroyed. the LuaObject tracking layer uses this to properly "forget"
+// about objects that are currently exposed to lua
+
 class DeleteEmitter {
 public:
 	DeleteEmitter() {}
@@ -12,6 +16,8 @@ public:
 	sigc::signal<void> onDelete;
 
 private:
+    // sigc++ signals cannot be copied, but long-standing design flaw means
+    // they don't have a private copy constructor. we protect them ourselves
 	DeleteEmitter(const DeleteEmitter &) {}
 };
 
