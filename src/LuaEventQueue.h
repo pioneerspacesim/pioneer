0aba40790a353f13bf7fea71d0a2d4c7aded9725
Robert Norris <rob@eatenbyagrue.org>
2011-03-25 19:21:23 +1100

    partial specialisation for event queue for 0,1&2 args. should be enough for now

--- src/LuaEventQueue.h
+++ src/LuaEventQueue.h
@@ -12,14 +12,32 @@ public:
 	virtual ~LuaEventBase() {}
 };
 
-template <typename T0>
+template <typename T0, typename T1>
 class LuaEvent : public LuaEventBase {
 public:
-	LuaEvent(T0 *o) : m_arg0(o) { }
+	LuaEvent(T0 *arg0, T1 *arg1) : m_arg0(arg0), m_arg1(arg1) { }
+	virtual ~LuaEvent() {}
+	
+	T0 *m_arg0;
+	T1 *m_arg1;
+};
+
+template <typename T0>
+class LuaEvent<T0,void> : public LuaEventBase {
+public:
+	LuaEvent(T0 *arg0) : m_arg0(arg0) { }
 	virtual ~LuaEvent() {}
 	
 	T0 *m_arg0;
 };
+
+template <>
+class LuaEvent<void,void> : public LuaEventBase {
+public:
+	LuaEvent() { }
+	virtual ~LuaEvent() {}
+};
+
 
 class LuaEventQueueBase : public DeleteEmitter {
 	friend class LuaSubObject<LuaEventQueueBase>;
@@ -42,20 +60,50 @@ private:
 	virtual void PrepareLuaStack(lua_State *l, const LuaEventBase *eb) = 0;
 };
 
-template <typename T0>
+template <typename T0=void, typename T1=void>
 class LuaEventQueue : public LuaEventQueueBase {
 public:
 	LuaEventQueue(const char *name) : LuaEventQueueBase(name) { }
 
-	inline void Queue(T0 *o) {
-		m_events.push_back(new LuaEvent<T0>(o));
+	inline void Queue(T0 *arg0, T1 *arg1) {
+		m_events.push_back(new LuaEvent<T0,T1>(arg0, arg1));
 	}
 
 protected:
 	inline void PrepareLuaStack(lua_State *l, const LuaEventBase *eb) {
-		const LuaEvent<T0> *e = static_cast<const LuaEvent<T0>*>(eb);
+		const LuaEvent<T0,T1> *e = static_cast<const LuaEvent<T0,T1>*>(eb);
+		LuaSubObject<T0>::PushToLua(e->m_arg0);
+		LuaSubObject<T1>::PushToLua(e->m_arg1);
+	}
+};
+
+template <typename T0>
+class LuaEventQueue<T0,void> : public LuaEventQueueBase {
+public:
+	LuaEventQueue(const char *name) : LuaEventQueueBase(name) { }
+
+	inline void Queue(T0 *arg0) {
+		m_events.push_back(new LuaEvent<T0,void>(arg0));
+	}
+
+protected:
+	inline void PrepareLuaStack(lua_State *l, const LuaEventBase *eb) {
+		const LuaEvent<T0,void> *e = static_cast<const LuaEvent<T0,void>*>(eb);
 		LuaSubObject<T0>::PushToLua(e->m_arg0);
 	}
+};
+
+template <>
+class LuaEventQueue<void,void> : public LuaEventQueueBase {
+public:
+	LuaEventQueue(const char *name) : LuaEventQueueBase(name) { }
+
+	inline void Queue() {
+		m_events.push_back(new LuaEvent<void,void>());
+	}
+
+protected:
+	inline void PrepareLuaStack(lua_State *l, const LuaEventBase *eb) { }
 };
 
 #endif
