54df9ab554f3796151139c404add6c238dc39d6d
Robert Norris <rob@eatenbyagrue.org>
2011-04-25 22:51:41 +1000

    epic nearby system finder

--- src/LuaStarSystem.cpp
+++ src/LuaStarSystem.cpp
@@ -12,6 +12,7 @@
 #include "Star.h"
 #include "Planet.h"
 #include "SpaceStation.h"
+#include "Sector.h"
 
 static int l_starsystem_get_name(lua_State *l)
 {
@@ -51,7 +52,7 @@ static int l_starsystem_get_station_paths(lua_State *l)
 		SBodyPath *path = new SBodyPath(loc.GetSectorX(), loc.GetSectorY(), loc.GetSystemNum());
 		path->sbodyId = (*i)->id;
 
-		lua_pushstring(l, (*i)->name.c_str());
+		lua_pushinteger(l, lua_objlen(l, -1)+1);
 		LuaSBodyPath::PushToLuaGC(path);
 		lua_rawset(l, -3);
 	}
@@ -99,6 +100,65 @@ static int l_starsystem_is_commodity_legal(lua_State *l)
 	return 1;
 }
 
+static int l_starsystem_get_nearby_systems(lua_State *l)
+{
+	LUA_DEBUG_START(l);
+
+	StarSystem *s = LuaStarSystem::GetFromLua(1);
+	double dist_ly = luaL_checknumber(l, 2);
+
+	bool filter = false;
+	if (lua_gettop(l) >= 3) {
+		if (!lua_isfunction(l, 3))
+			luaL_typerror(l, 3, lua_typename(l, LUA_TFUNCTION));
+		filter = true;
+	}
+
+	lua_newtable(l);
+	pi_lua_table_ro(l);
+
+	int here_x = s->SectorX();
+	int here_y = s->SectorY();
+	int here_idx = s->SystemIdx();
+	Sector here_sec(here_x, here_y);
+
+	int diff_sec = ceil(dist_ly/Sector::SIZE);
+
+	for (int x = here_x-diff_sec; x <= here_x+diff_sec; x++) {
+		for (int y = here_y-diff_sec; y <= here_y+diff_sec; y++) {
+			Sector sec(x, y);
+
+			for (unsigned int idx = 0; idx < sec.m_systems.size(); idx++) {
+				if (Sector::DistanceBetween(&here_sec, here_idx, &sec, idx) > dist_ly)
+					continue;
+
+				StarSystem *sys = StarSystem::GetCached(x, y, idx);
+				if (filter) {
+					lua_pushvalue(l, 3);
+					LuaStarSystem::PushToLua(sys);
+					lua_call(l, 1, 1);
+					if (!lua_toboolean(l, -1)) {
+						lua_pop(l, 1);
+						sys->Release();
+						continue;
+					}
+					lua_pop(l, 1);
+				}
+
+				lua_pushinteger(l, lua_objlen(l, -1)+1);
+				LuaStarSystem::PushToLua(sys);
+				lua_rawset(l, -3);
+
+				sys->Release();
+			}
+		}
+	}
+
+	LUA_DEBUG_END(l, 1);
+	
+	return 1;
+}
+
 template <> const char *LuaObject<StarSystem>::s_type = "StarSystem";
 
 template <> void LuaObject<StarSystem>::RegisterClass()
@@ -113,6 +173,8 @@ template <> void LuaObject<StarSystem>::RegisterClass()
 		{ "GetPopulation",                    l_starsystem_get_population                       },
 		{ "GetCommodityBasePriceAlterations", l_starsystem_get_commodity_base_price_alterations },
 		{ "IsCommodityLegal",                 l_starsystem_is_commodity_legal                   },
+
+        { "GetNearbySystems", l_starsystem_get_nearby_systems },
 		{ 0, 0 }
 	};
 
