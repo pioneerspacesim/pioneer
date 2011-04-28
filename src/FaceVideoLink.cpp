92384534f6fe6265e1b06e7b4eb9b6ee0a9da3e5
Robert Norris <rob@eatenbyagrue.org>
2011-04-28 16:45:18 +1000

    big cleanup; switch to proper rng

--- src/FaceVideoLink.cpp
+++ src/FaceVideoLink.cpp
@@ -1,90 +1,87 @@
 #include "FaceVideoLink.h"
-#include "StarSystem.h"
+
+// XXX these shouldn't really be hardcoded. it'd be much nicer to poke through
+// the facegen/ dir and figure out what we've got available. that or some
+// config file
+#define MAX_HEAD  20
+#define MAX_EYES  20
+#define MAX_NOSE  20
+#define MAX_MOUTH 20
+#define MAX_HAIR  20
+
+#define MAX_CLOTHES     20
+#define MAX_ACCESSORIES 3
+
+#define MAX_BACKGROUND 12
 
 FaceVideoLink::FaceVideoLink(float w, float h, unsigned long seed) : VideoLink(w, h) {
-	MTRand rand(seed);
-
-	int eyes_seed  = seed ;
-	//if (StationBBView()) {
-	//	eyes_seed = 1;
-	//}
-
-	if (eyes_seed < 0) {
-		eyes_seed  = eyes_seed * -1.0f ;
-	}
-
-	//int face_seed  = eyes_seed %20;
-	int face_seed  = (eyes_seed>>2)%21;
-	//int mouth_seed = eyes_seed %80 ;
-	int mouth_seed = (eyes_seed>>4)%21 ;
-	//int nose_seed  = eyes_seed ;
-	int nose_seed  = (eyes_seed>>6)%21 ;
-	//int hair_seed  = eyes_seed %100 ;
-	int hair_seed  = (eyes_seed>>8)%21 ;
-	//int extr1_seed = eyes_seed %240 ;
-	int extr1_seed = (eyes_seed>>10)%3 ;
-	//int extr2_seed = eyes_seed %1500 ;
-	int extr2_seed = (eyes_seed>>12)%14 ;
-	//int sex_seed   = rand.Double(0,1);
-	int sex_seed   = (eyes_seed>>14)%2;
-	//int race_seed  = eyes_seed %3 ;
-	int race_seed  = (eyes_seed>>16)%2 ;//should be %3
-	//int cloth_seed = eyes_seed %40 ;
-	int cloth_seed = (eyes_seed>>18)%21 ;
-	//eyes_seed      = rand.Int32(0,eyes_seed);
-	//eyes_seed      = eyes_seed %20 ;
-	eyes_seed	   = (eyes_seed>>20)%21 ;
-	//mouth_seed     = rand.Int32(0,mouth_seed);
-	//mouth_seed     = mouth_seed * 0.25f ;
-	//nose_seed      = rand.Int32(0,nose_seed);
-	//nose_seed      = nose_seed * rand.Int32(0,1);
-	//nose_seed      = nose_seed %20 ;
-	//hair_seed      = hair_seed * 0.2f ;
-	//extr1_seed     = extr1_seed / 12.0f ;
-	//extr2_seed     = extr2_seed / 75.0f ;
-	//cloth_seed     = rand.Int32(0,cloth_seed);
-	//cloth_seed     = cloth_seed %20 ;
-	//race_seed      = 0 ;  //temp
-	//extr1_seed     = extr1_seed %3 ;  //temp
-	//extr2_seed     = rand.Int32(0,12) ; // temp
 	m_created = SDL_GetTicks();
 	m_message = new Gui::ToolTip("Video link established");
-	std::string str = stringf(64, PIONEER_DATA_DIR "/facegen/race_%d/head/head_%d_%d.png", race_seed, sex_seed, face_seed);
-	printf("%s\n", str.c_str());
-	m_face = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/race_%d/eyes/eyes_%d_%d.png", race_seed, sex_seed, eyes_seed);
-	printf("%s\n", str.c_str());
-	m_eyes = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/race_%d/nose/nose_%d_%d.png", race_seed, sex_seed, nose_seed);
-	printf("%s\n", str.c_str());
-	m_nose = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/race_%d/mouth/mouth_%d_%d.png", race_seed, sex_seed, mouth_seed);
-	printf("%s\n", str.c_str());
-	m_mouth = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/race_%d/hair/hair_%d_%d.png", race_seed, sex_seed, hair_seed);
-	printf("%s\n", str.c_str());
-	m_hair = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/clothes/cloth_%d.png", cloth_seed);
-	printf("%s\n", str.c_str());
-	m_cloth = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/accessories/acc_%d.png", extr1_seed);
-	printf("%s\n", str.c_str());
-	m_extr1 = new Gui::Image(("" + str).c_str());
-	str = stringf(64, PIONEER_DATA_DIR "/facegen/backgrounds/background_%d.png", extr2_seed);
-	printf("%s\n", str.c_str());
-	m_extr2 = new Gui::Image(("" + str).c_str());
+
+	MTRand rand(seed);
+
+	int race = rand.Int32(0,1);    // XXX should be 3?
+	int gender = rand.Int32(0,1);
+
+	int head  = rand.Int32(0,MAX_HEAD);
+	int eyes  = rand.Int32(0,MAX_EYES);
+	int nose  = rand.Int32(0,MAX_NOSE);
+	int mouth = rand.Int32(0,MAX_MOUTH);
+	int hair  = rand.Int32(0,MAX_HAIR);
+
+	int clothes = rand.Int32(0,MAX_CLOTHES);
+	int accessories = rand.Int32(0,MAX_ACCESSORIES);
+
+	int background = rand.Int32(0,MAX_BACKGROUND);
+
+	char filename[1024];
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/race_%d/head/head_%d_%d.png", race, gender, head);
+	printf("%s\n", filename);
+	m_head = new Gui::Image(filename);
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/race_%d/eyes/eyes_%d_%d.png", race, gender, eyes);
+	printf("%s\n", filename);
+	m_eyes = new Gui::Image(filename);
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/race_%d/nose/nose_%d_%d.png", race, gender, nose);
+	printf("%s\n", filename);
+	m_nose = new Gui::Image(filename);
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/race_%d/mouth/mouth_%d_%d.png", race, gender, mouth);
+	printf("%s\n", filename);
+	m_mouth = new Gui::Image(filename);
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/race_%d/hair/hair_%d_%d.png", race, gender, hair);
+	printf("%s\n", filename);
+	m_hair = new Gui::Image(filename);
+
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/clothes/cloth_%d.png", clothes);
+	printf("%s\n", filename);
+	m_clothes = new Gui::Image(filename);
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/accessories/acc_%d.png", accessories);
+	printf("%s\n", filename);
+	m_accessories = new Gui::Image(filename);
+
+
+	snprintf(filename, sizeof(filename), PIONEER_DATA_DIR "/facegen/backgrounds/background_%d.png", background);
+	printf("%s\n", filename);
+	m_background = new Gui::Image(filename);
 }
 
 FaceVideoLink::~FaceVideoLink() {
 	delete m_message;
-	delete m_face;
+
+	delete m_head;
 	delete m_eyes;
 	delete m_nose;
 	delete m_mouth;
 	delete m_hair;
-	delete m_cloth;
-	delete m_extr1;
-	delete m_extr2;
+	delete m_clothes;
+	delete m_accessories;
+	delete m_background;
 }
 
 void FaceVideoLink::Draw() {
@@ -142,38 +139,45 @@ void FaceVideoLink::DrawMessage() {
 
 void FaceVideoLink::DrawFace() {
 	float size[2];
-	GetSize(size);
-	m_extr2->GetSize(size);
+
+	m_background->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 285 - size[1], 0);
-	m_extr2->Draw();	
+	m_background->Draw();	
 	glTranslatef((size[0] - 295)*0.5f, size[1] - 285, 0);
-	m_face->GetSize(size);
+
+	m_head->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 285 - size[1], 0);
-	m_face->Draw();	
+	m_head->Draw();	
 	glTranslatef((size[0] - 295)*0.5f, size[1] - 285, 0);
-	m_cloth->GetSize(size);
+
+	m_clothes->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 285 - size[1], 0);
-	m_cloth->Draw();
+	m_clothes->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] - 285, 0);
+
 	m_eyes->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 189 - size[1], 0);
 	m_eyes->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] -189, 0);
+
 	m_nose->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 221.5 - size[1], 0);
 	m_nose->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] -221.5, 0);
+
 	m_mouth->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 225 - size[1], 0);
 	m_mouth->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] -225, 0);
+
 	m_hair->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 285 - size[1], 0);
 	m_hair->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] - 285, 0);
-	m_extr1->GetSize(size);
+
+	m_accessories->GetSize(size);
 	glTranslatef((295 - size[0])*0.5f, 285 - size[1], 0);
-	m_extr1->Draw();
+	m_accessories->Draw();
 	glTranslatef((size[0] - 295)*0.5f, size[1] - 285, 0);
 }
 
--- src/FaceVideoLink.h
+++ src/FaceVideoLink.h
@@ -18,14 +18,14 @@ private:
 	GLuint m_tex;
 	Gui::ToolTip *m_message;
 
-	Gui::Image *m_face;
+	Gui::Image *m_head;
 	Gui::Image *m_eyes;
 	Gui::Image *m_nose;
-	Gui::Image *m_hair;
 	Gui::Image *m_mouth;
-	Gui::Image *m_cloth;
-	Gui::Image *m_extr1;
-	Gui::Image *m_extr2;
+	Gui::Image *m_hair;
+	Gui::Image *m_clothes;
+	Gui::Image *m_accessories;
+	Gui::Image *m_background;
 };
 
 #endif
