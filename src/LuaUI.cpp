b8efa0c88675f1e634ad431f7cced8e686af3380
Robert Norris <rob@eatenbyagrue.org>
2011-05-01 12:22:53 +1000

    ui doc stubs

--- src/LuaUI.cpp
+++ src/LuaUI.cpp
@@ -4,6 +4,25 @@
 #include "Pi.h"
 #include "ShipCpanel.h"
 
+/*
+ * Interface: UI
+ *
+ * User interface functions.
+ */
+
+/*
+ * Function: Message
+ *
+ * Post a message to the player's control panel.
+ *
+ * Availability:
+ *
+ *  alpha 10
+ *
+ * Status:
+ *
+ *  experimental
+ */
 static int l_ui_message(lua_State *l)
 {
 	std::string from = luaL_checkstring(l, 1);
@@ -12,6 +31,21 @@ static int l_ui_message(lua_State *l)
 	return 0;
 }
 
+/*
+ * Function: ImportantMessage
+ *
+ * Post an important message to the player's control panel. The only
+ * difference between this and <Message> is that if multiple messages arrive
+ * at the same time, the important ones will be shown first.
+ *
+ * Availability:
+ *
+ *  alpha 10
+ *
+ * Status:
+ *
+ *  experimental
+ */
 static int l_ui_important_message(lua_State *l)
 {
 	std::string from = luaL_checkstring(l, 1);
