89ad7a717f297982bb083fa6793625393091e631
Robert Norris <rob@eatenbyagrue.org>
2011-05-09 08:54:36 +1000

    body docs and IsDynamic method

--- src/LuaBody.cpp
+++ src/LuaBody.cpp
@@ -8,11 +8,25 @@
 /*
  * Class: Body
  *
- * Class representing that represents a physics body.
+ * Class represents a physical body.
+ *
+ * These objects only exist for the bodies of the system that the player is
+ * currently in. If you need to refer
  */
 
 /*
  * Attribute: label
+ *
+ * The label for the body. This is what is displayed in the HUD and usually
+ * matches the name of the planet, space station, etc if appropriate.
+ *
+ * Availability:
+ *
+ *   alpha 10
+ *
+ * Status:
+ *
+ *   stable
  */
 static int l_body_attr_label(lua_State *l)
 {
@@ -23,6 +37,19 @@ static int l_body_attr_label(lua_State *l)
 
 /*
  * Attribute: seed
+ *
+ * The random seed used to generate this <Body>. This is guaranteed to be the
+ * same for this body across runs of the same build of the game, and should be
+ * used to seed a <Rand> object when you want to ensure the same random
+ * numbers come out each time.
+ *
+ * Availability:
+ *
+ *   alpha 10
+ *
+ * Status:
+ *
+ *   stable
  */
 static int l_body_attr_seed(lua_State *l)
 {
@@ -37,6 +64,19 @@ static int l_body_attr_seed(lua_State *l)
 
 /*
  * Attribute: path
+ *
+ * The <SystemPath> that points to this body.
+ *
+ * If the body is a dynamic body it has no persistent path data, and its
+ * <path> value will be nil.
+ *
+ * Availability:
+ *
+ *   alpha 10
+ *
+ * Status:
+ *
+ *   stable
  */
 static int l_body_attr_path(lua_State *l)
 {
@@ -56,7 +96,67 @@ static int l_body_attr_path(lua_State *l)
 	return 1;
 }
 
-static int l_body_get_distance_to(lua_State *l)
+/* 
+ * Method: IsDynamic
+ *
+ * Determine if the body is a dynamic body
+ *
+ * > isdynamic = body:IsDynamic()
+ *
+ * A dynamic body is one that is not part of the generated system. Currently
+ * <Ships> and <CargoBodies> are dynamic bodies. <Stars>, <Planets> and
+ * <SpaceStations> are not.
+ *
+ * Being a dynamic body generally means that there is no way to reference the
+ * body outside of the context of the current system. A planet, for example,
+ * can always be referenced by its <SystemPath> (available via <Body.path>),
+ * even from outside the system. A <Ship> however can not be referenced in
+ * this way. If a script needs to retain information about a ship that is no
+ * longer in the <Player's> current system it must manage this itself.
+ *
+ * The above list of static/dynamic bodies may change in the future. Scripts
+ * should use this method to determine the difference rather than checking
+ * types directly.
+ *
+ * Availability:
+ *
+ *   alpha 10
+ *
+ * Status:
+ *
+ *   stable
+ */
+static int l_body_is_dynamic(lua_State *l)
+{
+	Body *b = LuaBody::GetFromLua(1);
+	lua_pushboolean(l, b->IsType(Object::DYNAMICBODY));
+	return 1;
+}
+
+/*
+ * Method: DistanceTo
+ *
+ * Calculate the distance between two bodies
+ *
+ * > dist = body:DistanceTo(otherbody)
+ *
+ * Parameters:
+ *
+ *   otherbody - the body to calculate the distance to
+ *
+ * Returns:
+ *
+ *   dist - distance between the two bodies in km
+ *
+ * Availability:
+ *
+ *   alpha 10
+ *
+ * Status:
+ *
+ *   stable
+ */
+static int l_body_distance_to(lua_State *l)
 {
 	Body *b1 = LuaBody::GetFromLua(1);
 	Body *b2 = LuaBody::GetFromLua(2);
@@ -69,7 +169,8 @@ template <> const char *LuaObject<Body>::s_type = "Body";
 template <> void LuaObject<Body>::RegisterClass()
 {
 	static luaL_reg l_methods[] = {
-		{ "GetDistanceTo", l_body_get_distance_to },
+		{ "IsDynamic",  l_body_is_dynamic  },
+		{ "DistanceTo", l_body_distance_to },
 		{ 0, 0 }
 	};
 
