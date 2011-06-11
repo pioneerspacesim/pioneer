8ff5ea7f9bc04f7e19e973354593bd1eb676ea67
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2011-06-11 16:02:11 +0100

    Improved performance with a small cache (usually ~25) of Sectors which only generate StarSystems once the view has stopped moving.

--- src/Sector.h
+++ src/Sector.h
@@ -7,6 +7,29 @@
 #include "StarSystem.h"
 #include "CustomSystem.h"
 
+class SectorLoc {
+public:
+	SectorLoc(): sectorX(0), sectorY(0) {}
+	SectorLoc(int sectorX_, int sectorY_): sectorX(sectorX_), sectorY(sectorY_) {}
+	int sectorX, sectorY;
+	int GetSectorX() const { return sectorX; }
+	int GetSectorY() const { return sectorY; }
+	friend bool operator==(const SectorLoc &a, const SectorLoc &b) {
+		if (a.sectorX != b.sectorX) return false;
+		if (a.sectorY != b.sectorY) return false;
+		return true;
+	}
+	friend bool operator!=(const SectorLoc &a, const SectorLoc &b) {
+		return !(a==b);
+	}
+	friend bool operator<(const SectorLoc &a, const SectorLoc &b) {
+		if (a.sectorX != b.sectorX) return (a.sectorX < b.sectorX);
+		if (a.sectorY != b.sectorY) return (a.sectorY < b.sectorY);
+		return false;
+	}
+};
+
+
 class Sector {
 public:
 	// lightyears
@@ -14,6 +37,8 @@ public:
 	Sector(int x, int y);
 	static float DistanceBetween(const Sector *a, int sysIdxA, const Sector *b, int sysIdxB);
 	static void Init();
+	// Sector is within a bounding rectangle - used for SectorView m_sectorCache pruning.
+	bool WithinBox(const int Xmin, const int Xmax, const int Ymin, const int Ymax) const;
 	
 	class System {
 	public:
