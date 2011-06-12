aab57b75a49da2925448cf76e3cdd14ca4cb02d5
kko <pioneer@paahdin.com>
2011-06-12 13:38:24 +0300

    Fading tests

--- data/modules/MusicPlayer.lua
+++ data/modules/MusicPlayer.lua
@@ -1,17 +1,20 @@
+--~ If you really need to have these :) http://paahdin.com/projects/pioneer/lol.zip
 local test = function()
 	Music.Play("under");
 end
 
 local test2 = function(ship, alert)
+	--todo: check alert level so this does not play so often
 	Music.Play("knighty");
 end
 
 local test3 = function(ship, station)
-	Music.Play("love")
+	Music.FadeIn("love", 0.5)
 end
 
 local test4 = function(ship, station)
-	Music.Play("biisi")
+	Music.FadeIn("biisi", 0.5)
+	--~ Music.FadeOut(0.5)
 end
 
 local test5 = function()
