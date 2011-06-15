bccb463c242b467c0e899449daca3b9994b45401
Paul B Mahol <onemda@gmail.com>
2011-06-15 19:56:08 +0200

    Simple customizable FOV support

--- src/WorldView.cpp
+++ src/WorldView.cpp
@@ -352,7 +352,7 @@ void WorldView::Draw3D()
 	float znear, zfar;
 	GetNearFarClipPlane(&znear, &zfar);
 	// why the hell do i give these functions such big names..
-	const float zoom = 0.9f; // angle of viewing = 2.0*atan(zoom);
+	const float zoom = tan(DEG2RAD(Clamp(Pi::config.Float("FOV"), 20.0f, 180.0f)/2.0f)); // angle of viewing = 2.0*atan(zoom);
 	const float left = zoom * znear;
 	const float fracH = left / Pi::GetScrAspect();
 	glFrustum(-left, left, -fracH, fracH, znear, zfar);
