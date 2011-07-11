873feb4e824471d3950003201617d0fc03e86e3f
Robert Norris <rob@eatenbyagrue.org>
2011-07-11 22:28:50 +1000

    add hyperspace ranges and move to a full box layout

--- src/StationShipViewForm.cpp
+++ src/StationShipViewForm.cpp
@@ -15,6 +15,15 @@ StationShipViewForm::StationShipViewForm(FormController *controller, ShipFlavour
 
 	Add(new ShipSpinnerWidget(m_flavour, 400, 400), 0, 0);
 
+
+	Gui::VBox *layoutBox = new Gui::VBox();
+	layoutBox->SetSpacing(10.0f);
+	Add(layoutBox, 420, 0);
+
+	Gui::HBox *statsBox = new Gui::HBox();
+	statsBox->SetSpacing(20.0f);
+	layoutBox->PackEnd(statsBox);
+
 	Gui::VBox *labelBox = new Gui::VBox();
 	labelBox->PackEnd(new Gui::Label("Ship type"));
 	labelBox->PackEnd(new Gui::Label("Price"));
@@ -30,7 +39,7 @@ StationShipViewForm::StationShipViewForm(FormController *controller, ShipFlavour
 	labelBox->PackEnd(new Gui::Label("Reverse accel (laden)"));
 	labelBox->PackEnd(new Gui::Label(" "));
 	labelBox->PackEnd(new Gui::Label("Hyperdrive fitted"));
-	Add(labelBox, 420, 0);
+	statsBox->PackEnd(labelBox);
 
 	float forward_accel_empty = type.linThrust[ShipType::THRUSTER_FORWARD] / (-9.81f*1000.0f*(type.hullMass));
 	float forward_accel_laden = type.linThrust[ShipType::THRUSTER_FORWARD] / (-9.81f*1000.0f*(type.hullMass+type.capacity));
@@ -52,5 +61,40 @@ StationShipViewForm::StationShipViewForm(FormController *controller, ShipFlavour
 	dataBox->PackEnd(new Gui::Label(stringf(64, "%.1f G", reverse_accel_laden)));
 	dataBox->PackEnd(new Gui::Label(" "));
 	dataBox->PackEnd(new Gui::Label(EquipType::types[type.hyperdrive].name));
-	Add(dataBox, 600, 0);
+	statsBox->PackEnd(dataBox);
+
+
+	Gui::HBox *row = new Gui::HBox();
+	row->SetSpacing(15.0f);
+
+	int row_size = 5, pos = 0;
+	for (int drivetype = Equip::DRIVE_CLASS1; drivetype <= Equip::DRIVE_CLASS9; drivetype++) {
+		if (type.capacity < EquipType::types[drivetype].mass)
+			break;
+
+		int hyperclass = EquipType::types[drivetype].pval;
+		// for the sake of hyperspace range, we count ships mass as 60% of original.
+		float range = Pi::CalcHyperspaceRange(hyperclass, type.hullMass + type.capacity);
+
+		Gui::VBox *cell = new Gui::VBox();
+		row->PackEnd(cell);
+
+		cell->PackEnd(new Gui::Label(stringf(128, "Class %d", hyperclass)));
+		if (type.capacity < EquipType::types[drivetype].mass)
+			cell->PackEnd(new Gui::Label("---"));
+		else
+			cell->PackEnd(new Gui::Label(stringf(128, "%.2f ly", range)));
+
+		if (++pos == row_size) {
+			layoutBox->PackEnd(row);
+
+			row = new Gui::HBox();
+			row->SetSpacing(15.0f);
+
+			pos = 0;
+		}
+	}
+
+	if (pos > 0)
+		layoutBox->PackEnd(row);
 }
