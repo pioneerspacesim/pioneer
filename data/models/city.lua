d78287e0701356df77855838575da63c9657d725
kko <pioneer@paahdin.com>
2011-07-20 22:22:46 +0300

    Optimize fancy tower

--- data/models/city.lua
+++ data/models/city.lua
@@ -211,25 +211,6 @@ define_model('church', {
 })
 --]]
 
-define_model('towerOfShit', {
-	info = {
-		bounding_radius=20,
-		materials={'mat1'},
-		tags = {'city_building'},
-	},
-	static = function(lod)
-		set_material("mat1", 1,1,1,1)
-		use_material("mat1")
-		for i = 0,20 do
-			local _xoff = 10*noise(v(0,i*10,0))
-			local _zoff = 10*noise(v(0,0,i*10))
-			local _start = v(30+_xoff,i*10,_zoff)
-			local _end = v(30+_xoff,10+i*10,_zoff)
-			xref_cylinder(16, _start, _end, v(1,0,0), 10.0+math.abs(10*noise(0,0.1*i,0)))
-		end
-	end
-})
-
 function biodome(lod, trans)
 	local d = 1/math.sqrt(2)
 	local height = 40
