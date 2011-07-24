pioneerspacesim/pioneer

commit effe2d1d629390e3bf2aa649518e234af85969ed

show the connecting/established text for a few seconds instead of flashing it forever. move the connecting text down to the bottom. closes #319

src/FaceVideoLink.cpp
@@ -135,55 +135,59 @@ FaceVideoLink::~FaceVideoLink() {
 }
 
 void FaceVideoLink::Draw() {
-	float size[2]; GetSize(size);
-	if (SDL_GetTicks() - m_created < 1500) {
+	float size[2];
+	GetSize(size);
+
+	Uint32 now = SDL_GetTicks();
+
+	if (now - m_created < 1500) {
+		glBegin(GL_QUADS);
+			glColor3f(0,0,0);
+			glVertex2f(0,0);
+			glVertex2f(0,size[1]);
+			glVertex2f(size[0],size[1]);
+			glVertex2f(size[0],0);
+		glEnd();
+
 		m_message->SetText("Connecting...");
-		glBegin(GL_QUADS);
-			glColor3f(0,0,0);
-			glVertex2f(0,0);
-			glVertex2f(0,size[1]);
-			glVertex2f(size[0],size[1]);
-			glVertex2f(size[0],0);
-		glEnd();
 		DrawMessage();
-	} else {
+
+		return;
+	}
+
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, m_tex);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBegin(GL_QUADS);
+		float w = float(FACE_WIDTH) / ceil_pow2(FACE_WIDTH);
+		float h = float(FACE_HEIGHT) / ceil_pow2(FACE_HEIGHT);
+		glColor3f(0,0,0);
+		glTexCoord2f(0,h);
+		glVertex2f(0,size[1]);
+		glTexCoord2f(w,h);
+		glVertex2f(size[0],size[1]);
+		glTexCoord2f(w,0);
+		glVertex2f(size[0],0);
+		glTexCoord2f(0,0);
+		glVertex2f(0,0);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+
+	if (now - m_created < 4500) {
 		m_message->SetText("Video link established.");
-
-		glEnable(GL_TEXTURE_2D);
-		glBindTexture(GL_TEXTURE_2D, m_tex);
-		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-		glBegin(GL_QUADS);
-			float w = float(FACE_WIDTH) / ceil_pow2(FACE_WIDTH);
-			float h = float(FACE_HEIGHT) / ceil_pow2(FACE_HEIGHT);
-			glColor3f(0,0,0);
-			glTexCoord2f(0,h);
-			glVertex2f(0,size[1]);
-			glTexCoord2f(w,h);
-			glVertex2f(size[0],size[1]);
-			glTexCoord2f(w,0);
-			glVertex2f(size[0],0);
-			glTexCoord2f(0,0);
-			glVertex2f(0,0);
-		glEnd();
-		glDisable(GL_TEXTURE_2D);
-
-		if (SDL_GetTicks() & 0x400)
-			DrawMessage();
+		DrawMessage();
 	}
 }
 
 void FaceVideoLink::DrawMessage() {
 	float size[2];
-	float msgSize[2];
 	GetSize(size);
+
+	float msgSize[2];
 	m_message->GetSize(msgSize);
+
 	glPushMatrix();
-	if (SDL_GetTicks() - m_created < 1500) {
-		glTranslatef(size[0]*0.5f-msgSize[0]*0.5f, size[1]*0.5f-msgSize[1]*0.5f, 0);
-	} else {
-		glTranslatef(size[0]*0.5f-msgSize[0]*0.5f, size[1]-msgSize[1]*1.5f, 0);
-	}
-	
+	glTranslatef(size[0]*0.5f-msgSize[0]*0.5f, size[1]-msgSize[1]*1.5f, 0);
 	m_message->Draw();
 	glPopMatrix();
 }

commit 459197704c93a063d01cf661b9aedf60d5267f6e

Added a string that got regressed by effe2d1d629390e3bf2aa649518e234af85969ed

src/FaceVideoLink.cpp
@@ -175,7 +175,7 @@ void FaceVideoLink::Draw() {
 	glDisable(GL_TEXTURE_2D);
 
 	if (now - m_created < 4500) {
-		m_message->SetText("Video link established.");
+		m_message->SetText(PiLang::VID_LINK_ESTABLISHED);
 		DrawMessage();
 	}
 }