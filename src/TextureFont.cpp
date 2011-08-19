d6e8e4bedb180420f1a6e7b6e8f2ccfbe937baa3
John Bartholomew <jpa.bartholomew@gmail.com>
2011-08-19 03:16:26 +0100

    Globally use GL_CLAMP_TO_EDGE rather than GL_CLAMP.
    GL_CLAMP clamps to the texture's border value, which is a separate value
    from the values of pixels on the texture's edge -- the border is black
    by default, so GL_CLAMP tends to result in darkened edges when textures
    are scaled, because the bilinear filter pulls in the black border value.

--- src/TextureFont.cpp
+++ src/TextureFont.cpp
@@ -343,8 +343,8 @@ TextureFont::TextureFont(FontManager &fm, const std::string &config_filename) : 
 		glGenTextures (1, &glfglyph.tex);
 		glBindTexture (GL_TEXTURE_2D, glfglyph.tex);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE_ALPHA, sz, sz, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, pixBuf);
-		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glDisable (GL_TEXTURE_2D);
