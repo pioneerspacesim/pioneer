03d7b634e68d71f36754bd79c48cf96a54d13f34
John Bartholomew <jpa.bartholomew@gmail.com>
2011-08-19 03:06:09 +0100

    Fix a bug in y-inverted mouse behaviour.

--- src/Player.cpp
+++ src/Player.cpp
@@ -205,15 +205,13 @@ void Player::PollControls(const float timeStep)
 			}
 			vector3d objDir = m_mouseDir * rot;
 
-			m_mouseX += mouseMotion[0] * 0.002;
+			const double radiansPerPixel = 0.002;
+
+			m_mouseX += mouseMotion[0] * radiansPerPixel;
 			double modx = clipmouse(objDir.x, m_mouseX);			
 			m_mouseX -= modx;
 
-			if (!Pi::IsMouseYInvert()) {
-				m_mouseY += mouseMotion[1] * 0.002;		// factor pixels => radians
-			} else {
-				m_mouseY =+ mouseMotion[1] * 0.002 * -1;
-			}
+			m_mouseY += mouseMotion[1] * radiansPerPixel * (Pi::IsMouseYInvert() ? -1 : 1);
 			double mody = clipmouse(objDir.y, m_mouseY);
 			m_mouseY -= mody;
 
