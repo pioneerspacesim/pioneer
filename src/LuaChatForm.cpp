9f1c0bfa98fd086abb40eecc5faf21ad18c0e7c3
John Bartholomew <jpa.bartholomew@gmail.com>
2011-08-24 06:13:08 +0100

    Fix LuaChatForm::OnClick{Buy,Sell} to use stringf() on a translated string.
    Rather than using a string literal and stringf_old()

--- src/LuaChatForm.cpp
+++ src/LuaChatForm.cpp
@@ -11,6 +11,8 @@
 #include "CommodityTradeWidget.h"
 #include "FaceVideoLink.h"
 #include "StationPoliceForm.h"
+#include "Lang.h"
+#include "StringF.h"
 
 void LuaChatForm::OnOptionClicked(int option)
 {
@@ -195,8 +197,7 @@ void LuaChatForm::OnClickBuy(int t) {
 
 	if (allow_buy) {
 		if (SellTo(Pi::player, static_cast<Equip::Type>(t), true)) {
-			// XXX this should use Lang::BOUGHT_1T_OF or be removed
-			Pi::Message(stringf_old(512, "You have bought 1t of %s.", EquipType::types[t].name));
+			Pi::Message(stringf(Lang::BOUGHT_1T_OF, formatarg("commodity", EquipType::types[t].name)));
 		}
 		m_commodityTradeWidget->UpdateStock(t);
 	}
@@ -220,8 +221,7 @@ void LuaChatForm::OnClickSell(int t) {
 
 	if (allow_sell) {
 		if (BuyFrom(Pi::player, static_cast<Equip::Type>(t), true)) {
-			// XXX this should use Lang::SOLD_1T_OF or be removed
-			Pi::Message(stringf_old(512, "You have sold 1t of %s.", EquipType::types[t].name));
+			Pi::Message(stringf(Lang::SOLD_1T_OF, formatarg("commodity", EquipType::types[t].name)));
 		}
 		m_commodityTradeWidget->UpdateStock(t);
 	}
