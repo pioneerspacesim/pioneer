6ef1d766d04e86a36b618160e497fde25e9cc8a6
Robert Norris <rob@eatenbyagrue.org>
2011-09-04 18:53:59 +1000

    move out all the star internals; terrainbody will deal with it now

--- src/Star.h
+++ src/Star.h
@@ -2,9 +2,6 @@
 #define _STAR_H
 
 #include "TerrainBody.h"
-#include "StarSystem.h"
-
-class Frame;
 
 class Star: public TerrainBody {
 public:
@@ -12,20 +9,6 @@ public:
 	Star(SBody *sbody);
 	Star();
 	virtual ~Star() {};
-	virtual void SetPosition(vector3d p);
-	virtual vector3d GetPosition() const;
-	virtual double GetBoundingRadius() const { return radius*2.0f; }
-	virtual void Render(const vector3d &viewCoords, const matrix4x4d &viewTransform);
-	virtual double GetMass() const { return mass; }
-	virtual const SBody *GetSBody() const { return m_sbody; }
-protected:
-	virtual void Save(Serializer::Writer &wr);
-	virtual void Load(Serializer::Reader &rd);
-private:
-	SBody *m_sbody;
-	vector3d pos;
-	double radius;
-	double mass;
 };
 
 #endif /* _STAR_H */
