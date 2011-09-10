eae994c64e73aec26a80b373cd503d182166796d
Robert Norris <rob@eatenbyagrue.org>
2011-09-11 09:44:17 +1000

    alpha 14 version bump

--- src/buildopts.h
+++ src/buildopts.h
@@ -3,7 +3,7 @@
 
 // game version. usually defined by configure
 #ifndef PIONEER_VERSION
-#define PIONEER_VERSION "alpha 14 freeze"
+#define PIONEER_VERSION "alpha 14"
 #endif
 #ifndef PIONEER_EXTRAVERSION
 #define PIONEER_EXTRAVERSION ""
