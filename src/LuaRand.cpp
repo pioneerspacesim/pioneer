f9a9441da6bbdb3c330275fc078efc8f96587923
Robert Norris <rob@eatenbyagrue.org>
2011-09-12 22:11:53 +1000

    use lua_tointeger instead of an int cast

--- src/LuaRand.cpp
+++ src/LuaRand.cpp
@@ -34,7 +34,7 @@ static int l_rand_new(lua_State *l)
 {
 	int seed = int(time(NULL));
 	if (lua_isnumber(l, 1))
-		seed = int(lua_tonumber(l, 1));
+		seed = lua_tointeger(l, 1);
 	LuaObject<MTRand>::PushToLuaGC(new MTRand(seed));
 	return 1;
 }
