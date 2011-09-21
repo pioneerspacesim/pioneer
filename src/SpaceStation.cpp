3f5c0d187e6b009064ef2446abac32a0688933b0
Paul B Mahol <onemda@gmail.com>
2011-09-21 21:56:34 +0000

    do not queue onCreateBB event for npc ships
    npc ships do not read bb

--- src/SpaceStation.cpp
+++ src/SpaceStation.cpp
@@ -394,7 +394,9 @@ void SpaceStation::DoDockingAnimation(const double timeStep)
 			if (dt.stage >= 0) {
 				// set docked
 				dt.ship->SetDockedWith(this, i);
-				CreateBB();
+				if (dt.ship->IsType(Object::PLAYER)) {
+					CreateBB();
+				}
 				Pi::luaOnShipDocked->Queue(dt.ship, this);
 			} else {
 				if (!dt.ship->IsEnabled()) {
@@ -708,7 +710,9 @@ bool SpaceStation::OnCollision(Object *b, Uint32 flags, double relVel)
 					s->SetFlightState(Ship::DOCKING);
 				} else {
 					s->SetDockedWith(this, port);
-					CreateBB();
+					if (s->IsType(Object::PLAYER)) {
+						CreateBB();
+					}
 					Pi::luaOnShipDocked->Queue(s, this);
 				}
 			}
