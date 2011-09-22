9386c6f94a80a295a71f23d978c6a0da5e183ae8
Brian Ronald <brian@ppcis.org>
2011-09-23 00:59:41 +0100

    Restored the default case (thanks again, jpab!)

--- data/modules/BreakdownServicing.lua
+++ data/modules/BreakdownServicing.lua
@@ -123,7 +123,7 @@ local onChat = function (form, ref, option)
         DRIVE_MIL2 = 1.6,
         DRIVE_MIL3 = 2.8,
         DRIVE_MIL4 = 4.0,
-    })[hyperdrive]
+    })[hyperdrive] or 10
 
     -- Now make it bigger (-:
     price = price * 10
