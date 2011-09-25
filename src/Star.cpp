cc4d7ec6497129b0663f574bf574bf48ef8d0b93
Dan <s20dan@hotmail.co.uk>
2011-09-25 02:19:26 +0100

    Tweak star halo and change it for Wolf-Rayet stars

--- src/Star.cpp
+++ src/Star.cpp
@@ -36,7 +36,9 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 	if (IsOnscreen()) {
 		const float *col = StarSystem::starRealColors[GetSBody()->type];
 		const float b = (Render::IsHDREnabled() ? 100.0f : 1.0f);
-
+		// if star is wolf-rayet it gets a very large halo effect
+		const float wf = ((GetSBody()->type < SBody::TYPE_STAR_S_BH && 
+			GetSBody()->type > SBody::TYPE_STAR_O_HYPER_GIANT) ? 100.0f : 1.0f);
 
 		/* Draw star spikes and halo to 2d ortho screen */
 		
@@ -44,14 +46,16 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 		vector3d pp;
 		Gui::Screen::Project(fpos, pp);
 
-		const float glowrad = float(20.0f+20000.0f*radius/viewCoords.Length());
+		MTRand(rand);
+
+		const float glowrad = float(20.0f+1000.0f*wf*radius/viewCoords.Length());
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE);	
 		glEnable(GL_BLEND);
 		glBegin(GL_TRIANGLE_FAN);
-		glColor4f(0.5f*col[0], 0.5f*col[1], 0.5f*col[2], 1);
+		glColor4f(col[0], col[1], col[2], 1);
 		glVertex3f(pp.x, pp.y, 0);
 		glColor4f(0,0,0,0);
-		for (float ang=0; ang<2*M_PI; ang+=0.2) {
+		for (float ang=0; ang<2*M_PI; ang+=0.26183+rand.Double(0,0.4)) {
 			glVertex3f(pp.x+glowrad*sin(ang), pp.y+glowrad*cos(ang), 0);
 		}
 		glVertex3f(pp.x, pp.y+glowrad, 0);
@@ -60,7 +64,7 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 		
 		Render::State::UseProgram(Render::simpleShader);
 		glEnable(GL_BLEND);
-		glColor4f(0.5f*b*col[0],0.5f*b*col[1],0.5f*b*col[2],1);
+		glColor4f(b*col[0],b*col[1],b*col[2],1);
 		glBegin(GL_TRIANGLE_FAN);
 		glVertex3f(pp.x,pp.y,0);
 		glColor4f(0,0,0,0);
@@ -103,7 +107,7 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 		Gui::Screen::LeaveOrtho();
 		glDisable(GL_BLEND);
 
-		
+		//We can just leave this check disabled as shader-less looks pretty good
 		//if (Render::AreShadersEnabled())
 
 
