62c4770d1ed9ef195499b37d036ca7da4f8c0dca
Dan <s20dan@hotmail.co.uk>
2011-09-28 02:46:22 +0100

    added #ifdefs to star shader. Still wants something like shader_src.push_back("#define DIM\n"); added to RenderShader.cpp

--- data/shaders/geosphere_star.frag.glsl
+++ data/shaders/geosphere_star.frag.glsl
@@ -1,6 +1,20 @@
 void main(void)
 {
-	gl_FragColor = gl_Color + gl_LightModel.ambient*gl_Color;
+	vec3 tnorm; // = normalize(vec3(gl_TexCoord[1]));
+	vec4 diff = 1.0; // = vec4(0.0);
+	
+#ifdef DIM
+	tnorm = normalize(vec3(gl_TexCoord[1]));
+	diff = vec4(0.0);
+	for (int i=0; i<NUM_LIGHTS; ++i) {
+		float nDotVP = max(0.0, dot(tnorm, normalize(vec3(gl_LightSource[i].position))));
+		diff += gl_LightSource[i].diffuse * nDotVP;
+	}
+//#else
+//	diff = 1.0;
+#endif
+
+	gl_FragColor = diff*gl_Color + gl_LightModel.ambient*gl_Color;
 	gl_FragColor *= gl_FrontMaterial.emission * gl_FrontMaterial.emission * 10.0;
 
 #ifdef ZHACK
