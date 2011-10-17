021ddd24e077275e09edd5e700519cb9186469c3
Paul B Mahol <onemda@gmail.com>
2011-10-17 22:24:22 +0000

    remove small textures

--- data/sub_models/nazzles/nazzles.lua
+++ data/sub_models/nazzles/nazzles.lua
@@ -7,19 +7,11 @@ define_model('nazzle_s', {
 	static = function(lod)
 	    set_material('nazzle', 	.75, .8, 1,1, .8, 1, 1.5, 50)
 		use_material('nazzle')
-        if lod > 1 then
-			texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		else
-			texture('naz_2s.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		end
+		texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
 		sphere_slice(6,2*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.1547,1.7,1.1547)))
 		tube(6,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
 		use_material('e_glow')
-		if lod > 1 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .9)
 	end,
 	dynamic = function(lod)
@@ -37,19 +29,11 @@ define_model('nazzle_l', {
 	static = function(lod)
 	    set_material('nazzle',	.75, .8, 1,1, .8, 1, 1.5, 50)
 		use_material('nazzle')
-        if lod > 2 then
-			texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		else
-			texture('naz_2s.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		end
+		texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
 		sphere_slice(18,3*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.01542,1.6,1.01542)))
 		tube(18,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
 		use_material('e_glow')
-		if lod > 2 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .85)
 	end,
 	dynamic = function(lod)
@@ -67,20 +51,12 @@ define_model('nazzle_n', {
 	static = function(lod)
 	    set_material('nazzle', 	.75, .8, 1,1, .8, 1, 1.5, 50)
 		use_material('nazzle')
-        if lod > 1 then
-			texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		else
-			texture('naz_2s.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		end
+		texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
 		sphere_slice(6,2*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.1547,1.7,1.1547)))
 		tube(6,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
         set_material('naz_no', 	.45, .5, .7)
 		use_material('naz_no')
-		if lod > 1 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .9)
 	end
 })
@@ -94,11 +70,7 @@ define_model('nazzle1_s', {
 			},
 			
 	static = function(lod)
-		if lod > 1 then
-			texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		else
-			texture('naz_2s.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		end
+		texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
 		load_obj('nazzle_s.obj', Matrix.new(v(-1,0,0),v(0,1,0),v(0,0,-1)))
 	end
 })
@@ -110,11 +82,7 @@ define_model('nazzle1_l', {
 			},
 			
 	static = function(lod)
-		if lod > 2 then
-			texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		else
-			texture('naz_2s.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
-		end
+		texture('naz_2.png', v(.5,.5,0), v(.2,0,0), v(0,0,1))
 		load_obj('nazzle_l.obj', Matrix.new(v(-1,0,0),v(0,1,0),v(0,0,-1)))
 	end
 })
@@ -126,19 +94,11 @@ define_model('nazzle2_s', {
 			materials = {'e_glow'},
 			},
 	static = function(lod)
-	    if lod > 1 then
-			texture('naz_n.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
-		else
-			texture('naz_ns.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
-		end
+		texture('naz_n.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
 		sphere_slice(6,2*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.1547,1.7,1.1547)))
 		tube(6,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
 		use_material('e_glow')
-		if lod > 1 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .9)
 	end,
 	dynamic = function(lod)
@@ -154,19 +114,11 @@ define_model('nazzle2_l', {
 			materials = {'e_glow'},
 			},
 	static = function(lod)
-	    if lod > 2 then
-			texture('naz_n.png', v(0,.68,0), v(.1,0,0), v(0,-.42,0))
-		else
-			texture('naz_ns.png', v(0,.68,0), v(.1,0,0), v(0,-.42,0))
-		end
+		texture('naz_n.png', v(0,.68,0), v(.1,0,0), v(0,-.42,0))
 		sphere_slice(18,3*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.01542,1.6,1.01542)))
 		tube(18,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
 		use_material('e_glow')
-		if lod > 2 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .85)
 	end,
 	dynamic = function(lod)
@@ -182,20 +134,12 @@ define_model('nazzle2_n', {
 			materials = {'naz_no'},
 			},
 	static = function(lod)
-	    if lod > 1 then
-			texture('naz_n.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
-		else
-			texture('naz_ns.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
-		end
+		texture('naz_n.png', v(0,.7,0), v(.1,0,0), v(0,-.4,0))
 		sphere_slice(6,2*lod, 0, 0.5*math.pi, Matrix.rotate(math.pi,v(0,0,1)) * Matrix.scale(v(1.1547,1.7,1.1547)))
 		tube(6,v(0,0,0), v(0,.75,0), v(0,0,1), .85, 1)
         set_material('naz_no', 	.45, .5, .7)
 		use_material('naz_no')
-		if lod > 1 then
-			texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		else
-		    texture('naz_1s.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
-		end
+		texture('naz_1.png', v(.5,.5,0), v(.5,0,0), v(0,0,1))
 		circle(6, v(0,.001,0), v(0,1,0), v(0,0,1), .9)
 	end
 })
