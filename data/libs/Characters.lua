fb54426977dbfb3bbd9b3c0dac0f434c89d6d1ad
Brian Ronald <brian@ppcis.org>
2011-10-18 20:27:40 +0100

    DiceRoll function (and some comments)

--- data/libs/Characters.lua
+++ data/libs/Characters.lua
@@ -8,10 +8,10 @@ Character = {
 	face = 0,
 
 	-- Attributes
-	luck = 0,
-	charisma = 0,
-	notoriety = 0,
-	lawfulness = 0,
+	luck = 130,
+	charisma = 130,
+	notoriety = 130,
+	lawfulness = 130,
 
 	-- Experience
 	killcount = 0,
@@ -19,12 +19,26 @@ Character = {
 	-- Methods
 
 	new = function (self,character)
+		-- initialise new character
 		local character = character or {}
+		-- preserve default name against randomization
 		local name = character.name
+		-- set inherited characteristics
 		setmetatable(character,{__index = self})
+		-- randomize name if it wasn't specified
 		character.name = name or NameGen.FullName(character.isfemale)
+		-- allocate a new table for character relationships
 		character.Relationships = {}
 		return character
+	end,
+
+	DiceRoll = function ()
+		return ( -- 4xD64, range is 4..256 averaging 130)
+			  Engine.rand:Integer(1,64)
+			+ Engine.rand:Integer(1,64)
+			+ Engine.rand:Integer(1,64)
+			+ Engine.rand:Integer(1,64)
+		)
 	end,
 
 	PrintStats = function (self)
