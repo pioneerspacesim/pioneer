3d414f3e0fe79d8ca5a4933ce692e3c381e657c9
Brian Ronald <brian@ppcis.org>
2011-10-18 18:55:19 +0100

    Added a debug method

--- data/libs/Characters.lua
+++ data/libs/Characters.lua
@@ -1,5 +1,8 @@
+-- Character class
+
 Character = {
 	-- Persona
+	Player = false, -- Almost always.  One exception. (-:
 	Name = '',
 	Face = 0,
 
@@ -12,10 +15,24 @@ Character = {
 	-- Experience
 	KillCount = 0,
 
+	-- Methods
+
 	new = function (self,character)
 		local character = character or {}
 		character.Relationships = {}
 		setmetatable(character,{__index = self})
 		return character
-	end
+	end,
+
+	SetName = function (self,name)
+		self.Name = name or 'Test'
+	end,
+
+	PrintStats = function (self)
+		print('Name:',self.Name)
+		print('Luck:',self.Luck)
+		print('Charisma:',self.Charisma)
+		print('Notoriety:',self.Notoriety)
+		print('Lawfulness:',self.Lawfulness)
+	end,
 }
