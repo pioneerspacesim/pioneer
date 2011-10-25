41633e250915588060af73457faee9cc872fd21d
kko <pioneer@paahdin.com>
2011-10-25 17:46:42 -0400

    GaugeType base class

---
+++ src/rocket/RocketGaugeType.cpp
@@ -0,0 +1,32 @@
+#include "RocketGaugeType.h"
+
+RocketGaugeType::RocketGaugeType(RocketGaugeElement *el) : element(el)
+{
+}
+
+RocketGaugeType::~RocketGaugeType()
+{
+}
+
+Rocket::Core::String RocketGaugeType::GetValue() const
+{
+	return element->GetAttribute< Rocket::Core::String >("value", "");
+}
+
+void RocketGaugeType::OnUpdate()
+{
+}
+
+void RocketGaugeType::OnRender()
+{
+}
+
+bool RocketGaugeType::OnAttributeChange(const Core::AttributeNameList& ROCKET_UNUSED(changed_attributes))
+{
+	return true;
+}
+
+void RocketGaugeType::OnPropertyChange(const Core::PropertyNameList& ROCKET_UNUSED(changed_properties))
+{
+}
+
---
+++ src/rocket/RocketGaugeType.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class RocketGaugeElement;
+
+/*
+ * GaugeType base class
+ * Gauge types do the actual drawing & important stuff
+ */
+class RocketGaugeType
+{
+public:
+	RocketGaugeType(RocketGaugeElement* element);
+	virtual ~RocketGaugeType();
+
+	virtual Rocket::Core::String GetValue() const;
+	virtual void OnUpdate();
+	virtual void OnRender();
+	virtual bool OnAttributeChange(const Core::AttributeNameList& changed_attributes);
+	virtual void OnPropertyChange(const Core::PropertyNameList& changed_properties);
+	virtual bool GetIntrinsicDimensions(Rocket::Core::Vector2f& dimensions) = 0;
+protected:
+	RocketGaugeElement* element;
+};
