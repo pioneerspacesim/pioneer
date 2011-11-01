928c67aeb52869c6c42d92fd15aeee51e4e65c9a
Robert Norris <rob@eatenbyagrue.org>
2011-11-01 19:59:19 +1100

    add some comments describing the choice of values for WINVER and _WIN32_IE

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,10 +5,16 @@
 #define PNG_SKIP_SETJMP_CHECK
 #include <png.h>
 
+// MinGW targets NT4 by default. We need to set some higher versions to ensure
+// that functions we need are available. Specifically, SHCreateDirectoryExA
+// requires Windows 2000 and IE5. We include w32api.h to get the symbolic
+// constants for these things.
 #ifdef __MINGW32__
-#	undef WINVER
-#	define WINVER 0x0500
 #	include <w32api.h>
+#	ifdef WINVER
+#		undef WINVER
+#	endif
+#	define WINVER Windows2000
 #	define _WIN32_IE IE5
 #endif
 
