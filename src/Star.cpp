1d9042058208c8610a6c85da925dc361fbbd3e6d
Robert Norris <rob@eatenbyagrue.org>
2011-11-03 08:36:55 +1100

    draw star halos in normal space instead of drawing to the ortho screen

--- src/Star.cpp
+++ src/Star.cpp
@@ -33,31 +33,32 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 
 	glTranslatef(float(fpos.x), float(fpos.y), float(fpos.z));
 	
+	// face the camera dammit
+	vector3d zaxis = viewCoords.NormalizedSafe();
+	vector3d xaxis = vector3d(0,1,0).Cross(zaxis).Normalized();
+	vector3d yaxis = zaxis.Cross(xaxis);
+	matrix4x4d rot = matrix4x4d::MakeRotMatrix(xaxis, yaxis, zaxis).InverseOf();
+	glMultMatrixd(&rot[0]);
+
 	const float *col = StarSystem::starRealColors[GetSBody()->type];
 	const float b = (Render::IsHDREnabled() ? 100.0f : 1.0f);
 	// if star is wolf-rayet it gets a very large halo effect
 	const float wf = ((GetSBody()->type < SBody::TYPE_STAR_S_BH && 
 		GetSBody()->type > SBody::TYPE_STAR_O_HYPER_GIANT) ? 100.0f : 1.0f);
 
-	/* Draw star spikes and halo to 2d ortho screen */
-	
-	Gui::Screen::EnterOrtho();
-	vector3d pp;
-	Gui::Screen::Project(fpos, pp);
-
 	MTRand(rand);
 
-	const float glowrad = float(20.0f+1000.0f*wf*radius/viewCoords.Length());
+	const float glowrad = float(rad*wf*8);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE);	
 	glEnable(GL_BLEND);
 	glBegin(GL_TRIANGLE_FAN);
 	glColor4f(col[0], col[1], col[2], 1);
-	glVertex3f(pp.x, pp.y, 0);
+	glVertex3f(0,0,0);
 	glColor4f(0,0,0,0);
 	for (float ang=0; ang<2*M_PI; ang+=0.26183+rand.Double(0,0.4)) {
-		glVertex3f(pp.x+glowrad*sin(ang), pp.y+glowrad*cos(ang), 0);
+		glVertex3f(glowrad*sin(ang), glowrad*cos(ang), 0);
 	}
-	glVertex3f(pp.x, pp.y+glowrad, 0);
+	glVertex3f(0, glowrad, 0);
 	glEnd();
 	glDisable(GL_BLEND);
 	
@@ -65,17 +66,16 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 	glEnable(GL_BLEND);
 	glColor4f(b*col[0],b*col[1],b*col[2],1);
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex3f(pp.x,pp.y,0);
+	glVertex3f(0,0,0);
 	glColor4f(0,0,0,0);
 	glEnd();
 	
 	Render::State::UseProgram(0);
-	Gui::Screen::LeaveOrtho();
 	glDisable(GL_BLEND);
-
-	TerrainBody::Render(viewCoords, viewTransform);
 
 	glPopMatrix();
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
+
+	TerrainBody::Render(viewCoords, viewTransform);
 }
