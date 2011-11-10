c9fe7ecae443f4799d89d43c33ab63a65977b7ea
Robert Norris <rob@eatenbyagrue.org>
2011-11-11 08:41:42 +1100

    fix my spelling

--- data/modules/AIWarning/AIWarning.lua
+++ data/modules/AIWarning/AIWarning.lua
@@ -5,7 +5,7 @@ EventQueue.onAICompleted:Connect(function (s, e)
 	local messages = {
 		GRAV_TOO_HIGH    = t('Cannot compensate for local gravity'),
 		REFUSED_PERM     = t('Starport refused docking permission'),
-		ORBIT_IMPOSSIBLE = t('Cannot compute orbit paramaters'),
+		ORBIT_IMPOSSIBLE = t('Cannot compute orbit parameters'),
 	}
 
 	if e == 'NONE' then return end
--- data/modules/AIWarning/Languages.lua
+++ data/modules/AIWarning/Languages.lua
@@ -2,6 +2,6 @@ Translate:Add({
 	English = {
 		['Cannot compensate for local gravity'] = 'Cannot compensate for local gravity',
 		['Starport refused docking permission'] = 'Starport refused docking permission',
-		['Cannot compute orbit paramaters'] = 'Cannot compute orbit paramaters',
+		['Cannot compute orbit parameters']     = 'Cannot compute orbit paremeters',
 	}
 })
