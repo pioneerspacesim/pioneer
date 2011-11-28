0ac90e602dcddcf378fdac7fb7ff547ec76df7a5
kko <pioneer@paahdin.com>
2011-11-27 21:02:15 -0500

    Navigation lights function

---
+++ data/models/lights.lua
@@ -0,0 +1,20 @@
+--collision lights (don't blink)
+--red blinking lights on the left (port) side of the craft
+--green blinking lights on the right (starboard) side of the craft
+navigation_lights = function(positions_coll, positions_red, positions_green)
+	local state = get_flight_state()
+	if ((state == 'DOCKING') or (state == 'FLYING' and get_animation_position('WHEEL_STATE') > 0)) then
+
+		billboard('smoke.png', 1.0,  v(1,1,1), positions_coll)
+
+		local lightphase = math.fmod((get_time('SECONDS')*0.75),1)
+
+		if lightphase > .1 and lightphase < .3 then
+			billboard('smoke.png', 1.0,  v(1,0,0), positions_red)
+		end
+
+		if lightphase > .3 and lightphase < .5 then
+			billboard('smoke.png', 1.0,  v(0,1,0), positions_green)
+		end
+	end
+end
