021220616826bc31f7d7d9344c0af29d5cf2d2e7
Robert Norris <rob@eatenbyagrue.org>
2011-12-01 14:01:15 +1100

    whitespace fix

--- src/MathUtil.h
+++ src/MathUtil.h
@@ -8,7 +8,7 @@ public:
 
 	// random point on a sphere, distributed uniformly by area
 	static vector3d RandomPointOnSphere(double minRadius, double maxRadius);
-    static inline vector3d RandomPointOnSphere(double radius) { return RandomPointOnSphere(radius, radius); }
+	static inline vector3d RandomPointOnSphere(double radius) { return RandomPointOnSphere(radius, radius); }
 
 };
 
