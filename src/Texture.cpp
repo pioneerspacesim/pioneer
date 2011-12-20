8b2e2cc5e65c35fbb15ae7560d36745dcd9233c4
Robert Norris <rob@eatenbyagrue.org>
2011-12-20 21:14:09 +1100

    implement wrap, filter and mipmaps

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -48,10 +48,30 @@ bool Texture::CreateFromSurface(SDL_Surface *s)
 
 	glGenTextures(1, &m_glTexture);
 	glBindTexture(m_target, m_glTexture);
-	glTexParameteri(m_target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(m_target, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
 
-	gluBuild2DMipmaps(m_target, GL_RGBA, s->w, s->h, GL_RGBA, GL_UNSIGNED_BYTE, s->pixels);
+	if (m_wrapMode == CLAMP) {
+		glTexParameterf(m_target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf(m_target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	}
+	else {
+		glTexParameterf(m_target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameterf(m_target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
+	if (m_filterMode == NEAREST) {
+		glTexParameteri(m_target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(m_target, GL_TEXTURE_MIN_FILTER, m_hasMipmaps ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST);
+	}
+	else {
+		glTexParameteri(m_target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(m_target, GL_TEXTURE_MIN_FILTER, m_hasMipmaps ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR);
+	}
+
+	// XXX use format options
+	if (m_hasMipmaps)
+		gluBuild2DMipmaps(m_target, GL_RGBA, s->w, s->h, GL_RGBA, GL_UNSIGNED_BYTE, s->pixels);
+	else
+		glTexImage2D(m_target, 0, GL_RGBA, s->w, s->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, s->pixels);
 
 	glBindTexture(m_target, 0);
 	glDisable(m_target);
