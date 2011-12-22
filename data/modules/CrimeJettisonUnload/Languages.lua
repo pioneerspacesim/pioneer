3beb577147927bf50a170cf4b3f7d02a87ca71f4
unknown <Florian Angermeier@FlorianAngermei.(none)>
2011-12-22 23:13:06 +0100

    * Added a LUA module that adds a crime for jettison or unloading rubbish * and radioactive waste.

---
+++ data/modules/CrimeJettisonUnload/Languages.lua
@@ -0,0 +1,9 @@
+  ---- ENGLISH / ENGLISH ----
+  
+Translate:Add({
+	English = {
+		['Police'] = 'Police',
+		['You are not allowed to jettison {cargo} here! Your crime has been recorded.'] = 'You are not allowed to jettison {cargo} here! Your crime has been recorded.',
+		['You are not allowed to unload {cargo} here! Your crime has been recorded.']   = 'You are not allowed to unload {cargo} here! Your crime has been recorded.',
+	}
+})
