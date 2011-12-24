743a1affd57727a8b7f48ba0848ae37b4ded8b06
Robert Norris <rob@eatenbyagrue.org>
2011-12-24 21:55:42 +1100

    rework Gui::Gradient as a Texture subclass

--- src/gui/GuiGradient.cpp
+++ src/gui/GuiGradient.cpp
@@ -4,41 +4,22 @@
 namespace Gui {
 
 Gradient::Gradient() :
-	m_direction(HORIZONTAL),
-	m_texture(0)
+	m_texture(Color(1.0f, 1.0f, 1.0f, 1.0f), Color(0.0f, 0.0f, 0.0f, 1.0f)),
+	m_direction(VERTICAL)
 {
-	SetSize(10.f, 10.f);
-	SetStop(Color(1.f,1.f,1.f,1.f), 0.f);
-	SetStop(Color(0.f,0.f,1.f,1.f), 1.f);
+	SetSize(10.0f, 10.0f);
 }
 
-Gradient::Gradient(float w, float h, const Color &begin, const Color &end, Direction dir) :
-	m_direction(dir),
-	m_texture(0)
+Gradient::Gradient(float width, float height, const Color &begin, const Color &end, Direction direction) :
+	m_texture(begin, end),
+	m_direction(direction)
 {
-	SetSize(w, h);
-	SetStop(begin, 0.f);
-	SetStop(end, 1.f);
-}
-
-Gradient::~Gradient()
-{
-	glDeleteTextures(1, &m_texture);
+	SetSize(width, height);
 }
 
 void Gradient::GetSizeRequested(float size[2])
 {
 	GetSize(size);
-}
-
-void Gradient::SetStop(const Color &col, float pos)
-{
-	pos = Clamp(pos, 0.f, 1.f);
-	m_stops[pos] = col;
-	if (m_texture > 0) {
-		glDeleteTextures(1, &m_texture);
-		m_texture = 0;
-	}
 }
 
 void Gradient::Draw()
@@ -54,7 +35,7 @@ void Gradient::Draw()
 	glEnable(GL_BLEND);
 	glActiveTexture(GL_TEXTURE0);
 	glEnable(GL_TEXTURE_1D);
-	glBindTexture(GL_TEXTURE_1D, GetTexture());
+	m_texture.Bind();
 	glBegin(GL_QUADS);
 	if (m_direction == VERTICAL) {
 		glTexCoord1f(1.f);
@@ -76,44 +57,45 @@ void Gradient::Draw()
 		glVertex2f(0, 0);
 	}
 	glEnd();
+	m_texture.Unbind();
+	glDisable(GL_TEXTURE_1D);
 	glPopAttrib();
 }
 
-//Get the current texture or regenerate if updated
-GLuint Gradient::GetTexture()
-{
-	if (m_texture == 0)
-		m_texture = GenerateTexture();
 
-	return m_texture;
+Gradient::GradientTexture::GradientTexture(const Color &begin, const Color &end) :
+	Texture(GL_TEXTURE_1D, TextureFormat(GL_RGBA, GL_RGBA, GL_FLOAT), CLAMP, LINEAR, false, false),
+	m_needGenerate(true)
+{
+	SetStop(begin, 0.0f);
+	SetStop(end, 1.0f);
 }
 
-//Generate a new texture
-//Remember to delete if you use this elsewhere
-GLuint Gradient::GenerateTexture()
+void Gradient::GradientTexture::SetStop(const Color &col, float pos)
 {
+	pos = Clamp(pos, 0.f, 1.f);
+	m_stops[pos] = col;
+	m_needGenerate = true;
+}
+
+void Gradient::GradientTexture::GenerateGradient()
+{
+	// XXX this only handles begin & end stops
+
 	const int width = 2;
 	assert(m_stops.size() > 0);
-	GLuint tex;
 
-	const Color beg = m_stops[0.f];
-	const Color end = m_stops[1.f];
+	const Color begin = m_stops[0.0f];
+	const Color end   = m_stops[1.0f];
 
-	const GLfloat buf[width][4] = {
-		{ beg.r, beg.g, beg.b, beg.a },
-		{ end.r, end.g, end.b, end.a },
+	const GLfloat data[width][4] = {
+		{ begin.r, begin.g, begin.b, begin.a },
+		{ end.r,   end.g,   end.b,   end.a   },
 	};
 
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_1D, tex);
-	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, width, 0,
-		GL_RGBA, GL_FLOAT, buf);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glBindTexture(GL_TEXTURE_1D, 0);
-	return tex;
+	CreateFromArray(data, width, 0);
+
+	m_needGenerate = false;
 }
 
 }
