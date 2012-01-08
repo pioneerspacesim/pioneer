8bcf42d0d0bf468be4e41c7c30c85d8e4a1cf925
Brian Ronald <brian@ppcis.org>
2012-01-08 16:46:45 +0000

    Initial comment in the stats tracking module.

---
+++ data/modules/StatsTracking/Statstracking.lua
@@ -0,0 +1,6 @@
+-- Stats-gathering module. Initially, gathers kill statistics for the player.
+-- Can (and should) be expanded in the future to gather other information.
+
+-- The information gathered here is stored in the player's character sheet.
+-- This is globally available to all Lua scripts. Retrieval methods should
+-- be implemented as part of Characters.lua.
