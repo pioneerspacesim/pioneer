9785590a5090c3a29f3ee5bf29bc331e10e00c8a
Brian Ronald <brian@ppcis.org>
2012-01-08 18:17:33 +0000

    Implemented kill counter and assist counter (the latter is disabled due to problems)

--- data/modules/StatsTracking/Statstracking.lua
+++ data/modules/StatsTracking/Statstracking.lua
@@ -4,3 +4,27 @@
 -- The information gathered here is stored in the player's character sheet.
 -- This is globally available to all Lua scripts. Retrieval methods should
 -- be implemented as part of Characters.lua.
+
+-- This is used for tracking which ships were damaged by the player, so that
+-- we can award assists as well as kills. One day, assists might contribute to
+-- the combat rating.
+local PlayerDamagedShips = {}
+
+local onShipDestroyed = function (ship, attacker)
+	if attacker:IsPlayer() then
+		-- Increment player's kill count
+		PersistentCharacters.player.killcount = PersistentCharacters.player.killcount + 1
+		PlayerDamagedShips[ship]=nil
+	elseif PlayerDamagedShips[ship] then
+		PersistentCharacters.player.assistcount = PersistentCharacters.player.assistcount + 1
+	end
+end
+
+local onShipHit = function (ship, attacker)
+	if attacker:IsPlayer() then
+		PlayerDamagedShips[ship]=true
+	end
+end
+
+EventQueue.onShipDestroyed:Connect(onShipDestroyed)
+--EventQueue.onShipHit:Connect(onShipHit)
