d43d4e98d91bf3c56f7d0cc5cc76099bc3c585ac
Brian Ronald <brian@ppcis.org>
2012-01-08 19:29:31 +0000

    "Right on, Commander!"

--- data/modules/StatsTracking/Statstracking.lua
+++ data/modules/StatsTracking/Statstracking.lua
@@ -10,11 +10,20 @@
 -- the combat rating.
 local PlayerDamagedShips = {}
 
+-- We need this to translate "Right on, Commander" etc
+local t = Translate:GetTranslator()
+
+
 local onShipDestroyed = function (ship, attacker)
 	if attacker:isa('Ship') and attacker:IsPlayer() then
 		-- Increment player's kill count
 		PersistentCharacters.player.killcount = PersistentCharacters.player.killcount + 1
 		PlayerDamagedShips[ship]=nil
+		if PersistentCharacters.player.killcount == 1 or (PersistentCharacters.player.killcount < 256 and PersistentCharacters.player.killcount % 16 == 0) or (PersistentCharacters.player.killcount % 256 == 0) then
+			-- On the first kill, every 16th kill until 256, and every 256th
+			-- kill thereafter
+			UI.ImportantMessage(t('Right on, Commander!'),t('Elite Federation of Pilots'))
+		end
 	elseif PlayerDamagedShips[ship] then
 		PersistentCharacters.player.assistcount = PersistentCharacters.player.assistcount + 1
 	end
@@ -27,4 +36,5 @@ local onShipHit = function (ship, attacker)
 end
 
 EventQueue.onShipDestroyed:Connect(onShipDestroyed)
+--Commented out pending issue #887
 --EventQueue.onShipHit:Connect(onShipHit)
