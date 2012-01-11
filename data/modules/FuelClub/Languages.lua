127d3fa447875267b5b5876b4723b3709d5676f8
Robert Norris <rob@eatenbyagrue.org>
2012-01-11 13:22:40 +1100

    rename Español to Spanish to match core translation file

--- data/modules/FuelClub/Languages.lua
+++ data/modules/FuelClub/Languages.lua
@@ -56,7 +56,7 @@ Translate:Add({Polski = {
 
    ---- SPANISH / ESPAÑOL ----
 
-Translate:AddFlavour('Español','FuelClub',{
+Translate:AddFlavour('Spanish','FuelClub',{
 	-- Translators: Feel free to change this in your language!
 	-- It's a proper name, so exact translation is not vital.
 	clubname = "Cooperativa Interestelar de Combustibles",
@@ -73,7 +73,7 @@ Apúntese! Afiliación anual por solo {membership_fee}]],
 	annual_fee = 400,
 })
 
-Translate:Add({Español = {
+Translate:Add({Spanish = {
 	["What conditions apply to {radioactives} disposal?"] = "Cuáles son las condiciones aplicables en la disponibilidad de {radioactives}?",
 	["We will only dispose of as many tonnes of {radioactives} as you have bought tonnes of {military_fuel} from us."] = "Solo dispensaremos una cantidad de {radioactives} equivalente a las toneladas de {military_fuel} adquiridas.",
 	["Apply for membership"] = "Solicitar el ingreso",
