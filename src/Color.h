7a77e9a8ec81e17629e00b70b5a42a8649002010
kko <pioneer@paahdin.com>
2012-01-17 21:27:31 -0500

    Single-value constructor for color

--- src/Color.h
+++ src/Color.h
@@ -5,6 +5,7 @@ class Color {
 public:
 	float r,g,b,a;
 	Color() {}
+	Color(float v_) : r(v_), g(v_), b(v_), a(v_) { }
 	Color(float r_, float g_, float b_, float a_): r(r_), g(g_), b(b_), a(a_) {}
 	operator float *() { return &r; }
 	operator const float *() const { return &r; }
