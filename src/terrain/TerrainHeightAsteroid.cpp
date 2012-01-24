410c3fe7cea88601e1b26268718cc9d48cdfd050
s20dan <s20dan@hotmail.co.uk>
2012-01-24 01:30:36 +0000

    New Asteroid terrain.

--- src/terrain/TerrainHeightAsteroid.cpp
+++ src/terrain/TerrainHeightAsteroid.cpp
@@ -5,20 +5,21 @@
 using namespace TerrainNoise;
 using namespace TerrainFeature;
 
+// Banded/Ridged pattern mountainous terrain, could pass for desert
+
 template <>
 const char *TerrainHeightFractal<TerrainHeightAsteroid>::GetHeightFractalName() const { return "Asteroid"; }
 
 template <>
 TerrainHeightFractal<TerrainHeightAsteroid>::TerrainHeightFractal(const SBody *body) : Terrain(body)
 {
-	SetFracDef(0, m_maxHeightInMeters, m_planetRadius);
-	// craters
-	SetFracDef(1, 5000.0, 1000000.0, 1000.0*m_fracmult);
 }
 
 template <>
 double TerrainHeightFractal<TerrainHeightAsteroid>::GetHeight(const vector3d &p)
 {
-	return std::max(0.0, m_maxHeight * (octavenoise(GetFracDef(0), 0.5, p) + 
-			GetFracDef(1).amplitude * crater_function(GetFracDef(1), p)));
+	float heightmap = octavenoise(8, 0.2*octavenoise(1, 0.3, 3.7, p), 15.0*octavenoise(1, 0.5, 4.0, p), p) - 
+		0.75*billow_octavenoise(16*octavenoise(1, 0.275, 3.2, p), 0.4*octavenoise(1, 0.4, 3.0, p), 4.0*octavenoise(1, 0.35, 3.7, p), p);
+
+	return m_maxHeight*heightmap;
 }
