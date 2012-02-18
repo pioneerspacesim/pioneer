29091f5583aab621594c8775c700f61d7c214cdd
John Bartholomew <jpa.bartholomew@gmail.com>
2012-02-18 06:53:35 +0000

    scrap utils GetDirectoryContents and replace with uses of FileEnumerator

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,27 +7,6 @@
 #define PNG_SKIP_SETJMP_CHECK
 #include <png.h>
 
-void GetDirectoryContents(const std::string &path, std::list<std::string> &files)
-{
-	DIR *dir = opendir(path.c_str());
-	if (!dir) {
-		//if (-1 == mkdir(name, 0770)
-		Error("%s", stringf(Lang::COULD_NOT_OPEN_FILENAME, formatarg("path", path)).c_str());
-		return;
-	}
-	struct dirent *entry;
-
-	while ((entry = readdir(dir))) {
-		if (strcmp(entry->d_name, ".")==0) continue;
-		if (strcmp(entry->d_name, "..")==0) continue;
-		files.push_back(entry->d_name);
-	}
-
-	closedir(dir);
-
-	files.sort();
-}
-
 FILE *fopen_or_die(const char *filename, const char *mode)
 {
 	FILE *f = fopen(filename, mode);
--- src/utils.h
+++ src/utils.h
@@ -37,8 +37,6 @@ void Error(const char *format, ...) __attribute((format(printf,1,2))) __attribut
 void Warning(const char *format, ...) __attribute((format(printf,1,2)));
 void SilentWarning(const char *format, ...) __attribute((format(printf,1,2)));
 
-void GetDirectoryContents(const std::string &path, std::list<std::string> &files);
-
 std::string string_join(std::vector<std::string> &v, std::string sep);
 std::string format_date(double time);
 std::string format_date_only(double time);
