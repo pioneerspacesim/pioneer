0b7a7a0617892745dfdb82e34e250f1ff4f27334
John Bartholomew <jpa.bartholomew@gmail.com>
2012-02-18 09:20:35 +0000

    call FileSystem::Init()/Uninit() in LuaModelViewer

--- src/LuaModelViewer.cpp
+++ src/LuaModelViewer.cpp
@@ -8,6 +8,7 @@
 #include "Ship.h" // for the flight state and ship animation enums
 #include "SpaceStation.h" // for the space station animation enums
 #include "TextureCache.h"
+#include "FileSystem.h"
 
 enum ModelCategory {
 	MODEL_OTHER,
@@ -737,6 +738,8 @@ int main(int argc, char **argv)
 		g_height = 600;
 	}
 
+	FileSystem::Init();
+
 	const SDL_VideoInfo *info = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
@@ -809,5 +812,6 @@ int main(int argc, char **argv)
 	}
 
 	g_viewer->MainLoop();
+	FileSystem::Uninit();
 	return 0;
 }
