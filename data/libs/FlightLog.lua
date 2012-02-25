2ff8ee5131f6798a0f6c4f3f82b512b94b58b2b6
Brian Ronald <brian@ppcis.org>
2012-02-25 14:50:56 +0000

    Need a new FlightLog class... let's start with the codedoc

---
+++ data/libs/FlightLog.lua
@@ -0,0 +1,67 @@
+--
+-- Class: FlightLog
+--
+-- A flight log, containing the last systems and stations visited by the
+-- player. Can be used by scripts to find out where the player has been
+-- recently.
+
+FlightLog = {
+
+--
+-- Group: Methods
+--
+
+--
+-- Method: GetPreviousSystemPath
+--
+-- Returns a SystemPath object that points to the star system where the
+-- player was before jumping to this one. If none is on record (such as
+-- before any hyperjumps have been made) it returns nil.
+--
+-- path = FlightLog.GetPreviousSystemPath()
+--
+-- Return:
+--
+--   path - a SystemPath object
+--
+-- Availability:
+--
+--   alpha 20
+--
+-- Status:
+--
+--   experimental
+--
+
+	GetPreviousSystemPath = function ()
+	end
+
+--
+-- Method: GetPreviousStationPath
+--
+-- Returns a SystemPath object that points to the starport most recently
+-- visited. If the player is currently docked, then the starport prior to
+-- the present one (which might be the same one, if the player launches
+-- and lands in the same port). If none is on record (such as before the
+-- player has ever launched) it returns nil.
+--
+-- path = FlightLog.GetPreviousStationPath()
+--
+-- Return:
+--
+--   path - a SystemPath object
+--
+-- Availability:
+--
+--   alpha 20
+--
+-- Status:
+--
+--   experimental
+--
+
+	GetPreviousStationPath = function ()
+	end
+
+}
+
