b8d56b345982068408c66688795a79e12dddc1c5
gernot schrader <gernot66@bluewin.ch>
2012-03-13 12:58:58 +0100

    courier update/fix 03-12 gernot66
    Conflicts:
    	data/models/ships/courier/courier.lua

---
+++ data/ships/imperial_trader.lua
@@ -0,0 +1,26 @@
+define_ship {
+	name = 'Imperial Trader',
+	model = 'trader',
+	forward_thrust = -8e7,
+	reverse_thrust = 3e7,
+	up_thrust = 3e7,
+	down_thrust = -1e7,
+	left_thrust = -1e7,
+	right_thrust = 1e7,
+	angular_thrust = 15e7,
+	gun_mounts =
+	{
+		{ v(0,0.6,-36), v(0,0,-1) },
+		{ v(0,0,22), v(0,0,1) },
+	},
+	max_cargo = 450,
+	max_laser = 1,
+	max_missile = 6,
+	max_cargoscoop = 0,
+	capacity = 450,
+	hull_mass = 300,
+	fuel_tank_mass = 150,
+	thruster_fuel_use = 0.0002,
+	price = 954000,
+	hyperdrive_class = 5,
+}
