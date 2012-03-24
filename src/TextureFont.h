012a333570a2da109471ae9e9e5d08abeb455c4d
Robert Norris <rob@eatenbyagrue.org>
2012-03-25 08:50:47 +1100

    pedantry

--- src/TextureFont.h
+++ src/TextureFont.h
@@ -49,8 +49,9 @@ private:
 
 	static int s_glyphCount;
 	std::map<Uint32,glfglyph_t> m_glyphs;
-	static const Uint32 m_firstCharacter = 0x20; //32
-	static const Uint32 m_lastCharacter = 0x1ff; //511
+
+	static const Uint32 s_firstCharacter = 0x20; //32
+	static const Uint32 s_lastCharacter = 0x1ff; //511
 };
 
 #endif
