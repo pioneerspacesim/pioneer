f357080f63d9c43e41dc1f970ddc98b383b2cef5
Robert Norris <rob@eatenbyagrue.org>
2012-03-26 21:53:23 +1100

    allow textures to be set even if material is not

--- src/lmr/GeomBuffer.cpp
+++ src/lmr/GeomBuffer.cpp
@@ -554,12 +554,11 @@ void GeomBuffer::EnsureSurface()
 		//mat->specular = lmrMat.specular;
 		//mat->emissive = lmrMat.emissive;
 		//mat->shininess = lmrMat.shininess
-		
-		if (curTexture)
-			mat->texture0 = Graphics::TextureBuilder::Model(*curTexture).GetOrCreateTexture(m_renderer);
-
-		mat->unlit = true;
 	}
+	if (curTexture)
+		mat->texture0 = Graphics::TextureBuilder::Model(*curTexture).GetOrCreateTexture(m_renderer);
+
+	mat->unlit = true;
 		
 	m_curSurface.Reset(new Graphics::Surface(Graphics::TRIANGLES, new Graphics::VertexArray(Graphics::ATTRIB_POSITION | Graphics::ATTRIB_NORMAL | Graphics::ATTRIB_UV0), RefCountedPtr<Graphics::Material>(mat)));
 }
