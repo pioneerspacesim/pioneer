76a3bfbe73eae12579517e24dbc249243ebfd72d
Robert Norris <rob@eatenbyagrue.org>
2012-04-07 21:09:39 +1000

    get font descriptors from config (I think)

--- src/FontConfig.cpp
+++ src/FontConfig.cpp
@@ -18,3 +18,19 @@ void FontConfig::SetDefaults()
 	m_map["PixelHeight"] = "12";
 	m_map["AdvanceXAdjustment"] = "0";
 }
+
+Text::FontDescriptor FontConfig::GetDescriptor()
+{
+	std::string filename(String("FontFile"));
+
+	int pixelWidth = Int("PixelWidth");
+	if (pixelWidth) {
+		int pixelHeight = Int("PixelHeight");
+		bool outline = Int("Outline") ? true : false;
+		float advanceXAdjustment = Float("AdvanceXAdjustment");
+		return Text::FontDescriptor(filename, pixelWidth, pixelHeight, outline, advanceXAdjustment);
+	}
+
+	float pointSize = Float("PointSize");
+	return Text::FontDescriptor(filename, pointSize);
+}
