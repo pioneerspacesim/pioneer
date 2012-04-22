951b3df811bb2d96562b60e2b9d89212bfa2a71d
Robert Norris <rob@eatenbyagrue.org>
2012-04-22 22:24:22 +1000

    FileSourceZip implementation

--- src/FileSourceZip.h
+++ src/FileSourceZip.h
@@ -2,6 +2,9 @@
 #define _FILESOURCEZIP_H
 
 #include "FileSystem.h"
+#include <SDL_stdinc.h>
+#include <map>
+#include <string>
 
 namespace FileSystem {
 
@@ -14,7 +17,18 @@ public:
 	virtual RefCountedPtr<FileData> ReadFile(const std::string &path);
 	virtual bool ReadDirectory(const std::string &path, std::vector<FileInfo> &output);
 
-	static void Init();
+private:
+	void *m_archive;
+
+	struct FileStat {
+		FileStat(Uint32 _index, Uint64 _size, FileInfo _info) : index(_index), size(_size), info(_info) {}
+		const Uint32 index;
+		const Uint64 size;
+		const FileInfo info;
+	};
+
+	typedef std::map<std::string,FileStat> FileMap;
+	FileMap m_index;
 };
 
 }
