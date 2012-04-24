75454d7945e2df8e6cae5b95872d81e7fe4f2d8a
Aaron Senese <aaronsenese@yahoo.com>
2012-04-23 19:09:45 -0700

    Adjusted Meteor mesh with an initial smoth-face pass. Next pass will be more selective. Increased Meteor's fuel tank mass and thrust. Increased fuel burn rate to fit the ship's short range role. Fixed nav lights to fit the ship's layout. Further inset thrusters so they don't float above the nozzles, making their position apepar nebulous. Scaled model by a factor of 1.2 Refined thruster positions.

--- data/models/ships/meteor/meteor.lua
+++ data/models/ships/meteor/meteor.lua
@@ -1,23 +1,8 @@
-define_model('gear', {
-	info = {
-		--scale = 1.0,
-		bounding_radius = 5,
-		lod_pixels = {50,140},
-		materials = {'default'},
-	},
-	static = function(lod)
-		set_material('default', 0.8,0.8,0.8,1, .3,.3,.3,5, 0,0,0)
-		use_material('default')
-		texture('gear_diff.png')
-		load_obj('gear.obj')
-	end
-})
-
 define_model('meteor', {
 	info = {
-		scale = 1,
+		scale = 1.2,
 		bounding_radius = 40,
-		lod_pixels = {50 , 200, 0},
+		lod_pixels = {60 , 200, 0},
 		materials = {'default'},
 		tags = {'ship'},
 	},
@@ -39,28 +24,30 @@ define_model('meteor', {
 
 		--thrusters from blender
 		--mains (rear)
-		thruster(v(0.600, 0.000, 5.000), v(0.000, 0.000, 1.000), 5, true)
-		thruster(v(-0.600, 0.000, 5.000), v(0.000, 0.000, 1.000), 5, true)
-		thruster(v(0.200, 0.000, 5.000), v(0.000, 0.000, 1.000), 5, true)
-		thruster(v(-0.200, 0.000, 5.000), v(0.000, 0.000, 1.000), 5, true)
+		thruster(v(0.600, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
+		thruster(v(-0.600, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
+		thruster(v(0.200, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
+		thruster(v(-0.200, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
+		thruster(v(1.000, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
+		thruster(v(-1.000, 0.000, 5.000), v(0.000, 0.000, 1.000), 6, true)
 		--dorsal fore
-		thruster(v(1.200, 0.060, -7.000), v(0.000, 1.000, 0.000), 2, false)
-		thruster(v(-1.200, 0.060, -7.000), v(0.000, 1.000, 0.000), 2, false)
+		thruster(v(1.300,-0.100, -7.500), v(0.000, 1.000, 0.000), 1, false)
+		thruster(v(-1.300,-0.100, -7.500), v(0.000, 1.000, 0.000), 1, false)
 		--ventral fore
-		thruster(v(1.200, -0.200, -7.200), v(0.000, -1.000, 0.000), 2, false)
-		thruster(v(-1.200,-0.200, -7.200), v(0.000, -1.000, 0.000), 2, false)
+		thruster(v(1.400, -0.400, -7.500), v(0.000, -1.000, 0.000), 1, false)
+		thruster(v(-1.400,-0.400, -7.500), v(0.000, -1.000, 0.000), 1, false)
 		--dorsal aft (upper wings)
-		thruster(v(2.8, 1.500, 4.000), v(-0.500, 1.000, 0.000), 3, false)
-		thruster(v(-2.8, 1.500, 4.000), v(0.500, 1.000, 0.000), 3, false)
+		thruster(v(2.900, 1.300, 4.200), v(-0.500, 1.000, 0.000), 2, false)
+		thruster(v(-2.900, 1.300, 4.200), v(0.500, 1.000, 0.000), 2, false)
 		--ventral aft (lower fins)
-		thruster(v(2.8, -1.500, 4.000), v(-0.500, -1.000, 0.000), 3, false)
-		thruster(v(-2.8, -1.500, 4.000), v(0.500, -1.000, 0.000), 3, false)
+		thruster(v(3.000, -1.600, 4.200), v(-0.500, -1.000, 0.000), 2, false)
+		thruster(v(-3.000, -1.600, 4.200), v(0.500, -1.000, 0.000), 2, false)
 		--flank fore
-		thruster(v( 2.000, 0.000, -6.400), v(1.000, 0.000, 0.000), 2, false)
-		thruster(v(-2.000, 0.000, -6.400), v(-1.000, 0.000, 0.000), 2, false)
+		thruster(v( 2.000,-0.200, -6.800), v(1.000, 0.000, 0.000), 1, false)
+		thruster(v(-2.000,-0.200, -6.800), v(-1.000, 0.000, 0.000), 1, false)
 		--flank aft
-		thruster(v( 2.000, 0.000, 4.400), v(1.000, 0.000, 0.000), 3, false)
-		thruster(v(-2.000, 0.000, 4.400), v(-1.000, 0.000, 0.000), 3, false)
+		thruster(v( 1.800, 0.000, 4.600), v(1.000, 0.000, 0.000), 2, false)
+		thruster(v(-1.800, 0.000, 4.600), v(-1.000, 0.000, 0.000), 2, false)
 
 	end,
 	dynamic = function(lod)
@@ -75,9 +62,9 @@ define_model('meteor', {
 		--lights visible when the craft is docking, landing
 		--or landing gear is down
 		navigation_lights(
-			{v(4.355, 0.763, -0.018), v(-4.467, 0.763, -0.018), },
-			{v(4.030, 0.028, 8.898), v(3.300, 0.028, -8.869), },
-			{v(-4.030, 0.028, 8.898), v(-3.300, 0.028, -8.869), }
+			{v(1.500,-1.000,-0.018), v(-1.500, -1.000, 0.018), },
+			{v( 3.100, 1.500, 4.000), v( 3.100,-2.000, 4.000), },
+			{v(-3.100, 1.500, 4.000), v(-3.100,-2.000, 4.000), }
 		)
 	end
 })
