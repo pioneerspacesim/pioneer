f61e1bad208edad6fe48520e7a16c6d56a425ac0
Robert Norris <rob@eatenbyagrue.org>
2012-05-03 17:35:41 +1000

    move list alpha values out to constants

--- src/ui/List.cpp
+++ src/ui/List.cpp
@@ -3,6 +3,11 @@
 
 namespace UI {
 
+// XXX skin
+static const float ALPHA_NORMAL = 0.0f;
+static const float ALPHA_HOVER  = 0.4f;
+static const float ALPHA_SELECT = 0.6f;
+
 List::List(Context *context) : Container(context), m_selected(0)
 {
 	Context *c = GetContext();
@@ -28,10 +33,11 @@ List *List::AddOption(const std::string &text)
 
 	VBox *vbox = static_cast<VBox*>(m_container->GetInnerWidget());
 
-	ColorBackground *background = c->ColorBackground(Color(0,0,0,0));
+	unsigned int index = m_optionBackgrounds.size();
+
+	ColorBackground *background = c->ColorBackground(Color(0,0,0, m_selected == index ? ALPHA_SELECT : ALPHA_NORMAL));
 	vbox->PackEnd(background->SetInnerWidget(c->Label(text)));
 
-	unsigned int index = m_optionBackgrounds.size();
 	background->onMouseOver.connect(sigc::bind(sigc::mem_fun(this, &List::HandleOptionMouseOver), index));
 	background->onMouseOut.connect(sigc::bind(sigc::mem_fun(this, &List::HandleOptionMouseOut), index));
 	background->onClick.connect(sigc::bind(sigc::mem_fun(this, &List::HandleOptionClick), index));
@@ -43,20 +49,20 @@ List *List::AddOption(const std::string &text)
 
 bool List::HandleOptionMouseOver(unsigned int index)
 {
-	m_optionBackgrounds[index]->SetColor(Color(0,0,0,0.4f));
+	m_optionBackgrounds[index]->SetColor(Color(0,0,0, ALPHA_HOVER));
 	return false;
 }
 
 bool List::HandleOptionMouseOut(unsigned int index)
 {
-	m_optionBackgrounds[index]->SetColor(Color(0,0,0, m_selected == index ? 0.6f : 0));
+	m_optionBackgrounds[index]->SetColor(Color(0,0,0, m_selected == index ? ALPHA_SELECT : ALPHA_NORMAL));
 	return false;
 }
 
 bool List::HandleOptionClick(unsigned int index)
 {
 	if (m_selected != index) {
-		m_optionBackgrounds[m_selected]->SetColor(Color(0,0,0,0));
+		m_optionBackgrounds[m_selected]->SetColor(Color(0,0,0, ALPHA_NORMAL));
 		m_selected = index;
 		onOptionSelected.emit(m_options[index]);
 	}
