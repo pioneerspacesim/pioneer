850d463f7b583740e59c029e24c09ac0b35f7d86
John Bartholomew <jpa.bartholomew@gmail.com>
2012-05-04 23:14:51 +0100

    remove some unused helper functions from LuaMathTypes.h

--- src/LuaMathTypes.h
+++ src/LuaMathTypes.h
@@ -28,31 +28,4 @@ namespace MyLuaMatrix {
 	int Matrix_register (lua_State *L);
 } /* namespace MyLuaMatrix */
 
-// i'll just throw them in here...
-static inline const char *luaPi_gettable_checkstring(lua_State *L, int table, int idx)
-{
-	lua_pushinteger(L, idx);
-	lua_gettable(L, (table<0 ? table-1 : table));
-	const char *r = luaL_checkstring(L, -1);
-	lua_pop(L, 1);
-	return r;
-}
-
-static inline double luaPi_gettable_checknumber(lua_State *L, int table, int idx)
-{
-	lua_pushinteger(L, idx);
-	lua_gettable(L, (table<0 ? table-1 : table));
-	double num = luaL_checknumber(L, -1);
-	lua_pop(L, 1);
-	return num;
-}
-
-static inline int luaPi_gettable_checkinteger(lua_State *L, int table, int idx)
-{
-	lua_pushinteger(L, idx);
-	lua_gettable(L, (table<0 ? table-1 : table));
-	int num = luaL_checkinteger(L, -1);
-	lua_pop(L, 1);
-	return num;
-}
 #endif /* _MYLUAMATHTYPES_H */
