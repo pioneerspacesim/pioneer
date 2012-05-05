5e912fa9c51907dfa29ae6ad756cc7ebe0fd7600
Simon Rinehart <spumantii@hotmail.com>
2012-05-04 18:09:38 -0700

    cobra rescale model thrusters lights

--- data/models/ships/CobraI/CobraI.lua
+++ data/models/ships/CobraI/CobraI.lua
@@ -1,6 +1,5 @@
 define_model('CobraI_front_gear', {
 	info = {
-		scale = 1,
 		bounding_radius = 5,
 		lod_pixels = {100},
 		materials = {'default'},
@@ -13,7 +12,6 @@ define_model('CobraI_front_gear', {
 
 define_model('CobraI_rear_gear', {
 	info = {
-		scale = 1,
 		bounding_radius = 5,
 		lod_pixels = {100},
 		materials = {'default'},
@@ -26,7 +24,6 @@ define_model('CobraI_rear_gear', {
 
 define_model('CobraI', {
 	info = {
-		scale = 6,
 		bounding_radius = 15,
 		lod_pixels = {100, 250, 500},
 		materials = {'default'},
@@ -44,32 +41,30 @@ define_model('CobraI', {
 		
 
 		--forward
-		xref_thruster(v(1.22, 0.22, 1.41), v(0.000, 0.000, 1.000), 4, false)
-		xref_thruster(v(0.911, 0.22, 1.41), v(0.000, 0.000, 1.000), 4, false)
-		xref_thruster(v(1.22, -0.22, 1.41), v(0.000, 0.000, 1.000), 4, false)
-		xref_thruster(v(0.911, -0.22, 1.41), v(0.000, 0.000, 1.000), 4, false)
+		xref_thruster(v(7.32, 1.32, 8.46), v(0.000, 0.000, 1.000), 4, false)
+		xref_thruster(v(5.47, 1.32, 8.46), v(0.000, 0.000, 1.000), 4, false)
+		xref_thruster(v(7.32, -1.32, 8.46), v(0.000, 0.000, 1.000), 4, false)
+		xref_thruster(v(5.47, -1.32, 8.46), v(0.000, 0.000, 1.000), 4, false)
 		
 		--backward 
-		xref_thruster(v(0.48, 0, -1.55), v(0, 0, -1.000), 3, false)
-		
-
+		xref_thruster(v(2.88, 0, -9.3), v(0, 0, -1.000), 3, false)
 
 		--sideslip left
-		thruster(v(2.7, 0, 0), v(1, 0, 0), 2, true)
+		thruster(v(16.2, 0, 0), v(1, 0, 0), 2, true)
 		--side right
-		thruster(v(-2.7, 0, 0), v(-1, 0, 0), 2, true)
+		thruster(v(-16.2, 0, 0), v(-1, 0, 0), 2, true)
 		--up
-		xref_thruster(v(0.66, -0.72, 1.12), v(0, -1, 0), 2, true)
+		xref_thruster(v(3.96, -4.32, 6.72), v(0, -1, 0), 2, true)
 		--down
-		xref_thruster(v(0.66, 0.72, 1.12), v(0, 1, 0), 2, true)
+		xref_thruster(v(3.96, 4.32, 6.72), v(0, 1, 0), 2, true)
 		
 		--pitch up
-		xref_thruster(v(0.48, 0, -1.55), v(0, -1, 0), 1, false)
-		xref_thruster(v(0.66, 0.72, 1.12), v(0, 1, 0), 1, false)
+		xref_thruster(v(2.88, 0, -9.3), v(0, -1, 0), 1, false)
+		xref_thruster(v(3.96, 4.32, 6.72), v(0, 1, 0), 1, false)
 		
 		--pitch down
-		xref_thruster(v(0.48, 0, -1.55), v(0, 1, 0), 1, false)
-		xref_thruster(v(0.66, -0.72, 1.12), v(0, -1, 0), 1, false)
+		xref_thruster(v(2.88, 0, -9.3), v(0, 1, 0), 1, false)
+		xref_thruster(v(3.96, -4.32, 6.72), v(0, -1, 0), 1, false)
 		
 
 
@@ -80,17 +75,17 @@ define_model('CobraI', {
 			--gear animation. In Blender we have determined that the gear
 			--should be translated 0.5 units downwards.
 			local gearpos = get_animation_position('WHEEL_STATE')
-			call_model('CobraI_front_gear', v(0,-0.67 * gearpos,0), v(1,0,0), v(0,1,0), 1.0)
-			call_model('CobraI_rear_gear', v(0,-0.3 * gearpos,0), v(1,0,0), v(0,1,0), 1.0)
+			call_model('CobraI_front_gear', v(0,-3.5 * gearpos,-3.5 * gearpos), v(1,0,0), v(0,1,0), 1.0)
+			call_model('CobraI_rear_gear', v(0,-1.8 * gearpos,0), v(1,0,0), v(0,1,0), 1.0)
 		end
 
 		--lights visible when the craft is docking, landing
 		--or landing gear is down
 		-- (white, red, green)
 		navigation_lights(
-			{v(2.62, 0, 0), v(-2.62, 0, 0), v(0, 0, -1.66), v(0, 0, 1.3), },
-			{v(-1.87, 0, -1.11), v(-0.3, 0, 1.3), },
-			{v(1.87, 0, -1.11), v(0.3, 0, 1.3), }
+			{v(15.5, 0, 0), v(-15.5, 0, 0), v(0, 0, -9.6), v(0, 0, 7.5), },
+			{v(-11, 0, -6.5), v(-1.8, 0, 7.5), },
+			{v(11, 0, -6.5), v(1.8, 0, 7.5), }
 		)
 	end,
 })
