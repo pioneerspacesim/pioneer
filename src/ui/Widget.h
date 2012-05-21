1dd87ebf8531be700df99c6e7cc40218d3422f9d
Robert Norris <rob@eatenbyagrue.org>
2012-05-21 17:29:54 +1000

    refcount widgets in containers so they can be freed easily

--- src/ui/Widget.h
+++ src/ui/Widget.h
@@ -4,6 +4,7 @@
 #include "libs.h"
 #include "vector2.h"
 #include "Event.h"
+#include "RefCounted.h"
 
 // Widget is the base class for all UI elements. There's a couple of things it
 // must implement, and a few more it might want to implement if it wants to do
@@ -75,7 +76,7 @@ class Context;
 class Container;
 class Metrics;
 	
-class Widget {
+class Widget : public RefCounted {
 protected:
 	// can't instantiate a base widget directly
 	Widget(Context *context);
