374c6548631e9304953d4158d06314648251b327
Ae-2222 <aeq6789@ymail.com>
2012-06-02 08:12:52 +0200

    Initial commit

--- src/terrain/TerrainHeightMapped3.cpp
+++ src/terrain/TerrainHeightMapped3.cpp
@@ -36,72 +36,67 @@ double TerrainHeightFractal<TerrainHeightMapped3>::GetHeight(const vector3d &p)
 		// p0,1 p1,1 p2,1 p3,1							
 		// p0,0 p1,0 p2,0 p3,0
 
-		double pp[4][4][4];
-		for (int x=-1; x<3; x++) {
-			for (int y=-1; y<3; y++) { 
-				
-				const int xx = Clamp(x+ix,0,(m_heightMapSizeX+3)-1)*4;
-				const int offset = Clamp(y+iy, 0, (m_heightMapSizeY+3)-1)*(m_heightMapSizeX+3)*4 +xx;
-				Uint16* hrmappoint = m_heightMapScaled+offset;
-				
-				pp[1+x][1+y][0] = hrmappoint[0];
-				pp[1+x][1+y][1] = hrmappoint[1];
-				pp[1+x][1+y][2] = hrmappoint[2];
-				pp[1+x][1+y][3] = hrmappoint[3];
-				static int i = 0;
-				
-				
-				// convert from map format
-				pp[1+x][1+y][3] = (pp[1+x][1+y][3]*m_roughnessScaling[2])+m_roughMin[2];
-				pp[1+x][1+y][2] = (pp[1+x][1+y][2]*m_roughnessScaling[1])+m_roughMin[1];
-				pp[1+x][1+y][1] = (pp[1+x][1+y][1]*m_roughnessScaling[0])+m_roughMin[0];
-				pp[1+x][1+y][0] = (pp[1+x][1+y][0]*m_heightScaling);//+m_minh;
+	double pp[4][4][4];
+	for (int x=-1; x<3; x++) {
+		for (int y=-1; y<3; y++) { 
 
-				if (i<10000&&(pp[0][0][0] != 44.3750||pp[0][0][0] !=0)){
+			const int xx = Clamp(x+ix,0,(m_heightMapSizeX+3)-1)*4;
+			const int offset = Clamp(y+iy, 0, (m_heightMapSizeY+3)-1)*(m_heightMapSizeX+3)*4 +xx;
+			Uint16* hrmappoint = m_heightMapScaled+offset;
 
-				//printf("hmap (%i,%i) = %f,%i",iy+1,ix+1,p[0][0][0],*(Uint16*)hrmappoint);
-				i++;
-				};
-			}
+			pp[1+x][1+y][0] = hrmappoint[0];
+			pp[1+x][1+y][1] = hrmappoint[1];
+			pp[1+x][1+y][2] = hrmappoint[2];
+			pp[1+x][1+y][3] = hrmappoint[3];
+			static int i = 0;
+
+
+			// convert from map format
+			pp[1+x][1+y][3] = (pp[1+x][1+y][3]*m_roughnessScaling[2])+m_roughMin[2];
+			pp[1+x][1+y][2] = (pp[1+x][1+y][2]*m_roughnessScaling[1])+m_roughMin[1];
+			pp[1+x][1+y][1] = (pp[1+x][1+y][1]*m_roughnessScaling[0])+m_roughMin[0];
+			pp[1+x][1+y][0] = (pp[1+x][1+y][0]*m_heightScaling);//+m_minh;
+
 		}
-		
-	
-		//{
-			double c[4][4];
-			for (int jj = 0; jj < 4; jj++){	// to do - sse
-				for (int j=0; j<4; j++) {
-					double d0 = pp[0][j][jj] - pp[1][j][jj];
-					double d2 = pp[2][j][jj] - pp[1][j][jj];
-					double d3 = pp[3][j][jj] - pp[1][j][jj];
-					double a0 = pp[1][j][jj];
-					double a1 = -(1/3.0)*d0 + d2 - (1/6.0)*d3;
-					double a2 = 0.5*d0 + 0.5*d2;
-					double a3 = -(1/6.0)*d0 - 0.5*d2 + (1/6.0)*d3;
-					c[j][jj] = a0 + a1*dx + a2*dx*dx + a3*dx*dx*dx;
-				}
-			}
-
-			double v[4];
-			
-			for (int j=0; j<4; j++){
-				double d0 = c[0][j] - c[1][j];
-				double d2 = c[2][j] - c[1][j];
-				double d3 = c[3][j] - c[1][j];
-				double a0 = c[1][j];
-				double a1 = -(1/3.0)*d0 + d2 - (1/6.0)*d3;
-				double a2 = 0.5*d0 + 0.5*d2;
-				double a3 = -(1/6.0)*d0 - 0.5*d2 + (1/6.0)*d3;
-				v[j] = a0 + a1*dy + a2*dy*dy + a3*dy*dy*dy;
-			}
+	}
 
 
-			//v[0] has height, v[1] to v[3] has roughness
-			//terrain code here
-			v[0]/=m_planetRadius;
-			//double h = v[0]/(double(2^32)*m_heightScaling);//expression for height on a scale of 0 to 1
-			v[0] = (v[0]<0 ? 0 : v[0]);
-			return v[0];
-			//(v[0])/30.0;//log10(log10(v[0]+10)*200+10)*100000-1000;
 
-	
+	double c[4][4];
+	for (int jj = 0; jj < 4; jj++){	// to do - sse
+		for (int j=0; j<4; j++) {
+			double d0 = pp[0][j][jj] - pp[1][j][jj];
+			double d2 = pp[2][j][jj] - pp[1][j][jj];
+			double d3 = pp[3][j][jj] - pp[1][j][jj];
+			double a0 = pp[1][j][jj];
+			double a1 = -(1/3.0)*d0 + d2 - (1/6.0)*d3;
+			double a2 = 0.5*d0 + 0.5*d2;
+			double a3 = -(1/6.0)*d0 - 0.5*d2 + (1/6.0)*d3;
+			c[j][jj] = a0 + a1*dx + a2*dx*dx + a3*dx*dx*dx;
+		}
+	}
+
+	double v[4];
+
+	for (int j=0; j<4; j++){
+		double d0 = c[0][j] - c[1][j];
+		double d2 = c[2][j] - c[1][j];
+		double d3 = c[3][j] - c[1][j];
+		double a0 = c[1][j];
+		double a1 = -(1/3.0)*d0 + d2 - (1/6.0)*d3;
+		double a2 = 0.5*d0 + 0.5*d2;
+		double a3 = -(1/6.0)*d0 - 0.5*d2 + (1/6.0)*d3;
+		v[j] = a0 + a1*dy + a2*dy*dy + a3*dy*dy*dy;
+	}
+
+
+	//v[0] has height, v[1] to v[3] has roughness
+	//terrain code here
+	v[0]/=m_planetRadius;
+	//double h = v[0]/(double(2^32)*m_heightScaling);//expression for height on a scale of 0 to 1
+	v[0] = (v[0]<0 ? 0 : v[0]);
+	return v[0];
+	//(v[0])/30.0;//log10(log10(v[0]+10)*200+10)*100000-1000;
+
+
 }
