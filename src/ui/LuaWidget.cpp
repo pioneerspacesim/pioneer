7d194591f5a86e7999f126918856eee1d0597b74
Robert Norris <rob@eatenbyagrue.org>
2012-06-18 21:50:02 +1000

    expose Widget::SetFontSize to Lua

--- src/ui/LuaWidget.cpp
+++ src/ui/LuaWidget.cpp
@@ -1,10 +1,18 @@
 #include "Widget.h"
 #include "LuaObject.h"
+#include "LuaConstants.h"
 
 namespace UI {
 
 class LuaWidget {
 public:
+
+	static int l_set_font_size(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		UI::Widget::FontSize fontSize = static_cast<UI::Widget::FontSize>(LuaConstants::GetConstantFromArg(l, "UIFontSize", 2));
+		w->SetFontSize(fontSize);
+		return 0;
+	}
 
 };
 
@@ -17,8 +25,8 @@ template <> const char *LuaObject<UI::Widget>::s_type = "UI.Widget";
 template <> void LuaObject<UI::Widget>::RegisterClass()
 {
 	static const luaL_Reg l_methods[] = {
-
-        { 0, 0 }
+		{ "SetFontSize", LuaWidget::l_set_font_size },
+		{ 0, 0 }
 	};
 
 	LuaObjectBase::CreateClass(s_type, 0, l_methods, 0, 0);
