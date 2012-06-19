4a86ef25576d212b483401cd0850b094b6f8824c
Robert Norris <rob@eatenbyagrue.org>
2012-06-19 17:41:02 +1000

    reimplement LuaEngine as a typeless object

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -14,71 +14,55 @@
  *
  */
 
-static int l_engine_meta_index(lua_State *l)
+/*
+ * Attribute: rand
+ *
+ * The global <Rand> object. Its stream of values will be different across
+ * multiple Pioneer runs. Use this when you just need a random number and
+ * don't care about the seed.
+ *
+ * Availability:
+ *
+ *  alpha 10
+ *
+ * Status:
+ *
+ *  stable
+ */
+static int l_engine_attr_rand(lua_State *l)
 {
-	const char *key = luaL_checkstring(l, 2);
+	LuaRand::PushToLua(&Pi::rng);
+	return 1;
+}
 
-	/*
-	 * Attribute: rand
-	 *
-	 * The global <Rand> object. Its stream of values will be different across
-     * multiple Pioneer runs. Use this when you just need a random number and
-     * don't care about the seed.
-	 *
-	 * Availability:
-	 *
-	 *  alpha 10
-	 *
-	 * Status:
-	 *
-	 *  stable
-	 */
-	if (strcmp(key, "rand") == 0) {
-		LuaRand::PushToLua(&Pi::rng);
-		return 1;
-	}
-
-	/*
-	 * Attribute: userdir
-	 *
-	 * The Pioneer configuration directory (should be writable).
-	 *
-	 * Availability:
-	 *
-	 *   alpha 14
-	 *
-	 * Status:
-	 *
-	 *   stable
-	 */
-	if (strcmp(key, "userdir") == 0) {
-		const std::string &userdir = FileSystem::GetUserDir();
-		lua_pushlstring(l, userdir.c_str(), userdir.size());
-		return 1;
-	}
-
-	lua_pushnil(l);
+/*
+ * Attribute: userdir
+ *
+ * The Pioneer configuration directory (should be writable).
+ *
+ * Availability:
+ *
+ *   alpha 14
+ *
+ * Status:
+ *
+ *   stable
+ */
+static int l_engine_attr_userdir(lua_State *l)
+{
+	const std::string &userdir = FileSystem::GetUserDir();
+	lua_pushlstring(l, userdir.c_str(), userdir.size());
 	return 1;
 }
 
 void LuaEngine::Register()
 {
-	lua_State *l = Pi::luaManager->GetLuaState();
+	static const luaL_Reg l_attrs[] = {
+		{ "rand",    l_engine_attr_rand    },
+		{ "userdir", l_engine_attr_userdir },
+		{ 0, 0 }
+	};
 
-	LUA_DEBUG_START(l);
-
-	lua_newtable(l);
-    pi_lua_table_ro(l);
-
-	luaL_newmetatable(l, "Engine");
-	
-	lua_pushstring(l, "__index");
-	lua_pushcfunction(l, l_engine_meta_index);
-	lua_rawset(l, -3);
-
-	lua_setmetatable(l, -2);
-
-	lua_setglobal(l, "Engine");
-	
-	LUA_DEBUG_END(l, 0);
+	LuaObjectBase::CreateObject(0, l_attrs, 0);
+	lua_setglobal(Pi::luaManager->GetLuaState(), "Engine");
 }
