71eea2389ebe448b01cd0d7eb4b504419090204d
Robert Norris <rob@eatenbyagrue.org>
2012-06-27 16:48:13 +1000

    stubs for signal attributes

--- src/ui/LuaWidget.cpp
+++ src/ui/LuaWidget.cpp
@@ -15,6 +15,56 @@ public:
 		return 1;
 	}
 
+	static int l_attr_on_key_down(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_key_up(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_key_press(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_down(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_up(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_move(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_wheel(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_over(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_mouse_out(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
+	static int l_attr_on_click(lua_State *l) {
+		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
+		return 0;
+	}
+
 };
 
 }
@@ -30,5 +80,19 @@ template <> void LuaObject<UI::Widget>::RegisterClass()
 		{ 0, 0 }
 	};
 
-	LuaObjectBase::CreateClass(s_type, 0, l_methods, 0, 0);
+	static const luaL_Reg l_attrs[] = {
+		{ "onKeyDown",    LuaWidget::l_attr_on_key_down    },
+		{ "onKeyUp",      LuaWidget::l_attr_on_key_up      },
+		{ "onKeyPress",   LuaWidget::l_attr_on_key_press   },
+		{ "onMouseDown",  LuaWidget::l_attr_on_mouse_down  },
+		{ "onMouseUp",    LuaWidget::l_attr_on_mouse_up    },
+		{ "onMouseMove",  LuaWidget::l_attr_on_mouse_move  },
+		{ "onMouseWheel", LuaWidget::l_attr_on_mouse_wheel },
+		{ "onMouseOver",  LuaWidget::l_attr_on_mouse_over  },
+		{ "onMouseOut",   LuaWidget::l_attr_on_mouse_out   },
+		{ "onClick",      LuaWidget::l_attr_on_click       },
+		{ 0, 0 }
+	};
+
+	LuaObjectBase::CreateClass(s_type, 0, l_methods, l_attrs, 0);
 }
