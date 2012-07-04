9414f97785dd51c8031293088d06db6d42ef7d89
Brian Ronald <brian@ppcis.org>
2012-07-05 00:40:50 +0100

    FuelClub.lua versioning structure

--- data/modules/FuelClub/FuelClub.lua
+++ data/modules/FuelClub/FuelClub.lua
@@ -1,3 +1,5 @@
+local VERSION = 1 -- Integer versioning; bump this up if the saved game format changes.
+
 ---------------
 -- Fuel Club --
 ---------------
@@ -205,11 +207,33 @@ local onGameStart = function ()
 end
 
 local serialize = function ()
-	return { ads = ads, memberships = memberships }
+	return {
+		VERSION = VERSION,
+		ads = ads,
+		memberships = memberships,
+	}
 end
 
 local unserialize = function (data)
 	loaded_data = data
+	if data.VERSION then
+		if data.VERSION < VERSION then
+			print('Old FuelClub data loaded, converting...')
+			-- No upgrade code yet
+			print(('FuelClub data converted to internal version {newversion}'):interp({newversion=VERSION}))
+			return
+		end
+		if data.VERSION > VERSION then
+			error(([[FuelClub load error - saved game is more recent than installed files
+			Saved game internal version: {saveversion}
+			Installed internal version: {ourversion}]]):interp({saveversion=data.VERSION,ourversion=VERSION}))
+		end
+	else
+		-- Hopefully, a few engine save-game bumps from now,
+		-- there will be no instance where this is acceptable,
+		-- and we can error() out of here.
+		print('Pre-versioning FuelClub data loaded')
+	end
 end
 
 EventQueue.onCreateBB:Connect(onCreateBB)
