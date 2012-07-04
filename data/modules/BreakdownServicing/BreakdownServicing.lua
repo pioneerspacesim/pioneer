f81168f3e36e3753103988b6b816930b3694a77e
Brian Ronald <brian@ppcis.org>
2012-07-05 00:40:12 +0100

    BreakdownServicing.lua versioning structure

--- data/modules/BreakdownServicing/BreakdownServicing.lua
+++ data/modules/BreakdownServicing/BreakdownServicing.lua
@@ -1,3 +1,5 @@
+local VERSION = 1 -- Integer versioning; bump this up if the saved game format changes.
+
 -- Get the translator function
 local t = Translate:GetTranslator()
 
@@ -198,11 +200,33 @@ local onEnterSystem = function (ship)
 end
 
 local serialize = function ()
-	return { ads = ads, service_history = service_history }
+	return {
+		VERSION = VERSION,
+		ads = ads,
+		service_history = service_history,
+	}
 end
 
 local unserialize = function (data)
 	loaded_data = data
+	if data.VERSION then
+		if data.VERSION < VERSION then
+			print('Old BreakdownServicing data loaded, converting...')
+			-- No upgrade code yet
+			print(('BreakdownServicing data converted to internal version {newversion}'):interp({newversion=VERSION}))
+			return
+		end
+		if data.VERSION > VERSION then
+			error(([[BreakdownServicing load error - saved game is more recent than installed files
+			Saved game internal version: {saveversion}
+			Installed internal version: {ourversion}]]):interp({saveversion=data.VERSION,ourversion=VERSION}))
+		end
+	else
+		-- Hopefully, a few engine save-game bumps from now,
+		-- there will be no instance where this is acceptable,
+		-- and we can error() out of here.
+		print('Pre-versioning BreakdownServicing data loaded')
+	end
 end
 
 EventQueue.onCreateBB:Connect(onCreateBB)
