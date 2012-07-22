999a26a1a6018276cb4109b98ce70d966ac4ab2e
kko <pioneer@paahdin.com>
2012-07-21 20:09:11 -0400

    Add error/warning prints to OSPosix.cpp

--- src/OSPosix.cpp
+++ src/OSPosix.cpp
@@ -4,9 +4,29 @@
 
 namespace OS {
 
+void Error(const char *format, ...)
+{
+	char buf[1024];
+	va_list ap;
+	va_start(ap, format);
+	vsnprintf(buf, sizeof(buf), format, ap);
+	va_end(ap);
+	fprintf(stderr, "Error: %s\n", buf);
+	abort();
+}
+
+void Warning(const char *format, ...)
+{
+	char buf[1024];
+	va_list ap;
+	va_start(ap, format);
+	vsnprintf(buf, sizeof(buf), format, ap);
+	va_end(ap);
+	fprintf(stderr, "Warning: %s\n", buf);
+}
+
 void LoadWindowIcon()
 {
-	// SDL doc says "Win32 icons must be 32x32".
 	SDLSurfacePtr surface = LoadSurfaceFromFile("icons/badge.png");
 	if (surface) {
 		SDL_WM_SetIcon(surface.Get(), 0);
