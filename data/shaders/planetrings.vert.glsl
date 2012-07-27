07a69419653d114423a922bae19015da304cb78d
John Bartholomew <jpab@cs.bris.ac.uk>
2012-07-27 04:08:52 +0100

    update the planet rings shader to use the ring texture

--- data/shaders/planetrings.vert.glsl
+++ data/shaders/planetrings.vert.glsl
@@ -5,6 +5,7 @@ void main(void)
 #else
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 #endif
+	gl_TexCoord[0] = gl_MultiTexCoord0;
 	gl_TexCoord[1] = gl_Vertex;
 	gl_FrontColor = gl_Color;
 }
