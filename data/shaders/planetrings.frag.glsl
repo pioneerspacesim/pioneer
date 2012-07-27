d34b1263ec6f7e18a4783672a759e00d34676b8c
John Bartholomew <jpa.bartholomew@gmail.com>
2012-07-27 14:58:07 +0100

    match up planetrings.frag.glsl texture sample name with the C++ code

--- data/shaders/planetrings.frag.glsl
+++ data/shaders/planetrings.frag.glsl
@@ -1,10 +1,10 @@
-uniform sampler2D tex;
+uniform sampler2D texture0;
 
 void main(void)
 {
 	// Bits of ring in shadow!
 	vec4 col = vec4(0.0);
-	vec4 texCol = texture2D(tex, gl_TexCoord[0].st);
+	vec4 texCol = texture2D(texture0, gl_TexCoord[0].st);
 
 	for (int i=0; i<NUM_LIGHTS; ++i) {
 		float l = findSphereEyeRayEntryDistance(-vec3(gl_TexCoord[1]), vec3(gl_ModelViewMatrixInverse * gl_LightSource[i].position), 1.0);
