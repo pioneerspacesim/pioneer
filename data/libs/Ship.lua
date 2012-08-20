8fb67fddb66d5fc5e1f294e2d2381787dc1c892b
Simon Chopin <chopin.simon@gmail.com>
2012-08-20 02:06:31 +0200

    Ship.lua: Add a Ship:Refuel method
    This method takes an amount of water to transverse from the cargo to the
    fuel tank, and does all the necessary calculation. I'm not sure about
    all the double equality stuff though.

--- data/libs/Ship.lua
+++ data/libs/Ship.lua
@@ -9,6 +9,42 @@
 --
 
 --
+-- Method: Refuel
+--
+-- Use the content of the cargo to refuel
+--
+-- > used_units = ship:Refuel(1)
+--
+-- Parameters:
+--
+--   amount - the amount of fuel (in tons) to take from the cargo
+--
+-- Result:
+--
+--   used_units - how much fuel units have been used to fuel the tank.
+--
+-- Availability:
+--
+--   alpha 26
+--
+-- Status:
+--
+--   experimental
+--
+function Ship:Refuel(amount)
+    local currentFuel = self.fuel
+    if currentFuel == 100 then
+        return 0
+    end
+    local ship_stats = self:GetStats()
+    local needed = math.max(math.ceil(ship_stats.maxFuelTankMass - ship_stats.fuelMassLeft), amount)
+    local removed = self:RemoveEquip('WATER', needed)
+    self:SetFuelPercent(self.fuel + removed * 100 / ship_stats.fuelMassLeft)
+    return removed
+end
+
+
+--
 -- Method: Jettison
 --
 -- Jettison one unit of the given cargo type
