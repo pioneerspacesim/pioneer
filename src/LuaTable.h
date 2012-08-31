d8b62964828e1f06098bfaa6bc7b40649db37750
Simon Chopin <chopin.simon@gmail.com>
2012-08-31 18:57:41 +0200

    LuaTable: Clean up constructors and remove dangerous setters

--- src/LuaTable.h
+++ src/LuaTable.h
@@ -12,9 +12,13 @@ class UninitalizedLuaTable: std::exception {};
 class LuaTable {
 public:
 	// For now, every lua_State * can only be NULL or Pi::LuaManager->GetLuaState();
-	LuaTable(): m_lua(0), m_index(0) {}
 	LuaTable(const LuaTable & ref): m_lua(ref.m_lua), m_index(ref.m_index) {} // Copy constructor.
-	explicit LuaTable(lua_State * l, int index): m_lua(l), m_index(lua_absindex(l, index)) {}; // Use an existing table on the stack. Index 0 will create a new table.
+	LuaTable(lua_State * l, int index): m_lua(l), m_index(lua_absindex(l, index)) {assert(lua_istable(m_lua, m_index));}
+	explicit LuaTable(lua_State * l): m_lua(l) {
+		lua_newtable(m_lua);
+		m_index = lua_gettop(l); 
+	}
+
 	virtual ~LuaTable() {};
 
 	const LuaTable & operator=(const LuaTable & ref) { m_lua = ref.m_lua; m_index = ref.m_index; return *this;}
@@ -27,14 +31,8 @@ public:
 	template <class Value, class Key> Value Get(const Key & key) const;
 	template <class Value, class Key> void Set(const Key & key, const Value & value) const;
 
-	// Push a copy of the current table on the top of the stack.
-	// The caller is expected to clean it up itself.
-
 	lua_State * GetLua() const { return m_lua; }
 	int GetIndex() const { return m_index; }
-
-	void SetLua(lua_State * l) { m_lua = l; }
-	void SetIndex(int new_index) { m_index = lua_absindex(m_lua, new_index); }
 
 protected:
 	lua_State * m_lua;
