3da9d3835e6472e0e1ca8874357dc5374cad5267
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2012-09-02 22:58:56 +0100

    Give each faction a spurious founding year.

--- data/factions/00_default.lua
+++ data/factions/00_default.lua
@@ -1,6 +1,7 @@
 local f = Faction:new('Independent')
 	:description_short('Worlds without a group allegience')
 	:description('Free and independent worlds, self governing and either self reliant or striving for it.')
+	:foundingDate(2900.0)
 	:military_name('Independent Space Fleet')
 	:police_name('Police')
 
