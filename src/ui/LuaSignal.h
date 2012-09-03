a42d8c30517c8aac4bf1a32759ef08da5bc4ab79
Robert Norris <rob@eatenbyagrue.org>
2012-09-04 08:18:04 +1000

    lua signal wrappers for non-accumulated signals

--- src/ui/LuaSignal.h
+++ src/ui/LuaSignal.h
@@ -30,6 +30,10 @@ public:
 		arg1.ToLuaTable(l);
 		return _trampoline_end(l, 2);
 	}
+
+    static inline void trampoline_void(T0 arg0, T1 arg1, lua_State *l, int idx) {
+        trampoline(arg0, arg1, l, idx);
+    }
 };
 
 template <typename T0>
@@ -40,6 +44,10 @@ public:
 		arg0.ToLuaTable(l);
 		return _trampoline_end(l, 1);
 	}
+
+    static inline void trampoline_void(T0 arg0, lua_State *l, int idx) {
+        trampoline(arg0, l, idx);
+    }
 };
 
 template <>
@@ -49,15 +57,30 @@ public:
 		_trampoline_start(l, idx);
 		return _trampoline_end(l, 0);
 	}
+
+    static inline void trampoline_void(lua_State *l, int idx) {
+        trampoline(l, idx);
+    }
 };
 
-template <typename T0 = sigc::nil, typename T1 = sigc::nil>
-class LuaSignal {
+template <>
+class LuaSignalTrampoline<unsigned int,const std::string &> : public LuaSignalTrampolineBase {
 public:
+	static inline bool trampoline(unsigned int arg0, const std::string &arg1, lua_State *l, int idx) {
+		_trampoline_start(l, idx);
+		lua_pushinteger(l, arg0);
+		lua_pushlstring(l, arg1.c_str(), arg1.size());
+		return _trampoline_end(l, 2);
+	}
 
-	typedef typename sigc::signal<bool,T0,T1>::template accumulated<UI::Widget::EventHandlerResultAccumulator> signal_type;
+	static inline void trampoline_void(unsigned int arg0, const std::string &arg1, lua_State *l, int idx) {
+        trampoline(arg0, arg1, l, idx);
+    }
+};
 
-	static inline int l_connect(lua_State *l) {
+class LuaSignalBase {
+protected:
+	static inline void *connect_base(lua_State *l, int &idx) {
 		luaL_checktype(l, 1, LUA_TTABLE);
 		luaL_checktype(l, 2, LUA_TFUNCTION);
 
@@ -67,7 +90,7 @@ public:
 			luaL_error(l, "signal pointer not found");
 			return 0;
 		}
-		signal_type *signal = static_cast<signal_type*>(lua_touserdata(l, -1));
+		void *signal = lua_touserdata(l, -1);
 		lua_pop(l, 1);
 
 		lua_getfield(l, LUA_REGISTRYINDEX, "PiUISignal");
@@ -77,12 +100,50 @@ public:
 			lua_setfield(l, LUA_REGISTRYINDEX, "PiUISignal");
 		}
 
-		int idx = lua_rawlen(l, -1)+1;
+		idx = lua_rawlen(l, -1)+1;
 		lua_pushvalue(l, 2);
 		lua_rawseti(l, -2, idx);
 
-		signal->connect(sigc::bind(sigc::ptr_fun(&LuaSignalTrampoline<T0,T1>::trampoline), l, idx));
+		return signal;
+	}
+};
 
+template <typename T0 = sigc::nil, typename T1 = sigc::nil>
+class LuaSignal : public LuaSignalBase {
+public:
+
+	typedef typename sigc::signal<void,T0,T1> signal_type;
+
+	static inline int l_connect(lua_State *l) {
+		int idx;
+		if (signal_type *signal = static_cast<signal_type *>(connect_base(l, idx)))
+			signal->connect(sigc::bind(sigc::ptr_fun(&LuaSignalTrampoline<T0,T1>::trampoline_void), l, idx));
+		return 0;
+	}
+
+	inline void Wrap(lua_State *l, signal_type &signal) {
+		lua_newtable(l);
+
+		lua_pushstring(l, "Connect");
+		lua_pushcfunction(l, l_connect);
+		lua_rawset(l, -3);
+
+		lua_pushstring(l, "__signal");
+		lua_pushlightuserdata(l, &signal);
+		lua_rawset(l, -3);
+	}
+};
+
+template <typename T0 = sigc::nil, typename T1 = sigc::nil>
+class LuaSignalAccumulated : public LuaSignalBase {
+public:
+
+	typedef typename sigc::signal<bool,T0,T1>::template accumulated<UI::Widget::EventHandlerResultAccumulator> signal_type;
+
+	static inline int l_connect(lua_State *l) {
+		int idx;
+		if (signal_type *signal = static_cast<signal_type *>(connect_base(l, idx)))
+			signal->connect(sigc::bind(sigc::ptr_fun(&LuaSignalTrampoline<T0,T1>::trampoline), l, idx));
 		return 0;
 	}
 
--- src/ui/LuaWidget.cpp
+++ src/ui/LuaWidget.cpp
@@ -19,61 +19,61 @@ public:
 
 	static int l_attr_on_key_down(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const KeyboardEvent &>().Wrap(l, w->onKeyDown);
+		LuaSignalAccumulated<const KeyboardEvent &>().Wrap(l, w->onKeyDown);
 		return 1;
 	}
 
 	static int l_attr_on_key_up(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const KeyboardEvent &>().Wrap(l, w->onKeyUp);
+		LuaSignalAccumulated<const KeyboardEvent &>().Wrap(l, w->onKeyUp);
 		return 1;
 	}
 
 	static int l_attr_on_key_press(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const KeyboardEvent &>().Wrap(l, w->onKeyPress);
+		LuaSignalAccumulated<const KeyboardEvent &>().Wrap(l, w->onKeyPress);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_down(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const MouseButtonEvent &>().Wrap(l, w->onMouseDown);
+		LuaSignalAccumulated<const MouseButtonEvent &>().Wrap(l, w->onMouseDown);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_up(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const MouseButtonEvent &>().Wrap(l, w->onMouseUp);
+		LuaSignalAccumulated<const MouseButtonEvent &>().Wrap(l, w->onMouseUp);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_move(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const MouseMotionEvent &>().Wrap(l, w->onMouseMove);
+		LuaSignalAccumulated<const MouseMotionEvent &>().Wrap(l, w->onMouseMove);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_wheel(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<const MouseWheelEvent &>().Wrap(l, w->onMouseWheel);
+		LuaSignalAccumulated<const MouseWheelEvent &>().Wrap(l, w->onMouseWheel);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_over(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<>().Wrap(l, w->onMouseOver);
+		LuaSignalAccumulated<>().Wrap(l, w->onMouseOver);
 		return 1;
 	}
 
 	static int l_attr_on_mouse_out(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<>().Wrap(l, w->onMouseOut);
+		LuaSignalAccumulated<>().Wrap(l, w->onMouseOut);
 		return 1;
 	}
 
 	static int l_attr_on_click(lua_State *l) {
 		UI::Widget *w = LuaObject<UI::Widget>::CheckFromLua(1);
-		LuaSignal<>().Wrap(l, w->onClick);
+		LuaSignalAccumulated<>().Wrap(l, w->onClick);
 		return 1;
 	}
 
