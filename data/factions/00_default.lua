ba044b3e7b28589187eb066b625e951071cefddc
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2012-09-03 11:13:06 +0100

    Update factions definitions with differing expansion rates. Change RED empires start date to balance it's expansion rate relative to the players game start time. Fix the current date calculation when getting the nearest faction. Early out of nearest faction test if the homeworld matches current sector.

--- data/factions/00_default.lua
+++ data/factions/00_default.lua
@@ -2,6 +2,7 @@ local f = Faction:new('Independent')
 	:description_short('Worlds without a group allegience')
 	:description('Free and independent worlds, self governing and either self reliant or striving for it.')
 	:foundingDate(2900.0)
+	:expansionRate(2.0)
 	:military_name('Independent Space Fleet')
 	:police_name('Police')
 
