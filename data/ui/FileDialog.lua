646304bb44cfd304b3217078eff887c3b0eddcc9
Robert Norris <rob@eatenbyagrue.org>
2012-09-04 08:47:09 +1000

    mostly finished file dialog layout

--- data/ui/FileDialog.lua
+++ data/ui/FileDialog.lua
@@ -5,23 +5,25 @@ ui.templates.FileDialog = function (args)
 	local root = args.root or "USER"
 	local path = args.path or ""
 
-	print(string.format("FileDialog: title %s root %s path %s", title, root, path))
-
 	local files, _ = FileSystem.ReadDirectory(root, path)
 
 	local list = ui:List()
-
 	for i = 1,#files do list:AddOption(files[i]) end
+
+	local loadButton = ui:Button():SetInnerWidget(ui:Label("Load game"))
+	local cancelButton = ui:Button():SetInnerWidget(ui:Label("Cancel"))
 
 	local dialog =
 		ui:Grid(3, 3)
 			:SetCell(1,1,
-				ui:Background():SetInnerWidget(
-					ui:VBox():PackEnd({
-						ui:Label(title),
-						list
-					})
-				)
+				ui:VBox():PackEnd({
+                    ui:Background():SetInnerWidget(ui:Label(title)),
+					list,
+					ui:HBox():PackEnd({
+						ui:Align("LEFT"):SetInnerWidget(loadButton),
+						ui:Align("RIGHT"):SetInnerWidget(cancelButton),
+					}, { "EXPAND", "FILL" } ),
+				})
 			)
 
     return dialog
