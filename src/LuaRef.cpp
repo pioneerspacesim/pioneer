7584b50a642d6b572f9ae343db3a30bcacd12d56
Simon Chopin <chopin.simon@gmail.com>
2012-09-06 01:05:44 +0200

    LuaRef: Use luaL_ref instead of homegrown id counter

--- src/LuaRef.cpp
+++ src/LuaRef.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 
 lua_State * LuaRef::g_lua = 0;
-static int id_count = 0;
 
 void LuaRef::Init(lua_State * l) {
 	assert(g_lua == 0); // Only one Lua stack is supported for now.
@@ -13,7 +12,6 @@ void LuaRef::Uninit(lua_State * l) {
 	assert(g_lua == l);
 	g_lua = 0;
 }
-
 
 LuaRef::LuaRef(const LuaRef & ref): m_lua(ref.m_lua), m_id(ref.m_id), m_copycount(ref.m_copycount) {
 	if (m_lua && g_lua == m_lua && m_id)
@@ -56,22 +54,19 @@ bool LuaRef::operator==(const LuaRef & ref) const {
 void LuaRef::CheckCopyCount() {
 	if (*m_copycount <= 0) {
 		PushGlobalToStack();
-		lua_pushinteger(g_lua, m_id);
-		lua_pushnil(g_lua);
-		lua_settable(g_lua, -3);
-		lua_pop(g_lua, 1);
+		luaL_unref(m_lua, -1, m_id);
+		lua_pop(m_lua, 1);
 	}
 }
 
-LuaRef::LuaRef(lua_State * l, int index): m_lua(l), m_id(++id_count) {
+LuaRef::LuaRef(lua_State * l, int index): m_lua(l), m_id(0) {
 	assert(g_lua && m_lua == g_lua && index != 0);
 	index = lua_absindex(m_lua, index);
 
 	PushGlobalToStack();
-	lua_pushinteger(m_lua, m_id);
 	lua_pushvalue(m_lua, index);
-	lua_settable(m_lua, -3);
-	lua_pop(m_lua, 1);
+	m_id = luaL_ref(m_lua, -2);
+	lua_pop(l, 1); // Pop global.
 
 	m_copycount = new int(1);
 }
