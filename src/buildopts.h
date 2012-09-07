f7fd050d8bee638877713231167fa73e45526711
Robert Norris <rob@eatenbyagrue.org>
2012-09-07 21:54:00 +1000

    version bumps for 26 freeze

--- src/buildopts.h
+++ src/buildopts.h
@@ -3,7 +3,7 @@
 
 // game version. usually defined by configure
 #ifndef PIONEER_VERSION
-#define PIONEER_VERSION "alpha 26 dev"
+#define PIONEER_VERSION "alpha 26 freeze"
 #endif
 #ifndef PIONEER_EXTRAVERSION
 #define PIONEER_EXTRAVERSION ""
