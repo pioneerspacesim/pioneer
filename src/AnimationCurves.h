7d77d6312aba4e5261d2447b0cd6b4271a80be0d
John Bartholomew <jpa.bartholomew@gmail.com>
2012-09-09 22:34:43 +0100

    fix the implementation of AnimationCurves::Approach

--- src/AnimationCurves.h
+++ src/AnimationCurves.h
@@ -8,18 +8,18 @@ namespace AnimationCurves {
 	// Animates a value with "approach style" over time (zooming).
 	// The speeds must be positive. Function will not go further than target.
 	template<class T>
-	inline void Approach(T & cur, const T target, float frameTime, const T deltaFactor=10, T targetFactor=1) {
+	inline void Approach(T & cur, const T target, float frameTime, const T exponentialFactor=10, T linearFactor=1) {
 		//static_assert(static_cast<T>(-1) <0); // Assert type is signed
 		if (frameTime>1) frameTime = 1; // Clamp in case game hangs for a second
-		assert(deltaFactor>0 && targetFactor >=0);
+		assert(exponentialFactor >= 0 && linearFactor >= 0);
 		if (is_equal_exact(target, cur))
 			return;
 		const T delta(target - cur);
-		if (delta<0) targetFactor=-targetFactor;
-		cur += (delta*deltaFactor + target*targetFactor)*frameTime;
-		// Check for arrival
+		if (delta < 0) linearFactor = -linearFactor;
+		cur += (delta*exponentialFactor + linearFactor) * frameTime;
+		// clamp to target (independent of the direction of motion)
 		const T newDelta(target - cur);
-		if (newDelta*delta <0) cur = target;
+		if (newDelta*delta < 0) cur = target;
 	}
 
 }
