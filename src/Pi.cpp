edd07bcdbcd7d3385adb7a1d347fc024c7baf52c
John Bartholomew <jpa.bartholomew@gmail.com>
2012-09-09 16:53:50 +0100

    change Earth start to Moscow

--- src/Pi.cpp
+++ src/Pi.cpp
@@ -826,7 +826,7 @@ void Pi::HandleMenuKey(int n)
 
 		case 0: // Earth start point
 		{
-			game = new Game(SystemPath(0,0,0,0,9));  // Los Angeles, Earth
+			game = new Game(SystemPath(0,0,0,0,7));  // Moscow, Earth
 			break;
 		}
 
