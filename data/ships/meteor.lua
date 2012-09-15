6cced77deed29a1aaa580993646c85ad0e4c6516
Robert Norris <rob@eatenbyagrue.org>
2012-09-15 22:38:07 +1000

    add header to ship defs

--- data/ships/meteor.lua
+++ data/ships/meteor.lua
@@ -1,3 +1,6 @@
+-- Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of CC-BY-SA 3.0. See COPYING.txt for details
+
 define_ship {
 	name='Meteor',
 	model='meteor',
