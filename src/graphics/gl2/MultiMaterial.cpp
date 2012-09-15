253ac2f1b1eddd94faa8a96223cc73dd6505b6f5
Robert Norris <rob@eatenbyagrue.org>
2012-09-15 22:45:56 +1000

    add headers to a few more files I missed

--- src/graphics/gl2/MultiMaterial.cpp
+++ src/graphics/gl2/MultiMaterial.cpp
@@ -1,3 +1,6 @@
+// Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See COPYING.txt for details
+
 #include "MultiMaterial.h"
 #include "graphics/Material.h"
 #include "graphics/TextureGL.h"
--- src/graphics/gl2/Uniform.h
+++ src/graphics/gl2/Uniform.h
@@ -1,3 +1,6 @@
+// Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See COPYING.txt for details
+
 #ifndef _GL2_UNIFORM_H
 #define _GL2_UNIFORM_H
 /*
