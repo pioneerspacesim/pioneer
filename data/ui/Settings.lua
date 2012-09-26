cada568f3e1605680893623ce16c37dd9ad01e30
John Bartholomew <jpab@cs.bris.ac.uk>
2012-09-26 22:27:38 +0100

    add a stub Settings page

---
+++ data/ui/Settings.lua
@@ -0,0 +1,17 @@
+local ui = Engine.ui
+local l = Lang.GetDictionary()
+
+local return_to_menu = ui:Button():SetInnerWidget(ui:Label(l.RETURN_TO_MENU))
+return_to_menu.onClick:Connect(function () ui:SetInnerWidget(ui.templates.MainMenu()) end)
+
+local settings =
+	ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
+		ui:VBox():PackEnd({
+			ui:Label(
+				"Sorry, the settings page is undergoing rennovation work at the moment.\n" ..
+				"Please edit settings in ~/.pioneer/config.ini"),
+			return_to_menu
+		})
+	))
+
+ui.templates.Settings = function (args) return settings end
