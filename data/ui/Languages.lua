0291cb50075dd56b1ef16b080d557dc724def0aa
Robert Norris <rob@eatenbyagrue.org>
2012-10-10 07:18:53 +1100

    ui translations

---
+++ data/ui/Languages.lua
@@ -0,0 +1,22 @@
+-- Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+Translate:Add({
+	English = {
+		-- Main menu
+		['Start at Earth']    = 'Start at Earth',
+		['Start at New Hope'] = 'Start at New Hope',
+		['Start at Lave']     = 'Start at Lave',
+		['Load game']         = 'Load game',
+		['Options']           = 'Options',
+		['Quit']              = 'Quit',
+
+		-- Generic file dialog
+		['Select file...'] = 'Select file...',
+		['Select']         = 'Select',
+		['Cancel']         = 'Cancel',
+
+		-- Load dialog
+		['Select game to load...'] = 'Select game to load...',
+	}
+})
