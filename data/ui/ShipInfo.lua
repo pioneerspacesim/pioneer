8a1812af7e639f092263764751ca582e6d623280
Robert Norris <rob@eatenbyagrue.org>
2012-10-15 08:20:07 +1100

    getting the spinner area working nicely

--- data/ui/ShipInfo.lua
+++ data/ui/ShipInfo.lua
@@ -1,10 +1,19 @@
 local ui = Engine.ui
 
 ui.templates.ShipInfo = function (args)
+	local shipId = Game.player.shipType
+	local shipType = ShipType.GetShipType(shipId)
+
 	return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
 		ui:Grid(2,1)
+			:SetColumn(0, {
+				ui:VBox(10):PackEnd({
+				})
+			})
 			:SetColumn(1, {
-				ui:VBox():PackEnd(ui:Align("TOP"):SetInnerWidget(UI.Game.ShipSpinner.New(ui, Game.player.shipType)))
+				ui:VBox(10)
+					:PackEnd(ui:Label(shipType.name):SetFont("HEADING_LARGE"))
+					:PackEnd(UI.Game.ShipSpinner.New(ui, Game.player.shipType))
 			})
 	))
 end
