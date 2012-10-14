6484a49e5069f8352b94e5cf0d39604e282a06e5
Robert Norris <rob@eatenbyagrue.org>
2012-10-14 22:36:38 +1100

    start of the layout

--- data/ui/ShipInfo.lua
+++ data/ui/ShipInfo.lua
@@ -1,7 +1,10 @@
 local ui = Engine.ui
 
 ui.templates.ShipInfo = function (args)
-    return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
-        UI.Game.ShipSpinner.New(ui, Game.player.shipType)
+	return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
+		ui:Grid(2,1)
+			:SetColumn(1, {
+				ui:VBox():PackEnd(ui:Align("TOP"):SetInnerWidget(UI.Game.ShipSpinner.New(ui, Game.player.shipType)))
+			})
 	))
 end
