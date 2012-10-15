50ce3c5b45fbf81c9a54bad037256b2981676f14
Robert Norris <rob@eatenbyagrue.org>
2012-10-15 14:50:15 +1100

    regular set of ship stats

--- data/ui/ShipInfo.lua
+++ data/ui/ShipInfo.lua
@@ -4,6 +4,12 @@ ui.templates.ShipInfo = function (args)
 	local shipId = Game.player.shipType
 	local shipType = ShipType.GetShipType(shipId)
 
+	local hyperdrive = Game.player:GetEquip("ENGINE",1)
+	local frontWeapon = Game.player:GetEquip("LASER",1)
+	local rearWeapon = Game.player:GetEquip("LASER",2)
+
+	local stats = Game.player:GetStats()
+
 	return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
 		ui:Grid(2,1)
 			:SetColumn(0, {
@@ -22,6 +28,25 @@ ui.templates.ShipInfo = function (args)
 								ui:Margin(10),
 								ui:Label("Front weapon:"),
 								ui:Label("Rear weapon:"),
+							})
+						})
+						:SetColumn(1, {
+							ui:VBox():PackEnd({
+								ui:Label(EquipType.GetEquipType(hyperdrive).name),
+								ui:Label(string.interp(
+									"{range} light years ({maxRange} max)", {
+										range    = string.format("%.1f",stats.hyperspaceRange),
+										maxRange = string.format("%.1f",stats.maxHyperspaceRange)
+									}
+								)),
+								ui:Margin(10),
+								ui:Label(string.format("%dt", stats.maxCapacity)),
+								ui:Label(string.format("%dt", stats.freeCapacity)),
+								ui:Label(string.format("%dt", stats.usedCapacity)),
+								ui:Label(string.format("%dt", stats.totalMass)),
+								ui:Margin(10),
+								ui:Label(EquipType.GetEquipType(frontWeapon).name),
+								ui:Label(EquipType.GetEquipType(rearWeapon).name),
 							})
 						})
 				})
