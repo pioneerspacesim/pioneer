475e0aec62b6a8b4daef1d23c3c24cd500ae33c8
John Bartholomew <jpa.bartholomew@gmail.com>
2012-10-16 05:29:55 +0100

    normalise CellSpec proportions rather than clamping them

--- src/ui/CellSpec.h
+++ src/ui/CellSpec.h
@@ -21,37 +21,37 @@ public:
 
 	inline CellSpec(float cp0, float cp1) : numCells(2) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2) : numCells(3) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2, float cp3) : numCells(4) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2; cellPercent[3] = cp3;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2, float cp3, float cp4) : numCells(5) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2; cellPercent[3] = cp3; cellPercent[4] = cp4;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2, float cp3, float cp4, float cp5) : numCells(6) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2; cellPercent[3] = cp3; cellPercent[4] = cp4; cellPercent[5] = cp5;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2, float cp3, float cp4, float cp5, float cp6) : numCells(7) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2; cellPercent[3] = cp3; cellPercent[4] = cp4; cellPercent[5] = cp5; cellPercent[6] = cp6;
-		ClampCells();
+		NormaliseCells();
 	}
 	inline CellSpec(float cp0, float cp1, float cp2, float cp3, float cp4, float cp5, float cp6, float cp7) : numCells(8) {
 		cellPercent[0] = cp0; cellPercent[1] = cp1; cellPercent[2] = cp2; cellPercent[3] = cp3; cellPercent[4] = cp4; cellPercent[5] = cp5; cellPercent[6] = cp6; cellPercent[7] = cp7;
-		ClampCells();
+		NormaliseCells();
 	}
 
 	inline CellSpec(const float cp[], int n) : numCells(n) {
 		assert(n >= 0 && n <= MAX_CELLS);
 		for (int i = 0; i < n; i++) cellPercent[i] = cp[i];
-		ClampCells();
+		NormaliseCells();
 	}
 
 	static CellSpec FromLuaTable(lua_State *l, int idx);
@@ -60,13 +60,10 @@ public:
 	float cellPercent[MAX_CELLS];
 
 private:
-	void ClampCells() {
+	void NormaliseCells() {
 		float total = 0.0f;
-		for (int i = 0; i < numCells; i++) {
-			if (total + cellPercent[i] > 1.0f)
-				cellPercent[i] = 1.0f-total;
-			total += cellPercent[i];
-		}
+		for (int i = 0; i < numCells; ++i) { total += cellPercent[i]; }
+		for (int i = 0; i < numCells; ++i) { cellPercent[i] /= total; }
 	}
 };
 
