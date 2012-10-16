b17e655415d7e6c9cb211bf1f7125baaaa707902
John Bartholomew <jpa.bartholomew@gmail.com>
2012-10-16 03:38:38 +0100

    fix style of include guards
    note: deliberately not using a leading underscore though, because you're
    not supposed to do that and I'd like to reduce instances of it in the code

--- src/LuaFaction.h
+++ src/LuaFaction.h
@@ -1,8 +1,8 @@
 // Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-#ifndef _LUAFaction_H
-#define _LUAFaction_H
+#ifndef LUAFACTION_H
+#define LUAFACTION_H
 
 #include "LuaObject.h"
 #include "Factions.h"
