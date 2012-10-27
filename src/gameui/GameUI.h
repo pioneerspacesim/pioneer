6af90f0d1bad118bb930e4a1f9faf814864b3d09
Robert Norris <rob@eatenbyagrue.org>
2012-10-27 22:07:00 +1100

    add copyright headers, fix include guards

--- src/gameui/GameUI.h
+++ src/gameui/GameUI.h
@@ -1,5 +1,8 @@
-#ifndef _GAMEUI_GAMEUI_H
-#define _GAMEUI_GAMEUI_H
+// Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#ifndef GAMEUI_GAMEUI_H
+#define GAMEUI_GAMEUI_H
 
 #include "ui/Context.h"
 
--- src/gameui/Lua.h
+++ src/gameui/Lua.h
@@ -1,8 +1,8 @@
 // Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-#ifndef _GAMEUI_LUA_H
-#define _GAMEUI_LUA_H
+#ifndef GAMEUI_LUA_H
+#define GAMEUI_LUA_H
 
 #include "LuaObject.h"
 #include "GameUI.h"
