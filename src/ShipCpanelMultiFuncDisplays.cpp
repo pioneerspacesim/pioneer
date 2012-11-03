ec9c6ce3a4a9208153fe5c496b70529c6ce7498d
Simon Chopin <chopin.simon@gmail.com>
2012-11-03 13:56:50 +0100

    Cpanel: Use the Lua functions to get the list of missiles.

--- src/ShipCpanelMultiFuncDisplays.cpp
+++ src/ShipCpanelMultiFuncDisplays.cpp
@@ -8,7 +8,9 @@
 #include "KeyBindings.h"
 #include "Lang.h"
 #include "libs.h"
+#include "LuaConstants.h"
 #include "LuaShip.h"
+#include "LuaTable.h"
 #include "Missile.h"
 #include "Pi.h"
 #include "Player.h"
@@ -556,13 +558,23 @@ void UseEquipWidget::FireMissile(int idx)
 void UseEquipWidget::UpdateEquip()
 {
 	DeleteAllChildren();
-	int numSlots = Pi::player->m_equipment.GetSlotSize(Equip::SLOT_MISSILE);
+	lua_State *l = Lua::manager->GetLuaState();
+	int pristine_stack = lua_gettop(l);
+	LuaShip::PushToLua(Pi::player);
+	lua_pushstring(l, "GetEquip");
+	lua_gettable(l, -2);
+	lua_pushvalue(l, -2);
+	lua_pushstring(l, "MISSILE");
+	lua_call(l, 2, 1);
+	std::vector<std::string> missiles = LuaTable(l, -1).GetVector<std::string>();
+	lua_settop(l, pristine_stack);
+	int numSlots = missiles.size();
 
 	if (numSlots) {
 		float spacing = 380.0f / numSlots;
 
 		for (int i = 0; i < numSlots; ++i) {
-			const Equip::Type t = Pi::player->m_equipment.Get(Equip::SLOT_MISSILE, i);
+			const Equip::Type t = static_cast<Equip::Type>(LuaConstants::GetConstant(l, "EquipType", missiles[i].c_str()));
 			if (t == Equip::NONE) continue;
 
 			Gui::ImageButton *b;
