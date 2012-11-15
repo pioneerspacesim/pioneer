6403cc4fd5d5fdbe2bd3564acca098f0722b3e19
Simon Chopin <chopin.simon@gmail.com>
2012-11-15 08:18:30 +0100

    utils.lua: use local variables within debug.deprecated

--- data/libs/00-utils.lua
+++ data/libs/00-utils.lua
@@ -3,8 +3,8 @@ math.clamp = function(v, min, max)
 end
 
 debug.deprecated = function()
-	deprecated_function = debug.getinfo(2)
-	caller = debug.getinfo(3)
+	local deprecated_function = debug.getinfo(2)
+	local caller = debug.getinfo(3)
 	print("The use of the function \""..deprecated_function.name.."\" as done at <"..caller.source..":"..caller.currentline.."> is deprecated")
 	print("Please check the changelogs and/or get in touch with the development team.")
 end
