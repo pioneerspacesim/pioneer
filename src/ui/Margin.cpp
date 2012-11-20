3eed82ea830aed7858f5bdade645467ec718fecf
Robert Norris <rob@eatenbyagrue.org>
2012-11-21 08:19:01 +1100

    size control for margin

--- src/ui/Margin.cpp
+++ src/ui/Margin.cpp
@@ -8,13 +8,13 @@ namespace UI {
 Point Margin::PreferredSize()
 {
 	if (!GetInnerWidget()) return Point(m_margin*2.0f);
-	return SizeAdd(GetInnerWidget()->PreferredSize(), Point(m_margin*2.0f));
+	return SizeAdd(CalcLayoutContribution(GetInnerWidget()), Point(m_margin*2.0f));
 }
 
 void Margin::Layout()
 {
 	if (!GetInnerWidget()) return;
-	SetWidgetDimensions(GetInnerWidget(), Point(m_margin), Point(std::max(GetSize().x-m_margin*2.0f,0.0f), std::max(GetSize().y-m_margin*2.0f,0.0f)));
+	SetWidgetDimensions(GetInnerWidget(), Point(m_margin), CalcSize(GetInnerWidget(), Point(std::max(GetSize().x-m_margin*2.0f,0.0f), std::max(GetSize().y-m_margin*2.0f,0.0f))));
 	GetInnerWidget()->Layout();
 }
 
