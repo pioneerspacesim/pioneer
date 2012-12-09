ce79a1f0e9bb900e4a76a968658ed20136b2c6aa
Robert Norris <rob@eatenbyagrue.org>
2012-12-10 08:15:45 +1100

    command line options

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,86 @@
 #include "libs.h"
 #include "Pi.h"
 #include "NewModelViewer.h"
+#include <cstdio>
+
+enum RunMode {
+	MODE_GAME,
+	MODE_MODELVIEWER,
+	MODE_VERSION,
+	MODE_USAGE,
+	MODE_USAGE_ERROR
+};
 
 int main(int argc, char** argv)
 {
-	if (argc <= 1) {
-		Pi::Init();
-		for (;;) Pi::Start();
-	} else {
-		ModelViewer::Run(argc, argv);
+	RunMode mode = MODE_GAME;
+
+	if (argc > 1) {
+		const char switchchar = argv[1][0];
+		if (!(switchchar == '-' || switchchar == '/')) {
+			mode = MODE_USAGE_ERROR;
+			goto start;
+		}
+
+		const std::string modeopt(std::string(argv[1]).substr(1));
+
+		if (modeopt == "game" || modeopt == "g") {
+			mode = MODE_GAME;
+			goto start;
+		}
+
+		if (modeopt == "modelviewer" || modeopt == "mv") {
+			mode = MODE_MODELVIEWER;
+			goto start;
+		}
+
+		if (modeopt == "version" || modeopt == "v") {
+			mode = MODE_VERSION;
+			goto start;
+		}
+
+		if (modeopt == "help" || modeopt == "h" || modeopt == "?") {
+			mode = MODE_USAGE;
+			goto start;
+		}
+
+		mode = MODE_USAGE_ERROR;
 	}
+
+start:
+
+	switch (mode) {
+		case MODE_GAME:
+			Pi::Init();
+			for (;;) Pi::Start();
+			break;
+
+		case MODE_MODELVIEWER:
+			ModelViewer::Run(argc-1, &argv[1]);
+			break;
+
+		case MODE_VERSION: {
+			std::string version(PIONEER_VERSION);
+			if (strlen(PIONEER_EXTRAVERSION)) version += " (" PIONEER_EXTRAVERSION ")";
+			fprintf(stderr, "pioneer %s\n", version.c_str());
+			break;
+		}
+
+		case MODE_USAGE_ERROR:
+			fprintf(stderr, "pioneer: unknown mode %s\n", argv[1]);
+			// fall through
+
+		case MODE_USAGE:
+			fprintf(stderr,
+				"usage: pioneer [mode] [options...]\n"
+				"available modes:\n"
+				"    -game        [-g]     game (default)\n"
+				"    -modelviewer [-mv]    model viewer\n"
+				"    -version     [-v]     show version\n"
+				"    -help        [-h,-?]  this help\n"
+			);
+			break;
+	}
+
 	return 0;
 }
