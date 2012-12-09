a84b498e198c1915f36561df170e1994534f49fc
Robert Norris <rob@eatenbyagrue.org>
2012-12-10 07:49:07 +1100

    remove old segfault stuff entirely

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,10 @@
 
 #include "libs.h"
 #include "Pi.h"
-//#include <signal.h>
 #include "NewModelViewer.h"
-
-/*void sigsegv_handler(int signum)
-{
-	if (signum == SIGSEGV) {
-		printf("Segfault! All is lost! Abandon ship!\n");
-		SDL_Quit();
-		abort();
-	}
-}*/
 
 int main(int argc, char** argv)
 {
-//	signal(SIGSEGV, sigsegv_handler);
 	if (argc <= 1) {
 		Pi::Init();
 		for (;;) Pi::Start();
