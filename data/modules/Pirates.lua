539645cf43fff0873d7a1021392eff1877c8911b
Chronothan <conathan192@gmail.com>
2012-12-25 20:30:09 -0600

    Update data/modules/Pirates.lua

--- data/modules/Pirates.lua
+++ data/modules/Pirates.lua
@@ -1,6 +1,8 @@
 -- Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
+
+
 local onEnterSystem = function (player)
 	if not player:IsPlayer() then return end
 
@@ -40,4 +42,55 @@ local onEnterSystem = function (player)
 	end
 end
 
+
+
+
+local onFrameChanged = function (body)
+	if not body:IsPlayer() then return end
+
+	local shiptypes = ShipType.GetShipTypes('SHIP', function (t)
+		local mass = t.hullMass
+		return mass >= 50 and mass <= 150
+	end)
+	if #shiptypes == 0 then return end
+
+	local lawlessness = Game.system.lawlessness
+	local population = Game.system.population
+	local superType = body.frameBody.superType
+	local path = body.frameBody.path
+	local systembody = path:GetSystemBody ()
+	local planetPopulation = systembody.population
+
+	-- XXX number should be some combination of population, lawlessness,
+	-- proximity to shipping lanes, etc
+	
+	local modifiers = (lawlessness + (population / 100) - (planetPopulation / 100))
+	
+	local max_pirates = 6
+	while max_pirates > 0 and Engine.rand:Number(1) < lawlessness do
+		max_pirates = max_pirates-1
+
+		local shipid = shiptypes[Engine.rand:Integer(1,#shiptypes)]
+		local shiptype = ShipType.GetShipType(shipid)
+		local default_drive = shiptype.defaultHyperdrive
+
+		-- select a laser. this is naive - it simply chooses at random from
+		-- the set of lasers that will fit, but never more than one above the
+		-- player's current weapon.
+		-- XXX this should use external factors (eg lawlessness) and not be
+		-- dependent on the player in any way
+		local max_laser_size = shiptype.capacity - EquipType.GetEquipType(default_drive).mass
+		local lasers = EquipType.GetEquipTypes('LASER', function (e,et)
+			return et.mass <= max_laser_size and string.sub(e,0,11) == 'PULSECANNON'
+		end)
+		local laser = lasers[Engine.rand:Integer(1,#lasers)]
+
+		local ship = Space.SpawnShipNear(shipid, body, 10, 30)
+		ship:AddEquip(default_drive)
+		ship:AddEquip(laser)
+		ship:AIKill(Game.player)
+	end
+end
+
 Event.Register("onEnterSystem", onEnterSystem)
+Event.Register("onFrameChanged", onFrameChanged)
