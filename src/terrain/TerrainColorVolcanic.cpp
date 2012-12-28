b2013ef744e8d86e18e5ce382abe0005949a9f9a
John Bartholomew <jpa.bartholomew@gmail.com>
2012-12-28 16:51:07 +0000

    whitespace fixes

--- src/terrain/TerrainColorVolcanic.cpp
+++ src/terrain/TerrainColorVolcanic.cpp
@@ -29,21 +29,21 @@ vector3d TerrainColorFractal<TerrainColorVolcanic>::GetColor(const vector3d &p, 
 	vector3d col;
 
 	if (n > 0.4){
-	col = interpolate_color(equatorial_desert, vector3d(.3,.2,0), vector3d(.3, .1, .0));
-	col = interpolate_color(n, col, vector3d(.1, .0, .0));
-	col = interpolate_color(flatness, color_cliffs, col);
+		col = interpolate_color(equatorial_desert, vector3d(.3,.2,0), vector3d(.3, .1, .0));
+		col = interpolate_color(n, col, vector3d(.1, .0, .0));
+		col = interpolate_color(flatness, color_cliffs, col);
 	} else if (n > 0.2){
-	col = interpolate_color(equatorial_desert, vector3d(1.2,1,0), vector3d(.9, .3, .0));
-	col = interpolate_color(n, col, vector3d(-1.1, -1, .0));
-	col = interpolate_color(flatness, color_cliffs, col);
+		col = interpolate_color(equatorial_desert, vector3d(1.2,1,0), vector3d(.9, .3, .0));
+		col = interpolate_color(n, col, vector3d(-1.1, -1, .0));
+		col = interpolate_color(flatness, color_cliffs, col);
 	} else if (n > 0.1){
-	col = interpolate_color(equatorial_desert, vector3d(.2,.1,0), vector3d(.1, .05, .0));
-	col = interpolate_color(n, col, vector3d(2.5, 2, .0));
-	col = interpolate_color(flatness, color_cliffs, col);
+		col = interpolate_color(equatorial_desert, vector3d(.2,.1,0), vector3d(.1, .05, .0));
+		col = interpolate_color(n, col, vector3d(2.5, 2, .0));
+		col = interpolate_color(flatness, color_cliffs, col);
 	} else {
-	col = interpolate_color(equatorial_desert, vector3d(.75,.6,0), vector3d(.75, .2, .0));
-	col = interpolate_color(n, col, vector3d(-2, -2.2, .0));
-	col = interpolate_color(flatness, color_cliffs, col);
+		col = interpolate_color(equatorial_desert, vector3d(.75,.6,0), vector3d(.75, .2, .0));
+		col = interpolate_color(n, col, vector3d(-2, -2.2, .0));
+		col = interpolate_color(flatness, color_cliffs, col);
 	}
 	return col;
 }
