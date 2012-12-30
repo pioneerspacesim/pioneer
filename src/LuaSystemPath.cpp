cbdd481e428e3a2e63d3ffcc33a17999b643dd12
Robert Norris <rob@eatenbyagrue.org>
2012-12-30 22:26:15 +1100

    fix type that could cause a crash if it ended up in a sector with no systems

--- src/LuaSystemPath.cpp
+++ src/LuaSystemPath.cpp
@@ -244,7 +244,7 @@ static int l_sbodypath_distance_to(lua_State *l)
 	}
 
 	Sector sec1(loc1->sectorX, loc1->sectorY, loc1->sectorZ);
-	Sector sec2(loc2->sectorX, loc2->sectorY, loc1->sectorZ);
+	Sector sec2(loc2->sectorX, loc2->sectorY, loc2->sectorZ);
 
 	double dist = Sector::DistanceBetween(&sec1, loc1->systemIndex, &sec2, loc2->systemIndex);
 
