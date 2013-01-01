9ef726edc6a7dffe541de2ec35bb1e97cc2db1aa
Robert Norris <rob@eatenbyagrue.org>
2013-01-01 12:53:52 +1100

    change the count of bulk ships so we don't flood systems with high population but few orbitals

--- data/modules/BulkShips.lua
+++ data/modules/BulkShips.lua
@@ -18,27 +18,8 @@ local spawnShips = function ()
 	local shiptypes = ShipType.GetShipTypes('STATIC_SHIP')
 	if #shiptypes == 0 then return end
 
-	--[[
-	assuming these are huge supply ships and not your run-of-the-mill
-	traders and not warships or whatever, we'll do it like this:
-
-	- first one is free
-	- one ship per billion up to 4 billion
-	- one ship per 5 billion after that
-	]]
-
-	local num_bulk_ships = 1
-	while population > 1 do
-		if num_bulk_ships < 4 then
-			population = population-1
-			num_bulk_ships = num_bulk_ships+1
-		elseif population > 5 then
-			population = population-5
-			num_bulk_ships = num_bulk_ships+1
-		else
-			break
-		end
-	end
+	-- one ship per three billion, min 1, max 2*num of stations
+	local num_bulk_ships = math.min(#stations*2, math.floor((math.ceil(population)+2)/3))
 
 	for i=1, num_bulk_ships do
 	local station = stations[Engine.rand:Integer(1,#stations)]
