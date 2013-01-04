086ae43d3355d091c96b7e744e2c5762e4315456
Elite-Games.ru: HeadHunter & Maksim Kulikov aka sapog662 <HeadHunter.EG@gmail.com>
2013-01-04 14:31:26 +0400

    RussianTranslation
    RussianTranslation for "cyrillic-support-2".

--- data/modules/StatsTracking/Languages.lua
+++ data/modules/StatsTracking/Languages.lua
@@ -2,3 +2,8 @@ Translate:Add({ English = {
 	["Right on, Commander!"] = "Right on, Commander!",
 	["Elite Federation of Pilots"] = "Elite Federation of Pilots",
 } })
+
+Translate:Add({ Russian = {
+	["Right on, Commander!"] = "Так держать, пилот!",
+	["Elite Federation of Pilots"] = "Федерация Пилотов Класса Элита",
+} })
