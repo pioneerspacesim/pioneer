0159fb66e4aa71d04ffff2aeb5a11d5dd17e99ae
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2013-02-10 23:22:28 +0000

    A nice easy test start position... must try to remember to remove this before doing a pull request.

--- data/ui/MainMenu.lua
+++ data/ui/MainMenu.lua
@@ -24,6 +24,17 @@ local setupPlayerCobra = function ()
 	Game.player:SetMoney(100)
 end
 
+local setupPlayerWave = function ()
+	Game.player:SetShipType("wave")
+	Game.player:AddEquip("PULSECANNON_1MW")
+	Game.player:AddEquip("ATMOSPHERIC_SHIELDING")
+	Game.player:AddEquip("AUTOPILOT")
+	Game.player:AddEquip("SCANNER")
+	Game.player:AddEquip("MISSILE_GUIDED", 2)
+	Game.player:AddEquip("HYDROGEN")
+	Game.player:SetMoney(100)
+end
+
 local doLoadDialog = function ()
 	ui:SetInnerWidget(
 		ui.templates.FileDialog({
@@ -40,6 +51,7 @@ local buttonDefs = {
 	{ t("Start at Earth"),    function () Game.StartGame(SystemPath.New(0,0,0,0,9))   setupPlayerEagle() end },
 	{ t("Start at New Hope"), function () Game.StartGame(SystemPath.New(1,-1,-1,0,4)) setupPlayerEagle() end },
 	{ t("Start at Lave"),     function () Game.StartGame(SystemPath.New(-2,1,90,0,2)) setupPlayerCobra() end },
+	{ t("Lave - Andy"),       function () Game.StartGame(SystemPath.New(-2,1,90,0,2)) setupPlayerWave() end },
 	{ t("Load game"),         doLoadDialog },
 	{ t("Options"),           function () Engine.SettingsView() end },
 	{ t("Quit"),              function () Engine.Quit() end },
