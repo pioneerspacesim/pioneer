f6fef5065e429bbd51e023d54f37e9efacb33bab
Robert Norris <rob@eatenbyagrue.org>
2013-02-21 21:03:17 +1100

    don't hit the copy cache if the node isn't shared

--- src/scenegraph/NodeCopyCache.h
+++ src/scenegraph/NodeCopyCache.h
@@ -1,10 +1,11 @@
 // Copyright © 2008-2013 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-#include <map>
-
 #ifndef SCENEGRAPH_NODECOPYCACHE_H
 #define SCENEGRAPH_NODECOPYCACHE_H
+
+#include "RefCounted.h"
+#include <map>
 
 namespace SceneGraph {
 
@@ -13,11 +14,15 @@ class Node;
 class NodeCopyCache {
 public:
 	template <typename T> T *Copy(const T *origNode) {
-		std::map<const Node*,Node*>::const_iterator i = m_cache.find(origNode);
-		if (i != m_cache.end())
-			return static_cast<T*>((*i).second);
+		const bool doCache = origNode->GetRefCount() > 1;
+		if (doCache) {
+			std::map<const Node*,Node*>::const_iterator i = m_cache.find(origNode);
+			if (i != m_cache.end())
+				return static_cast<T*>((*i).second);
+		}
 		T *newNode = new T(*origNode, this);
-		m_cache.insert(std::make_pair(origNode, newNode));
+		if (doCache)
+			m_cache.insert(std::make_pair(origNode, newNode));
 		return newNode;
 	}
 
