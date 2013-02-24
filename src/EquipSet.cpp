c034a981207859f3c698e8b50eb03fa409194c37
kko <pioneer@paahdin.com>
2013-02-23 20:09:13 -0500

    Split EquipSet into h/cpp

---
+++ src/EquipSet.cpp
@@ -0,0 +1,100 @@
+// Copyright © 2008-2013 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "EquipSet.h"
+
+EquipSet::EquipSet()
+{
+}
+
+void EquipSet::InitSlotSizes(const ShipType::Id &t)
+{
+	const ShipType &st = ShipType::types[t];
+	for (int i=0; i<Equip::SLOT_MAX; i++) {
+		// vector swap idiom (de-allocates unneeded space)
+		std::vector<Equip::Type>(st.equipSlotCapacity[i], Equip::NONE).swap(equip[i]);
+	}
+	onChange.emit(Equip::NONE);
+}
+
+int EquipSet::GetSlotSize(Equip::Slot s) const
+{
+	return equip[s].size();
+}
+
+Equip::Type EquipSet::Get(Equip::Slot s) const
+{
+	return Get(s, 0);
+}
+
+Equip::Type EquipSet::Get(Equip::Slot s, int idx) const
+{
+	assert(idx >= 0);
+	if (signed(equip[s].size()) <= idx) return Equip::NONE;
+	else return equip[s][idx];
+}
+
+bool EquipSet::Set(Equip::Slot s, int idx, Equip::Type e)
+{
+	assert(idx >= 0);
+	assert(e < Equip::TYPE_MAX);
+	if (signed(equip[s].size()) <= idx) return false;
+	equip[s][idx] = e;
+	onChange.emit(e);
+	return true;
+}
+
+int EquipSet::Add(Equip::Type e, int num)
+{
+	return ChangeType(Equip::NONE, e, num);
+}
+
+int EquipSet::Add(Equip::Type e)
+{
+	return Add(e, 1);
+}
+
+// returns number removed
+int EquipSet::Remove(Equip::Type e, int num)
+{
+	return ChangeType(e, Equip::NONE, num);
+}
+
+int EquipSet::Count(Equip::Slot s, Equip::Type e) const
+{
+	assert(e < Equip::TYPE_MAX);
+	int num = 0;
+	for (unsigned int i=0; i<equip[s].size(); i++) {
+		if (equip[s][i] == e) num++;
+	}
+	return num;
+}
+
+int EquipSet::FreeSpace(Equip::Slot s) const
+{
+	return Count(s, Equip::NONE);
+}
+
+int EquipSet::ChangeType(Equip::Type from, Equip::Type to, int num)
+{
+	assert(num >= 0);
+	assert((from < Equip::TYPE_MAX) && (to < Equip::TYPE_MAX));
+
+	if (from == to) return 0;
+
+	assert((from == Equip::NONE) || (to == Equip::NONE) || (Equip::types[from].slot == Equip::types[to].slot));
+	const Equip::Type e = (from == Equip::NONE) ? to : from;
+	const Equip::Slot s = Equip::types[e].slot;
+	int numDone = 0;
+	for (unsigned int i=0; (numDone < num) && (i < equip[s].size()); i++) {
+		if (equip[s][i] == from) {
+			equip[s][i] = to;
+			numDone++;
+		}
+	}
+	if (numDone) {
+		if (from != Equip::NONE) onChange.emit(from);
+		if (to != Equip::NONE) onChange.emit(to);
+	}
+	return numDone;
+}
