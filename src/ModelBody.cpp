fa01170d7b8d1b17ba09d8d8d91e6a7ac6ee9eaf
Martin Bays <mbays@sdf.lonestar.org>
2013-03-16 22:58:16 -0400

    adjust ambient/direct lighting calculations to handle high model positions

--- src/ModelBody.cpp
+++ src/ModelBody.cpp
@@ -159,88 +159,94 @@ void ModelBody::SetFrame(Frame *f)
 //    * As suns set the split is biased towards ambient
 void ModelBody::CalcLighting(double &ambient, double &direct, const Camera *camera)
 {
-	// position relative to the rotating frame of the planet
+	ambient = 0.0;
+	direct = 1.0;
 	Body *astro = GetFrame()->GetBody();
-	if (astro && astro->IsType(Object::PLANET)) {
-		Planet *planet = static_cast<Planet*>(astro);
-		vector3d upDir = GetPosition();
-		const double dist = upDir.Length();
-		upDir = upDir.Normalized();
-		double pressure, density;
-		planet->GetAtmosphericState(dist, &pressure, &density);
-		double surfaceDensity;
-		Color cl;
-		planet->GetSystemBody()->GetAtmosphereFlavor(&cl, &surfaceDensity);
+	if ( ! (astro && astro->IsType(Object::PLANET)) )
+		return;
 
-		// approximate optical thickness fraction as fraction of density remaining relative to earths
-		double opticalThicknessFraction = density/EARTH_ATMOSPHERE_SURFACE_DENSITY;
-		// tweak optical thickness curve - lower exponent ==> higher altitude before ambient level drops
-		opticalThicknessFraction = pow(std::max(0.00001,opticalThicknessFraction),0.15); //max needed to avoid 0^power
+	Planet *planet = static_cast<Planet*>(astro);
 
-		//step through all the lights and calculate contributions taking into account sun position
-		double light = 0.0;
-		double light_clamped = 0.0;
+	// position relative to the rotating frame of the planet
+	vector3d upDir = GetInterpPositionRelTo(planet->GetFrame());
+	const double planetRadius = planet->GetSystemBody()->GetRadius();
+	const double dist = std::max(planetRadius, upDir.Length());
+	upDir = upDir.Normalized();
 
-		const std::vector<Camera::LightSource> &lightSources = camera->GetLightSources();
-		for(std::vector<Camera::LightSource>::const_iterator l = lightSources.begin();
-				l != lightSources.end(); ++l) {
+	double pressure, density;
+	planet->GetAtmosphericState(dist, &pressure, &density);
+	double surfaceDensity;
+	Color cl;
+	planet->GetSystemBody()->GetAtmosphereFlavor(&cl, &surfaceDensity);
 
-			double sunAngle;
-			// calculate the extent the sun is towards zenith
-			if (l->GetBody()){
-				// relative to the rotating frame of the planet
-				const vector3d lightDir = (l->GetBody()->GetInterpPositionRelTo(planet->GetFrame()).Normalized());
-				sunAngle = lightDir.Dot(upDir);
-			} else {
-				// light is the default light for systems without lights
-				sunAngle = 1.0;
-			}
 
-			//0 to 1 as sunangle goes from 0.0 to 1.0
-			double sunAngle2 = (Clamp(sunAngle, 0.0,1.0))/1.0;
+	// approximate optical thickness fraction as fraction of density remaining relative to earths
+	double opticalThicknessFraction = density/EARTH_ATMOSPHERE_SURFACE_DENSITY;
 
-			//0 to 1 as sunAngle goes from endAngle to startAngle
+	// tweak optical thickness curve - lower exponent ==> higher altitude before ambient level drops
+	// Commenting this out, since it leads to a sharp transition at
+	// atmosphereRadius, where density is suddenly 0
+	//opticalThicknessFraction = pow(std::max(0.00001,opticalThicknessFraction),0.15); //max needed to avoid 0^power
 
-			// angle at which light begins to fade on Earth
-			const double startAngle = 0.3;
-			// angle at which sun set completes, which should be after sun has dipped below the horizon on Earth
-			const double endAngle = -0.18;
+	if (opticalThicknessFraction < 0.0001)
+		return;
 
-			const double start = std::min((startAngle*opticalThicknessFraction),1.0);
-			const double end = std::max((endAngle*opticalThicknessFraction),-0.2);
+	//step through all the lights and calculate contributions taking into account sun position
+	double light = 0.0;
+	double light_clamped = 0.0;
 
-			sunAngle = (Clamp(sunAngle, end, start)-end)/(start-end);
+	const std::vector<Camera::LightSource> &lightSources = camera->GetLightSources();
+	for(std::vector<Camera::LightSource>::const_iterator l = lightSources.begin();
+			l != lightSources.end(); ++l) {
 
-			light += sunAngle;
-			light_clamped += sunAngle2;
+		double sunAngle;
+		// calculate the extent the sun is towards zenith
+		if (l->GetBody()){
+			// relative to the rotating frame of the planet
+			const vector3d lightDir = (l->GetBody()->GetInterpPositionRelTo(planet->GetFrame()).Normalized());
+			sunAngle = lightDir.Dot(upDir);
+		} else {
+			// light is the default light for systems without lights
+			sunAngle = 1.0;
 		}
 
+		const double critAngle = -sqrt(dist*dist-planetRadius*planetRadius)/dist;
 
-		// brightness depends on optical depth and intensity of light from all the stars
-		direct = (Clamp((light),0.0,1.0));
+		//0 to 1 as sunangle goes from critAngle to 1.0
+		double sunAngle2 = (Clamp(sunAngle, critAngle, 1.0)-critAngle)/(1.0-critAngle);
 
+		// angle at which light begins to fade on Earth
+		const double surfaceStartAngle = 0.3;
+		// angle at which sun set completes, which should be after sun has dipped below the horizon on Earth
+		const double surfaceEndAngle = -0.18;
 
-		// ambient light fraction
-		// alter ratio between directly and ambiently lit portions towards ambiently lit as sun sets
-		const double fraction = (0.1+0.8*(
-					1.0-light_clamped*(Clamp((opticalThicknessFraction),0.0,1.0))
-					)+0.1); //fraction goes from 0.6 to 1.0
+		const double start = std::min((surfaceStartAngle*opticalThicknessFraction),1.0);
+		const double end = std::max((surfaceEndAngle*opticalThicknessFraction),-0.2);
 
+		sunAngle = (Clamp(sunAngle-critAngle, end, start)-end)/(start-end);
 
-		// fraction of light left over to be lit directly
-		direct = (1.0-fraction)*direct;
-
-		// scale ambient by amount of light
-		ambient = fraction*(Clamp((light),0.0,1.0))*0.25;
-
-		const double minAmbient = std::min(1.0,density)*0.05;
-		ambient = std::max(minAmbient, ambient);
+		light += sunAngle;
+		light_clamped += sunAngle2;
 	}
-	else {
-		// not in an atmosphere
-		ambient = 0.0;
-		direct = 1.0;
-	}
+
+	light_clamped /= lightSources.size();
+	light /= lightSources.size();
+
+	// brightness depends on optical depth and intensity of light from all the stars
+	direct = 1.0 -  Clamp((1.0 - light),0.0,1.0) * Clamp(opticalThicknessFraction,0.0,1.0);
+
+	// ambient light fraction
+	// alter ratio between directly and ambiently lit portions towards ambiently lit as sun sets
+	const double fraction = ( 0.2 + 0.8 * (1.0-light_clamped) ) * Clamp(opticalThicknessFraction,0.0,1.0);
+
+	// fraction of light left over to be lit directly
+	direct = (1.0-fraction)*direct;
+
+	// scale ambient by amount of light
+	ambient = fraction*(Clamp((light),0.0,1.0))*0.25;
+
+	const double minAmbient = std::min(1.0,density)*0.05;
+	ambient = std::max(minAmbient, ambient);
 }
 
 void ModelBody::RenderModel(Graphics::Renderer *r, const Camera *camera, const vector3d &viewCoords, const matrix4x4d &viewTransform)
