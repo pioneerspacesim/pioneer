3990c62e027732df495cedc4083cedaea185de14
Lionheart <lionheart1588@gmail.com>
2013-04-03 15:38:46 +0100

    new stations and ship with new definition files

---
+++ data/stations/frontier.lua
@@ -0,0 +1,15 @@
+define_orbital_station {
+	model = 'frontier',
+	angular_velocity = 0.1,
+	num_docking_ports = 4,
+	parking_distance = 5000.0,
+	parking_gap_size = 500.0,
+	ship_launch_stage = 3,
+	-- define groups of bays, in this case 1 group with 1 bay.
+	-- params are = {minSize, maxSize, {list,of,bay,numbers}}
+	bay_groups = {
+		{0, 500, {1}},
+	},
+	dock_anim_stage_duration = { DOCKING_TIMEOUT_SECONDS, 10.0, 5.0, 5.0 },
+	undock_anim_stage_duration = { 5.0, 5.0, 10.0 },
+}
