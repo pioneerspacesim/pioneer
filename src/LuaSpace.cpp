806bb44b7952219d4fe57113e34fad76368516b9
shadmar <shadmar@gmail.com>
2013-06-22 00:26:54 +0200

    just use 0.26 of phyradius instead of fixed distance

--- src/LuaSpace.cpp
+++ src/LuaSpace.cpp
@@ -393,7 +393,9 @@ static int l_space_spawn_ship_parked_offset(lua_State *l)
 	Ship *ship = new Ship(type);
 	assert(ship);
 
-	double parkDist = station->GetStationType()->parkingDistance*0.5+300000.0;
+	double dist = station->GetFrame()->GetBody()->GetPhysRadius()*0.26;
+
+	double parkDist = station->GetStationType()->parkingDistance*0.5+dist;
 	parkDist -= ship->GetPhysRadius();		// park inside parking radius
 	double parkOffset = 0.5 * station->GetStationType()->parkingGapSize;
 	parkOffset += ship->GetPhysRadius();	// but outside the docking gap
