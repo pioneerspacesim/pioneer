e5a64994de3a3beb06eed8dc38123a7fc28372e6
Shadmar <shadmar@gmail.com>
2013-07-06 22:22:09 +0200

    Update Police.lua
    oh there I get for hacking directly...

--- data/modules/Police.lua
+++ data/modules/Police.lua
@@ -43,7 +43,7 @@ local spawnPolice = function()
 
 	if starport~=nil then
 
-		if starport~=nil and Game.player:DistanceTo(starport)<400000 then
+		if starport~=nil and Game.player:DistanceTo(starport)<400000 then return end
 
 		-- XXX number should be some combination of population, lawlessness,
 		-- proximity to shipping lanes, etc
