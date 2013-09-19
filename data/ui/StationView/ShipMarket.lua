9637c2f3702b4433fb14aafb7f68c77870513d22
Robert Norris <rob@eatenbyagrue.org>
2013-09-19 20:34:30 +1000

    basic auto-updating ship market

--- data/ui/StationView/ShipMarket.lua
+++ data/ui/StationView/ShipMarket.lua
@@ -2,11 +2,34 @@
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
 local Engine = import("Engine")
+local SpaceStation = import("SpaceStation")
+local Game = import("Game")
+local Event = import("Event")
 
 local ui = Engine.ui
 
+local shipTable =
+	ui:Table()
+		:SetRowSpacing(5)
+		:SetColumnSpacing(10)
+		:SetHeadingRow("id")
+		:SetMouseEnabled(true)
+
+local function updateStation (station, shipsOnSale)
+	if station ~= Game.player:GetDockedWith() then return end
+
+	shipTable:ClearRows()
+
+	for i = 1,#shipsOnSale do
+		shipTable:AddRow({shipsOnSale[i].id})
+	end
+end
+
+Event.Register("onShipMarketUpdate", updateStation)
+
 local shipMarket = function (args)
-    return ui:Label("ship market")
+	updateStation(station, SpaceStation.shipsOnSale[Game.player:GetDockedWith()])
+	return shipTable
 end
 
 return shipMarket
