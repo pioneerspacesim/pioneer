0394754f52c553a89c47c6bab25b8b3eba456547
John Bartholomew <jpa.bartholomew@gmail.com>
2013-10-10 12:39:16 +0100

    rewrite the ShipRepairs form
    The new version is more attractive and follows the shop-on-left,
    state-on-right model used in other station screens. It also has better
    widget management, re-using existing widgets and updating their content
    rather than discarding them and creating new ones.

--- data/ui/StationView/ShipRepairs.lua
+++ data/ui/StationView/ShipRepairs.lua
@@ -2,79 +2,127 @@
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
 local Engine = import("Engine")
+local Lang = import("Lang")
 local Game = import("Game")
 local ShipDef = import("ShipDef")
-local Lang = import("Lang")
 local Format = import("Format")
+local Rand = import("Rand")
+local InfoGauge = import("ui/InfoGauge")
+local InfoFace = import("ui/InfoFace")
+local ModelSpinner = import("UI.Game.ModelSpinner")
+local Character = import("Character")
 
 local l = Lang.GetResource("ui-core")
 local ui = Engine.ui
 
-local getRepairCost = function (percent)
+local getRepairCost = function (percent, shipDef)
 	-- repairing 1% hull damage costs 0.1% of ship price
-	local shipDef = ShipDef[Game.player.shipId]
+	shipDef = shipDef or ShipDef[Game.player.shipId]
 	return math.ceil(shipDef.basePrice * (percent * 0.1)) * 0.01
 end
 
-local repairItemTemplate = function (damage, price, handler)
-	local btn = ui:Button(ui:Expand('HORIZONTAL', ui:Label(string.interp(
-            l.REPAIR_X_HULL_DAMAGE_FOR_X, {
-				damage = string.format('%.1f', damage),
-				price = Format.Money(price)
-			}
-		))))
-	btn.onClick:Connect(function () handler(damage, price); end)
-	return btn
-end
-
-local function createRepairOptions (root)
-	local hullPercent = Game.player.hullPercent
-
-	if root ~= nil then
-		root:Clear()
-	end
-
-	-- XXX this display should probably include the player's current cash somewhere
-
-	if hullPercent >= 100 then
-		root:PackEnd(ui:Label(l.SHIP_IS_ALREADY_FULLY_REPAIRED))
-	else
-		local hullDamage = 100 - hullPercent
-		local smallDamage = math.min(hullDamage, 1)
-		local costRepair1 = getRepairCost(smallDamage)
-		local costRepairAll = getRepairCost(hullDamage)
-		local feedback = ui:Label('')
-
-		local repairDamage = function (damage, price)
-			-- repair the hull for the money!
-			if Game.player:GetMoney() >= price then
-				Game.player:AddMoney(-price)
-				Game.player:SetHullPercent(hullPercent + damage)
-				createRepairOptions(root)
-			else
-				feedback:SetText(t("You don't have enough money for that option."))
-			end
-		end
-
-		local rows = {}
-		rows[1] = ui:Label(string.interp(l.YOUR_HULL_IS_AT_X_INTEGRITY,
-			{value = string.format('%.1f', hullPercent)}))
-		rows[2] = repairItemTemplate(smallDamage, costRepair1, repairDamage)
-		if hullDamage > 1 then
-			rows[3] = repairItemTemplate(hullDamage, costRepairAll, repairDamage)
-		end
-		rows[4] = feedback
-
-		root:PackEnd(rows)
-	end
-
-	return root
+local getRepairMessage = function (damage, price)
+	return string.interp(
+		l.REPAIR_X_HULL_DAMAGE_FOR_X, {
+			damage = string.format('%.1f', damage),
+			price = Format.Money(price)
+		})
 end
 
 local shipRepairs = function (args)
-	local root = ui:VBox(5)
-	createRepairOptions(root)
-	return root
+	local messageLabel = ui:Label('<message>')
+	local feedbackLabel = ui:Label('<feedback>')
+	local repairButtons = ui:VBox(5)
+	local repair1Label = ui:Label('Repair 1% hull damage for $.')
+	local repairAllLabel = ui:Label('Repair all hull damage $$$.')
+	local repair1Btn = ui:Button(ui:Expand('HORIZONTAL', repair1Label))
+	local repairAllBtn = ui:Button(ui:Expand('HORIZONTAL', repairAllLabel))
+
+	local cashLabel = ui:Label('<cash>')
+	local integrityGauge = InfoGauge.New({
+		formatter = function (v) return string.format('%.0f%%', v * 100); end,
+		warningLevel = 0.5,
+		criticalLevel = 0.2,
+		levelAscending = false,
+	})
+
+	local damageAll, damage1, costRepairAll, costRepair1
+
+	local update = function (feedbackText)
+		local shipDef = ShipDef[Game.player.shipId]
+		local hullPercent = Game.player.hullPercent
+
+		cashLabel:SetText(string.interp(l.CASH_N,
+			{money = Format.Money(Game.player:GetMoney())}))
+		integrityGauge:SetValue(hullPercent / 100)
+
+		if hullPercent >= 100 then
+			messageLabel:SetText(l.SHIP_IS_ALREADY_FULLY_REPAIRED)
+			feedbackLabel:SetText(feedbackText)
+			repairButtons:Clear()
+		else
+			damageAll = 100 - hullPercent
+			damage1 = math.min(damageAll, 1)
+			costRepair1 = getRepairCost(damage1, shipDef)
+			costRepairAll = getRepairCost(damageAll, shipDef)
+
+			messageLabel:SetText(string.interp(l.YOUR_HULL_IS_AT_X_INTEGRITY,
+				{value = string.format('%.1f', hullPercent)}))
+			feedbackLabel:SetText(feedbackText)
+
+			repair1Label:SetText(getRepairMessage(damage1, costRepair1))
+			repairAllLabel:SetText(getRepairMessage(damageAll, costRepairAll))
+
+			repairButtons:Clear()
+			repairButtons:PackEnd(repair1Btn)
+			if damageAll > damage1 then
+				repairButtons:PackEnd(repairAllBtn)
+			end
+		end
+	end
+
+	local tryRepair = function (damage, price)
+		if Game.player:GetMoney() >= price then
+			Game.player:AddMoney(-price)
+			Game.player:SetHullPercent(Game.player.hullPercent + damage)
+			update('')
+		else
+			update(l.YOU_DONT_HAVE_ENOUGH_MONEY_FOR_THAT_OPTION)
+		end
+	end
+
+	repair1Btn.onClick:Connect(function () tryRepair(damage1, costRepair1); end)
+	repairAllBtn.onClick:Connect(function () tryRepair(damageAll, costRepairAll); end)
+
+	-- initialise UI state
+	update('')
+
+	-- XXX need a better way of seeding this
+	local station = Game.player:GetDockedWith()
+	local rand = Rand.New(util.hash_random(station.seed .. '-repair-guy', 4294967296) - 1)
+	local face = InfoFace.New(Character.New({ title = "Repair Guy" }, rand))
+
+	local repairShop = ui:VBox(5):PackEnd({
+		ui:Grid({1,1,1},1):SetCell(1, 0, face),
+		messageLabel,
+		repairButtons,
+		feedbackLabel,
+		ui:Expand('VERTICAL'),
+	})
+
+	local shipDef = ShipDef[Game.player.shipId]
+	local shipView = ui:VBox(5):PackEnd({
+		ui:Grid({1,2,1},1):SetCell(1, 0, ModelSpinner.New(ui, shipDef.modelName, Game.player:GetSkin())),
+		ui:Expand('VERTICAL'),
+		cashLabel,
+		ui:Label(l.HULL_INTEGRITY),
+		integrityGauge,
+	})
+
+	return
+		ui:Grid(2,1)
+			:SetCell(0, 0, ui:Margin(20, 'RIGHT', repairShop))
+			:SetCell(1, 0, ui:Margin(20, 'LEFT', shipView))
 end
 
 return shipRepairs
