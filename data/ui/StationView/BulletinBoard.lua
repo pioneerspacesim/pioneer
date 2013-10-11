438b21cd013c520908f418246871bc5f5a05dafd
Robert Norris <rob@eatenbyagrue.org>
2013-10-11 16:55:18 +1100

    initial bb list just to prove it works

--- data/ui/StationView/BulletinBoard.lua
+++ data/ui/StationView/BulletinBoard.lua
@@ -2,11 +2,15 @@
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
 local Engine = import("Engine")
+local Game = import("Game")
+local SpaceStation = import("SpaceStation")
+local utils = import("utils")
 
 local ui = Engine.ui
 
 local bulletinBoard = function (args)
-    return ui:Label("bulletin board")
+    local station = Game.player:GetDockedWith()
+    return ui:Table():AddRows(utils.build_array(utils.map(function (k,v) return k,{v[1]} end, ipairs(SpaceStation.adverts[station]))))
 end
 
 return bulletinBoard
