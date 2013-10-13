4c3586706925eab5680f99a51cab186c703ab1e4
John Bartholomew <jpa.bartholomew@gmail.com>
2013-10-13 20:12:11 +0100

    extend KeyBindingCapture to capture joystick events

--- src/gameui/BindingCapture.cpp
+++ src/gameui/BindingCapture.cpp
@@ -14,10 +14,33 @@ KeyBindingCapture::KeyBindingCapture(UI::Context *context): Single(context), m_c
 	m_binding.u.keyboard.mod = KMOD_NONE;
 }
 
+KeyBindingCapture::~KeyBindingCapture()
+{
+	Disconnect();
+}
+
 void KeyBindingCapture::Capture()
 {
-	GetContext()->SelectWidget(this);
-	m_capturing = true;
+	if (!m_capturing) {
+		m_capturing = true;
+		if (IsVisible()) {
+			Connect();
+		}
+	}
+}
+
+void KeyBindingCapture::HandleVisible()
+{
+	if (m_capturing) {
+		Connect();
+	}
+}
+
+void KeyBindingCapture::HandleInvisible()
+{
+	if (m_capturing) {
+		Disconnect();
+	}
 }
 
 void KeyBindingCapture::HandleKeyDown(const UI::KeyboardEvent &event)
@@ -27,8 +50,61 @@ void KeyBindingCapture::HandleKeyDown(const UI::KeyboardEvent &event)
 		m_binding.u.keyboard.key = event.keysym.sym;
 		m_binding.u.keyboard.mod = event.keysym.mod;
 		m_capturing = false;
+		Disconnect();
 		onCapture.emit(m_binding);
 	}
+}
+
+void KeyBindingCapture::Connect()
+{
+	assert(m_capturing);
+	assert(IsVisible());
+	GetContext()->SelectWidget(this);
+	m_connJoystickAxisMove = GetContext()->onJoystickAxisMove.connect(sigc::mem_fun(this, &KeyBindingCapture::OnJoystickAxisMove));
+	m_connJoystickHatMove = GetContext()->onJoystickHatMove.connect(sigc::mem_fun(this, &KeyBindingCapture::OnJoystickHatMove));
+	m_connJoystickButtonDown = GetContext()->onJoystickButtonDown.connect(sigc::mem_fun(this, &KeyBindingCapture::OnJoystickButtonDown));
+}
+
+void KeyBindingCapture::Disconnect()
+{
+	m_connJoystickAxisMove.disconnect();
+	m_connJoystickHatMove.disconnect();
+	m_connJoystickButtonDown.disconnect();
+}
+
+bool KeyBindingCapture::OnJoystickAxisMove(const UI::JoystickAxisMotionEvent &event)
+{
+	// capture axis binding
+	return false;
+}
+
+bool KeyBindingCapture::OnJoystickHatMove(const UI::JoystickHatMotionEvent &event)
+{
+	if (m_capturing) {
+		m_binding.type = KeyBindings::JOYSTICK_HAT;
+		m_binding.u.joystickHat.joystick = event.joystick;
+		m_binding.u.joystickHat.hat = event.hat;
+		m_binding.u.joystickHat.direction = static_cast<int>(event.direction);
+		m_capturing = false;
+		Disconnect();
+		onCapture.emit(m_binding);
+		return true;
+	} else
+		return false;
+}
+
+bool KeyBindingCapture::OnJoystickButtonDown(const UI::JoystickButtonEvent &event)
+{
+	if (m_capturing) {
+		m_binding.type = KeyBindings::JOYSTICK_BUTTON;
+		m_binding.u.joystickButton.joystick = event.joystick;
+		m_binding.u.joystickButton.button = event.button;
+		m_capturing = false;
+		Disconnect();
+		onCapture.emit(m_binding);
+		return true;
+	} else
+		return false;
 }
 
 }
--- src/gameui/BindingCapture.h
+++ src/gameui/BindingCapture.h
@@ -13,6 +13,7 @@ namespace GameUI {
 class KeyBindingCapture : public UI::Single {
 public:
 	KeyBindingCapture(UI::Context *context);
+	~KeyBindingCapture();
 
 	virtual bool IsSelectable() const { return true; }
 
@@ -22,9 +23,20 @@ public:
 	sigc::signal<void,const KeyBindings::KeyBinding &> onCapture;
 
 protected:
+	virtual void HandleVisible();
+	virtual void HandleInvisible();
 	virtual void HandleKeyDown(const UI::KeyboardEvent &event);
 
 private:
+	void Connect();
+	void Disconnect();
+	bool OnJoystickAxisMove(const UI::JoystickAxisMotionEvent &event);
+	bool OnJoystickHatMove(const UI::JoystickHatMotionEvent &event);
+	bool OnJoystickButtonDown(const UI::JoystickButtonEvent &event);
+
+	sigc::connection m_connJoystickAxisMove;
+	sigc::connection m_connJoystickHatMove;
+	sigc::connection m_connJoystickButtonDown;
 	KeyBindings::KeyBinding m_binding;
 	bool m_capturing;
 };
