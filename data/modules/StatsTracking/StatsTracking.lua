0f56601d0e08505ccb28970541c648a14a0655b2
Robert Norris <rob@eatenbyagrue.org>
2013-10-26 21:52:29 +1100

    convert StatsTracking strings to json

--- data/modules/StatsTracking/StatsTracking.lua
+++ data/modules/StatsTracking/StatsTracking.lua
@@ -1,10 +1,12 @@
 -- Copyright © 2008-2013 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-local Translate = import("Translate")
 local Event = import("Event")
 local Character = import("Character")
 local Comms = import("Comms")
+local Lang = import("Lang")
+
+local l = Lang.GetResource("module-statstracking")
 
 -- Stats-gathering module. Initially, gathers kill statistics for the player.
 -- Can (and should) be expanded in the future to gather other information.
@@ -18,10 +20,6 @@ local Comms = import("Comms")
 -- the combat rating.
 local PlayerDamagedShips = {}
 
--- We need this to translate "Right on, Commander" etc
--- Until dependencies are fixed, this can't move to data/libs/
-local t = Translate:GetTranslator()
-
 
 local onShipDestroyed = function (ship, attacker)
 	if attacker:isa('Ship') and attacker:IsPlayer() then
@@ -31,7 +29,7 @@ local onShipDestroyed = function (ship, attacker)
 		if Character.persistent.player.killcount == 1 or (Character.persistent.player.killcount < 256 and Character.persistent.player.killcount % 16 == 0) or (Character.persistent.player.killcount % 256 == 0) then
 			-- On the first kill, every 16th kill until 256, and every 256th
 			-- kill thereafter
-			Comms.Message(t("Well done commander! Your combat rating has improved!"),t("Pioneering Pilots' Guild"))
+			Comms.Message(l.WELL_DONE_COMMANDER_YOUR_COMBAT_RATING_HAS_IMPROVED,l.PIONEERING_PILOTS_GUILD)
 		end
 	elseif PlayerDamagedShips[ship] then
 		Character.persistent.player.assistcount = Character.persistent.player.assistcount + 1
