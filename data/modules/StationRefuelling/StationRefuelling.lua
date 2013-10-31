53e8efd449eb2bb659333b525b776efd60583529
Robert Norris <rob@eatenbyagrue.org>
2013-10-31 17:32:50 +1100

    convert StationRefuelling to json

--- data/modules/StationRefuelling/StationRefuelling.lua
+++ data/modules/StationRefuelling/StationRefuelling.lua
@@ -1,14 +1,13 @@
 -- Copyright © 2008-2013 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
+local Lang = import("Lang")
 local Game = import("Game")
 local Comms = import("Comms")
-local Translate = import("Translate")
 local Event = import("Event")
 local Format = import("Format")
 
--- Get the translator function
-local t = Translate:GetTranslator()
+local l = Lang.GetResource("module-stationrefuelling")
 
 local minimum_fee = 1
 local maximum_fee = 6
@@ -32,10 +31,10 @@ local onShipDocked = function (ship, station)
 	end
 	local fee = calculateFee()
 	if ship:GetMoney() < fee then
-		Comms.Message(t("This is {station}. You do not have enough for your docking fee of {fee}. Your fuel has been witheld."):interp({station = station.label,fee = Format.Money(fee)}))
+		Comms.Message(l.THIS_IS_STATION_YOU_DO_NOT_HAVE_ENOUGH:interp({station = station.label,fee = Format.Money(fee)}))
 		ship:SetMoney(0)
 	else
-		Comms.Message(t("Welcome aboard {station}. Your docking and fuelling fee of {fee} has been deducted."):interp({station = station.label,fee = Format.Money(fee)}))
+		Comms.Message(l.WELCOME_ABOARD_STATION_FEE_DEDUCTED:interp({station = station.label,fee = Format.Money(fee)}))
 		ship:AddMoney(0 - fee)
 		ship:SetFuelPercent()
 	end
