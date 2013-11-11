ba19410b94362195cf4ce9ee86e16da009772101
John Bartholomew <jpa.bartholomew@gmail.com>
2013-11-11 01:34:11 +0000

    declare key bindings for all the SectorView controls

--- src/KeyBindings.inc.h
+++ src/KeyBindings.inc.h
@@ -90,6 +90,28 @@ KEY_BINDING(cameraRotateLeft, "BindCameraRotateLeft", Lang::ROTATE_LEFT, SDLK_KP
 KEY_BINDING(cameraRotateRight, "BindCameraRotateRight", Lang::ROTATE_RIGHT, SDLK_KP_6, SDLK_RIGHT)
 KEY_BINDING(resetCamera, "BindResetCamera", Lang::RESET, SDLK_HOME, 0)
 
+BINDING_GROUP(Lang::SECTOR_MAP_VIEW)
+KEY_BINDING(mapStartSearch, "BindMapStartSearch", Lang::SEARCH_MAP, SDLK_SLASH, SDLK_KP_DIVIDE)
+KEY_BINDING(mapLockHyperspaceTarget, "BindMapLockHyperspaceTarget", Lang::MAP_LOCK_HYPERSPACE_TARGET, SDLK_SPACE, 0)
+KEY_BINDING(mapToggleInfoPanel, "BindMapToggleInfoPanel", Lang::MAP_TOGGLE_INFO_PANEL, SDLK_TAB, 0)
+KEY_BINDING(mapToggleSelectionFollowView, "BindMapToggleSelectionFollowView", Lang::MAP_TOGGLE_SELECTION_FOLLOW_VIEW, SDLK_RETURN, SDLK_KP_ENTER)
+KEY_BINDING(mapWarpToCurrent, "BindMapWarpToCurrent", Lang::MAP_WARP_TO_CURRENT_SYSTEM, SDLK_c, 0)
+KEY_BINDING(mapWarpToSelected, "BindMapWarpToSelection", Lang::MAP_WARP_TO_SELECTED_SYSTEM, SDLK_g, 0)
+KEY_BINDING(mapWarpToHyperspaceTarget, "BindMapWarpToHyperspaceTarget", Lang::MAP_WARP_TO_HYPERSPACE_TARGET, SDLK_h, 0)
+
+KEY_BINDING(mapViewShiftForward, "BindMapViewShiftForward", Lang::MAP_VIEW_SHIFT_FORWARD, SDLK_PAGEUP, 0)
+KEY_BINDING(mapViewShiftBackward, "BindMapViewShiftBackward", Lang::MAP_VIEW_SHIFT_BACKWARD, SDLK_PAGEDOWN, 0)
+KEY_BINDING(mapViewShiftLeft, "BindMapViewShiftLeft", Lang::MAP_VIEW_SHIFT_LEFT, SDLK_LEFT, 0)
+KEY_BINDING(mapViewShiftRight, "BindMapViewShiftRight", Lang::MAP_VIEW_SHIFT_RIGHT, SDLK_RIGHT, 0)
+KEY_BINDING(mapViewShiftUp, "BindMapViewShiftUp", Lang::MAP_VIEW_SHIFT_UP, SDLK_UP, 0)
+KEY_BINDING(mapViewShiftDown, "BindMapViewShiftDown", Lang::MAP_VIEW_SHIFT_DOWN, SDLK_DOWN, 0)
+
+KEY_BINDING(mapViewRotateLeft, "BindMapViewRotateLeft", Lang::MAP_VIEW_ROTATE_LEFT, SDLK_a, 0)
+KEY_BINDING(mapViewRotateRight, "BindMapViewRotateRight", Lang::MAP_VIEW_ROTATE_RIGHT, SDLK_d, 0)
+KEY_BINDING(mapViewRotateUp, "BindMapViewRotateUp", Lang::MAP_VIEW_ROTATE_UP, SDLK_w, 0)
+KEY_BINDING(mapViewRotateDown, "BindMapViewRotateDown", Lang::MAP_VIEW_ROTATE_DOWN, SDLK_s, 0)
+KEY_BINDING(mapViewReset, "BindMapViewReset", Lang::RESET_ORIENTATION_AND_ZOOM, SDLK_r, 0)
+
 BINDING_PAGE_END()
 
 #undef KEY_BINDING
