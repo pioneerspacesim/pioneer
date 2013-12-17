b31c6e80f140b1838fc99f37100971564b40c8bf
Robert Norris <rob@eatenbyagrue.org>
2013-12-17 11:29:16 +1100

    show ship class in market

--- data/ui/StationView/ShipMarket.lua
+++ data/ui/StationView/ShipMarket.lua
@@ -20,6 +20,27 @@ local l = Lang.GetResource("ui-core")
 -- XXX equipment strings are in core. this sucks
 local lcore = Lang.GetResource("core")
 
+local shipClassString = {
+	light_cargo_shuttle        = l.LIGHT_CARGO_SHUTTLE,
+	light_courier              = l.LIGHT_COURIER,
+	light_fighter              = l.LIGHT_FIGHTER,
+	light_freighter            = l.LIGHT_FREIGHTER,
+	light_passenger_shuttle    = l.LIGHT_PASSENGER_SHUTTLE,
+	light_passenger_transport  = l.LIGHT_PASSENGER_TRANSPORT,
+	medium_cargo_shuttle       = l.MEDIUM_CARGO_SHUTTLE,
+	medium_courier             = l.MEDIUM_COURIER,
+	medium_fighter             = l.MEDIUM_FIGHTER,
+	medium_freighter           = l.MEDIUM_FREIGHTER,
+	medium_passenger_shuttle   = l.MEDIUM_PASSENGER_SHUTTLE,
+	medium_passenger_transport = l.MEDIUM_PASSENGER_TRANSPORT,
+	heavy_cargo_shuttle        = l.HEAVY_CARGO_SHUTTLE,
+	heavy_courier              = l.HEAVY_COURIER,
+	heavy_fighter              = l.HEAVY_FIGHTER,
+	heavy_freighter            = l.HEAVY_FREIGHTER,
+	heavy_passenger_shuttle    = l.HEAVY_PASSENGER_SHUTTLE,
+	heavy_passenger_transport  = l.HEAVY_PASSENGER_TRANSPORT,
+}
+
 local shipTable =
 	ui:Table()
 		:SetRowSpacing(5)
@@ -96,7 +117,12 @@ shipTable.onRowClicked:Connect(function (row)
 	shipInfo:SetInnerWidget(
 		ui:VBox():PackEnd({
 			ui:HBox():PackEnd({
-				ui:Align("LEFT", ui:Label(def.name):SetFont("HEADING_LARGE")),
+				ui:Align("LEFT",
+					ui:VBox():PackEnd({
+						ui:Label(def.name):SetFont("HEADING_LARGE"),
+						ui:Label(shipClassString[def.shipClass]):SetFont("HEADING_SMALL"),
+					})
+				),
 				ui:Expand("HORIZONTAL", ui:Align("RIGHT", manufacturerIcon(def.manufacturer))),
 			}),
 			ui:Grid(2,1):SetRow(0, {
