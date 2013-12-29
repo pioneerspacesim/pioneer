6a0b3220aa53722c7f4b5e3342101e57617398f9
Robert Norris <rob@eatenbyagrue.org>
2013-12-29 21:03:44 +1100

    savefile version bump for new stationview

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,7 +23,7 @@
 #include "FileSystem.h"
 #include "graphics/Renderer.h"
 
-static const int  s_saveVersion   = 68;
+static const int  s_saveVersion   = 69;
 static const char s_saveStart[]   = "PIONEER";
 static const char s_saveEnd[]     = "END";
 
