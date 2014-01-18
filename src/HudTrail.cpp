1e3d9457c87405744a2ddef4de4e889b55f3548b
Robert Norris <rob@eatenbyagrue.org>
2014-01-18 22:50:57 +1100

    copyright headers

--- src/HudTrail.cpp
+++ src/HudTrail.cpp
@@ -1,3 +1,6 @@
+// Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
 #include "HudTrail.h"
 
 const float UPDATE_INTERVAL = 0.1f;
