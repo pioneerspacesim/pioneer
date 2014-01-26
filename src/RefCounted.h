ab65a97c17158da5a97a6268a4c693a252ba1dcc
lwho <lwho@github.invalid>
2014-01-26 14:17:56 +0100

    Do not copy/assign reference counter.

--- src/RefCounted.h
+++ src/RefCounted.h
@@ -10,7 +10,14 @@
 class RefCounted : public LuaWrappable {
 public:
 	RefCounted() : m_refCount(0) {}
+	RefCounted(const RefCounted& other) : LuaWrappable(static_cast<const LuaWrappable&>(other)), m_refCount(0) { }
 	virtual ~RefCounted() {}
+
+	RefCounted& operator=(const RefCounted& other) {
+		LuaWrappable::operator=(static_cast<const LuaWrappable&>(other));
+		 // Do *not* assign reference counter
+		return *this;
+	}
 
 	inline void IncRefCount() const { m_refCount++; }
 	inline void DecRefCount() const { assert(m_refCount > 0); if (! --m_refCount) delete this; }
