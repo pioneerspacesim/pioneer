1f435d5c76ecbb482a398f612e2b1bcf06094260
Robert Norris <rob@eatenbyagrue.org>
2014-02-10 16:32:15 +1100

    rename trigger flag and invert in most places. it was confusing and buggy before. fixes #2737

--- src/ui/Widget.h
+++ src/ui/Widget.h
@@ -338,31 +338,31 @@ private:
 
 	// event triggers. when called:
 	//  - calls the corresponding Handle* method on this widget (always)
-	//  - fires the corresponding on* signal on this widget (iff emit is true)
-	//  - calls the container Trigger* method with the new emit value returned
+	//  - fires the corresponding on* signal on this widget (iff handled is false)
+	//  - calls the container Trigger* method with the new handled value returned
 	//    by the on* signal
 	//
 	// what this means in practic is that Handle* will be called for every
 	// widget from here to the root, whereas signals will only be fired as
 	// long as the signals continue to return false (unhandled).
-	bool TriggerKeyDown(const KeyboardEvent &event, bool emit = true);
-	bool TriggerKeyUp(const KeyboardEvent &event, bool emit = true);
-	bool TriggerTextInput(const TextInputEvent &event, bool emit = true);
-	bool TriggerMouseDown(const MouseButtonEvent &event, bool emit = true);
-	bool TriggerMouseUp(const MouseButtonEvent &event, bool emit = true);
-	bool TriggerMouseMove(const MouseMotionEvent &event, bool emit = true);
-	bool TriggerMouseWheel(const MouseWheelEvent &event, bool emit = true);
+	bool TriggerKeyDown(const KeyboardEvent &event, bool handled = false);
+	bool TriggerKeyUp(const KeyboardEvent &event, bool handled = false);
+	bool TriggerTextInput(const TextInputEvent &event, bool handled = false);
+	bool TriggerMouseDown(const MouseButtonEvent &event, bool handled = false);
+	bool TriggerMouseUp(const MouseButtonEvent &event, bool handled = false);
+	bool TriggerMouseMove(const MouseMotionEvent &event, bool handled = false);
+	bool TriggerMouseWheel(const MouseWheelEvent &event, bool handled = false);
 
-	bool TriggerJoystickButtonDown(const JoystickButtonEvent &event, bool emit = true);
-	bool TriggerJoystickButtonUp(const JoystickButtonEvent &event, bool emit = true);
-	bool TriggerJoystickAxisMove(const JoystickAxisMotionEvent &event, bool emit = true);
-	bool TriggerJoystickHatMove(const JoystickHatMotionEvent &event, bool emit = true);
+	bool TriggerJoystickButtonDown(const JoystickButtonEvent &event, bool handled = false);
+	bool TriggerJoystickButtonUp(const JoystickButtonEvent &event, bool handled = false);
+	bool TriggerJoystickAxisMove(const JoystickAxisMotionEvent &event, bool handled = false);
+	bool TriggerJoystickHatMove(const JoystickHatMotionEvent &event, bool handled = false);
 
-	bool TriggerClick(bool emit = true);
+	bool TriggerClick(bool handled = false);
 
 	// stop is used during disable/enable to stop delivery at the given widget
-	bool TriggerMouseOver(const Point &pos, bool emit = true, Widget *stop = 0);
-	bool TriggerMouseOut(const Point &pos, bool emit = true, Widget *stop = 0);
+	bool TriggerMouseOver(const Point &pos, bool handled = false, Widget *stop = 0);
+	bool TriggerMouseOut(const Point &pos, bool handled = false, Widget *stop = 0);
 
 	void TriggerMouseActivate();
 	void TriggerMouseDeactivate();
