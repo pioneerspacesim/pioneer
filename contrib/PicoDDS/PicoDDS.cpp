015477b7569e9cd2597215cd91893be83a7a74e4
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-02-16 19:33:21 +0000

    Exempt PicoDDS from the std::max/min abolition.

--- contrib/PicoDDS/PicoDDS.cpp
+++ contrib/PicoDDS/PicoDDS.cpp
@@ -128,9 +128,9 @@ int DDSImage::CalculateStoreageSize() const
 		for (int m=0; m<imgdata_.numMipMaps; ++m)
 		{
 			size+=GetMipLevelSize(width, height, depth, imgdata_.format);
-			width = Max(width>>1, 1);
-			height = Max(height>>1, 1);
-			depth = Max(depth>>1, 1);
+			width = std::max(width>>1, 1);
+			height = std::max(height>>1, 1);
+			depth = std::max(depth>>1, 1);
 		}
 	}
 
