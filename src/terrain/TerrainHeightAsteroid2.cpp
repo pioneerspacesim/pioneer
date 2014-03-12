7ac5b725d228ed1ee7b33a0e5e561c5f020162be
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-03-12 21:08:38 +0000

    Seed the fracDef and then use more noise to produce better results.

--- src/terrain/TerrainHeightAsteroid2.cpp
+++ src/terrain/TerrainHeightAsteroid2.cpp
@@ -16,13 +16,19 @@ const char *TerrainHeightFractal<TerrainHeightAsteroid2>::GetHeightFractalName()
 template <>
 TerrainHeightFractal<TerrainHeightAsteroid2>::TerrainHeightFractal(const SystemBody *body) : Terrain(body)
 {
+	SetFracDef(0, m_maxHeightInMeters*0.05, 1e6, 10000.0*m_fracmult);
+	const double height = m_maxHeightInMeters*0.3;
+	SetFracDef(1, height, m_rand.Double(4.0, 20.0)*height);
+	SetFracDef(2, m_maxHeightInMeters, m_rand.Double(50.0, 100.0)*m_maxHeightInMeters);
+	SetFracDef(3, m_maxHeightInMeters*0.07, 1e6, 100.0*m_fracmult);
+	SetFracDef(4, m_maxHeightInMeters*0.05, 8e5, 100.0*m_fracmult);
 }
 
 template <>
 double TerrainHeightFractal<TerrainHeightAsteroid2>::GetHeight(const vector3d &p) const
 {
-	double n = voronoiscam_octavenoise(6, 0.2*octavenoise(2, 0.3, 3.7, p), 15.0*octavenoise(2, 0.5, 4.0, p), p) *
-		0.75*ridged_octavenoise(16*octavenoise(2, 0.275, 3.2, p), 0.4*ridged_octavenoise(4, 0.4, 3.0, p), 4.0*octavenoise(3, 0.35, 3.7, p), p);
+	const double n = voronoiscam_octavenoise(6, 0.2 * octavenoise(GetFracDef(0), 0.3, p), 15.0 * octavenoise(GetFracDef(1), 0.5, p), p) *
+		0.75 * ridged_octavenoise(16.0 * octavenoise(GetFracDef(2), 0.275, p), 0.4 * ridged_octavenoise(GetFracDef(3), 0.4, p), 4.0 * octavenoise(GetFracDef(4), 0.35, p), p);
 
-	return (n > 0.0? m_maxHeight*n : 0.0);
+	return (n > 0.0 ? m_maxHeight*n : 0.0);
 }
