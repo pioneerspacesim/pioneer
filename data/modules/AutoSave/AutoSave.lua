ea08934bdedada1c0ec696face07d776d165fd92
John Bartholomew <jpa.bartholomew@gmail.com>
2014-03-20 00:39:29 +0000

    autosave: name files _autosave1 to _autosave9 (and _exit on game end)

--- data/modules/AutoSave/AutoSave.lua
+++ data/modules/AutoSave/AutoSave.lua
@@ -1,19 +1,19 @@
 local Game = import("Game")
 local Event = import("Event")
 
-local function Saver(savename)
-	return function (ship)
-		if ship:IsPlayer() then
-			Game.SaveGame(savename)
-		end
+local next_autosave = 1
+
+local function AutoSave()
+	Game.SaveGame('_autosave' .. next_autosave)
+	next_autosave = next_autosave + 1
+	if next_autosave > 9 then
+		next_autosave = 1
 	end
 end
 
-local SaveDocked = Saver('_docked')
-local SaveUndocked = Saver('_undocked')
-
-Event.Register('onShipDocked', SaveDocked)
-Event.Register('onShipLanded', SaveDocked)
-Event.Register('onShipUndocked', SaveUndocked)
-Event.Register('onShipTakeOff', SaveUndocked)
+local f = function (ship) if ship:IsPlayer() then AutoSave(); end; end
+Event.Register('onShipDocked', f)
+Event.Register('onShipLanded', f)
+Event.Register('onShipUndocked', f)
+Event.Register('onShipTakeOff', f)
 Event.Register('onGameEnd', function() Game.SaveGame('_exit'); end)
