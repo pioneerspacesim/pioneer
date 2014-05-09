e183e4752d23adb894dbbf466f54e95d3aa8b6cf
Karl F <karlfogel@gmail.com>
2014-05-09 18:55:02 +0200

    Added printing of crime tracking under Police tab

--- data/ui/StationView/Police.lua
+++ data/ui/StationView/Police.lua
@@ -8,6 +8,7 @@ local Rand = import("Rand")
 local Character = import("Character")
 local Format = import("Format")
 local utils = import("utils")
+local Legal = import("Legal")
 
 local InfoFace = import("ui/InfoFace")
 local SmallLabeledButton = import("ui/SmallLabeledButton")
@@ -16,14 +17,9 @@ local MessageBox = import("ui/MessageBox")
 local ui = Engine.ui
 local l = Lang.GetResource("ui-core")
 
-local crimeStrings = {
-	TRADING_ILLEGAL_GOODS = l.TRADING_ILLEGAL_GOODS,
-	WEAPON_DISCHARGE      = l.UNLAWFUL_WEAPONS_DISCHARGE,
-	PIRACY                = l.PIRACY,
-	MURDER                = l.MURDER,
-}
 
 local police = function (tab)
+
 	local station = Game.player:GetDockedWith()
 
 	local rand = Rand.New(util.hash_random(station.seed .. '-police', 2^31-1) - 1)
@@ -34,12 +30,22 @@ local police = function (tab)
 
 	local crimes, fine = Game.player:GetCrime()
 
+	local tmp_table = {}
+	for k,v in pairs(crimes) do
+		table.insert(tmp_table,k)
+	end
+
+	local crimeStat = function (k,v)
+		local s = crimes[v].count.."\t"..Legal.CrimeType[v].name
+		return k, s
+	end
+
 	local infoBox = ui:VBox(10)
-	if #crimes > 0 then
+	if #utils.build_array(pairs(crimes)) > 0 then
 		infoBox:PackEnd({
 			ui:Label(l.CRIMINAL_RECORD):SetFont("HEADING_LARGE"),
 			ui:VBox():PackEnd(
-				utils.build_table(utils.map(function (k,v) return k,crimeStrings[v] end, pairs(crimes)))
+				utils.build_table(utils.map(crimeStat, pairs(tmp_table)))
 			),
 		})
 	end
