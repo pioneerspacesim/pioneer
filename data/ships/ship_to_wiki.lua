348e9bf6769310fe865d8d14eea49e02ced65743
Florian Schmidt <florian.schmidt@lhsystems.com>
2014-05-31 22:06:02 +0200

    showing all thrusts and the effective exhaust velocity in G for empty and full now

--- data/ships/ship_to_wiki.lua
+++ data/ships/ship_to_wiki.lua
@@ -1,9 +1,5 @@
--- TODO:
--- - check about gunmounts
--- - check about max_engine/Hyperdrive mount = yes
--- - check exhaust velocity
--- - wiki all ships table
--- - update ships in wiki
+-- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of CC-BY-SA 3.0. See licenses/CC-BY-SA-3.0.txt
 
 round = function(number, significand)
 	last_number = number * 10^(significand+1)
@@ -32,6 +28,7 @@ thrust_full = function(thrust, hull_mass, fuel_tank_mass, capacity)
 end
 
 define_ship = function(shipTable)
+	-- calculate thrust values
 	forward_thrust_empty = round(thrust_empty(shipTable.forward_thrust, shipTable.hull_mass, shipTable.fuel_tank_mass), 1)
 	forward_thrust_full = round(thrust_full(shipTable.forward_thrust, shipTable.hull_mass, shipTable.fuel_tank_mass, shipTable.capacity), 1)
 	reverse_thrust_empty = round(thrust_empty(shipTable.reverse_thrust, shipTable.hull_mass, shipTable.fuel_tank_mass), 1)
@@ -47,18 +44,31 @@ define_ship = function(shipTable)
 	angular_thrust_empty = round(thrust_empty(shipTable.angular_thrust, shipTable.hull_mass, shipTable.fuel_tank_mass), 1)
 	angular_thrust_full = round(thrust_full(shipTable.angular_thrust, shipTable.hull_mass, shipTable.fuel_tank_mass, shipTable.capacity), 1)
 
+	-- check for hyperdrive
+	if shipTable.max_engine == nil or shipTable.max_engine == 1 then
+		max_engine = "yes"
+	else
+		max_engine = "no"
+	end
+
+	-- calculate effective exhaust velocity
+	effective_exhaust_velocity_empty = shipTable.effective_exhaust_velocity * math.log((shipTable.hull_mass + shipTable.fuel_tank_mass) / shipTable.hull_mass)
+	effective_exhaust_velocity_full = shipTable.effective_exhaust_velocity * math.log((shipTable.hull_mass + shipTable.fuel_tank_mass + shipTable.capacity) / (shipTable.hull_mass + shipTable.capacity))
+	effective_exhaust_velocity_empty = round(effective_exhaust_velocity_empty, -3) / 1000
+	effective_exhaust_velocity_full = round(effective_exhaust_velocity_full, -3) / 1000
+
+	-- print wiki syntax
 	print('{{Infobox_Ship')
 	print("|name = "..shipTable.name)
 	print("|type = "..shipTable.ship_class)
 	print("|manufacturer = "..shipTable.manufacturer)
-	print("|forward_thrust = "..forward_thrust_empty.."G ("..forward_thrust_full.."G full)")
-	print("|reverse_thrust = "..reverse_thrust_empty.."G ("..reverse_thrust_full.."G full)")
-	print("|up_thrust = "..up_thrust_empty.."G ("..up_thrust_full.."G full)")
-	print("|down_thrust = "..down_thrust_empty.."G ("..down_thrust_full.."G full)")
-	print("|left_thrust = "..left_thrust_empty.."G ("..left_thrust_full.."G full)")
-	print("|right_thrust = "..right_thrust_empty.."G ("..right_thrust_full.."G full)")
-	print("|angular_thrust = "..angular_thrust_empty.."G ("..angular_thrust_full.."G full)")
-	print("|gun_mounts = ")
+	print("|forward_thrust = empty: "..forward_thrust_empty.."G full: "..forward_thrust_full.."G")
+	print("|reverse_thrust = empty: "..reverse_thrust_empty.."G full: "..reverse_thrust_full.."G")
+	print("|up_thrust = empty: "..up_thrust_empty.."G full: "..up_thrust_full.."G")
+	print("|down_thrust = empty: "..down_thrust_empty.."G full: "..down_thrust_full.."G")
+	print("|left_thrust = empty: "..left_thrust_empty.."G full: "..left_thrust_full.."G")
+	print("|right_thrust = empty: "..right_thrust_empty.."G full: "..right_thrust_full.."G")
+	print("|angular_thrust = empty: "..angular_thrust_empty.."G full: "..angular_thrust_full.."G")
 	print("|max_cargo = "..shipTable.max_cargo)
 	print("|max_laser = 1"..shipTable.max_laser)
 	print("|max_missile = "..shipTable.max_missile)
@@ -69,10 +79,10 @@ define_ship = function(shipTable)
 	print("|capacity = "..shipTable.capacity)
 	print("|hull_mass = "..shipTable.hull_mass)
 	print("|fuel_tank_mass = "..shipTable.fuel_tank_mass)
-	print("|effective_exhaust_velocity = "..shipTable.effective_exhaust_velocity)
+	print("|effective_exhaust_velocity = empty: "..effective_exhaust_velocity_empty.."km/s full: "..effective_exhaust_velocity_full.."km/s")
 	print("|price = "..shipTable.price)
 	print("|hyperdrive_class = "..shipTable.hyperdrive_class)
-	print("|max_engine = ")
+	print("|max_engine = "..max_engine)
 	print("}}")
 end
 
