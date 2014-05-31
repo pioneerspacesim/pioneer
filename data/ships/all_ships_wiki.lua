37f12fe50434245e1657335adb30cec65a7efba9
Florian Schmidt <florian.schmidt@lhsystems.com>
2014-05-31 23:25:33 +0200

    added some more attributes to ship chart

--- data/ships/all_ships_wiki.lua
+++ data/ships/all_ships_wiki.lua
@@ -5,10 +5,16 @@ require("wiki_commons")
 
 define_ship = function(ship_table)
 	forward_thrust_empty = round(thrust_empty(ship_table.forward_thrust, ship_table.hull_mass, ship_table.fuel_tank_mass), 1)
+	angular_thrust_empty = round(thrust_empty(ship_table.angular_thrust, ship_table.hull_mass, ship_table.fuel_tank_mass), 1)
 
 	print("|-")
 	print("|[["..ship_table.name.."]]")
+	print("|"..ship_table.max_laser)
+	print("|"..ship_table.max_missile)
+	print("|"..ship_table.max_cargo)
 	print("|"..forward_thrust_empty)
+	print("|"..angular_thrust_empty)
+	print("|"..ship_table.price)
 end
 
 -- ship files
@@ -17,9 +23,13 @@ table.sort(ships)
 
 -- print wiki intro/heading
 print('{| class="wikitable sortable"')
-print("|+Ships Comparison")
 print("! Name")
+print("! max Laser")
+print("! max Missile")
+print("! max Cargo")
 print("! Forward Thrust")
+print("! Angular Thrust")
+print("! Price")
 
 -- for each print stats
 for i = 1, #ships, 1 do
