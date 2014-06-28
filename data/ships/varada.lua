60797f4b0c2311e30a1f9f1c11dc57cbb1932bb1
Szlrd <nozmajner@vipmail.hu>
2014-06-28 15:14:40 +0200

    Adding Varada light shuttle

---
+++ data/ships/varada.lua
@@ -0,0 +1,32 @@
+-- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of CC-BY-SA 3.0. See licenses/CC-BY-SA-3.0.txt
+
+define_ship {
+	name='Varada',
+	ship_class='light_courier',
+	manufacturer='mandarava_csepel',
+	model='varada',
+	forward_thrust = 600e3,
+	reverse_thrust = 200e3,
+	up_thrust = 120e3,
+	down_thrust = 120e3,
+	left_thrust = 240e3,
+	right_thrust = 240e3,
+	angular_thrust = 45e5,
+	
+	hull_mass = 2,
+	fuel_tank_mass = 3,
+	capacity = 5,
+	max_cargo = 5,
+	max_laser = 1,
+	max_missile = 0,
+	max_cargoscoop = 0,
+	max_fuelscoop = 0,
+	min_crew = 1,
+	max_crew = 1,
+	-- Exhaust velocity Vc [m/s] is equivalent of engine efficiency and depend on used technology. Higher Vc means lower fuel consumption.
+	-- Smaller ships built for speed often mount engines with higher Vc. Another way to make faster ship is to increase fuel_tank_mass.
+	effective_exhaust_velocity = 140e5,
+	price = 123e3,
+	hyperdrive_class = 0,
+}
