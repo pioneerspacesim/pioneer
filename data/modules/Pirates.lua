20c638bdd1f3fb2a51e73e213c82355f21b69c19
Karl F <karlfogel@gmail.com>
2014-06-29 12:23:21 +0200

    Fix default drive 0 in Pirate.lua

--- data/modules/Pirates.lua
+++ data/modules/Pirates.lua
@@ -26,14 +26,15 @@ local onEnterSystem = function (player)
 		max_pirates = max_pirates-1
 
 		local shipdef = shipdefs[Engine.rand:Integer(1,#shipdefs)]
-		local default_drive = Equipment.hyperspace['drive_class'..tostring(shipdef.hyperdriveClass)]
+		local drive_class = shipdef.hyperdriveClass
+		local default_drive = drive_class > 0 and Equipment.hyperspace['drive_class'..tostring(drive_class)]
 
 		-- select a laser. this is naive - it simply chooses at random from
 		-- the set of lasers that will fit, but never more than one above the
 		-- player's current weapon.
 		-- XXX this should use external factors (eg lawlessness) and not be
 		-- dependent on the player in any way
-		local max_laser_size = shipdef.capacity - default_drive.capabilities.mass
+		local max_laser_size = shipdef.capacity - (drive_class > 0 and default_drive.capabilities.mass or 0)
 		local laserdefs = utils.build_array(utils.filter(
 			function (k,l) return l:IsValidSlot('laser_front') and l.capabilities.mass <= max_laser_size and string.sub(k.id,0,11) == 'pulsecannon' end,
 			pairs(Equipment.laser)
@@ -42,7 +43,9 @@ local onEnterSystem = function (player)
 
 		local ship = Space.SpawnShip(shipdef.id, 8, 12)
 		ship:SetLabel(Ship.MakeRandomLabel())
-		ship:AddEquip(default_drive)
+		if drive_class > 0 then
+			ship:AddEquip(default_drive)
+		end
 		ship:AddEquip(laserdef)
 
 		local playerCargoCapacity = ShipDef[player.shipId].capacity
