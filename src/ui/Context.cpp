72167ff9556b84cf3f56134031493aa4e7068e3f
Simon Chopin <chopin.simon@gmail.com>
2014-07-01 21:15:45 +0200

    Avoid some Lua stack leaks in the UI code.
    (Closes #3000)
    (Closes #3001)
    (Closes #2999)
    (Closes #2997)
    (Closes #2996)

--- src/ui/Context.cpp
+++ src/ui/Context.cpp
@@ -167,23 +167,12 @@ void Context::Draw()
 
 Widget *Context::CallTemplate(const char *name, const LuaTable &args)
 {
-	lua_State *l = m_lua->GetLuaState();
-
-	m_templateStore.PushCopyToStack();
-	const LuaTable t(l, -1);
-	if (!t.Get<bool,const char *>(name))
-		return 0;
-
-	t.PushValueToStack<const char*>(name);
-	lua_pushvalue(l, args.GetIndex());
-	pi_lua_protected_call(m_lua->GetLuaState(), 1, 1);
-
-	return LuaObject<UI::Widget>::CheckFromLua(-1);
+	return ScopedTable(m_templateStore).Call<UI::Widget *>(name, args);
 }
 
 Widget *Context::CallTemplate(const char *name)
 {
-	return CallTemplate(name, LuaTable(m_lua->GetLuaState()));
+	return CallTemplate(name, ScopedTable(m_lua->GetLuaState()));
 }
 
 void Context::DrawWidget(Widget *w)
