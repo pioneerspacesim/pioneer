061981070f78bdacab742de5d2750493baaeab52
Karl F <karlfogel@gmail.com>
2014-07-01 09:38:02 +0200

    fixes #2998, pirates getting nil hyperdrive

--- data/modules/Pirates.lua
+++ data/modules/Pirates.lua
@@ -26,26 +26,24 @@ local onEnterSystem = function (player)
 		max_pirates = max_pirates-1
 
 		local shipdef = shipdefs[Engine.rand:Integer(1,#shipdefs)]
-		local drive_class = shipdef.hyperdriveClass
-		local default_drive = drive_class > 0 and Equipment.hyperspace['drive_class'..tostring(drive_class)]
+		local default_drive = Equipment.hyperspace['hyperdrive_'..tostring(shipdef.hyperdriveClass)]
+		assert(default_drive)  -- never be nil.
 
 		-- select a laser. this is naive - it simply chooses at random from
 		-- the set of lasers that will fit, but never more than one above the
 		-- player's current weapon.
 		-- XXX this should use external factors (eg lawlessness) and not be
 		-- dependent on the player in any way
-		local max_laser_size = shipdef.capacity - (drive_class > 0 and default_drive.capabilities.mass or 0)
+		local max_laser_size = shipdef.capacity - default_drive.capabilities.mass
 		local laserdefs = utils.build_array(utils.filter(
-			function (k,l) return l:IsValidSlot('laser_front') and l.capabilities.mass <= max_laser_size and string.sub(k.id,0,11) == 'pulsecannon' end,
+			function (k,l) return l:IsValidSlot('laser_front') and l.capabilities.mass <= max_laser_size and l.l10n_key:find("PULSECANNON") end,
 			pairs(Equipment.laser)
 		))
 		local laserdef = laserdefs[Engine.rand:Integer(1,#laserdefs)]
 
 		local ship = Space.SpawnShip(shipdef.id, 8, 12)
 		ship:SetLabel(Ship.MakeRandomLabel())
-		if drive_class > 0 then
-			ship:AddEquip(default_drive)
-		end
+		ship:AddEquip(default_drive)
 		ship:AddEquip(laserdef)
 
 		local playerCargoCapacity = ShipDef[player.shipId].capacity
