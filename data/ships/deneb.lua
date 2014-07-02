199d9d58b5cc15f63dc52520cab44bdfc2b5cec5
Alan Briggs <git@zordey.com>
2014-07-02 15:48:52 +0100

    Facelift to the ALBR corp ships

--- data/ships/deneb.lua
+++ data/ships/deneb.lua
@@ -1,4 +1,4 @@
--- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+﻿-- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of CC-BY-SA 3.0. See licenses/CC-BY-SA-3.0.txt
 
 define_ship {
@@ -6,21 +6,21 @@ define_ship {
 	ship_class='medium_freighter',
 	manufacturer='albr',
 	model='deneb',
-	forward_thrust = 10e6,
-	reverse_thrust = 6e6,
-	up_thrust = 6e6,
-	down_thrust = 3e6,
-	left_thrust = 3e6,
-	right_thrust = 3e6,
-	angular_thrust = 50e6,
+	forward_thrust = 5e7,
+	reverse_thrust = 3e7,
+	up_thrust = 4e7,
+	down_thrust = 1e7,
+	left_thrust = 1e7,
+	right_thrust = 1e7,
+	angular_thrust = 50e7,
 	atmoshield = 1,
 	min_atmoshield = 1,
-
-	hull_mass = 75,
-	fuel_tank_mass = 100,
-	capacity = 235,
+	
+	hull_mass = 175,
+	fuel_tank_mass = 225,
+	capacity = 1400,
 	slots = {
-		cargo = 235,
+		cargo = 1400,
 		laser_front = 1,
 		missile = 8,
 		cargo_scoop = 1,
@@ -28,8 +28,7 @@ define_ship {
 	},
 	min_crew = 2,
 	max_crew = 3,
-	--thruster_fuel_use = 0.0001,
-	effective_exhaust_velocity = 186e5,
-	price = 5096e2,
+	effective_exhaust_velocity = 51784e3,
+	price = 4300e3,
 	hyperdrive_class = 3,
 }
--- data/ships/venturestar.lua
+++ data/ships/venturestar.lua
@@ -1,4 +1,4 @@
--- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+﻿-- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of CC-BY-SA 3.0. See licenses/CC-BY-SA-3.0.txt
 
 define_ship {
@@ -6,33 +6,30 @@ define_ship {
 	ship_class='medium_freighter',
 	manufacturer='albr',
 	model='venturestar',
-	forward_thrust = 35e6,
-	reverse_thrust = 115e5,
-	up_thrust = 15e6,
-	down_thrust = 8e6,
-	left_thrust = 8e6,
-	right_thrust = 8e6,
-	angular_thrust = 50e6,
-	camera_offset = v(0,0,-18),
-	gun_mounts =
-	{
-		{ v(0,-0.5,-10.7), v(0,0,-1), 5, 'HORIZONTAL' },
-	},
-	hull_mass = 200,
-	fuel_tank_mass = 200,
-	capacity = 500,
+	forward_thrust = 10e7,
+	reverse_thrust = 50e6,
+	up_thrust = 50e6,
+	down_thrust = 16e6,
+	left_thrust = 16e6,
+	right_thrust = 16e6,
+	angular_thrust = 10e7,
+
+	hull_mass = 900,
+	fuel_tank_mass = 500,
+	capacity = 3000,
 	slots = {
-		cargo = 500,
+		cargo = 3000,
 		laser_front = 1,
-		missile = 16,
+		missile = 12,
 		cargo_scoop = 1,
 		fuel_scoop = 1,
 		atmo_shield = 1,
 	},
-	min_crew = 3,
+	
+	min_crew = 2,
 	max_crew = 5,
-
-	effective_exhaust_velocity = 2548e4,
-	price = 1346e3,
-	hyperdrive_class = 4,
+	
+	effective_exhaust_velocity = 81784e3,
+	price = 5800e3,
+	hyperdrive_class = 9,
 }
