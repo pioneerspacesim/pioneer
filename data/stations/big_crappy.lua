25c0140943116a548689f148d168c89fe4265ae7
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-07-20 15:19:46 +0100

    Update the data used by the models to match the new docking system.

--- data/stations/big_crappy.lua
+++ data/stations/big_crappy.lua
@@ -6,12 +6,5 @@ define_orbital_station {
 	angular_velocity = 0.1,
 	parking_distance = 5000.0,
 	parking_gap_size = 500.0,
-	ship_launch_stage = 3,
-	-- define groups of bays, in this case 1 group with 1 bay.
-	-- params are = {minSize, maxSize, {list,of,bay,numbers}}
-	bay_groups = {
-		{0, 500, {1,2,3,4}},
-	},
-	dock_anim_stage_duration = { 300, 10.0, 5.0, 5.0 },
-	undock_anim_stage_duration = { 5.0, 5.0, 10.0 },
+	pad_offset = 150.0,
 }
