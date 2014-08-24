b31925df776267fbeb09d03e4f0c33a35c183c1c
John Bartholomew <jpa.bartholomew@gmail.com>
2014-08-24 23:34:35 +0100

    autosave: pick the next autosave number based on the most recent autosave
    Untested, probably buggy, and probably more logically complex than it
    needs to be.

--- data/modules/AutoSave/AutoSave.lua
+++ data/modules/AutoSave/AutoSave.lua
@@ -1,10 +1,36 @@
 local Game = import("Game")
 local Event = import("Event")
+local FileSystem = import("FileSystem")
 
--- We deliberately retain the next_autosave state between games.
--- This is likely to break in the future, as the engine will probably
--- be changed to destroy the Lua context between games.
-local next_autosave = 1
+local max_autosaves = 9
+
+local function PickNextAutosave()
+	local ok, files, _ = pcall(FileSystem.ReadDirectory, 'USER', 'savefiles')
+	if not ok then
+		print('Error picking auto-save')
+		return '_autosave1'
+	end
+
+	-- find the most recent autosave
+	local next_save_number = 1
+	local best_tstamp
+	for i = 1, #files do
+		local f = files[i]
+		local num = string.match(f.name, '^_autosave(%d+)$')
+		if num ~= nil then
+			local tstamp = f.mtime.timestamp
+			if best_tstamp == nil or tstamp > best_tstamp then
+				best_tstamp = tstamp
+				next_save_number = tonumber(num) + 1
+				if next_save_number > max_autosaves then
+					next_save_number = 1
+				end
+			end
+		end
+	end
+
+	return '_autosave' .. next_save_number
+end
 
 local function CheckedSave(filename)
 	local ok, err = pcall(Game.SaveGame, filename)
@@ -14,15 +40,7 @@ local function CheckedSave(filename)
 	end
 end
 
-local function AutoSave()
-	CheckedSave('_autosave' .. next_autosave)
-	next_autosave = next_autosave + 1
-	if next_autosave > 9 then
-		next_autosave = 1
-	end
-end
-
-local f = function (ship) if ship:IsPlayer() then AutoSave(); end; end
+local f = function (ship) if ship:IsPlayer() then CheckedSave(PickNextAutosave()); end; end
 Event.Register('onShipDocked', f)
 Event.Register('onShipLanded', f)
 Event.Register('onShipUndocked', f)
