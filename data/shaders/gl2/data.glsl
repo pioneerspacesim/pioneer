cf3df44fb5517d7b3636525e94a1d78aa663b3c2
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-09-18 20:09:08 +0100

    New common declaration shader file for attributes.

---
+++ data/shaders/gl2/data.glsl
@@ -0,0 +1,9 @@
+// Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#ifdef VERTEX_SHADER
+attribute vec4 a_vertex;
+attribute vec3 a_normal;
+attribute vec4 a_color;
+attribute vec2 a_uv0;
+#endif
