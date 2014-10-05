777d5378fa5b6947870436d09c0f6e95377b4f47
Karl F <karlfogel@gmail.com>
2014-10-05 11:26:58 +0200

    Update econTrade if just bought trade analyzer

--- src/SystemInfoView.cpp
+++ src/SystemInfoView.cpp
@@ -24,6 +24,9 @@ SystemInfoView::SystemInfoView(Game* game) : UIView(), m_game(game)
 	SetTransparency(true);
 	m_refresh = REFRESH_NONE;
 	m_unexplored = true;
+	int trade_analyzer = 0;
+	Pi::player->Properties().Get("trade_analyzer_cap", trade_analyzer);
+	m_hasTradeAnalyzer = bool(trade_analyzer);
 }
 
 void SystemInfoView::OnBodySelected(SystemBody *b)
@@ -151,13 +154,16 @@ void SystemInfoView::UpdateEconomyTab()
 	/* imports and exports */
 	const RefCountedPtr<StarSystem> hs = m_game->GetSpace()->GetStarSystem();
 
-	// check if trade analyser is installed
-	int trade_analyser = 0;
-	Pi::player->Properties().Get("trade_analyzer_cap", trade_analyser);
+	// check if trade analyzer is installed
+	int trade_analyzer = 0;
+	Pi::player->Properties().Get("trade_analyzer_cap", trade_analyzer);
+
+	// we might be here because we changed equipment, update that as well:
+	m_hasTradeAnalyzer = bool (trade_analyzer);
 
 	// If current system is defined and not equal to selected we will compare them
 	const bool compareSelectedWithCurrent =
-		(hs && !m_system->GetPath().IsSameSystem(hs->GetPath()) && trade_analyser > 0);
+		(hs && !m_system->GetPath().IsSameSystem(hs->GetPath()) && trade_analyzer > 0);
 
 	const std::string meh       = "#999";
 	const std::string ok        = "#fff";
@@ -514,6 +520,12 @@ SystemInfoView::RefreshType SystemInfoView::NeedsRefresh()
 		return REFRESH_ALL;
 
 	if (m_system->GetUnexplored() != m_unexplored)
+		return REFRESH_ALL;
+
+	// If we changed equipment since last refresh
+	int trade_analyzer = 0;
+	Pi::player->Properties().Get("trade_analyzer_cap", trade_analyzer);
+	if (m_hasTradeAnalyzer != trade_analyzer)
 		return REFRESH_ALL;
 
 	if (m_system->GetUnexplored())
