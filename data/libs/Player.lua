962e00829d6c1d353ee6765aa6f0f28205cebb38
Karl F <karlfogel@gmail.com>
2014-11-05 15:49:38 +0100

    Player.lua:AddCrime,GetCrime added on Lua side and a crimerecord class for storing dirty deeds.

--- data/libs/Player.lua
+++ data/libs/Player.lua
@@ -5,6 +5,160 @@ local Player = import_core("Player")
 local Serializer = import("Serializer")
 local Event = import("Event")
 local Game = import("Game")
+local utils = import("utils")
+local Legal = import("Legal")
+
+Player.record = {}
+
+local CrimeRecord = {} -- the table representing the class, which will double as the metatable for the instances
+CrimeRecord.__index = CrimeRecord -- failed table lookups on the instances should fallback to the class table, to get methods
+
+function CrimeRecord.New()
+  local self = setmetatable({}, CrimeRecord)
+  self.fine = 0
+  self.crimetype = {}
+  return self
+end
+
+
+function CrimeRecord:Add(crime, fine)
+
+	if not fine then
+		fine = Legal.CrimeType[crime].basefine
+	end
+
+	-- if first time for this crime type
+	if not self.crimetype[crime] then
+		self.crimetype[crime] = {}
+		self.crimetype[crime].count = 0
+	end
+
+	self.crimetype[crime].count = self.crimetype[crime].count +1
+	self.fine = self.fine + fine
+	return self.fine
+end
+
+
+function CrimeRecord:SetFine(fine)
+	self.fine = fine
+	return self.fine
+end
+
+
+--
+-- Method: AddCrime
+--
+-- Add a crime to the player's criminal record
+--
+-- > Game.player:AddCrime(crime, fine, faction)
+--
+-- Parameters:
+--
+--   crime - a string constant describing the crime
+--
+--   fine - an amount to add to the player's fine
+--
+--   faction - optional argument, defaults to the system the player is in
+--
+--
+-- Availability:
+--
+--   2015 August
+--
+-- Status:
+--
+--   experimental
+--
+function Player:AddCrime (crime, fine, faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+
+	-- first time for this faction
+	if not self.record[forFaction] then
+		self.record[forFaction] = CrimeRecord.New()
+	end
+
+	self.record[forFaction]:Add(crime, fine)
+
+	-- todo: could have systemPath as faction for independent?
+end
+
+--
+-- Method: GetCrime
+--
+-- Get criminal record and total fine for player for faction
+--
+-- > criminalrecord, fine = Game.player:GetCrime(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the system the player is in
+--
+-- Return:
+--
+--   criminalRecord - a table with count and fine for each crime committed
+--
+--   fine - the total fine of the player in faction
+--
+--
+-- Availability:
+--
+--   2015 August
+--
+-- Status:
+--
+--   experimental
+--
+function Player:GetCrime (faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+
+	-- return crime record and total fine for faction
+	local listOfCrime, fine
+
+	if Game.player.flightState == "HYPERSPACE" then
+		-- no crime in hyperspace
+		listOfCrime = {}
+		fine = 0
+	elseif not self.record[forFaction] then
+		-- first time for this faction, clean record
+		listOfCrime = {}
+		fine = 0
+	else
+		listOfCrime = self.record[forFaction].crimetype
+		fine = self.record[forFaction].fine
+	end
+
+	return listOfCrime, fine
+end
+
+
+
+--
+-- Method: ClearCrimeFine
+--
+-- Clear the crime record for player
+--
+-- > Game.player:ClearCrimeFine(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the system the player is in
+--
+--
+-- Availability:
+--
+--   2015 August
+--
+-- Status:
+--
+--   experimental
+--
+function Player:ClearCrimeFine (faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+
+	-- TODO: what about paying fine but keeping record?
+
+	self.record[forFaction] = nil
+end
 
 --
 -- Method: GetMoney
@@ -82,21 +236,38 @@ end
 
 local loaded_data
 
-Event.Register("onGameStart", function ()
+local onGameStart = function ()
 	if (loaded_data) then
-		Game.player:setprop("cash", loaded_data)
+		Game.player:setprop("cash", loaded_data.cash)
+		Player.record = loaded_data.record
 		loaded_data = nil
 	end
-end)
+end
 
-Serializer:Register("Player",
-	function ()
-		return Game.player.cash
-	end,
-	function (data)
-		loaded_data = data
-	end
-)
+local serialize = function ()
 
+	local data = {
+		cash = Game.player.cash,
+		record = Game.player.record,
+	}
+
+	return data
+end
+
+
+local unserialize = function (data)
+	loaded_data = data
+	Player.cash = data.cash
+	Player.record = data.record
+end
+
+local onGameEnd = function ()
+	-- clean up for next game:
+	Player.record = {}
+end
+
+Event.Register("onGameEnd", onGameEnd)
+Event.Register("onGameStart", onGameStart)
+Serializer:Register("Player", serialize, unserialize)
 
 return Player
