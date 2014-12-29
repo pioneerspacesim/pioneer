54f3e16d675998d9f3d9d186b13b51bb0a4143fd
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-12-29 15:54:01 +0000

    Cleanup indentation and change scope of AABB.

--- src/scenegraph/Loader.cpp
+++ src/scenegraph/Loader.cpp
@@ -797,7 +797,7 @@ void Loader::ConvertNodes(aiNode *node, Group *_parent, std::vector<RefCountedPt
 			m_model->AddTag(nodename, new MatrixTransform(m_renderer, m));
 		} else if (starts_with(nodename, "loc_")) {
 			m_model->AddTag(nodename, new MatrixTransform(m_renderer, m));
-		}else if (starts_with(nodename, "exit_")) {
+		} else if (starts_with(nodename, "exit_")) {
 			m_model->AddTag(nodename, new MatrixTransform(m_renderer, m));
 		}
 		return;
