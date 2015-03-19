06b22e7a9730d439570bbdda26b52434c8c1c2ee
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2015-03-19 08:44:29 +0000

    Remove the transposition of the normal matrix after we invert it. Already in the correct state!

--- src/graphics/opengl/MaterialGL.cpp
+++ src/graphics/opengl/MaterialGL.cpp
@@ -4,7 +4,6 @@
 #include "MaterialGL.h"
 #include "Program.h"
 #include "RendererGL.h"
-#include "MathUtil.h"
 
 namespace Graphics {
 namespace OGL {
@@ -19,17 +18,11 @@ void Material::Unapply()
 {
 }
 
-using namespace MathUtil;
 void Material::SetCommonUniforms(const matrix4x4f& mv, const matrix4x4f& proj)
 {
 	const matrix4x4f ViewProjection = proj * mv;
-	//const matrix3x3f orient(mv.GetOrient().Inverse());
-	//const matrix3x3f NormalMatrix(orient.Transpose());
-	const matrix3x3f mvo(mv.GetOrient());
-	const matrix3x3f NormalMatrix(Transpose(Inverse(mvo)));
-	
-	//const matrix3x3f mvo2(Transpose(Transpose(mvo)));
-	//assert(0==memcmp(&mvo, &mvo2, sizeof(matrix3x3f)));
+	const matrix3x3f orient(mv.GetOrient());
+	const matrix3x3f NormalMatrix(orient.Inverse());
 
 	m_program->uProjectionMatrix.Set( proj );
 	m_program->uViewMatrix.Set( mv );
