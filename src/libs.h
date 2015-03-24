4ee00126d556a1e93f1e83bad072a08ff37bcb2e
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2015-03-24 13:19:12 +0000

    Ask for a opengl 3.1 context again, but also add compatibility stuff to the glloadgen profile

--- src/libs.h
+++ src/libs.h
@@ -26,7 +26,7 @@
 #include <memory>
 
 // The glLoadGen header was generated using the following command line:
-// lua LoadGen.lua -style=pointer_c -spec=gl -version=3.1 -profile=core core_3_x -stdext=gl_ubiquitous.txt -stdext=gl_core_post_3_3.txt -ext ARB_seamless_cube_map ARB_seamless_cubemap_per_texture
+// lua LoadGen.lua -style=pointer_c -spec=gl -version=3.3 -profile=compatibility core_3_x -stdext=gl_ubiquitous.txt -stdext=gl_core_post_3_3.txt -ext ARB_seamless_cube_map ARB_seamless_cubemap_per_texture ARB_draw_instanced ARB_uniform_buffer_object ARB_instanced_arrays
 #include "graphics/opengl/gl_core_3_x.h"
 
 #ifdef _WIN32
