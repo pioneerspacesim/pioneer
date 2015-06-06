e226bf735845a8cb9d9a7b447668502d07bb5bd5
Preston Cammarata <prestotron7@gmail.com>
2015-06-06 14:55:59 -0400

    Add SpawnPirate() function

--- data/modules/Taxi/Taxi.lua
+++ data/modules/Taxi/Taxi.lua
@@ -331,32 +331,7 @@ local onEnterSystem = function (player)
 			local ship
 
 			while ships > 0 do
-				ships = ships-1
-
-				if Engine.rand:Number(1) <= risk then
-					local shipdef = shipdefs[Engine.rand:Integer(1,#shipdefs)]
-					local default_drive = eq.hyperspace['hyperdrive_'..tostring(shipdef.hyperdriveClass)]
-
-					local max_laser_size = shipdef.capacity - default_drive.capabilities.mass
-					local laserdefs = utils.build_array(utils.filter(
-						function (k,l) return l:IsValidSlot('laser_front') and l.capabilities.mass <= max_laser_size and l.l10n_key:find("PULSECANNON") end,
-						pairs(eq.laser)
-					))
-					local laserdef = laserdefs[Engine.rand:Integer(1,#laserdefs)]
-
-					ship = Space.SpawnShipNear(shipdef.id, Game.player, 50, 100)
-					ship:SetLabel(Ship.MakeRandomLabel())
-					ship:AddEquip(default_drive)
-					ship:AddEquip(laserdef)
-					ship:AddEquip(eq.misc.shield_generator, math.ceil(risk * 3))
-					if Engine.rand:Number(2) <= risk then
-						ship:AddEquip(eq.misc.laser_cooling_booster)
-					end
-					if Engine.rand:Number(3) <= risk then
-						ship:AddEquip(eq.misc.shield_energy_booster)
-					end
-					ship:AIKill(Game.player)
-				end
+				SpawnPirate(risk)
 			end
 
 			if ship then
