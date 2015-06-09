054ccae98cb73b8cf2d955f4c409ccbcaaa5d492
Preston Cammarata <prestotron7@gmail.com>
2015-06-09 15:33:17 -0400

    Update DeliverPackage.lua

--- data/modules/DeliverPackage/DeliverPackage.lua
+++ data/modules/DeliverPackage/DeliverPackage.lua
@@ -375,7 +375,7 @@ local onEnterSystem = function (player)
 
 				if Engine.rand:Number(1) <= risk then
 					local shipdef = shipdefs[Engine.rand:Integer(1,#shipdefs)]
-					SpawnPirate(risk)
+					SpawnPirate(risk, shipdefs)
 					ship:AIKill(Game.player)
 				end
 			end
