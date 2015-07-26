eec4e6e6df29de7024cce32ba206e6f78cfdba5c
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2015-07-26 11:40:25 +0100

    Save the config after changing the star density slider

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -383,6 +383,7 @@ static int l_engine_set_amount_stars(lua_State *l)
 {
 	const float amount = Clamp(luaL_checknumber(l, 1), 0.0, 1.0);
 	Pi::config->SetFloat("AmountOfBackgroundStars", amount);
+	Pi::config->Save();
 	Pi::SetAmountBackgroundStars(amount);
 	return 0;
 }
