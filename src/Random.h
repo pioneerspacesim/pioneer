72555f61e355689c9aab583bc2cf01423235ab22
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2015-10-07 22:28:25 +0100

    Use pcg32 typedef for Random class to provide higher quality random numbers.

--- src/Random.h
+++ src/Random.h
@@ -20,10 +20,13 @@ extern "C" {
 #include "jenkins/lookup3.h"
 }
 
+#define PCG_LITTLE_ENDIAN 1
+#include "pcg-cpp/pcg_random.hpp"
+
 // A deterministic random number generator
 class Random : public RefCounted
 {
-    Uint32 current;
+	pcg32 mPCG;
 
 	// For storing second rand from Normal
 	bool cached;
@@ -36,7 +39,7 @@ public:
 	//
 
 	// Construct a new random generator using the given seed
-	Random(Uint32 initialSeed=0)
+	Random(const Uint32 initialSeed = 0xabcd1234)
 	{
 		seed(initialSeed);
 	}
@@ -51,7 +54,7 @@ public:
 	// seeds.
 	Random(const Uint64* const seeds, size_t length)
 	{
-		seed( reinterpret_cast<const Uint32* const>(seeds), length*2);
+		seed(reinterpret_cast<const Uint32* const>(seeds), length * 2);
 	}
 
 	//
@@ -61,14 +64,15 @@ public:
 	// Seed the RNG using the hash of the given array of seeds.
 	void seed(const Uint32* const seeds, size_t length)
 	{
-		current = lookup3_hashword(seeds, length, 0);
+		const Uint32 hash = lookup3_hashword(seeds, length, 0);
+		mPCG.seed(hash);
 		cached = false;
 	}
 
 	// Seed using an array of 64-bit integers
 	void seed(const Uint64* const seeds, size_t length)
 	{
-		seed( reinterpret_cast<const Uint32* const>(seeds), length*2);
+		seed(reinterpret_cast<const Uint32* const>(seeds), length * 2);
 	}
 
 	// Seed using a single 32-bit integer
@@ -87,83 +91,79 @@ public:
 
 	// Get the next integer from the sequence
 	// interval [0, 2**32)
-	Uint32 Int32()
+	inline Uint32 Int32()
 	{
-		current ^= (current << 17);
-		current ^= (current >> 13);
-		current ^= (current << 5);
-		return current;
+		return mPCG();
 	}
 
 	// Pick an integer like you're rolling a "choices" sided die,
 	// a 6 sided die would return a number between 0 and 5.
 	// interval [0, choices)
-	Uint32 Int32(int choices)
+	inline Uint32 Int32(const int choices)
 	{
-		assert(choices > 0);
 		return Int32() % choices;
 	}
 
 	// Pick a number between min and max, inclusive.
 	// interval [min, max]
-	int Int32(int min, int max)
+	inline int Int32(const int min, const int max)
 	{
-		return (Int32()%(1+max-min))+min;
+		return (Int32() % (1 + max - min)) + min;
 	}
 
 	// Pick a number in the half open interval [0, 1)
-	double Double()
+	inline double Double()
 	{
 		return double(Int32()) * (1. / 4294967296.); // divided by 2^32
 	}
 
 	// Pick a number in the closed interval [0, 1]
-	double Double_closed()
+	inline double Double_closed()
 	{
 		return double(Int32()) * (1. / 4294967295.); // divided by 2^32 - 1
 	}
 
 	// Pick a number in the open interval (0, 1)
-	double Double_open()
+	inline double Double_open()
 	{
 		return (double(Int32()) + .5) * (1. / 4294967296.); // divided by 2^32
 	}
 
 	// Pick a 53-bit resolution double in the half open interval [0, 1)
 	// This method consumes two 32-bit numbers from the sequence.
-	double Double53()
+	inline double Double53()
 	{
 		return (double(Int32() >> 5) * 67108864. + double(Int32() >> 6)) * (1. / 9007199254740992.);
 	}
 
 	// Pick a number in the half-open interval [min, limit)
-	double Double(double min, double limit)
+	inline double Double(double min, double limit)
 	{
-		return Double(limit-min)+min;
+		return Double(limit - min) + min;
 	}
 
 	// Pick a number in the half-open interval [0, limit)
-	double Double(double limit)
+	inline double Double(double limit)
 	{
 		return limit*Double();
 	}
 
 	// Pick a number between 0 and max inclusive
 	// interval [0, max]
-	double Double_closed(double max)
+	inline double Double_closed(double max)
 	{
 		return max*Double_closed();
 	}
 
 	// Pick a number between min and max inclusive
 	// interval [min, max]
-	double Double_closed(double min, double max)
+	inline double Double_closed(double min, double max)
 	{
-		return Double_closed(max-min)+min;
+		return Double_closed(max - min) + min;
 	}
 
 	// interval [0,1)
-	double NDouble(int p)
+	inline double NDouble(int p)
 	{
 		double o = Double(1.0);
 		while (--p > 0)
@@ -172,19 +172,19 @@ public:
 	}
 
 	// Normal distribution with zero mean, and unit variance
-	double Normal()
+	inline double Normal()
 	{
-		return Normal(0.0,1.0);
+		return Normal(0.0, 1.0);
 	}
 
 	// Normal distribution with unit variance
-	double Normal(double mean)
+	inline double Normal(const double mean)
 	{
 		return Normal(mean, 1.0);
 	}
 
 	//Normal (Gauss) distribution
-	double Normal(double mean, double stddev)
+	inline double Normal(const double mean, const double stddev)
 	{
 		//https://en.wikipedia.org/wiki/Box-Muller_transform#Polar_form
 		double u, v, s, z0;
@@ -211,7 +211,7 @@ public:
 		return  mean + z0 * stddev;
 	}
 
-	int Poisson(double lambda)
+	inline int Poisson(const double lambda)
 	{
 		int k = 0;
 		double p = Double();
@@ -225,14 +225,13 @@ public:
 	}
 
 	// Pick a fixed-point integer half open interval [0,1)
-	fixed Fixed()
+	inline fixed Fixed()
 	{
-		assert(fixed::FRAC == 32);
 		return fixed(Int32());
 	}
 
 	// interval [0,1)
-	fixed NFixed(int p)
+	inline fixed NFixed(int p)
 	{
 		fixed o = Fixed();
 		while (--p > 0)
