f3861a7e7e3a6acf49520bc94d2b11ce92b62cc1
John Bartholomew <jpa.bartholomew@gmail.com>
2015-12-06 01:59:06 +0000

    add support for applying a scale&translate transform to UI::Image contents
    Implementing this for use in the GalaxyMap widget which needs to display
    the galaxy image and support zooming (with an arbitrary centre-point).
    
    The implementation adds two UI::Image methods:
    - UI::Image::SetPreserveAspect -- makes the widget preserve the aspect ratio
      of its contents for display. This is different to the PRESERVE_ASPECT flag
      that can be set with UI::Widget::SetSizeControlFlags(), which controls how
      the widget's size is computed at UI layout time.
    
    - UI::Image::SetTransform -- sets a scale factor and point within the image.
      The specified point is centered within the widget, and then scale factor
      is applied. The centre point is specified in normalised coordinates (x, y
      coordinates in the range [-1, 1]).

--- src/ui/Image.cpp
+++ src/ui/Image.cpp
@@ -8,6 +8,9 @@
 namespace UI {
 
 Image::Image(Context *context, const std::string &filename, Uint32 sizeControlFlags): Widget(context)
+	, m_centre(0.0f, 0.0f)
+	, m_scale(1.0f)
+	, m_preserveAspect(false)
 {
 	Graphics::TextureBuilder b = Graphics::TextureBuilder::UI(filename);
 	m_texture.Reset(b.GetOrCreateTexture(GetContext()->GetRenderer(), "ui"));
@@ -54,23 +57,59 @@ Image *Image::SetNaturalSize()
 	return this;
 }
 
+void Image::SetTransform(float scale, const vector2f &centre)
+{
+	m_scale = scale;
+	m_centre = centre;
+}
+
+void Image::SetPreserveAspect(bool preserve_aspect)
+{
+	m_preserveAspect = preserve_aspect;
+}
+
 void Image::Draw()
 {
 	const Point &offset = GetActiveOffset();
 	const Point &area = GetActiveArea();
+	const auto &descriptor = m_texture->GetDescriptor();
 
-	const float x = offset.x;
-	const float y = offset.y;
-	const float sx = area.x;
-	const float sy = area.y;
+	const float half_sx = area.x*0.5f;
+	const float half_sy = area.y*0.5f;
 
-	const vector2f texSize = m_texture->GetDescriptor().texSize;
+	float cx = offset.x + half_sx;
+	float cy = offset.y + half_sy;
+	float rx, ry;
+
+	if (m_preserveAspect) {
+		const vector2f sz = descriptor.GetOriginalSize();
+		const float wantRatio = sz.x / sz.y;
+		const float haveRatio = float(area.x) / float(area.y);
+		if (wantRatio > haveRatio) {
+			// limited by width
+			rx = half_sx;
+			ry = half_sx / wantRatio;
+		} else {
+			// limited by height
+			rx = half_sy * wantRatio;
+			ry = half_sy;
+		}
+	} else {
+		rx = half_sx;
+		ry = half_sy;
+	}
+
+	rx *= m_scale;
+	ry *= m_scale;
+	cx -= rx*m_centre.x;
+	cy -= ry*m_centre.y;
+	const vector2f texSize = descriptor.texSize;
 
 	Graphics::VertexArray va(Graphics::ATTRIB_POSITION | Graphics::ATTRIB_UV0);
-	va.Add(vector3f(x,    y,    0.0f), vector2f(0.0f,      0.0f));
-	va.Add(vector3f(x,    y+sy, 0.0f), vector2f(0.0f,      texSize.y));
-	va.Add(vector3f(x+sx, y,    0.0f), vector2f(texSize.x, 0.0f));
-	va.Add(vector3f(x+sx, y+sy, 0.0f), vector2f(texSize.x, texSize.y));
+	va.Add(vector3f(cx-rx, cy-ry, 0.0f), vector2f(0.0f,      0.0f));
+	va.Add(vector3f(cx-rx, cy+ry, 0.0f), vector2f(0.0f,      texSize.y));
+	va.Add(vector3f(cx+rx, cy-ry, 0.0f), vector2f(texSize.x, 0.0f));
+	va.Add(vector3f(cx+rx, cy+ry, 0.0f), vector2f(texSize.x, texSize.y));
 
 	Graphics::Renderer *r = GetContext()->GetRenderer();
 	auto renderState = GetContext()->GetSkin().GetAlphaBlendState();
