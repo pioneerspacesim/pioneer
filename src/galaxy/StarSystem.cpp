54fa606849aa088db2d71efcabfa03db241210f0
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2016-01-29 14:04:13 +0000

    MERRY HAPPY 2016 - older copyright notices

--- src/galaxy/StarSystem.cpp
+++ src/galaxy/StarSystem.cpp
@@ -988,7 +988,7 @@ void StarSystem::ExportToLua(const char *filename) {
 	if(f == 0)
 		return;
 
-	fprintf(f,"-- Copyright © 2008-2012 Pioneer Developers. See AUTHORS.txt for details\n");
+	fprintf(f,"-- Copyright © 2008-2016 Pioneer Developers. See AUTHORS.txt for details\n");
 	fprintf(f,"-- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt\n\n");
 
 	std::string stars_in_system = GetStarTypes(m_rootBody.Get());
