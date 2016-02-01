7356079232d3ea97361509efaa949cad2b39715c
Claudius Mueller <claudius.mueller@gmail.com>
2016-01-31 19:00:38 -0500

    remove unused import of Ship.lua

--- data/libs/Character.lua
+++ data/libs/Character.lua
@@ -52,7 +52,6 @@ local Game = import("Game")
 local Event = import("Event")
 local NameGen = import("NameGen")
 local Serializer = import("Serializer")
-local Ship = import("Ship")
 
 local Character;
 Character = {
