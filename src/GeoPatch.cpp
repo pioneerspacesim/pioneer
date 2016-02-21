2336b493333a2ab37a3695939f43721ec83e24ad
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2016-02-21 16:29:33 +0000

    I was a bit aggressive with the crack avoidance.

--- src/GeoPatch.cpp
+++ src/GeoPatch.cpp
@@ -133,7 +133,7 @@ void GeoPatch::UpdateVBOs(Graphics::Renderer *renderer)
 				++vtxPtr; // next vertex
 			}
 		}
-		const double minhScale = (minh + 1.0) * 0.9999999;
+		const double minhScale = (minh + 1.0) * 0.99999;
 		// ----------------------------------------------------
 		// vertical edges
 		// left-edge
