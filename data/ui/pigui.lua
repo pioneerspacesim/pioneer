774a906e34ab2ec29cc5d7003d549fcdd3641300
Peter <craven@gmx.net>
2016-09-04 22:03:39 +0200

    extracting Vector to data/libs/Vector.lua

--- data/ui/pigui.lua
+++ data/ui/pigui.lua
@@ -30,6 +30,7 @@ local Space = import('Space')
 local Engine = import('Engine')
 local Event = import("Event")
 local ShipDef = import("ShipDef")
+local Vector = import("Vector")
 
 local player
 local system
@@ -94,6 +95,7 @@ local pionicons = {
 	 small = { name = "pionicons", size = 12, offset = 14 },
 	 large = { name = "pionicons", size = 30, offset = 28 }
 }
+
 local pionillium = {
 	 large = { name = "pionillium", size = 30, offset = 24 },
 	 medium = { name = "pionillium", size = 18, offset = 14 },
@@ -117,91 +119,6 @@ local MyFormat = {
 	 end
 }
 
-local Vector = {}
-do
-	 local meta = {
-			_metatable = "Private metatable",
-			_DESCRIPTION = "Vectors in 3D"
-	 }
-
-	 meta.__index = meta
-
-	 function meta:__add( v )
-			if(type(v) == "number") then
-				 return Vector(self.x + v, self.y + v, self.z + v)
-			else
-				 return Vector(self.x + v.x, self.y + v.y, self.z + v.z)
-			end
-	 end
-
-	 function meta:__sub( v )
-			if(type(v) == "number") then
-				 return Vector(self.x - v, self.y - v, self.z - v)
-			else
-				 return Vector(self.x - v.x, self.y - v.y, self.z - v.z)
-			end
-	 end
-
-	 function meta:__mul( v )
-			if(type(v) == "number") then
-				 return Vector(self.x * v, self.y * v, self.z * v)
-			else
-				 return Vector(self.x * v.x, self.y * v.y, self.z * v.z)
-			end
-	 end
-
-	 function meta:__div( v )
-			if(type(v) == "number") then
-				 return Vector(self.x / v, self.y / v, self.z / v)
-			else
-				 return Vector(self.x / v.x, self.y / v.y, self.z / v.z)
-			end
-	 end
-
-	 function meta:__tostring()
-			return ("<%g, %g, %g>"):format(self.x, self.y, self.z)
-	 end
-
-	 function meta:magnitude()
-			return math.sqrt( self.x * self.x + self.y * self.y + self.z * self.z)
-	 end
-
-	 function meta:normalized()
-			local len = math.abs(self:magnitude())
-			return Vector(self.x / len, self.y / len, self.z / len)
-	 end
-
-	 function meta:left()
-			return Vector(-self.y, self.x, 0)
-	 end
-
-	 function meta:right()
-			return Vector(self.y, -self.x, 0)
-	 end
-
-	 function meta:dot(other)
-			return self.x * other.x + self.y * other.y + self.z * other.z
-	 end
-
-	 function meta:cross(other)
-			return Vector(self.y * other.z - self.z * other.y,
-										self.x * other.z - self.z * other.x,
-										self.x * other.y - self.y * other.x)
-	 end
-
-	 setmetatable( Vector, {
-										__call = function( V, x ,y ,z )
-											 local result
-											 if type(x) == "table" then
-													result = { x = x.x or 0, y = x.y or 0, z = x.z or 0 }
-											 else
-													result = {x = x or 0, y = y or 0, z = z or 0}
-											 end
-											 return setmetatable( result, meta ) end
-	 } )
-end
-
-Vector.__index = Vector
 
 function map(func, array)
 	 local new_array = {}
