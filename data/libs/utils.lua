9bd8a07f7dc421d0e894535cb2134dd8d3e7071c
Peter <craven@gmx.net>
2016-09-04 22:27:28 +0200

    add clamp to lua utils

--- data/libs/utils.lua
+++ data/libs/utils.lua
@@ -206,4 +206,14 @@ utils.inherits = function (baseClass, name)
 	return new_class
 end
 
+function utils.clamp(value, min, max)
+	 if value < min then
+			return min
+	 elseif value > max then
+			return max
+	 else
+			return value
+	 end
+end
+
 return utils
