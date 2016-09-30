8b8da71553f855781a9b68b30a4a6e23b3ee3176
Karl F <karlfogel@gmail.com>
2016-09-30 12:00:17 +0200

    Rename "Trade analyzer" to "Trade Computer".
    Bonus: Less friction between American/Cambridge spelling and UK/Oxford
    spelling. Also, the name describes what it is better.

--- data/libs/Equipment.lua
+++ data/libs/Equipment.lua
@@ -774,7 +774,7 @@ misc.thrusters_best = EquipType.New({
 	icon_name="thrusters_best"
 })
 misc.trade_analyzer = EquipType.New({
-	l10n_key="TRADE_ANALYZER", slots="trade_analyzer", price=400,
+	l10n_key="TRADE_COMPUTER", slots="trade_analyzer", price=400,
 	capabilities={mass=0, trade_analyzer=1}, purchasable=true, tech_level=9
 })
 misc.planetscanner = BodyScannerType.New({
