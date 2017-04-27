ca841720589152dd5dc5eb9e616e64dfa32662e1
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2017-04-27 10:39:21 +0100

    Build a unique video name from timestamp

--- src/Pi.cpp
+++ src/Pi.cpp
@@ -508,10 +508,15 @@ void Pi::Init(const std::map<std::string,std::string> &options, bool no_gui)
 
 	if (config->Int("RecordVideo"))
 	{
-		char cmd[256] = { 0 };
+
+		char videoName[256];
+		const time_t t = time(0);
+		struct tm *_tm = localtime(&t);
+		strftime(videoName, sizeof(videoName), "pioneer-%Y%m%d-%H%M%S", _tm);
 		// start ffmpeg telling it to expect raw rgba 720p-60hz frames
 		// -i - tells it to read frames from stdin
-		_snprintf(cmd, 256, "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip output.mp4", videoSettings.width, videoSettings.height);
+		char cmd[256] = { 0 };
+		_snprintf(cmd, sizeof(cmd), "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s %dx%d -i - -threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip %s.mp4", videoSettings.width, videoSettings.height, videoName);
 
 		// open pipe to ffmpeg's stdin in binary write mode
 #if defined(_MSC_VER) || defined(__MINGW32__)
