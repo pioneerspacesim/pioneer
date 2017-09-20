b4ee8069e6ea6ad3b0e4c5043741467354d9c9c3
Phil Howlett <phil.g.howlett@gmail.com>
2017-09-21 07:24:33 +1000

    Update build script with updated libraries.

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -36,29 +36,14 @@ cp -r $PIONEER/data $DIST/data
 
 # Copy over libs
 mkdir $DIST/libs
-cp -v /opt/local/lib/libfreetype.6.dylib $DIST/libs
-cp -v /opt/local/lib/libGLEW.1.9.0.dylib $DIST/libs
-cp -v /opt/local/lib/libassimp.3.dylib $DIST/libs
-cp -v /opt/local/lib/libsigc-2.0.0.dylib $DIST/libs
-cp -v /opt/local/lib/libSDL-1.2.0.dylib $DIST/libs
-cp -v /opt/local/lib/libSDL_image-1.2.0.dylib $DIST/libs
-cp -v /opt/local/lib/libSDL_sound-1.0.1.0.2.dylib $DIST/libs
-cp -v /opt/local/lib/libvorbisfile.3.dylib $DIST/libs
-cp -v /opt/local/lib/libbz2.1.0.dylib $DIST/libs
-cp -v /opt/local/lib/libjpeg.9.dylib $DIST/libs
-cp -v /opt/local/lib/libogg.0.dylib $DIST/libs
-cp -v /opt/local/lib/libpng15.15.dylib $DIST/libs
-cp -v /opt/local/lib/libtiff.5.dylib $DIST/libs
-cp -v /opt/local/lib/libvorbis.0.dylib $DIST/libs
-cp -v /opt/local/lib/libX11.6.dylib $DIST/libs
-cp -v /opt/local/lib/libXau.6.dylib $DIST/libs
-cp -v /opt/local/lib/libxcb.1.dylib $DIST/libs
-cp -v /opt/local/lib/libXdmcp.6.dylib $DIST/libs
-cp -v /opt/local/lib/libXext.6.dylib $DIST/libs
-cp -v /opt/local/lib/libXrandr.2.dylib $DIST/libs
-cp -v /opt/local/lib/libXrender.1.dylib $DIST/libs
-cp -v /opt/local/lib/libz.1.dylib $DIST/libs
-cp -v /opt/local/lib/liblzma.5.dylib $DIST/libs
+cp -v /usr/local/opt/freetype/lib/libfreetype.6.dylib $DIST/libs
+cp -v /usr/local/opt/sdl2_image/lib/libSDL2_image-2.0.0.dylib $DIST/libs
+cp -v /usr/local/opt/sdl2/lib/libSDL2-2.0.0.dylib $DIST/libs
+cp -v /usr/local/opt/libsigc++/lib/libsigc-2.0.0.dylib $DIST/libs
+cp -v /usr/local/opt/libvorbis/lib/libvorbisfile.3.dylib $DIST/libs
+cp -v /usr/local/opt/libpng/lib/libpng16.16.dylib $DIST/libs
+cp -v /usr/lib/libz.1.dylib $DIST/libs
+cp -v /usr/local/opt/assimp/lib/libassimp.4.dylib $DIST/libs
 
 # Copy over the shell script
 cp $PIONEER/osx/pioneer.sh $DIST/pioneer
