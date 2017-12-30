eeca357e25a0742d8b736ad2ee148aa743cacdff
joonicks <god@joonicks.eu>
2017-12-31 00:20:35 +0100

    declaration bug
    visual studio suxx

--- src/Factions.h
+++ src/Factions.h
@@ -53,7 +53,7 @@ public:
 	typedef std::vector<SystemPath> ClaimList;
 	ClaimList			m_ownedsystemlist;
 	void PushClaim(SystemPath path) { m_ownedsystemlist.push_back(path); }
-	bool Faction::IsClaimed(SystemPath) const;
+	bool IsClaimed(SystemPath) const;
 
 	// commodity legality
 	typedef std::map<GalacticEconomy::Commodity, Uint32> CommodityProbMap;
