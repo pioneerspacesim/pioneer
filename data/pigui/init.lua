4d993ccbe80b09b52a8b907825132e69c3e84e08
Peter <craven@gmx.net>
2018-09-28 08:33:52 +0200

    Fix loading screen

--- data/pigui/init.lua
+++ data/pigui/init.lua
@@ -12,40 +12,46 @@ local utils = import("utils")
 
 local base = Color(0,1,33)
 local highlight = Color(0,63,112)
+local transparent = Color(0,0,0,0)
 
 local logo = ui.loadTextureFromSVG(ui.dataDirPath({"icons", "logo.svg"}), 512, 512)
+
+-- support minimum resolution 800x600
+local window_width = 700
 
 ui.registerHandler(
 	'init',
 	function(progress)
-		ui.setNextWindowPos(Vector(0, ui.screenHeight/3*2), "Always")
+		ui.setNextWindowPos(Vector(ui.screenWidth / 2 - window_width / 2, ui.screenHeight/3*2), "Always")
+		ui.setNextWindowSize(Vector(window_width,200), "Always")
 		ui.withFont("orbiteer", 18, function()
-			ui.window("test", {"NoTitleBar", "NoResize", "NoMove"}, function()
-				local age = string.format("%.1f", 13.7 * progress)
-				local agestring = string.interp(lui.SIMULATING_UNIVERSE_EVOLUTION_N_BYEARS, { age = age })
-				local size = ui.calcTextSize(agestring)
+									ui.withStyleColors( {["WindowBg"] = transparent }, function ()
+											ui.window("test", {"NoTitleBar", "NoResize", "NoMove"}, function()
+																	local age = string.format("%.1f", 13.7 * progress)
+																	local agestring = string.interp(lui.SIMULATING_UNIVERSE_EVOLUTION_N_BYEARS, { age = age })
+																	local agesize = ui.calcTextSize(agestring)
+																	ui.dummy(Vector((window_width - agesize.x) / 2, 0))
+																	ui.sameLine()
+																	ui.text(agestring)
 
-				ui.dummy(Vector((ui.screenWidth - size.x) / 2, 0))
-				ui.sameLine()
-				ui.text(agestring)
+																	ui.dummy(Vector(15,15))
 
-				ui.dummy(Vector(15,15))
-
-				ui.dummy(Vector(ui.screenWidth/4,0))
-				ui.sameLine()
-				ui.withStyleColors({ ["PlotHistogram"] = highlight, ["FrameBg"] = base }, function()
-						ui.progressBar(progress, Vector(ui.screenWidth / 2, ui.screenHeight / 43), "") -- 1080 / 43 -> 25
-				end)
-			end)
+																	ui.withStyleColors({ ["PlotHistogram"] = highlight, ["FrameBg"] = base }, function()
+																			ui.progressBar(progress, Vector(window_width - 20, ui.screenHeight / 43), "") -- 1080 / 43 -> 25
+																	end)
+											end)
+									end)
 		end)
 		local logosize = (ui.screenHeight / 2.5)
 		local leftup = Vector(ui.screenWidth/2 - logosize/2, ui.screenHeight/7)
 		ui.setNextWindowPos(leftup, "Always")
 		ui.setNextWindowSize(Vector(logosize, logosize), "Always")
-		ui.window("logo", {"NoTitleBar", "NoResize", "NoMove"}, function ()
-			local size = Vector(logosize,logosize)
-			local pos = Vector(0,0) -- Vector(ui.screenWidth / 2, ui.screenHeight / 3 * 2)
-			local offset = ui.getWindowPos()
-			ui.addImage(logo, pos + offset, pos + size + offset, Vector(0.0, 0.0), Vector(1.0, 1.0), Color(255,255,255))
+		ui.withStyleColors( {["WindowBg"] = transparent }, function ()
+				ui.window("logo", {"NoTitleBar", "NoResize", "NoMove"}, function ()
+										local size = Vector(logosize,logosize)
+										local pos = Vector(0,0) -- Vector(ui.screenWidth / 2, ui.screenHeight / 3 * 2)
+										local offset = ui.getWindowPos()
+										ui.addImage(logo, pos + offset, pos + size + offset, Vector(0.0, 0.0), Vector(1.0, 1.0), Color(255,255,255))
+				end)
 		end)
 end)
