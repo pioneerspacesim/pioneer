pioneerspacesim/pioneer

commit d90e14ec137f38cd0debbb64a66ac3fac7b3817d

Speedup pilua (#4548)
* Add a Rotate function to vector2 and copy-pasted some specializations to be used by Lua

Copy-Paste vector3 Lua interface in LuaVector2 so it works for vector2

Expose LuaVector (vector3) and LuaVector2 (vector2) to LVM (example in 'pigui.lua')

Add some utility function (used in PiGui) to LuaVector2

* mainmenu.lua use LuaVector2

Return vector2 for screen position of an object

Pull a vector2d from Lua instead of a size_v

* Port ui.pointOnClock in C++;

Store last 'center' in ui.pointOnClock;

Provide two pointOnClock overrided; Provide lineOnClock;

Use pointOnClock and lineOnClock in Lua;

* Eradicate dependencies of C++ from Vector.lua (see LuaPiGui); vectors in Lua are either LuaVector or LuaVector2

Remove Vector.lua

Remove remaining dependencies from Vector.lua

* Use length() instead of magnitude()

Use length instead of magnitude in order to re-enable radar; Pass vector2 instead of vector3 for aep; Fix some calcultations because of length

* Add '__newindex' function to LuaVector&LuaVector2; CheckFromLua don't return a 'const'; Use vector.x|y|z where useful; Implement some improvement here and there

* New ctor for LuaTable; Avoid rehashing in 'l_pigui_get_projected_bodies';

Use new ctor of LuaTable, caching results;

Some little explanation on new ctor in LuaTable;

* Remove remaining '&' in Space.*

* Use 'lua_typename' in LuaVector and LuaVector2; Move CalcTextAlignment in C++; Cache variable in 'displayDirectionalMarkers'

* Remove 'ImVec2' push/pull, use vector2 instead

* Use __index in game.lua

* Bugfixes: seems vector2 assign value as reference; use 'new_index'"

* Group bodies in C++ using 'Hierarchical Agglomerative Clustering';
Pass results in Lua; Print them
Use reverse iterator in 'l_pigui_get_projected_bodies_grouped';
Reserve memory ahead of time; use group head to check nearest;
use std::accumulate Reduce memory waste; Use std::for_each

data/pigui/mainmenu.lua
@@ -9,7 +9,6 @@ local SystemPath = import("SystemPath")
 local Equipment = import("Equipment")
 local Format = import("Format")
 local ui = import('pigui/pigui.lua')
-local Vector = import('Vector')
 local Event = import('Event')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core")
@@ -29,8 +28,8 @@ local pionillium = ui.fonts.pionillium
 local orbiteer = ui.fonts.orbiteer
 local icons = ui.theme.icons
 
-local mainButtonSize = Vector(400,46) * (ui.screenHeight / 1200)
-local dialogButtonSize = Vector(150,46) * (ui.screenHeight / 1200)
+local mainButtonSize = Vector2(400,46) * (ui.screenHeight / 1200)
+local dialogButtonSize = Vector2(150,46) * (ui.screenHeight / 1200)
 local mainButtonFontSize = 24 * (ui.screenHeight / 1200)
 
 local showQuitConfirm = false
@@ -109,12 +108,12 @@ local function confirmQuit()
 				ui.textWrapped(quitConfirmMsg)
 				ui.popTextWrapPos()
 			end)
-			ui.dummy(Vector(5,15)) -- small vertical spacing
-			ui.dummy(Vector(30, 5))
+			ui.dummy(Vector2(5,15)) -- small vertical spacing
+			ui.dummy(Vector2(30, 5))
 			ui.sameLine()
 			dialogTextButton(qlc.YES, true, Engine.Quit)
 			ui.sameLine()
-			ui.dummy(Vector(30, 5))
+			ui.dummy(Vector2(30, 5))
 			ui.sameLine()
 			dialogTextButton(qlc.NO, true, function() showQuitConfirm = false end)
 		end)
@@ -161,25 +160,25 @@ local function showMainMenu()
 	local canContinue = Game.CanLoadGame('_exit')
 	local buttons = 4
 
-	local winPos = Vector(ui.screenWidth - mainButtonSize.x - 100, ui.screenHeight/2 - (buttons * mainButtonSize.y)/2 - (2*mainButtonSize.y)/2 - 8)
+	local winPos = Vector2(ui.screenWidth - mainButtonSize.x - 100, ui.screenHeight/2 - (buttons * mainButtonSize.y)/2 - (2*mainButtonSize.y)/2 - 8)
 
-	ui.setNextWindowPos(Vector(110,65),'Always')
+	ui.setNextWindowPos(Vector2(110,65),'Always')
 	ui.withStyleColors({["WindowBg"]=colors.transparent}, function()
 		ui.window("headingWindow", {"NoTitleBar","NoResize","NoFocusOnAppearing","NoBringToFrontOnFocus","AlwaysAutoResize"}, function()
 			ui.withFont("orbiteer",36 * (ui.screenHeight/1200),function() ui.text("Pioneer") end)
 		end)
 	end)
 	if Engine.IsIntroZooming() then
-		ui.setNextWindowPos(Vector(0,0),'Always')
-		ui.setNextWindowSize(Vector(ui.screenWidth, ui.screenHeight), 'Always')
+		ui.setNextWindowPos(Vector2(0,0),'Always')
+		ui.setNextWindowSize(Vector2(ui.screenWidth, ui.screenHeight), 'Always')
 		ui.withStyleColors({["WindowBg"]=colors.transparent}, function()
 			ui.window("shipinfoWindow", {"NoTitleBar","NoResize","NoFocusOnAppearing","NoBringToFrontOnFocus","AlwaysAutoResize"}, function()
 				local mn = Engine.GetIntroCurrentModelName()
 				if mn then
 					local sd = ShipDef[mn]
 					if sd then
-						ui.addFancyText(Vector(ui.screenWidth / 3, ui.screenHeight / 5.0 * 4), ui.anchor.center, ui.anchor.bottom, {{text=sd.name, color=colors.white, font=orbiteer.large}}, colors.transparent)
-						ui.addFancyText(Vector(ui.screenWidth / 3, ui.screenHeight / 5.0 * 4.02), ui.anchor.center, ui.anchor.top, {{text=lui[sd.shipClass:upper()], color=colors.white, font=orbiteer.medium}}, colors.transparent)
+						ui.addFancyText(Vector2(ui.screenWidth / 3, ui.screenHeight / 5.0 * 4), ui.anchor.center, ui.anchor.bottom, {{text=sd.name, color=colors.white, font=orbiteer.large}}, colors.transparent)
+						ui.addFancyText(Vector2(ui.screenWidth / 3, ui.screenHeight / 5.0 * 4.02), ui.anchor.center, ui.anchor.top, {{text=lui[sd.shipClass:upper()], color=colors.white, font=orbiteer.medium}}, colors.transparent)
 					end
 				end
 			end)
@@ -188,7 +187,7 @@ local function showMainMenu()
 	local build_text = Engine.version
 	ui.withFont("orbiteer", 16 * (ui.screenHeight/1200),
 							function()
-								ui.setNextWindowPos(Vector(ui.screenWidth - ui.calcTextSize(build_text).x * 1.2,ui.screenHeight - 50), 'Always')
+								ui.setNextWindowPos(Vector2(ui.screenWidth - ui.calcTextSize(build_text).x * 1.2,ui.screenHeight - 50), 'Always')
 								ui.withStyleColors({["WindowBg"] = colors.transparent}, function()
 										ui.window("buildLabel", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "AlwaysAutoResize"},
 															function()

data/libs/Vector.lua
@@ -1,114 +1,0 @@
--- Copyright © 2008-2019 Pioneer Developers. See AUTHORS.txt for details
--- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
-
-local Vector = {}
-
-local function binaryOp(a, b, op)
-	local typ = type(b)
-	if(typ == "number") then
-		return Vector(op(a.x, b), op(a.y, b), op(a.z, b))
-	elseif(typ == "table") then
-		return Vector(op(a.x, b.x), op(a.y, b.y), op(a.z, b.z))
-	else
-		error("operand to binary op in Vector not number or table but " .. typ)
-	end
-end
-
-do
-	local meta = {
-		_metatable = "Private metatable",
-		_DESCRIPTION = "Vectors in 3D",
-		class = "Vector"
-	}
-
-	meta.__index = meta
-
-	function meta:__unm( v )
-		return Vector(-self.x, -self.y, -self.z)
-	end
-
-	function meta:__add( v )
-		return binaryOp(self, v, function(a,b) return a+b end)
-	end
-
-	function meta:__sub( v )
-		return binaryOp(self, v, function(a,b) return a-b end)
-	end
-
-	function meta:__mul( v )
-		return binaryOp(self, v, function(a,b) return a*b end)
-	end
-
-	function meta:__div( v )
-		return binaryOp(self, v, function(a,b) return a/b end)
-	end
-
-	function meta:__tostring()
-		return ("(%.02g, %.02g, %.02g)"):format(self.x, self.y, self.z)
-	end
-
-	function meta:magnitude()
-		return math.sqrt( self.x * self.x + self.y * self.y + self.z * self.z)
-	end
-
-	function meta:normalized()
-		local len = math.abs(self:magnitude())
-		return Vector(self.x / len, self.y / len, self.z / len)
-	end
-
-	function meta:left()
-		return Vector(-self.y, self.x, 0)
-	end
-
-	function meta:right()
-		return Vector(self.y, -self.x, 0)
-	end
-
-	function meta:dot(other)
-		if(type(other) == "table" and type(other.x) == "number" and type(other.y) == "number" and type(other.z) == "number") then
-			return self.x * other.x + self.y * other.y + self.z * other.z
-		else
-			error("Incorrect parameter to dot product");
-		end
-	end
-
-	function meta:cross(other)
-		if(type(other) == "table" and type(other.x) == "number" and type(other.y) == "number" and type(other.z) == "number") then
-			return Vector(self.y * other.z - self.z * other.y,
-						  self.x * other.z - self.z * other.x,
-						  self.x * other.y - self.y * other.x)
-		else
-			error("Incorrect parameter to cross product");
-		end
-	end
-
-	function meta:angle()
-		local n = self:normalized()
-		local x = math.atan2(n.x, n.y)
-		return math.pi * 2 - x
-	end
-
-	function meta:rotate2d(angle_rad)
-		if(type(angle_rad) ~= "number") then
-			error("Parameter to Vector:rotate2d not a number")
-		end
-		local cs = math.cos(angle_rad)
-		local sn = math.sin(angle_rad)
-		return Vector(self.x * cs - self.y * sn, self.x * sn + self.y * cs, 0)
-	end
-
-	setmetatable( Vector, {
-					  __call = function( V, x ,y ,z )
-						  local result
-						  if type(x) == "table" then
-							  result = { x = x.x or 0, y = x.y or 0, z = x.z or 0 }
-						  else
-							  result = {x = x or 0, y = y or 0, z = z or 0}
-						  end
-						  return setmetatable( result, meta ) end
-	} )
-end
-
-Vector.__index = Vector
-
-return Vector

data/pigui/init.lua
@@ -3,7 +3,6 @@
 
 local Engine = import('Engine')
 local ui = import('pigui')
-local Vector = import('Vector')
 local Color = import('Color')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core");
@@ -22,36 +21,36 @@ local window_width = 700
 ui.registerHandler(
 	'init',
 	function(progress)
-		ui.setNextWindowPos(Vector(ui.screenWidth / 2 - window_width / 2, ui.screenHeight/3*2), "Always")
-		ui.setNextWindowSize(Vector(window_width,200), "Always")
+		ui.setNextWindowPos(Vector2(ui.screenWidth / 2 - window_width / 2, ui.screenHeight/3*2), "Always")
+		ui.setNextWindowSize(Vector2(window_width,200), "Always")
 		ui.withFont("orbiteer", 18, function()
 									ui.withStyleColors( {["WindowBg"] = transparent }, function ()
 											ui.window("test", {"NoTitleBar", "NoResize", "NoMove"}, function()
 																	local age = string.format("%.1f", 13.7 * progress)
 																	local agestring = string.interp(lui.SIMULATING_UNIVERSE_EVOLUTION_N_BYEARS, { age = age })
 																	local agesize = ui.calcTextSize(agestring)
-																	ui.dummy(Vector((window_width - agesize.x) / 2, 0))
+																	ui.dummy(Vector2((window_width - agesize.x) / 2, 0))
 																	ui.sameLine()
 																	ui.text(agestring)
 
-																	ui.dummy(Vector(15,15))
+																	ui.dummy(Vector2(15,15))
 
 																	ui.withStyleColors({ ["PlotHistogram"] = highlight, ["FrameBg"] = base }, function()
-																			ui.progressBar(progress, Vector(window_width - 20, ui.screenHeight / 43), "") -- 1080 / 43 -> 25
+																			ui.progressBar(progress, Vector2(window_width - 20, ui.screenHeight / 43), "") -- 1080 / 43 -> 25
 																	end)
 											end)
 									end)
 		end)
 		local logosize = (ui.screenHeight / 2.5)
-		local leftup = Vector(ui.screenWidth/2 - logosize/2, ui.screenHeight/7)
+		local leftup = Vector2(ui.screenWidth/2 - logosize/2, ui.screenHeight/7)
 		ui.setNextWindowPos(leftup, "Always")
-		ui.setNextWindowSize(Vector(logosize, logosize), "Always")
+		ui.setNextWindowSize(Vector2(logosize, logosize), "Always")
 		ui.withStyleColors( {["WindowBg"] = transparent }, function ()
 				ui.window("logo", {"NoTitleBar", "NoResize", "NoMove"}, function ()
-										local size = Vector(logosize,logosize)
-										local pos = Vector(0,0) -- Vector(ui.screenWidth / 2, ui.screenHeight / 3 * 2)
+										local size = Vector2(logosize,logosize)
+										local pos = Vector2(0,0) -- Vector2(ui.screenWidth / 2, ui.screenHeight / 3 * 2)
 										local offset = ui.getWindowPos()
-										ui.addImage(logo, pos + offset, pos + size + offset, Vector(0.0, 0.0), Vector(1.0, 1.0), Color(255,255,255))
+										ui.addImage(logo, pos + offset, pos + size + offset, Vector2(0.0, 0.0), Vector2(1.0, 1.0), Color(255,255,255))
 				end)
 		end)
 end)

data/pigui/modules/autopilot-window.lua
@@ -4,7 +4,6 @@
 local Engine = import('Engine')
 local Game = import('Game')
 local ui = import('pigui/pigui.lua')
-local Vector = import('Vector')
 local Color = import('Color')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core");
@@ -16,7 +15,7 @@ local player = nil
 local colors = ui.theme.colors
 local icons = ui.theme.icons
 
-local mainButtonSize = Vector(32,32) * (ui.screenHeight / 1200)
+local mainButtonSize = Vector2(32,32) * (ui.screenHeight / 1200)
 local mainButtonFramePadding = 3
 
 local function mainMenuButton(icon, selected, tooltip, color)
@@ -148,8 +147,8 @@ local function displayAutoPilotWindow()
 	if ui.showOptionsWindow then return end
 	player = Game.player
 	local current_view = Game.CurrentView()
-	ui.setNextWindowSize(Vector(mainButtonSize.x * 6, mainButtonSize.y * 2), "Always")
-	ui.setNextWindowPos(Vector(ui.screenWidth/2 + ui.reticuleCircleRadius / 4 * 3, ui.screenHeight - mainButtonSize.y * 1.5 - 8) , "Always")
+	ui.setNextWindowSize(Vector2(mainButtonSize.x * 6, mainButtonSize.y * 2), "Always")
+	ui.setNextWindowPos(Vector2(ui.screenWidth/2 + ui.reticuleCircleRadius / 4 * 3, ui.screenHeight - mainButtonSize.y * 1.5 - 8) , "Always")
 	ui.window("AutoPilot", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus"},
 						function()
 							if current_view == "world" then

data/pigui/modules/comms.lua
@@ -4,7 +4,6 @@
 local Engine = import('Engine')
 local Game = import('Game')
 local ui = import('pigui/pigui.lua')
-local Vector = import('Vector')
 local Color = import('Color')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core");
@@ -17,7 +16,7 @@ local icons = ui.theme.icons
 local commsLogRetainTime = 60 -- how long messages are shown
 local commsLinesToShow = 5 -- how many messages are shown
 
-local mainButtonSize = Vector(32,32) * (ui.screenHeight / 1200)
+local mainButtonSize = Vector2(32,32) * (ui.screenHeight / 1200)
 local mainButtonFramePadding = 3
 local fullComms
 
@@ -33,9 +32,10 @@ local function showItem(item)
 end
 
 local function displayCommsLog()
+	local aux = Vector2(0, 0)
 	local current_view = Game.CurrentView()
 	if current_view == "world" then
-		ui.setNextWindowPos(Vector(10, 10) , "Always")
+		ui.setNextWindowPos(Vector2(10, 10) , "Always")
 		ui.window("CommsLogButton", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus"},
 							function()
 								if ui.coloredSelectedIconButton(icons.comms, mainButtonSize, nil, mainButtonFramePadding, colors.buttonBlue, colors.white, 'Toggle full comms window') then
@@ -44,8 +44,10 @@ local function displayCommsLog()
 		end)
 		ui.withFont(ui.fonts.pionillium.medium.name, ui.fonts.pionillium.medium.size, function()
 									if not fullComms then -- not fullComms, show small window
-										ui.setNextWindowSize(Vector(ui.screenWidth / 4, ui.screenHeight / 8) , "Always")
-										ui.setNextWindowPos(Vector(mainButtonSize.x + 2 * mainButtonFramePadding + 15, 10) , "Always")
+										aux = Vector2(ui.screenWidth / 4, ui.screenHeight / 8)
+										ui.setNextWindowSize(aux , "Always")
+										aux = Vector2(mainButtonSize.x + 2 * mainButtonFramePadding + 15, 10)
+										ui.setNextWindowPos(aux , "Always")
 										ui.window("ShortCommsLog", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus"},
 															function()
 																local last = nil
@@ -78,8 +80,10 @@ local function displayCommsLog()
 																ui.popTextWrapPos()
 										end)
 									else  -- fullComms, show large window
-										ui.setNextWindowSize(Vector(ui.screenWidth / 3, ui.screenHeight / 4) , "Always")
-										ui.setNextWindowPos(Vector(mainButtonSize.x + 2 * mainButtonFramePadding + 25, 20) , "Always")
+										aux = Vector2(ui.screenWidth / 3, ui.screenHeight / 4)
+										ui.setNextWindowSize(aux , "Always")
+										aux = Vector2(mainButtonSize.x + 2 * mainButtonFramePadding + 25, 20)
+										ui.setNextWindowPos(aux , "Always")
 										ui.withStyleColors({ ["WindowBg"] = colors.commsWindowBackground }, function()
 												ui.withStyleVars({ ["WindowRounding"] = 0.0 }, function()
 														ui.window("CommsLog", {"NoResize"},

data/pigui/modules/equipment.lua
@@ -4,7 +4,6 @@
 local Engine = import('Engine')
 local Game = import('Game')
 local ui = import('pigui/pigui.lua')
-local Vector = import('Vector')
 local Color = import('Color')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core");
@@ -17,8 +16,8 @@ local player = nil
 local colors = ui.theme.colors
 local icons = ui.theme.icons
 
-local mainIconSize = Vector(32,32) * (ui.screenHeight / 1200)
-local mainWideIconSize = Vector(64,32) * (ui.screenHeight / 1200)
+local mainIconSize = Vector2(32,32) * (ui.screenHeight / 1200)
+local mainWideIconSize = Vector2(64,32) * (ui.screenHeight / 1200)
 local mainForegroundColor = colors.reticuleCircle
 local mainBackgroundColor = colors.lightBlueBackground
 local mainHoverColor = colors.lightBlueBackground:tint(0.2)
@@ -28,11 +27,12 @@ local function iconButton(leftupper, icon, is_wide, icon_size, text, is_disabled
 	local textsize = ui.calcTextSize(text)
 	local x = icon_size.x + textsize.x + mainButtonPadding * 2
 	local y = math.max(icon_size.y, textsize.y) -- + mainButtonPadding * 2
+	local delta = Vector2(x, y)
 	local clicked = false
 	if is_disabled then
 		bg_color = colors.grey
 	end
-	if ui.isMouseHoveringRect(leftupper, leftupper + Vector(x, y)) and not ui.isMouseHoveringAnyWindow() then
+	if ui.isMouseHoveringRect(leftupper, leftupper + delta) and not ui.isMouseHoveringAnyWindow() then
 		if not is_disabled then
 			if ui.isMouseDown(0) then
 				bg_color = pressed_color
@@ -45,14 +45,14 @@ local function iconButton(leftupper, icon, is_wide, icon_size, text, is_disabled
 		end
 		ui.setTooltip(tooltip)
 	end
-	ui.addRectFilled(leftupper, leftupper + Vector(x, y), bg_color, 0.1, 15)
+	ui.addRectFilled(leftupper, leftupper + delta, bg_color, 0.1, 15)
 	if is_wide then
-		ui.addWideIcon(leftupper + Vector(mainButtonPadding, mainButtonPadding), icon, fg_color, icon_size, ui.anchor.left, ui.anchor.top)
+		ui.addWideIcon(leftupper + Vector2(mainButtonPadding, mainButtonPadding), icon, fg_color, icon_size, ui.anchor.left, ui.anchor.top)
 	else
-		ui.addIcon(leftupper + Vector(mainButtonPadding, mainButtonPadding), icon, fg_color, icon_size, ui.anchor.left, ui.anchor.top)
+		ui.addIcon(leftupper + Vector2(mainButtonPadding, mainButtonPadding), icon, fg_color, icon_size, ui.anchor.left, ui.anchor.top)
 	end
-	ui.addText(leftupper + Vector(icon_size.x + mainButtonPadding, (icon_size.y - textsize.y)/2), fg_color, text) --  + mainButtonPadding
-	return Vector(x, y), clicked
+	ui.addText(leftupper + Vector2(icon_size.x + mainButtonPadding, (icon_size.y - textsize.y)/2), fg_color, text) --  + mainButtonPadding
+	return delta, clicked
 end
 
 local function displayECM(uiPos)
@@ -62,7 +62,7 @@ local function displayECM(uiPos)
 		local ecms = player:GetEquip('ecm')
 		for i,ecm in ipairs(ecms) do
 			local size, clicked = iconButton(uiPos, icons[ecm.ecm_type], false, mainIconSize, "ECM", not player:IsECMReady(), mainBackgroundColor, mainForegroundColor, mainHoverColor, mainPressedColor, 'ECM')
-			uiPos = uiPos + Vector(0, size.y + 10)
+			uiPos.y = uiPos.y + size.y + 10
 			if clicked then
 				player:UseECM()
 			end
@@ -78,7 +78,7 @@ local function displayBodyScanner(uiPos)
 		local sensors = player:GetEquip('sensor')
 		for i,sensor in ipairs(sensors) do
 			local size,clicked = iconButton(uiPos, icons.scanner, false, mainIconSize, sensor:GetName(), false, mainBackgroundColor, mainForegroundColor, mainHoverColor, mainPressedColor, 'Body scanner')
-			uiPos = uiPos + Vector(0, size.y + 10)
+			uiPos.y = uiPos.y + size.y + 10
 			if clicked then
 				player:StartSensor(i)
 				-- or StopSensor(i)
@@ -121,7 +121,7 @@ local function displayMissiles(uiPos)
 		for t,missile in pairs(types) do
 			local c = count[t]
 			local size,clicked = iconButton(uiPos, getMissileIcon(missile), true, mainWideIconSize, c, c == 0, mainBackgroundColor, mainForegroundColor, mainHoverColor, mainPressedColor, lec[missile.l10n_key])
-			uiPos = uiPos + Vector(0, size.y + 10)
+			uiPos.y = uiPos.y + size.y + 10
 			if clicked then
 				print("firing missile " .. t .. ", " .. index[t])
 				fireMissile(index[t])
@@ -133,10 +133,10 @@ end
 
 local function displayEquipment()
 	if ui.showOptionsWindow then return end
-	local uiPos = Vector(15, ui.screenHeight / 3 + 10)
+	local uiPos = Vector2(15, ui.screenHeight / 3 + 10)
 	uiPos = displayMissiles(uiPos)
-	uiPos = displayECM(uiPos + Vector(0, 10))
-	uiPos = displayBodyScanner(uiPos + Vector(0, 10))
+	uiPos = displayECM(uiPos + Vector2(0, 10))
+	uiPos = displayBodyScanner(uiPos + Vector2(0, 10))
 end
 
 ui.registerModule("game", displayEquipment)

data/pigui/modules/fx-window.lua
@@ -4,7 +4,6 @@
 local Engine = import('Engine')
 local Game = import('Game')
 local ui = import('pigui/pigui.lua')
-local Vector = import('Vector')
 local Color = import('Color')
 local Lang = import("Lang")
 local lc = Lang.GetResource("core");
@@ -16,7 +15,7 @@ local player = nil
 local colors = ui.theme.colors
 local icons = ui.theme.icons
 
-local mainButtonSize = Vector(32,32) * (ui.screenHeight / 1200)
+local mainButtonSize = Vector2(32,32) * (ui.screenHeight / 1200)
 local mainButtonFramePadding = 3
 local function mainMenuButton(icon, selected, tooltip, color)
 	if color == nil then
@@ -138,8 +137,10 @@ local function displayFxWindow()
 	if ui.showOptionsWindow then return end
 	player = Game.player
 	local current_view = Game.CurrentView()
-	ui.setNextWindowSize(Vector((mainButtonSize.x + mainButtonFramePadding * 2) * 10, (mainButtonSize.y + mainButtonFramePadding * 2) * 1.5), "Always")
-	ui.setNextWindowPos(Vector(ui.screenWidth/2 - (mainButtonSize.x + 4 * mainButtonFramePadding) * 7.5/2, 0) , "Always")
+	local aux = Vector2((mainButtonSize.x + mainButtonFramePadding * 2) * 10, (mainButtonSize.y + mainButtonFramePadding * 2) * 1.5)
+	ui.setNextWindowSize(aux, "Always")
+	aux = Vector2(ui.screenWidth/2 - (mainButtonSize.x + 4 * mainButtonFramePadding) * 7.5/2, 0)
+	ui.setNextWindowPos(aux , "Always")
 	ui.window("Fx", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus"},
 						function()
 							button_world(current_view)

commit dc1d427e9539b9acb1d203407b2c75ad5db7a15f

Fix crash on main menu
Introduced by omission in #4548.

data/pigui/mainmenu.lua
@@ -197,7 +197,7 @@ local function showMainMenu()
 	end)
 
 	ui.setNextWindowPos(winPos,'Always')
-	ui.setNextWindowSize(Vector(0,0), 'Always')
+	ui.setNextWindowSize(Vector2(0,0), 'Always')
 	ui.withStyleColors({["WindowBg"] = colors.lightBlackBackground}, function()
 		ui.window("MainMenuButtons", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus"}, function()
 			mainTextButton(lui.CONTINUE_GAME, nil, canContinue, continueGame)