97c9a2ea4b4846c66990a11b737fc156ea199409
mike-f1 <mike_f@libero.it>
2019-05-27 19:19:21 +0200

    Remove Pi.h from Terrain.cpp moving 'Dump' in a separate file

--- src/terrain/Terrain.cpp
+++ src/terrain/Terrain.cpp
@@ -6,8 +6,8 @@
 #include "FileSystem.h"
 #include "FloatComparison.h"
 #include "GameConfig.h"
-#include "Pi.h"
 #include "perlin.h"
+#include "../utils.h"
 #include "../galaxy/SystemBody.h"
 
 // static instancer. selects the best height and color classes for the body
@@ -702,19 +702,4 @@ Terrain::MinBodyData::MinBodyData(const SystemBody *body)
 	m_aspectRatio = body->GetAspectRatio();
 	m_path = body->GetPath();
 	m_name = body->GetName();
-}
-
-void Terrain::DebugDump() const
-{
-	Output("Terrain state dump:\n");
-	Output("  Height fractal: %s\n", GetHeightFractalName());
-	Output("  Color fractal: %s\n", GetColorFractalName());
-	Output("  Config: DetailPlanets %d   FractalMultiple %d  Textures  %d\n", Pi::config->Int("DetailPlanets"), Pi::config->Int("FractalMultiple"), Pi::config->Int("Textures"));
-	Output("  Seed: %d\n", m_seed);
-	Output("  Body: %s [%d,%d,%d,%u,%u]\n", m_minBody.m_name.c_str(), m_minBody.m_path.sectorX, m_minBody.m_path.sectorY, m_minBody.m_path.sectorZ, m_minBody.m_path.systemIndex, m_minBody.m_path.bodyIndex);
-	Output("  Aspect Ratio: %g\n", m_minBody.m_aspectRatio);
-	Output("  Fracdefs:\n");
-	for (int i = 0; i < 10; i++) {
-		Output("    %d: amp %f  freq %f  lac %f  oct %d\n", i, m_fracdef[i].amplitude, m_fracdef[i].frequency, m_fracdef[i].lacunarity, m_fracdef[i].octaves);
-	}
 }
---
+++ src/terrain/TerrainDbg.cpp
@@ -0,0 +1,22 @@
+// Copyright © 2008-2019 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "Terrain.h"
+
+#include "GameConfig.h"
+#include "Pi.h"
+
+void Terrain::DebugDump() const
+{
+	Output("Terrain state dump:\n");
+	Output("  Height fractal: %s\n", GetHeightFractalName());
+	Output("  Color fractal: %s\n", GetColorFractalName());
+	Output("  Config: DetailPlanets %d   FractalMultiple %d  Textures  %d\n", Pi::config->Int("DetailPlanets"), Pi::config->Int("FractalMultiple"), Pi::config->Int("Textures"));
+	Output("  Seed: %d\n", m_seed);
+	Output("  Body: %s [%d,%d,%d,%u,%u]\n", m_minBody.m_name.c_str(), m_minBody.m_path.sectorX, m_minBody.m_path.sectorY, m_minBody.m_path.sectorZ, m_minBody.m_path.systemIndex, m_minBody.m_path.bodyIndex);
+	Output("  Aspect Ratio: %g\n", m_minBody.m_aspectRatio);
+	Output("  Fracdefs:\n");
+	for (int i = 0; i < 10; i++) {
+		Output("    %d: amp %f  freq %f  lac %f  oct %d\n", i, m_fracdef[i].amplitude, m_fracdef[i].frequency, m_fracdef[i].lacunarity, m_fracdef[i].octaves);
+	}
+}
