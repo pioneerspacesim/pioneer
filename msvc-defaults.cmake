29dc6f8ca2a79cffd7207407f0fb20a96c89f80f
Paul Cercueil <paul@crapouillou.net>
2019-06-24 17:23:27 +0200

    CMake: Support MSVC build using thirdparty repo
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ msvc-defaults.cmake
@@ -0,0 +1,17 @@
+set(FREETYPE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include/freetype)
+set(FREETYPE_LIBRARY ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/freetype271MT.lib)
+
+set(SDL2_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include)
+set(SDL2_LIBRARIES ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/SDL2.lib;${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/SDL2main.lib)
+
+set(SDL2_IMAGE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include)
+set(SDL2_IMAGE_LIBRARIES ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/SDL2_image.lib)
+
+set(ASSIMP_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include)
+set(ASSIMP_LIBRARIES ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/assimp-vc141-mt.lib)
+
+set(VORBISFILE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include)
+set(VORBISFILE_LIBRARIES ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/libogg_static.lib;${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/libvorbis_static.lib;${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/libvorbisfile_static.lib)
+
+set(SIGCPP_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/include/sigc++-2.0)
+set(SIGCPP_LIBRARIES ${CMAKE_SOURCE_DIR}/pioneer-thirdparty/win32/lib/x64/vs2017/sigc-vc140-2_0.lib)
