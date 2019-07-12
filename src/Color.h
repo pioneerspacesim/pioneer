pioneerspacesim/pioneer

commit c19aab4e482d4574b7b6af2e738a1574db1dbc99

Color.h: Fix conversion from float to u8
Fixes an *annoying* reccurent warning message when compiling under
Visual Studio.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

src/Color.h
@@ -129,15 +129,17 @@ struct Color4ub {
 	static Color4ub FromLuaTable(lua_State *l, int idx);
 
 	Uint8 GetLuminance() const;
-	void Shade(float factor) {
-		r *= (1 - factor);
-		g *= (1 - factor);
-		b *= (1 - factor);
+	void Shade(float factor)
+	{
+		r *= static_cast<Uint8>(1.0f - factor);
+		g *= static_cast<Uint8>(1.0f - factor);
+		b *= static_cast<Uint8>(1.0f - factor);
 	}
-	void Tint(float factor) {
-		r += (255 - r) * factor;
-		g += (255 - g) * factor;
-		b += (255 - b) * factor;
+	void Tint(float factor)
+	{
+		r += static_cast<Uint8>((255.0f - r) * factor);
+		g += static_cast<Uint8>((255.0f - g) * factor);
+		b += static_cast<Uint8>((255.0f - b) * factor);
 	}
 
 	static const Color4ub BLACK;

commit d05f06067e6ec2b0e5ef6f7dd143b0f3a5ef3a07

Fix transparency on menu items
Fix a bug introduced in c19aab4e4 ("Color.h: Fix conversion from float
to u8"). The order of operations was wrong.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

src/Color.h
@@ -131,15 +131,15 @@ struct Color4ub {
 	Uint8 GetLuminance() const;
 	void Shade(float factor)
 	{
-		r *= static_cast<Uint8>(1.0f - factor);
-		g *= static_cast<Uint8>(1.0f - factor);
-		b *= static_cast<Uint8>(1.0f - factor);
+		r = static_cast<Uint8>(r * (1.0f - factor));
+		g = static_cast<Uint8>(g * (1.0f - factor));
+		b = static_cast<Uint8>(b * (1.0f - factor));
 	}
 	void Tint(float factor)
 	{
-		r += static_cast<Uint8>((255.0f - r) * factor);
-		g += static_cast<Uint8>((255.0f - g) * factor);
-		b += static_cast<Uint8>((255.0f - b) * factor);
+		r = static_cast<Uint8>(r + (255.0f - r) * factor);
+		g = static_cast<Uint8>(g + (255.0f - g) * factor);
+		b = static_cast<Uint8>(b + (255.0f - b) * factor);
 	}
 
 	static const Color4ub BLACK;