5cb068ff12df6da3faea5440db04124f6bee51bb
Webster Sheets <webster@web-eworks.com>
2020-02-02 21:45:27 -0500

    Remove old lua_import() code.

--- src/lua/core/CoreFwdDecl.h
+++ src/lua/core/CoreFwdDecl.h
@@ -11,9 +11,5 @@ int luaopen_utils(lua_State *L);
 // Set up the import() functionality
 int luaopen_import(lua_State *L);
 
-// Internal functions corresponding to pi_lua_import, but which leave the error message on the stack for Lua
-bool lua_import_core(lua_State *L, const std::string &importName);
-bool lua_import(lua_State *L, const std::string &importName, bool isFullName = false);
-
 // Forward-declared from Sandbox.cpp
 void pi_lua_dofile(lua_State *l, const FileSystem::FileData &code, int nret = 0);
