bcc508124c5b8e6f3faa1945d1094574229403a0
Paul Cercueil <paul@crapouillou.net>
2020-02-19 13:44:33 -0300

    Re-introduce support for remote Lua console
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- src/lua/LuaConsole.cpp
+++ src/lua/LuaConsole.cpp
@@ -1,9 +1,11 @@
 // Copyright © 2008-2020 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-#include "LuaConsole.h"
+#include "buildopts.h"
+
 #include "FileSystem.h"
 #include "KeyBindings.h"
+#include "LuaConsole.h"
 #include "LuaManager.h"
 #include "LuaUtils.h"
 #include "Pi.h"
