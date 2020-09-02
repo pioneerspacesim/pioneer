d155a709d026bf93354282c49d88435e158c47df
Webster Sheets <webster@web-eworks.com>
2020-09-02 16:05:08 -0400

    Refactor view control API
    - Rename InteractionController->ViewController
    - Use Body's flag mechanism to exclude bodies from drawing
    - Pave the way for non-ShipView ViewControllers via SetViewController()

---
+++ src/ship/ViewController.h
@@ -0,0 +1,28 @@
+// Copyright © 2008-2020 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#pragma once
+
+class Camera;
+class WorldView;
+
+class ViewController {
+public:
+	ViewController(WorldView *v) :
+		m_parentView(v) {}
+
+	// Called when the view owning this controller becomes active
+	virtual void Activated() = 0;
+
+	// Called when the view owning this controller becomes inactive
+	virtual void Deactivated() = 0;
+
+	// Called every frame to update the controller before the camera is updated
+	virtual void Update() = 0;
+
+	// Do view-specific drawing here, called after the camera has drawn the world
+	virtual void Draw(Camera *camera) = 0;
+
+protected:
+	WorldView *m_parentView;
+};
