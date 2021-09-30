d1ff88c7729c4544b3d57f1810ca9fdccef29fce
Webster Sheets <webster@web-eworks.com>
2021-09-30 02:53:38 -0400

    Add compile-time type ID utility based on fnv-1a
    Also added literal operators for returning the hash of a literal string.

---
+++ src/core/StringHash.h
@@ -0,0 +1,16 @@
+// Copyright © 2008-2021 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#pragma once
+
+#include "FNV1a.h"
+
+inline constexpr size_t operator""_hash(const char *str, size_t len)
+{
+	return hash_64_fnv1a(str, len);
+}
+
+inline constexpr uint32_t operator""_hash32(const char *str, size_t len)
+{
+	return hash_32_fnv1a(str, len);
+}
---
+++ src/core/TypeId.h
@@ -0,0 +1,39 @@
+// Copyright © 2008-2021 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#pragma once
+
+#include "core/FNV1a.h"
+
+#ifdef _MSC_VER
+#define FUNC_SOURCE __FUNCSIG__
+#else
+#define FUNC_SOURCE __PRETTY_FUNCTION__
+#endif
+
+/**
+ * Simple class to provide compile-type constant type identifiers.
+ * These identifiers work across boundaries and remain the same between
+ * different runs of the program.
+ * They are not portable between compilers and should not be serialized.
+ */
+
+template<typename T>
+class TypeId {
+public:
+	inline static constexpr size_t Get()
+	{
+		return hash_64_fnv1a(FUNC_SOURCE, Len(FUNC_SOURCE));
+	}
+
+	inline static constexpr uint32_t Get32()
+	{
+		return hash_32_fnv1a(FUNC_SOURCE, Len(FUNC_SOURCE));
+	}
+
+private:
+	template<size_t N>
+	inline static constexpr size_t Len(char const (&s)[N]) { return N; }
+};
+
+#undef FUNC_SOURCE
