3e2e8517d62595e675b4436a64e7adc5b872d762
Webster Sheets <webster@web-eworks.com>
2021-10-11 00:29:25 -0400

    Add doctest framework

---
+++ src/test/unittest.cpp
@@ -0,0 +1,5 @@
+// Copyright © 2008-2021 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
