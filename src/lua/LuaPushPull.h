eb5f632b412ebcef56c1000cdebfe241bc79f242
Webster Sheets <webster@web-eworks.com>
2021-10-14 04:29:34 -0400

    Add LuaObjectBase::AllocateNew
    Add a templated function to allocate a new userdata sized for a specific C++ type and construct it in one line.
    This reduces the amount of type-casting required to create an object and improves readability.
    
    Added LuaPushPull overrides for string_view, size_t and uint64_t.
    Use c++14 helper typename defs.

--- src/lua/LuaPushPull.h
+++ src/lua/LuaPushPull.h
@@ -4,8 +4,8 @@
 #ifndef _LUAPUSHPULL_H
 #define _LUAPUSHPULL_H
 
-#include <lua.hpp>
 #include "Lua.h"
+#include <lua.hpp>
 
 #include <cstddef>
 #include <string>
@@ -13,18 +13,26 @@
 
 inline void pi_lua_generic_push(lua_State *l, bool value) { lua_pushboolean(l, value); }
 inline void pi_lua_generic_push(lua_State *l, int value) { lua_pushinteger(l, value); }
+inline void pi_lua_generic_push(lua_State *l, int64_t value) { lua_pushinteger(l, value); }
 inline void pi_lua_generic_push(lua_State *l, unsigned int value) { lua_pushinteger(l, value); }
+inline void pi_lua_generic_push(lua_State *l, size_t value) { lua_pushinteger(l, value); }
 inline void pi_lua_generic_push(lua_State *l, double value) { lua_pushnumber(l, value); }
 inline void pi_lua_generic_push(lua_State *l, const char *value) { lua_pushstring(l, value); }
 inline void pi_lua_generic_push(lua_State *l, const std::string &value)
 {
 	lua_pushlstring(l, value.c_str(), value.size());
 }
+inline void pi_lua_generic_push(lua_State *l, std::string_view &value)
+{
+	lua_pushlstring(l, value.data(), value.size());
+}
 inline void pi_lua_generic_push(lua_State *l, const std::nullptr_t &value) { lua_pushnil(l); }
 
 inline void pi_lua_generic_pull(lua_State *l, int index, bool &out) { out = lua_toboolean(l, index); }
 inline void pi_lua_generic_pull(lua_State *l, int index, int &out) { out = luaL_checkinteger(l, index); }
+inline void pi_lua_generic_pull(lua_State *l, int index, int64_t &out) { out = luaL_checkinteger(l, index); }
 inline void pi_lua_generic_pull(lua_State *l, int index, unsigned int &out) { out = luaL_checkunsigned(l, index); }
+inline void pi_lua_generic_pull(lua_State *l, int index, size_t &out) { out = luaL_checkunsigned(l, index); }
 inline void pi_lua_generic_pull(lua_State *l, int index, float &out) { out = luaL_checknumber(l, index); }
 inline void pi_lua_generic_pull(lua_State *l, int index, double &out) { out = luaL_checknumber(l, index); }
 inline void pi_lua_generic_pull(lua_State *l, int index, const char *&out) { out = luaL_checkstring(l, index); }
@@ -34,6 +42,13 @@ inline void pi_lua_generic_pull(lua_State *l, int index, std::string &out)
 	const char *buf = luaL_checklstring(l, index, &len);
 	std::string(buf, len).swap(out);
 }
+// the returned string view is only valid until the lua object is removed from the stack
+inline void pi_lua_generic_pull(lua_State *l, int index, std::string_view &out)
+{
+	size_t len;
+	const char *buf = luaL_checklstring(l, index, &len);
+	out = { buf, len };
+}
 
 template <typename Type>
 inline void LuaPush(lua_State *l, Type value)
@@ -42,9 +57,9 @@ inline void LuaPush(lua_State *l, Type value)
 }
 
 template <typename Type>
-inline typename std::remove_reference<Type>::type LuaPull(lua_State *l, int index)
+inline std::remove_reference_t<Type> LuaPull(lua_State *l, int index)
 {
-	typename std::decay<Type>::type value;
+	std::decay_t<Type> value;
 	pi_lua_generic_pull(l, index, value);
 	return value;
 }
