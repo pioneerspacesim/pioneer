6069814b411809564ddb8a35c95403ecd3ecc175
Webster Sheets <webster@web-eworks.com>
2023-01-06 00:13:35 -0500

    Improve missile targeting
    - Prevent missiles considering projectiles as a target to trigger detonation
    - Guided missiles will only detonate for their target
    - Missiles will attempt to get as close as possible to the target before exploding, rather than explode as soon as within 100m
    - Added coefficient to model annular-blast / shaped-charge missile payloads instead of an omnidirectional explosive

--- src/Missile.h
+++ src/Missile.h
@@ -24,7 +24,10 @@ public:
 	virtual void PostLoadFixup(Space *space) override;
 	virtual void Render(Graphics::Renderer *r, const Camera *camera, const vector3d &viewCoords, const matrix4x4d &viewTransform) override;
 	void ECMAttack(int power_val);
+
 	Body *GetOwner() const { return m_owner; }
+	const Body *GetTarget() const;
+
 	bool IsArmed() const { return m_armed; }
 	void Arm();
 	void Disarm();
@@ -35,6 +38,8 @@ protected:
 
 private:
 	void Explode();
+	bool IsValidTarget(const Body *body);
+
 	AICommand *m_curAICmd;
 	int m_power;
 	Body *m_owner;
