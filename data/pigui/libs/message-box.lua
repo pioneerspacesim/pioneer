01eca7898a7ce2d78b2e4347526a8bfe764b795f
Max5377 <69468517+Max5377@users.noreply.github.com>
2023-11-25 12:52:00 +0300

    Add new message box type
    Adds new message box "OK_CANCEL" with two buttons "OK" and "Cancel". Pressing "OK" will call function specified in "callback" parameter. As the side change, all types of message box will be positioned at the center of the screen.

--- data/pigui/libs/message-box.lua
+++ data/pigui/libs/message-box.lua
@@ -3,6 +3,8 @@
 
 local ui = require 'pigui'
 local ModalWindow = require 'pigui.libs.modal-win'
+local Lang = require 'Lang'
+local lui = Lang.GetResource("ui-core")
 
 local OK_BUTTON_WIDTH = 100
 
@@ -16,9 +18,35 @@ msgbox.OK = function(msg)
 			ui.dummy(Vector2((width - OK_BUTTON_WIDTH) / 2, 0))
 			ui.sameLine()
 		end
-		if ui.button("OK", Vector2(OK_BUTTON_WIDTH, 0)) then
+		if ui.button(lui.OK, Vector2(OK_BUTTON_WIDTH, 0)) then
 			self:close()
 		end
+	end,
+	function (_, drawPopupFn)
+		ui.setNextWindowPosCenter('Always')
+		ui.withStyleColors({ PopupBg = ui.theme.colors.modalBackground }, drawPopupFn)
+	end):open()
+end
+
+msgbox.OK_CANCEL = function(msg, callback)
+	ModalWindow.New('PopupMessageBox', function(self)
+		ui.text(msg)
+		local width = ui.getContentRegion().x
+		local okButton = ui.button(lui.OK, Vector2(OK_BUTTON_WIDTH, 0))
+		if okButton then
+			if callback then
+				callback(okButton)
+			end
+			self:close()
+		end
+		ui.sameLine(width - OK_BUTTON_WIDTH, 0)
+		if ui.button(lui.CANCEL, Vector2(OK_BUTTON_WIDTH, 0)) then
+			self:close()
+		end
+	end,
+	function (_, drawPopupFn)
+		ui.setNextWindowPosCenter('Always')
+		ui.withStyleColors({ PopupBg = ui.theme.colors.modalBackground }, drawPopupFn)
 	end):open()
 end
 
