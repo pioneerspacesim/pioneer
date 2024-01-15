d9ccc6be08802dfc0debdb32164bab86d56d8202
Webster Sheets <webster@web-eworks.com>
2024-01-15 00:27:29 -0500

    ItemCard improvements
    - Unify background drawing across callsites
    - More efficient and resilient draw cursor manipulation
    - Better handling of whole-card vs detail tooltips
    - Reserve space for "highlight bar" drawn in left margin

--- data/pigui/libs/item-card.lua
+++ data/pigui/libs/item-card.lua
@@ -59,32 +59,30 @@ end
 -- Draw an empty item card background
 function ItemCard:drawBackground(isSelected)
 
-	local lineSpacing = self.lineSpacing
 	local totalHeight = self:calcHeight()
 
 	-- calculate the background area
-	local highlightBegin = ui.getCursorScreenPos()
 	local highlightSize = Vector2(ui.getContentRegion().x, totalHeight)
-	local highlightEnd = highlightBegin + highlightSize
 
 	ui.dummy(highlightSize)
+	local tl, br = ui.getItemRect()
 
-	local isHovered = ui.isMouseHoveringRect(highlightBegin, highlightEnd + Vector2(0, lineSpacing.y)) and ui.isWindowHovered()
+	local isHovered = ui.isItemHovered()
+	local isClicked = ui.isItemClicked(0)
+
 	local bgColor = ui.getButtonColor(self.colors, isHovered, isSelected)
 
 	if self.highlightBar then
 		-- if we're hovered, we want to draw a little bar to the left of the background
 		if isHovered or isSelected then
-			ui.addRectFilled(highlightBegin - Vector2(self.rounding, 0), highlightBegin + Vector2(0, totalHeight), colors.equipScreenHighlight, 2, 5)
+			ui.addRectFilled(tl - Vector2(self.rounding, 0), tl + Vector2(0, totalHeight), colors.equipScreenHighlight, 2, 5)
 		end
 
-		ui.addRectFilled(highlightBegin, highlightEnd, bgColor, self.rounding, (isHovered or isSelected) and 10 or 0) -- 10 == top-right | bottom-right
+		ui.addRectFilled(tl, br, bgColor, self.rounding, (isHovered or isSelected) and 10 or 0) -- 10 == top-right | bottom-right
 	else
 		-- otherwise just draw a normal rounded rectangle
-		ui.addRectFilled(highlightBegin, highlightEnd, bgColor, self.rounding, 0)
+		ui.addRectFilled(tl, br, bgColor, self.rounding, 0)
 	end
-
-	local isClicked = isHovered and ui.isMouseClicked(0)
 
 	return isClicked, isHovered, highlightSize
 
@@ -99,6 +97,10 @@ end
 function ItemCard:draw(data, isSelected)
 	local lineSpacing = self.lineSpacing
 
+	if self.highlightBar then
+		ui.addCursorPos(Vector2(self.lineSpacing.x, 0))
+	end
+
 	-- initial sizing setup
 	local textHeight = pionillium.body.size + pionillium.details.size + lineSpacing.y
 
@@ -107,55 +109,33 @@ function ItemCard:draw(data, isSelected)
 
 	local textWidth = ui.getContentRegion().x - iconSize.x - lineSpacing.x * 3
 
-	local totalHeight = math.max(iconSize.y, textHeight) + lineSpacing.y * 2
-
-	-- calculate the background area
-	local highlightBegin = ui.getCursorScreenPos()
-	local highlightSize = Vector2(ui.getContentRegion().x, totalHeight)
-	local highlightEnd = highlightBegin + highlightSize
-
 	ui.beginGroup()
-	ui.dummy(highlightSize)
 
-	local isHovered = ui.isItemHovered()
-	local isClicked = ui.isItemClicked(0)
+	local isClicked, isHovered, highlightSize = self:drawBackground(isSelected)
 
-	local bgColor = ui.getButtonColor(self.colors, isHovered, isSelected)
-
-	if self.highlightBar then
-		-- if we're hovered, we want to draw a little bar to the left of the background
-		if isHovered or isSelected then
-			ui.addRectFilled(highlightBegin - Vector2(self.rounding, 0), highlightBegin + Vector2(0, totalHeight), colors.equipScreenHighlight, 2, 5)
-		end
-
-		ui.addRectFilled(highlightBegin, highlightEnd, bgColor, self.rounding, (isHovered or isSelected) and 10 or 0) -- 10 == top-right | bottom-right
-	else
-		-- otherwise just draw a normal rounded rectangle
-		ui.addRectFilled(highlightBegin, highlightEnd, bgColor, self.rounding, 0)
-	end
+	-- constrain rendering inside the frame padding area
+	local tl, br = ui.getItemRect()
+	PiGui.PushClipRect(tl + lineSpacing, br - lineSpacing, true)
 
 	local detailTooltip = nil
 
-	PiGui.PushClipRect(highlightBegin + lineSpacing, highlightEnd - lineSpacing, true)
-	ui.setCursorScreenPos(highlightBegin)
+	ui.setCursorScreenPos(tl + lineSpacing)
 
 	ui.withStyleVars({ ItemSpacing = lineSpacing }, function()
-		-- Set up padding for the top and left sides
-		ui.addCursorPos(lineSpacing + Vector2(0, iconOffset))
+		-- Draw the main icon
+		-- The icon is offset vertically to center it in the available space if
+		-- smaller than the height of the text
+		ui.addIconSimple(tl + lineSpacing + Vector2(0, iconOffset), data.icon, iconSize, colors.white)
 
-		-- Draw the main icon and add some spacing next to it
-		ui.icon(data.icon, iconSize, colors.white)
-		-- ui.addCursorPos(Vector2(iconHeight + lineSpacing.x, 0))
-		ui.sameLine()
-		ui.addCursorPos(Vector2(0, -iconOffset))
+		-- Position the cursor for the title and details
+		local textLinePos = tl + lineSpacing + Vector2(iconSize.x + lineSpacing.x, 0)
+		ui.setCursorScreenPos(textLinePos)
 
 		-- Draw the title line
-		local pos = ui.getCursorPos()
 		self:drawTitle(data, textWidth, isSelected)
 
 		-- Set up the details line
-		pos = pos + Vector2(0, ui.getTextLineHeightWithSpacing())
-		ui.setCursorPos(pos)
+		local pos = textLinePos + Vector2(0, ui.getTextLineHeightWithSpacing())
 
 		-- This block draws several small icons with text next to them
 		ui.withFont(pionillium.details, function()
@@ -166,7 +146,7 @@ function ItemCard:draw(data, isSelected)
 			-- do all of the text first to generate as few draw commands as possible
 			for i, v in ipairs(data) do
 				local offset = fieldSize * (i - 1) + smIconSize.x + 2
-				ui.setCursorPos(pos + Vector2(offset, 1)) -- HACK: force 1-pixel offset here to align baselines
+				ui.setCursorScreenPos(pos + Vector2(offset, 1)) -- HACK: force 1-pixel offset here to align baselines
 				ui.text(v[2])
 				if v[3] and ui.isItemHovered() then
 					detailTooltip = v
@@ -176,22 +156,15 @@ function ItemCard:draw(data, isSelected)
 			-- Then draw the icons
 			for i, v in ipairs(data) do
 				local offset = fieldSize * (i - 1)
-				ui.setCursorPos(pos + Vector2(offset, 0))
+				ui.setCursorScreenPos(pos + Vector2(offset, 0))
 				ui.icon(v[1], smIconSize, colors.white)
-			end
-
-			-- ensure we consume the appropriate amount of space if we don't have any details
-			if #data == 0 then
-				ui.newLine()
 			end
 		end)
 
 		-- Add a bit of spacing after the slot
 		ui.spacing()
 
-		if isHovered and not detailTooltip then
-			self:drawTooltip(data, isSelected)
-		elseif detailTooltip then
+		if isHovered and detailTooltip then
 			self:drawDetailTooltip(detailTooltip, detailTooltip[3])
 		end
 	end)
@@ -199,6 +172,10 @@ function ItemCard:draw(data, isSelected)
 	PiGui.PopClipRect()
 
 	ui.endGroup()
+
+	if ui.isItemHovered() and not detailTooltip then
+		self:drawTooltip(data, isSelected)
+	end
 
 	return isClicked, isHovered, highlightSize
 end
