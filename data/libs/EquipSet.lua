b2a3173064a88befac89482a906d4d70240cb8dc
Webster Sheets <webster@web-eworks.com>
2024-01-19 15:58:55 -0500

    New EquipSet implementation
    - Unlike the previous iteration, EquipSet v2 is concerned with modelling individual equipment items installed in unique slots
    - It also supports "loose" items which are installed somewhere in the ship's equipment volume without requiring a slot
    - Equipment volume is now completely separated from cargo volume and equipment cannot be installed in cargo space or vice versa.
    - Support for trading off equipment volume for cargo volume may be added later if deemed a good idea from a gameplay balance perspective.

---
+++ data/libs/EquipSet.lua
@@ -0,0 +1,405 @@
+-- Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+local ShipConfig = require 'ShipConfig'
+local Serializer = require 'Serializer'
+
+local utils = require 'utils'
+
+-- Class: EquipSet
+--
+-- EquipSet is responsible for managing all installed ship equipment items.
+-- It provides helpers to query installed items of specific types, and
+-- centralizes the management of "capability properties" set on the owning ship.
+
+---@class EquipSet
+---@field New fun(ship: Ship): EquipSet
+local EquipSet = utils.class("EquipSet")
+
+local function slotTypeMatches(equipType, slotType)
+	return string.sub(equipType, 1, #slotType) == slotType
+end
+
+-- Function: CompatibleWithSlot
+--
+-- Static helper function to check if the given equipment item is compatible
+-- with the given slot object. Validates type and size parameters of the slot.
+---@param equip EquipType
+---@param slot ShipDef.Slot?
+function EquipSet.CompatibleWithSlot(equip, slot)
+	local equipSlot = equip.slot or false
+	if not slot then return not equipSlot end
+
+	return equipSlot and
+		slotTypeMatches(equip.slot.type, slot.type)
+		and (equip.slot.size <= slot.size)
+		and (equip.slot.size >= (slot.size_min or slot.size))
+end
+
+---@param ship Ship
+function EquipSet:Constructor(ship)
+	self.ship = ship
+	self.config = ShipConfig[ship.shipId]
+
+	-- Stores a mapping of slot id -> equipment item
+	-- Non-slot equipment is stored in the array portion
+	self.installed = {} ---@type table<string, EquipType>|EquipType[]
+	-- Note: the integer value stored in the cache is NOT the current array
+	-- index of the given item. It's simply a non-nil integer to indicate the
+	-- item is not installed in a slot.
+	self.cache = {} ---@type table<EquipType, string|integer>
+
+	self.listeners = {}
+
+	-- Initialize ship properties we're responsible for modifying
+	self.ship:setprop("mass_cap", self.ship["mass_cap"] or 0)
+	self.ship:setprop("equipVolume", self.ship.equipVolume or 0)
+	self.ship:setprop("totalVolume", self.ship.totalVolume or self.config.capacity)
+end
+
+-- Function: GetFreeVolume
+--
+-- Returns the available volume for mounting equipment
+---@return number
+function EquipSet:GetFreeVolume()
+	return self.ship.totalVolume - self.ship.equipVolume
+end
+
+-- Function: GetSlotHandle
+--
+-- Return a reference to the slot with the given ID managed by this EquipSet.
+-- The returned slot should be considered immutable.
+---@param id string
+---@return ShipDef.Slot?
+function EquipSet:GetSlotHandle(id)
+	return self.config.slots[id]
+end
+
+-- Function: GetItemInSlot
+--
+-- Return the equipment item installed in the given slot, if present.
+---@param slot ShipDef.Slot
+---@return EquipType?
+function EquipSet:GetItemInSlot(slot)
+	-- The equipment item is not stored in the slot itself to reduce savefile
+	-- size. While the API would be marginally simpler if so, there would be a
+	-- significant amount of (de)serialization overhead.
+	return self.installed[slot.id]
+end
+
+-- Function: GetFreeSlotForEquip
+--
+-- Attempts to find an available slot where the passed equipment item could be
+-- installed. Does not attempt to find the most optimal slot - the first slot
+-- which meets the type and size constraints for the equipment item is returned.
+---@param equip EquipType
+---@return ShipDef.Slot?
+function EquipSet:GetFreeSlotForEquip(equip)
+	if not equip.slot then return nil end
+
+	local filter = function(_, slot)
+		return not slot.item
+			and slot.hardpoint == equip.slot.hardpoint
+			and self:CanInstallInSlot(slot, equip)
+	end
+
+	for _, slot in pairs(self.config.slots) do
+		if filter(_, slot) then
+			return slot
+		end
+	end
+
+	return nil
+end
+
+-- Function: GetAllSlotsOfType
+--
+-- Return a list of all slots matching the given filter parameters.
+-- If hardpoint is not specified, returns both hardpoint and internal slots.
+---@param type string
+---@param hardpoint boolean?
+---@return ShipDef.Slot[]
+function EquipSet:GetAllSlotsOfType(type, hardpoint)
+	local t = {}
+
+	for _, slot in pairs(self.config.slots) do
+		local match = (hardpoint == nil or hardpoint == slot.hardpoint)
+			and slotTypeMatches(slot.type, type)
+		if match then table.insert(t, slot) end
+	end
+
+	return t
+end
+
+-- Function: CountInstalledWithFilter
+--
+-- Return a count of all installed equipment matching the given filter function
+---@param filter fun(equip: EquipType): boolean
+---@return integer
+function EquipSet:CountInstalledWithFilter(filter)
+	local count = 0
+
+	for _, equip in pairs(self.installed) do
+		if filter(equip) then
+			count = count + (equip.count or 1)
+		end
+	end
+
+	return count
+end
+
+-- Function: GetInstalledWithFilter
+--
+-- Return a list of all installed equipment matching the given filter function
+---@param filter fun(equip: EquipType): boolean
+---@return EquipType[]
+function EquipSet:GetInstalledWithFilter(filter)
+	local out = {}
+
+	for _, equip in pairs(self.installed) do
+		if filter(equip) then
+			table.insert(out, equip)
+		end
+	end
+
+	return out
+end
+
+-- Function: GetInstalledOfType
+--
+-- Return a list of all installed equipment matching the given slot type
+---@param type string type filter
+---@return EquipType[]
+function EquipSet:GetInstalledOfType(type)
+	local out = {}
+
+	for _, equip in pairs(self.installed) do
+		if equip.slot and slotTypeMatches(equip.slot.type, type) then
+			table.insert(out, equip)
+		end
+	end
+
+	return out
+end
+
+-- Function: GetInstalledEquipment
+--
+-- Returns an array containing all equipment items installed on this ship,
+-- including both slot-based equipment and freely-installed equipment.
+---@return EquipType[]
+function EquipSet:GetInstalledEquipment()
+	local out = {}
+
+	for _, equip in pairs(self.installed) do
+		table.insert(out, equip)
+	end
+
+	return out
+end
+
+-- Function: GetInstalledNonSlot
+--
+-- Returns an array containing all non-slot equipment items installed on this
+-- ship. Items installed in a specific slot are not returned.
+--
+-- Status:
+--
+--    experimental
+--
+---@return EquipType[]
+function EquipSet:GetInstalledNonSlot()
+	local out = {}
+
+	for i, equip in ipairs(self.installed) do
+		out[i] = equip
+	end
+
+	return out
+end
+
+-- Function: CanInstallInSlot
+--
+-- Checks if the given equipment item could potentially fit in the passed slot,
+-- given the current state of the ship.
+--
+-- If there is an item in the current slot, validates the fit as though that
+-- item were not currently installed.
+-- Returns false if the equipment item is not compatible with slot mounting.
+---@param slotHandle ShipDef.Slot
+---@param equipment EquipType
+function EquipSet:CanInstallInSlot(slotHandle, equipment)
+	local equipped = self:GetItemInSlot(slotHandle)
+	local freeVolume = self:GetFreeVolume() + (equipped and equipped.volume or 0)
+
+	return (equipment.slot or false)
+		and EquipSet.CompatibleWithSlot(equipment, slotHandle)
+		and (freeVolume >= equipment.volume)
+end
+
+-- Function: CanInstallLoose
+--
+-- Checks if the given equipment item can be installed in the free equipment
+-- volume of the ship. Returns false if the equipment item requires a slot.
+---@param equipment EquipType
+function EquipSet:CanInstallLoose(equipment)
+	return not equipment.slot
+		and self:GetFreeVolume() >= equipment.volume
+end
+
+-- Function: AddListener
+--
+-- Register an event listener function to be notified of changes to this ship's
+-- equipment loadout.
+function EquipSet:AddListener(fun)
+	table.insert(self.listeners, fun)
+end
+
+-- Function: RemoveListener
+--
+-- Remove a previously-registered event listener function.
+function EquipSet:RemoveListener(fun)
+	utils.remove_elem(self.listeners, fun)
+end
+
+-- Function: Install
+--
+-- Install an equipment item in the given slot or in free equipment volume.
+--
+-- The passed equipment item must be an equipment item instance, not an
+-- equipment item prototype.
+--
+-- The passed slot must be a slot returned from caling GetSlotHandle or
+-- GetAllSlotsOfType on this EquipSet.
+---@param equipment EquipType
+---@param slotHandle ShipDef.Slot?
+---@return boolean
+function EquipSet:Install(equipment, slotHandle)
+	print("Installing equip {} in slot {}" % { equipment:GetName(), slotHandle })
+
+	if slotHandle then
+		if not self:CanInstallInSlot(slotHandle, equipment) then
+			return false
+		end
+
+		if self.installed[slotHandle.id] then
+			return false
+		end
+
+		self.installed[slotHandle.id] = equipment
+	else
+		if not self:CanInstallLoose(equipment) then
+			return false
+		end
+
+		table.insert(self.installed, equipment)
+	end
+
+	self:_InstallInternal(equipment)
+	equipment:OnInstall(self.ship, slotHandle)
+
+	for _, fun in ipairs(self.listeners) do
+		fun("install", equipment, slotHandle)
+	end
+
+	return true
+end
+
+-- Function: Remove
+--
+-- Remove a previously-installed equipment item from this ship.
+--
+-- The equipment item must be the same item instance that was installed prior;
+-- passing an equipment prototype instance will not result in any equipment
+-- item being removed.
+---@param equipment EquipType
+---@return boolean
+function EquipSet:Remove(equipment)
+	local cacheKey = self.cache[equipment]
+
+	if not cacheKey then
+		return false
+	end
+
+	local slotHandle = nil
+
+	if type(cacheKey) == "string" then
+		slotHandle = self:GetSlotHandle(cacheKey)
+	end
+
+	equipment:OnRemove(self.ship, slotHandle)
+	self:_RemoveInternal(equipment)
+
+	self.cache[equipment] = nil
+
+	if slotHandle then
+		self.installed[cacheKey] = nil
+	else
+		utils.remove_elem(self.installed, equipment)
+	end
+
+	for _, fun in ipairs(self.listeners) do
+		fun("remove", equipment, slotHandle)
+	end
+
+	return true
+end
+
+-- Update ship properties after installing an equipment item
+---@param equipment EquipType
+---@private
+function EquipSet:_InstallInternal(equipment)
+	self.ship:setprop("mass_cap", self.ship["mass_cap"] + equipment.mass)
+	self.ship:setprop("equipVolume", self.ship.equipVolume + equipment.volume)
+
+	if equipment.capabilities then
+		for k, v in pairs(equipment.capabilities) do
+			local cap = k .. "_cap"
+			self.ship:setprop(cap, (self.ship:hasprop(cap) and self.ship[cap] or 0) + v)
+		end
+	end
+
+	self.ship:UpdateEquipStats()
+end
+
+-- Update ship properties after removing an equipment item
+---@param equipment EquipType
+---@private
+function EquipSet:_RemoveInternal(equipment)
+	self.ship:setprop("mass_cap", self.ship["mass_cap"] - equipment.mass)
+	self.ship:setprop("equipVolume", self.ship.equipVolume - equipment.volume)
+
+	if equipment.capabilities then
+		for k, v in pairs(equipment.capabilities) do
+			local cap = k .. "_cap"
+			self.ship:setprop(cap, self.ship[cap] - v)
+		end
+	end
+
+	self.ship:UpdateEquipStats()
+end
+
+-- Remove transient fields from the serialized copy of the EquipSet
+function EquipSet:Serialize()
+	local obj = table.copy(self)
+
+	obj.cache = nil
+	obj.listeners = nil
+
+	return obj
+end
+
+-- Restore transient fields to the unserialized version of the EquipSet
+function EquipSet:Unserialize()
+	self.cache = {}
+	self.listeners = {}
+
+	for k, v in pairs(self.installed) do
+		self.cache[v] = k
+	end
+
+	return setmetatable(self, EquipSet.meta)
+end
+
+Serializer:RegisterClass("EquipSet", EquipSet)
+
+return EquipSet
