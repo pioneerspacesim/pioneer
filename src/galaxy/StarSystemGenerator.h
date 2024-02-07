cdc03a682460dd6929d8ca612a74f2f1d97efcd2
Oleg Solovyov <mcpain@altlinux.org>
2024-02-07 14:57:28 +0300

    move gravitation center creation code to a function

--- src/galaxy/StarSystemGenerator.h
+++ src/galaxy/StarSystemGenerator.h
@@ -45,6 +45,7 @@ public:
 	static constexpr uint32_t BODY_SATELLITE_SALT = 0xf5123a90;
 
 	SystemBody *MakeSystemBody(RefCountedPtr<StarSystem::GeneratorAPI> system, SystemBody *parent, const std::string &systemName, const char *bodySuffix);
+	SystemBody *MakeGravFor(RefCountedPtr<StarSystem::GeneratorAPI> system, const std::string &systemName, SystemBody *body1, SystemBody *body2);
 
 	virtual bool Apply(Random &rng, RefCountedPtr<Galaxy> galaxy, RefCountedPtr<StarSystem::GeneratorAPI> system, GalaxyGenerator::StarSystemConfig *config);
 
