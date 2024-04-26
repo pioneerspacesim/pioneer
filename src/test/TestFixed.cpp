0ed527ccee020bc3a50b113ce48d011e53837b1d
hexagon-recursion <hexagon-recursion@posteo.net>
2024-04-26 12:53:58 +0300

    Add more tests; Fix a bug in test; Better comments

--- src/test/TestFixed.cpp
+++ src/test/TestFixed.cpp
@@ -21,16 +21,21 @@ TEST_CASE("Fixed")
 	{
 		// 3.0 / 3.0 == 1.0
 		CHECK(fixed(3, 1) / fixed(3, 1) == fixed(1, 1));
-		// -3.0 / 3.0 == -1.0
-		// There used to be a bug causing this to produce an incorrect result
-		CHECK(fixed(-3'00000000) / fixed(3, 1) == fixed(-1'00000000));
-		// 0x40000000.00000000 / 0x40000000.00000001 == 0x00000000.ffffffff (because we round down)
-		// There used to be a bug causing this to produce an incorrect result
+		// Negative numerators used to cause incorrect results
+		// Example: -3.0 / 3.0 should be -1.0
+		CHECK(fixed(-0x3'00000000) / fixed(3, 1) == fixed(-0x1'00000000));
+		// Denominators greater then 0x40000000.00000000 or less then -0x40000000.00000000 used to cause incorrect results
+		// Example: 0x40000000.00000000 / 0x40000000.00000001 should return 0x00000000.ffffffff (because we round down)
 		CHECK(fixed(0x40000000'00000000) / fixed(0x40000000'00000001) == fixed(0x00000000'ffffffff));
 		// There used to be an edge case that triggered a signed int overflow when negating
 		// the final output if the truncation to 64 bits and conversio to Sint64 produced INT64_MIN
 		// because -INT64_MIN is undefined behavour
 		// Example: 0x00000000.80000000 / -0x00000000.00000001
 		CHECK(fixed(0x80000000) / fixed(-1) == fixed(INT64_MIN));
+		// For completeness:
+		// 3.0 / -3.0 == -1.0
+		CHECK(fixed(3, 1) / fixed(-0x3'00000000) == fixed(-0x1'00000000));
+		// -3.0 / -3.0 == 1.0
+		CHECK(fixed(-0x3'00000000) / fixed(-0x3'00000000) == fixed(1, 1));
 	}
 }
