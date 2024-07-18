463c7209891b38457576cab226c9868fe738a2db
Webster Sheets <webster@web-eworks.com>
2024-07-18 01:33:12 -0400

    ui-timer: cleanup and add type information
    - Update copyright header
    - Cleanup some unnecessary logic
    - Ensure the ui.*Timer methods are visible to the language server

--- data/pigui/libs/ui-timer.lua
+++ data/pigui/libs/ui-timer.lua
@@ -1,15 +1,16 @@
--- Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+-- Copyright © 2008-2024 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
+---@class ui
 local ui = require 'pigui'
 
 local timers = {}
 
 function ui.createTimer(name, endTime, callback)
-	timers[name] = {}
-	local timer = timers[name]
+	local timer = {}
 	timer.endTime = ui.getTime() + endTime
 	timer.callback = callback
+	timers[name] = timer
 end
 
 function ui.deleteTimer(name)
@@ -22,11 +23,9 @@ function ui.deleteTimer(name)
 end
 
 local function updateTimers()
-	if next(timers) then
-		for name, timer in pairs(timers) do
-			if ui.getTime() > timer.endTime then
-				ui.deleteTimer(name)
-			end
+	for name, timer in pairs(timers) do
+		if ui.getTime() > timer.endTime then
+			ui.deleteTimer(name)
 		end
 	end
 end
