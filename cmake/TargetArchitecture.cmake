f236209481a6f7c5da16c6df0ed518933db04095
Webster Sheets <webster@web-eworks.com>
2024-08-12 17:27:37 -0400

    Use CMake string matching instead of strict equality

--- cmake/TargetArchitecture.cmake
+++ cmake/TargetArchitecture.cmake
@@ -1,26 +1,10 @@
 # This script detects supported target architectures and configures test flags
 # accordingly
 
-if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
-    set(TARGET_ARCH x86)
-else()
-    set(TARGET_ARCH x64)
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES x86|x64)
+	set(PIONEER_TARGET_INTEL ON)
 endif()
 
-if (CMAKE_SYSTEM_PROCESSOR STREQUAL "ARM64")
-	set(TARGET_ARCH ARM64)
-endif()
-
-if (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-	set(TARGET_ARCH ARM64)
-endif()
-
-if (TARGET_ARCH STREQUAL "x86" OR TARGET_ARCH STREQUAL "x64")
-	set(PIONEER_TARGET_ARM 0)
-	set(PIONEER_TARGET_INTEL 1)
-endif()
-
-if (TARGET_ARCH STREQUAL "ARM64")
-	set(PIONEER_TARGET_ARM 1)
-	set(PIONEER_TARGET_INTEL 0)
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES aarch64|ARM64)
+	set(PIONEER_TARGET_ARM64 ON)
 endif()
