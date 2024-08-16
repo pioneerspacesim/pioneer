33027e6eaa1cd1791ab48d07373e840b22b4f433
Oskar Wallgren <oskar.wallgren13@gmail.com>
2024-08-16 19:44:36 +0200

    IsMiddleMouseButton -> EmulateMiddleMouseButton

--- src/Input.h
+++ src/Input.h
@@ -191,7 +191,7 @@ public:
 	bool IsMouseYInvert() { return mouseYInvert; }
 	void SetMouseYInvert(bool state);
 
-	bool IsMiddleMouseButton() { return noMiddleMouseButton; }
+	bool EmulateMiddleMouseButton() { return noMiddleMouseButton; }
 	void SetMiddleMouseButton(bool state);
 
 	bool IsMouseButtonPressed(int button) { return mouseButton[button] == 1; }
