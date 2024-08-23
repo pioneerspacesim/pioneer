cdf2469ec7c22ea736da36b83e491014fc880f4c
Webster Sheets <webster@web-eworks.com>
2024-08-23 01:24:16 -0400

    utils: prevent feedback of transformed keys in utils.map
    - If a transformer function returned a modified key, that key was accidentally fed back into the next call to the underlying iterator
    - Fix occurrences of two-space indentation / mis-indentation

--- data/libs/utils.lua
+++ data/libs/utils.lua
@@ -20,15 +20,15 @@ local utils = {}
 --   > for i, v in numbered_keys(pairs(table)) do ... end
 --
 function utils.numbered_keys(step, context, position)
-  local k = position
-  local f = function(s, i)
-	local v
-	k,v = step(s, k)
-	if k ~= nil then
-	  return (i+1), v
+	local k = position
+	local f = function(s, i)
+		local v
+		k,v = step(s, k)
+		if k ~= nil then
+			return (i+1), v
+		end
 	end
-  end
-  return f, context, 0
+	return f, context, 0
 end
 
 --
@@ -40,12 +40,12 @@ end
 --   > for k,v in filter(function (k,v) ... return true end, pairs(table))
 --
 function utils.filter(predicate, step, context, position)
-  local f = function (s, k)
-	local v
-	repeat k,v = step(s,k); until (k == nil) or predicate(k,v)
-	return k,v
-  end
-  return f, context, position
+	local f = function (s, k)
+		local v
+		repeat k,v = step(s,k); until (k == nil) or predicate(k,v)
+		return k,v
+	end
+	return f, context, position
 end
 
 --
@@ -57,14 +57,15 @@ end
 --   > for k,v in map(function (k,v) ... return newk, newv end, pairs(table))
 --
 function utils.map(transformer, step, context, position)
-  local f = function (s, k)
-	local v
-	k, v = step(s, k)
-	if k ~= nil then
-	  return transformer(k,v)
+	-- fully wrap the iterator so that transformed keys don't feed back into the step function
+	local f = function ()
+		local v
+		position, v = step(context, position)
+		if position ~= nil then
+			return transformer(position, v)
+		end
 	end
-  end
-  return f, context, position
+	return f, context, position
 end
 
 --
@@ -76,14 +77,14 @@ end
 --   > array = build_array(pairs(table))
 --
 function utils.build_array(f, s, k)
-  local v
-  local t = {}
-  while true do
+	local v
+	local t = {}
+	while true do
 	k, v = f(s, k)
 	if k == nil then break end
 	table.insert(t, v)
-  end
-  return t
+	end
+	return t
 end
 
 --
@@ -95,14 +96,14 @@ end
 --   > filtered = build_table(filter(function () ... end, pairs(table)))
 --
 function utils.build_table(f, s, k)
-  local v
-  local t = {}
-  while true do
+	local v
+	local t = {}
+	while true do
 	k, v = f(s, k)
 	if k == nil then break end
 	t[k] = v
-  end
-  return t
+	end
+	return t
 end
 
 --
@@ -246,47 +247,47 @@ function utils.stable_sort(values, cmp)
 	end
 
 	local split = function (values)
-	   local a = {}
-	   local b = {}
-	   local len = #values
-	   local mid = math.floor(len/2)
-	   for i = 1, mid do
-		  a[i] = values[i]
-	   end
-	   for i = mid+1, len do
-		  b[i-mid] = values[i]
-	   end
-	   return a,b
+		local a = {}
+		local b = {}
+		local len = #values
+		local mid = math.floor(len/2)
+		for i = 1, mid do
+			a[i] = values[i]
+		end
+		for i = mid+1, len do
+			b[i-mid] = values[i]
+		end
+		return a,b
 	end
 
 	local merge = function (a,b)
-	   local result = {}
-	   local a_len = #(a)
-	   local b_len = #(b)
-	   local i1 = 1
-	   local i2 = 1
-	   for j = 1, a_len+b_len do
-		  if i2 > b_len
-			 or (i1 <= a_len and cmp(a[i1], b[i2]))
-		  then
-			 result[j] = a[i1]
-			 i1 = i1 + 1
-		  else
-			 result[j] = b[i2]
-			 i2 = i2 + 1
-		  end
-	   end
-	   return result
+		local result = {}
+		local a_len = #(a)
+		local b_len = #(b)
+		local i1 = 1
+		local i2 = 1
+		for j = 1, a_len+b_len do
+			if i2 > b_len
+				or (i1 <= a_len and cmp(a[i1], b[i2]))
+			then
+				result[j] = a[i1]
+				i1 = i1 + 1
+			else
+				result[j] = b[i2]
+				i2 = i2 + 1
+			end
+		end
+		return result
 	end
 
 	local function merge_sort (values)
-	   if #values > 1 then
-		  local a, b = split(values)
-		  a = merge_sort(a)
-		  b = merge_sort(b)
-		  values = merge(a, b)
-	   end
-	   return values
+		if #values > 1 then
+			local a, b = split(values)
+			a = merge_sort(a)
+			b = merge_sort(b)
+			values = merge(a, b)
+		end
+		return values
 	end
 
 	return merge_sort(values)
