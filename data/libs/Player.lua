pioneerspacesim/pioneer

commit 99b0acdb9843d65fbb6f19e720c3e59a5abec86e

feat: reset view on hyperspace exit
Add an option to reset the view to the WorldView on hyperspace exit
to the "Game Options" tab of the settings window.

When this option is enabled, the view is reset to the WorldView after a
hyperspace jump.

data/libs/Player.lua
@@ -11,6 +11,7 @@
 local Player = package.core["Player"]
 
 local Serializer = require 'Serializer'
+local Engine = require 'Engine'
 local Event = require 'Event'
 local Game = require 'Game'
 local utils = require 'utils'
@@ -430,8 +431,16 @@ local onGameEnd = function ()
 	-- clean up for next game:
 end
 
+local onEnterSystem = function ()
+	-- Return to game view when we exit hyperspace
+	if Engine.GetResetViewOnHyperspaceExit() and Game.CurrentView() ~= "world" then
+		Game.SetView("world")
+	end
+end
+
 Event.Register("onGameEnd", onGameEnd)
 Event.Register("onGameStart", onGameStart)
+Event.Register("onEnterSystem", onEnterSystem)
 Serializer:RegisterClass("CrimeRecord", CrimeRecord)
 Serializer:Register("Player", serialize, unserialize)
 

data/pigui/modules/settings-window.lua
@@ -633,11 +633,17 @@ end
 
 local function showGameOptions()
 	local enableAutoSave = Engine.GetAutosaveEnabled()
+	local resetViewOnHyperspaceExit = Engine.GetResetViewOnHyperspaceExit()
 	local c
 
 	c,enableAutoSave = checkbox(lui.ENABLE_AUTOSAVE, enableAutoSave, lui.ENABLE_AUTOSAVE_DESC)
 	if c then
 		Engine.SetAutosaveEnabled(enableAutoSave)
+	end
+
+	c,resetViewOnHyperspaceExit = checkbox(lui.RESET_VIEW_ON_HYPERSPACE_EXIT, resetViewOnHyperspaceExit, lui.RESET_VIEW_ON_HYPERSPACE_EXIT_DESC)
+	if c then
+		Engine.SetResetViewOnHyperspaceExit(resetViewOnHyperspaceExit)
 	end
 end
 

src/lua/LuaEngine.cpp
@@ -580,6 +580,21 @@ static int l_engine_set_autosave_enabled(lua_State *l)
 	Pi::config->Save();
 	return 0;
 }
+static int l_engine_get_reset_view_on_hyperspace_exit(lua_State *l)
+{
+	lua_pushboolean(l, Pi::config->Int("ResetViewOnHyperspaceExit") != 0);
+	return 1;
+}
+
+static int l_engine_set_reset_view_on_hyperspace_exit(lua_State *l)
+{
+	if (lua_isnone(l, 1))
+		return luaL_error(l, "SetResetViewOnHyperspaceExit takes one boolean argument");
+	const bool enabled = lua_toboolean(l, 1);
+	Pi::config->SetInt("ResetViewOnHyperspaceExit", (enabled ? 1 : 0));
+	Pi::config->Save();
+	return 0;
+}
 
 static int l_engine_get_display_hud_trails(lua_State *l)
 {
@@ -1103,6 +1118,9 @@ void LuaEngine::Register()
 
 		{ "GetAutosaveEnabled", l_engine_get_autosave_enabled },
 		{ "SetAutosaveEnabled", l_engine_set_autosave_enabled },
+
+		{ "GetResetViewOnHyperspaceExit", l_engine_get_reset_view_on_hyperspace_exit },
+		{ "SetResetViewOnHyperspaceExit", l_engine_set_reset_view_on_hyperspace_exit },
 
 		{ "GetDisplayHudTrails", l_engine_get_display_hud_trails },
 		{ "SetDisplayHudTrails", l_engine_set_display_hud_trails },

commit c070cf13cb39e48b60f02dbcbcbf6d7af0f9710c

fix: only reset view to WorldView when Player enters system
Fixes #5935

Bug introduced in 99b0acdb9843d65fbb6f19e720c3e59a5abec86e, the
'onEnterSystem' event needs to check whether the ship which entered the
system is the Player.

data/libs/Player.lua
@@ -431,7 +431,8 @@ local onGameEnd = function ()
 	-- clean up for next game:
 end
 
-local onEnterSystem = function ()
+local onEnterSystem = function (ship)
+	if not ship.IsPlayer() then return end
 	-- Return to game view when we exit hyperspace
 	if Engine.GetResetViewOnHyperspaceExit() and Game.CurrentView() ~= "world" then
 		Game.SetView("world")