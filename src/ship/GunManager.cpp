ab67426e88b1992c8f22a93b4e774a93f7d251e1
Webster Sheets <webster@web-eworks.com>
2024-12-03 23:13:47 -0500

    Add GunManager implementation
    - Responsible for managing all "permanent" weapons (i.e. not missiles) attached to a ship.
    - Stores all associated data about a ship's hull configuration and mounted weapons directly, due to a lack of support for serialization of LuaSharedObject.
    - Each weapon is assigned to a single group, which is responsible for sending fire commands to that weapon and managing targeting.
    - Originally written (but not merged) in 2021.
    - Future expansion is intended to support more flexible damage types, explosive projectiles, and dumbfire rocket launchers.
    - This system is intended to handle turrets in the future, via extensions to the weapon mounting system to support multiple traverse options.
    
    GunManager: add FiredThisFrame/StoppedThisFrame
    
    - Supports our extremely hacky sound code system
    - Ideally a "better" solution would be found at some point, but this is adequate for the time being
    
    GunManager: add GetGroupLeadPos
    
    - Temporary accessor that sums together the lead positions of all weapons in the group to support indicator rendering
    
    GunManager: add GetGroupTemperatureState
    
    - Temporary API to query the average temperature of all weapons in the group relative to their overheat threshold

---
+++ src/ship/GunManager.cpp
@@ -0,0 +1,519 @@
+// Copyright © 2008-2024 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "GunManager.h"
+
+#include "Beam.h"
+#include "Projectile.h"
+#include "Game.h"
+#include "ModelBody.h"
+#include "Pi.h"
+#include "core/Log.h"
+#include "lua/LuaBodyComponent.h"
+#include "matrix4x4.h"
+#include "scenegraph/Tag.h"
+
+#include "imgui/imgui.h"
+
+REGISTER_COMPONENT_TYPE(GunManager) {
+	BodyComponentDB::RegisterComponent<GunManager>("GunManager");
+	BodyComponentDB::RegisterSerializer<GunManager>();
+	BodyComponentDB::RegisterLuaInterface<GunManager>();
+}
+
+void GunManager::Init(ModelBody *b)
+{
+	m_parent = b;
+
+	// Ensure we always have a default group for weapons
+	if (m_groups.empty()) {
+		m_groups.push_back({});
+	}
+}
+
+void GunManager::SaveToJson(Json &jsonObj, Space *space)
+{
+}
+
+void GunManager::LoadFromJson(const Json &jsonObj, Space *space)
+{
+}
+
+void GunManager::AddWeaponMount(StringName id, StringName tagName, vector2f gimbalLimit)
+{
+	WeaponMount mount = {};
+
+	mount.id = id;
+	mount.tag = m_parent->GetModel()->FindTagByName(tagName);
+	mount.gimbalLimitTan = vector2f(
+		tan(DEG2RAD(gimbalLimit.x)),
+		tan(DEG2RAD(gimbalLimit.y))
+	);
+
+	m_mounts.try_emplace(id, mount);
+}
+
+void GunManager::RemoveWeaponMount(StringName id)
+{
+	auto iter = m_mounts.find(id);
+	if (iter == m_mounts.end())
+		return;
+
+	if (IsWeaponMounted(id))
+		return;
+
+	m_mounts.erase(iter);
+}
+
+bool GunManager::MountWeapon(StringName hardpoint, const WeaponData &gunData)
+{
+	auto iter = m_mounts.find(hardpoint);
+	if (iter == m_mounts.end())
+		return false;
+
+	if (IsWeaponMounted(hardpoint))
+		return false;
+
+	WeaponState ws = {};
+
+	ws.mount = &iter->second;
+	ws.data = gunData;
+
+	// TODO: render weapon models on hardpoint
+	// if (!gunData.modelPath.empty()) {
+	// 	ws.model = Pi::FindModel(gunData.modelPath);
+	// }
+
+	// assign the new weapon to group zero
+	// TODO: make a new group for this weapon?
+	m_groups.front().weapons[m_weapons.size()] = true;
+	m_weapons.push_back(ws);
+
+	return true;
+}
+
+void GunManager::UnmountWeapon(StringName hardpoint)
+{
+	auto iter = std::find_if(m_weapons.begin(), m_weapons.end(), [&](const WeaponState &ws) {
+		return ws.mount->id == hardpoint;
+	});
+
+	if (iter != m_weapons.end()) {
+		uint32_t index = std::distance(m_weapons.begin(), iter);
+
+		// Update all group weapon assignments
+		for (auto &group : m_groups) {
+			RemoveGroupIndex(group.weapons, index);
+		}
+
+		// TODO: any additional cleanup needed for other systems that hook into GunManager, e.g. heat, targeting, etc.
+		m_weapons.erase(iter);
+	}
+}
+
+bool GunManager::IsWeaponMounted(StringName hardpoint) const
+{
+	return std::find_if(m_weapons.begin(), m_weapons.end(), [&](const WeaponState &ws) {
+		return ws.mount->id == hardpoint;
+	}) != m_weapons.end();
+}
+
+void GunManager::RemoveGroupIndex(WeaponIndexSet &group, uint32_t index)
+{
+	// Mask containing all of the indices below index
+	WeaponIndexSet keep = WeaponIndexSet().set() >> (NUM_WEAPON_INDICES - index);
+	// Mask containing all of the indices above index
+	WeaponIndexSet mask = WeaponIndexSet().set() << (index + 1);
+
+	// Decrement all contained indices in the set above the removed index down by one
+	group = (group & keep) | ((group & mask) >> 1);
+}
+
+uint32_t GunManager::GetWeaponIndexForHardpoint(StringName hardpoint) const
+{
+	auto iter = std::find_if(m_weapons.begin(), m_weapons.end(), [&](const WeaponState &ws) {
+		return ws.mount->id == hardpoint;
+	});
+
+	if (iter == m_weapons.end()) {
+		return UINT32_MAX;
+	}
+
+	return std::distance(m_weapons.begin(), iter);
+}
+
+void GunManager::SetupDefaultGroups()
+{
+	m_groups.clear();
+
+	GroupState group = {};
+
+	for (size_t i = 0; i < m_weapons.size(); i++) {
+		group.weapons[i] = true;
+	}
+
+	m_groups.push_back(group);
+}
+
+void GunManager::SetAllGroupsFiring(bool firing)
+{
+	for (size_t idx = 0; idx < m_groups.size(); idx++) {
+		SetGroupFiring(idx, firing);
+	}
+
+	m_isAnyFiring = firing;
+}
+
+void GunManager::AssignWeaponToGroup(uint32_t numWeapon, uint32_t group)
+{
+	if (numWeapon > m_weapons.size())
+		return;
+
+	if (group >= m_groups.size()) {
+		m_groups.resize(group + 1);
+	}
+
+	WeaponState &ws = m_weapons[numWeapon];
+
+	uint32_t oldGroup = ws.group;
+	m_groups[oldGroup].weapons[numWeapon] = false;
+
+	// Prune empty groups when the last weapon is removed
+	if (m_groups[oldGroup].weapons.none()) {
+		RemoveGroup(oldGroup);
+	}
+
+	ws.group = group;
+	m_groups[group].weapons[numWeapon] = true;
+}
+
+void GunManager::RemoveGroup(uint32_t group)
+{
+	if (group == 0 || group >= m_groups.size())
+		return;
+
+	GroupState &gzero = m_groups[0];
+
+	for (size_t idx = 0; idx < m_weapons.size(); idx++) {
+		WeaponState &weapon = m_weapons[idx];
+
+		// Reassign weapons from this group to group zero
+		if (weapon.group == group) {
+			weapon.group = 0;
+			gzero.weapons[idx] = true;
+		}
+
+		// Reduce index of all groups above this
+		if (weapon.group > group) {
+			weapon.group--;
+		}
+	}
+
+	// Finally, clear the group from the list
+	m_groups.erase(m_groups.begin() + group);
+}
+
+const Body *GunManager::GetGroupTarget(uint32_t group)
+{
+	if (group >= m_groups.size())
+		return nullptr;
+
+	return m_groups[group].target;
+}
+
+void GunManager::SetGroupTarget(uint32_t group, const Body *target)
+{
+	if (group >= m_groups.size())
+		return;
+
+	GroupState &gs = m_groups[group];
+	// No weapons at all, ignore
+	if (!gs.weapons.any())
+		return;
+
+	// Clear the prior callback, if any
+	if (gs.target)
+		gs.onTargetDestroyed.disconnect();
+
+	m_groups[group].target = target;
+
+	if (target) {
+		gs.onTargetDestroyed = target->onDelete.connect([=]() {
+			this->SetGroupTarget(group, nullptr);
+		});
+	}
+}
+
+void GunManager::SetGroupFiring(uint32_t group, bool firing)
+{
+	if (group >= m_groups.size())
+		return;
+
+	GroupState &gs = m_groups[group];
+	// No weapons at all, ignore
+	if (!gs.weapons.any())
+		return;
+
+	gs.firing = firing;
+	m_isAnyFiring |= firing;
+
+	if (firing) {
+		// Update the next-firing time for all weapons in this group so they actually fire
+		// (Ensures you can't spam the fire command to shoot more rapidly than the weapon's rate of fire)
+		for (size_t idx = 0; idx < m_weapons.size(); idx++) {
+			if (gs.weapons[idx]) {
+				m_weapons[idx].nextFireTime = std::max(m_weapons[idx].nextFireTime, Pi::game->GetTime());
+			}
+		}
+	} else {
+		m_stoppedNextFrame |= gs.weapons;
+	}
+}
+
+void GunManager::SetGroupFireWithoutTargeting(uint32_t group, bool enabled)
+{
+	if (group >= m_groups.size())
+		return;
+
+	GroupState &gs = m_groups[group];
+	gs.fireWithoutTargeting = enabled;
+}
+
+vector3d GunManager::GetGroupLeadPos(uint32_t group)
+{
+	if (group >= m_groups.size() || !m_groups[group].target || m_groups[group].weapons.none())
+		return vector3d(0, 0, 0);
+
+	GroupState &gs = m_groups[group];
+	double inv_count = 1.0 / double(gs.weapons.count());
+	vector3d lead_pos = vector3d(0, 0, 0);
+
+	for (size_t idx = 0; idx < m_weapons.size(); idx++) {
+		if (gs.weapons[idx]) {
+			lead_pos += m_weapons[idx].currentLeadPos * inv_count;
+		}
+	}
+
+	return lead_pos;
+}
+
+float GunManager::GetGroupTemperatureState(uint32_t group)
+{
+	if (group >= m_groups.size() || m_groups[group].weapons.none())
+		return 0.f;
+
+	GroupState &gs = m_groups[group];
+	double inv_count = 1.0 / double(gs.weapons.count());
+	float temperature = 0.f;
+
+	for (size_t idx = 0; idx < m_weapons.size(); idx++) {
+		if (gs.weapons[idx]) {
+			temperature += inv_count * (m_weapons[idx].temperature / m_weapons[idx].data.overheatThreshold);
+		}
+	}
+
+	return temperature;
+}
+
+void GunManager::StaticUpdate(float deltaTime)
+{
+	bool isAnyFiring = false;
+
+	static ImGuiOnceUponAFrame thisFrame;
+	bool shouldDrawDebug = m_parent && m_parent->IsType(ObjectType::PLAYER) && thisFrame;
+	if (shouldDrawDebug)
+		ImGui::Begin("GunMangerDebug", nullptr, ImGuiWindowFlags_NoFocusOnAppearing);
+
+	if (shouldDrawDebug) {
+		ImGui::Text("Gun mounts: %ld, mounted: %ld", m_mounts.size(), m_weapons.size());
+		ImGui::Spacing();
+	}
+
+	m_stoppedThisFrame = m_stoppedNextFrame;
+	m_stoppedNextFrame.reset();
+	m_firedThisFrame.reset();
+
+	// TODO(sensors): go through groups and collate sensor information on group target
+
+	for (WeaponState &gun : m_weapons) {
+
+		GroupState &gs = m_groups[gun.group];
+
+		// REMOVE: Draw debug state
+		if (shouldDrawDebug) {
+			ImGui::Text("== Gun Mount: %s, target %s", gun.mount->id.c_str(), gs.target ? gs.target->GetLabel().c_str() : "<none>");
+			ImGui::Indent();
+			ImGui::Text("Temperature: %f", gun.temperature);
+			ImGui::Text("Firing: %d", gs.firing);
+			ImGui::Text("Damage: %f", gun.data.projectile.damage);
+			ImGui::Text("NextFireTime: %f", gun.nextFireTime);
+			ImGui::Text("Current Time: %f", Pi::game->GetTime());
+			ImGui::Text("Firing RPM: %f", gun.data.firingRPM);
+			ImGui::Text("Firing Heat: %f", gun.data.firingHeat);
+			ImGui::Text("Overheat At: %f", gun.data.overheatThreshold);
+			ImGui::Text("Beam: %d", gun.data.projectile.beam);
+			ImGui::Text("Mining: %d", gun.data.projectile.mining);
+			const auto &leadDir = gun.currentLead;
+			ImGui::Text("CurrentLeadDir: %f, %f, %f", leadDir.x, leadDir.y, leadDir.z);
+			ImGui::Unindent();
+			ImGui::Spacing();
+		}
+
+		bool isBeam = gun.data.projectile.beam || gun.data.projectileType == PROJECTILE_BEAM;
+
+		// Compute weapon lead direction
+		if (gs.target) {
+			const matrix3x3d &orient = m_parent->GetOrient();
+			const vector3d relPosition = gs.target->GetPositionRelTo(m_parent);
+			const vector3d relVelocity = gs.target->GetVelocityRelTo(m_parent->GetFrame()) - m_parent->GetVelocity();
+
+			// bring velocity and acceleration into ship-space
+			CalcWeaponLead(&gun, relPosition * orient, relVelocity * orient);
+		} else {
+			gun.currentLead = vector3f(0, 0, 1);
+			gun.currentLeadPos = vector3d(0, 0, 0);
+		}
+
+		// Update gun cooling per tick
+		gun.temperature = std::max(0.0f, gun.temperature - gun.data.coolingPerSecond * m_coolingBoost * deltaTime);
+		double currentTime = Pi::game->GetTime();
+
+		// determine if we should fire this update
+		isAnyFiring |= gs.firing;
+
+		// determine if something prevents this gun from firing
+		// Temperature, gimbal checks, etc.
+		bool canFire = gun.temperature < gun.data.overheatThreshold && (gs.fireWithoutTargeting || gs.target && gun.withinGimbalLimit);
+
+		if (gs.firing && currentTime >= gun.nextFireTime) {
+
+			// Determine how much time we missed since the gun was supposed to fire
+			double missedTime = currentTime - gun.nextFireTime;
+			// time between shots, used to determine how many shots we need to 'catch up' on this timestep
+			double deltaShot = 60.0 / gun.data.firingRPM;
+			// only fire multiple shots per timestep if the accumulated error and the length of the timestep require it
+			// given that timescale is set to 1 while in combat, this is likely not going to be required except for NPCs
+			uint32_t numShots = 1 + floor((missedTime + deltaTime) / deltaShot);
+
+			for (uint32_t i = 0; i < numShots; ++i) {
+				Fire(&gun, &gs);
+			}
+
+			// set the next fire time, making sure to preserve accumulated (fractional) shot time
+			gun.nextFireTime += deltaShot * numShots;
+
+			size_t weaponIdx = std::distance(&m_weapons.front(), &gun);
+			m_firedThisFrame[weaponIdx] = true;
+
+		}
+
+		// ensure next fire time is properly handled when the gun is meant to be firing
+		// but unable to fire (e.g. during gun overheat)
+		if (gs.firing)
+			gun.nextFireTime = std::max(gun.nextFireTime, currentTime);
+
+	}
+
+	m_isAnyFiring = isAnyFiring;
+
+	if (shouldDrawDebug)
+		ImGui::End();
+}
+
+void GunManager::Fire(WeaponState *weapon, GroupState *group)
+{
+	WeaponData *data = &weapon->data;
+
+	// either fire the next barrel in sequence or fire all at the same time
+	size_t firstBarrel = 0;
+	size_t numBarrels = 1;
+	if (data->staggerBarrels || data->numBarrels == 1) {
+		firstBarrel = (weapon->lastBarrel + 1) % data->numBarrels;
+		weapon->lastBarrel = firstBarrel;
+	} else {
+		numBarrels = data->numBarrels;
+	}
+
+	const matrix4x4f &xform = GetMountTransform(weapon);
+	const vector3d wpn_pos = vector3d(xform.GetTranslate());
+	const matrix3x3f wpn_orient = xform.GetOrient();
+
+	const matrix3x3d &orient = m_parent->GetOrient();
+
+	// mount-relative aiming direction
+	const vector3d leadDir = vector3d(wpn_orient * weapon->currentLead).Normalized();
+
+	for (size_t idx = firstBarrel; idx < firstBarrel + numBarrels; idx++) {
+		weapon->temperature += data->firingHeat;
+		// TODO: get individual barrel locations from gun model and cache them
+		const vector3d dir = orient * leadDir;
+		const vector3d pos = orient * wpn_pos + m_parent->GetPosition();
+
+		// TODO: projectile system using new ProjectileDef data
+		if (data->projectile.beam) {
+			Beam::Add(m_parent, data->projectile, pos, m_parent->GetVelocity(), dir);
+		} else {
+			Projectile::Add(m_parent, data->projectile, pos, m_parent->GetVelocity(), data->projectile.speed * dir);
+		}
+	}
+}
+
+// Note that position and relative velocity are in the coordinate system of the host body
+void GunManager::CalcWeaponLead(WeaponState *state, vector3d position, vector3d relativeVelocity)
+{
+	// Compute the forward vector for the weapon mount
+	const matrix4x4f &xform = GetMountTransform(state);
+	const vector3f forward = vector3f(0, 0, 1);
+
+	if (state->data.projectileType == PROJECTILE_BALLISTIC) {
+		// Calculate firing solution and relative velocity along our z axis by
+		// computing the position along the enemy ship's lead vector at which to aim
+		const double projspeed = state->data.projectile.speed;
+		// Account for the distance between the weapon mount and the center of the parent
+		position -= vector3d(xform.GetTranslate());
+
+		// 1. First, generate a basic approximation to estimate travel time
+		vector3d leadpos = position + relativeVelocity * (position.Length() / projspeed);
+		// 2. This second-order approximation yields a much more accurate estimate
+		float travelTime = leadpos.Length() / projspeed;
+		leadpos = position + relativeVelocity * travelTime;
+		// A third-order approximation here would need to take into account the target's
+		// expected acceleration over the duration by using a moving average of acceleration
+		// rather than taking the instantaneous forces acting on the body.
+		// Thus, we refrain from implementing it for now.
+
+		state->currentLeadPos = leadpos;
+	} else if (state->data.projectileType == PROJECTILE_BEAM) {
+		// Beam weapons should just aim at the target
+		state->currentLeadPos = position;
+	}
+
+	// Transform the target's direction into the coordinate space of the mount,
+	// with the barrel pointing "forward" towards +Z.
+	// float has plenty of precision when working with normalized directions.
+	vector3f targetDir = vector3f(state->currentLeadPos.Normalized()) * xform.GetOrient();
+
+	// We represent the maximum traverse of the weapon as an ellipse relative
+	// to the +Z axis of the mount.
+	// To determine whether the lead target is within this traverse, we modify
+	// the coordinate system such that the ellipse becomes the unit circle in
+	// 2D space, and test the length of the 2D components of the direction
+	// vector.
+	// Note that we scale the targetDir vector such that the z component has a length of 1.0,
+	// so that the comparison with the tangent of the gimbal limit is correct.
+	vector2f traverseRel = (targetDir * (1.0 / targetDir.z)).xy() / state->mount->gimbalLimitTan;
+
+	state->withinGimbalLimit = targetDir.z > 0 && traverseRel.LengthSqr() <= 1.0;
+	state->currentLead = state->withinGimbalLimit ? targetDir : forward;
+}
+
+static const matrix4x4f s_noMountTransform = matrix4x4f::RotateXMatrix(M_PI);
+
+const matrix4x4f &GunManager::GetMountTransform(WeaponState *state)
+{
+	if (state->mount->tag) {
+		return state->mount->tag->GetGlobalTransform();
+	}
+
+	return s_noMountTransform;
+}
---
+++ src/ship/GunManager.h
@@ -0,0 +1,222 @@
+// Copyright © 2008-2024 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#pragma once
+
+#include "ConnectionTicket.h"
+#include "Projectile.h"
+#include "core/StringName.h"
+#include "lua/LuaWrappable.h"
+#include "scenegraph/Model.h"
+#include <bitset>
+#include <string>
+
+class ModelBody;
+
+/**
+ * Class: GunManager
+ *
+ * The GunManager is responsible for managing all combat-related aspects of ship
+ * weaponry.
+ *
+ * The typical lifecycle of the GunManager component is as follows:
+ * - A GunManager is constructed when a ship is created or its type changed
+ * - It is informed about the available weapon mounts on a ship shortly after construction
+ * - Weapons are attached to those mounts by the equipment code
+ * - Weapons are assigned group indices to allow separate fire control and targeting
+ * - Targets are assigned to individual groups
+ * - A higher-level input manager sends fire commands for each group
+ * - Weapon mounts are added or removed as needed by equipment attached to the ship
+ * - Finally, when the ship is destroyed or its type changed, the GunManager is destroyed
+ */
+class GunManager : public LuaWrappable {
+public:
+	static constexpr size_t NUM_WEAPON_INDICES = 256;
+	using WeaponIndexSet = std::bitset<NUM_WEAPON_INDICES>;
+
+	struct WeaponMount;
+
+	enum ProjectileType : uint8_t { // <enum scope='GunManager' name='ProjectileType' prefix='PROJECTILE_' public>
+		PROJECTILE_BALLISTIC, // Impact weapons: pulsecannons, railguns, etc.
+		PROJECTILE_BEAM,      // Instantaneous-hit weapons: lasers
+	};
+
+	struct ProjectileDef {
+		float lifespan = 5.0;
+		float speed = 1000.0;
+
+		// Damage
+		float impactDamage = 0.0; // How much damage is dealt when this projectile hits a target
+		float explosiveDamage = 0.0; // When this projectile detonates (impact or proximity fuse) how much damage is dealt in the radius?
+		float explosionRadius = 0.0; // How large of an explosion radius does this projectile have?
+		float explosionFalloff = 1.0; // How does the damage fall off from the center to the edge of the explosion? 2.0 = quadratic, 1.0 = linear, 0.0 = no falloff, use "impact area" approximation instead
+		// TODO: damage type
+
+		// Feature flags
+		uint8_t isMining : 1; // Does this projectile deal "mining" damage? TODO: better damage type system
+		uint8_t hasProxyFuse : 1; // Does this projectile have a proximity fuse?
+		uint8_t proxyIFF : 1; // Is the proximity fuse capable of reading IFF data?
+
+		// Fusing
+		float proxyFuseRadius = 0.0; // This projectile will detonate when it detects a body within this range
+		float proxyFuseArmTime = 0.5; // How long after firing before the projectile fuse is armed?
+
+		// Visual settings
+		float length = 0;
+		float width = 0;
+		Color color;
+	};
+
+	// Information about a specific "weapon type"
+	// TODO: create one of these in Lua per weapon definition and reference them from each mounted gun
+	// TODO: create a separate projectile definition and reference it
+	struct WeaponData {
+		float firingRPM = 240;	// number of shots per minute (60s / time between shots)
+		float firingHeat = 4.5; // amount of thermal energy(kJ) emitted into the system per shot
+
+		//TODO: integrate this with a whole-ship cooling system
+		float coolingPerSecond = 14.2; // nominal amount of thermal energy removed per second (kW)
+		float overheatThreshold = 280; // total amount of thermal energy(kJ) the gun can store while functioning
+
+		std::string modelPath; // model to render this weapon with
+
+		ProjectileData projectile; // deprecated, to replace with RefCountedPtr<ProjectileDef>
+		ProjectileType projectileType = PROJECTILE_BALLISTIC;
+		uint8_t numBarrels = 1;		 // total number of barrels on the model
+		bool staggerBarrels = false; // should we fire one barrel after another, or both at the same time?
+	};
+
+	// Information about a specific mounted weapon (serialized directly)
+	struct WeaponState {
+		WeaponMount *mount;					// Mounted hardpoint
+		uint8_t group = 0;					// Group this hardpoint belongs to
+		bool withinGimbalLimit = false;		// does the gun have a valid firing solution on the target?
+
+		uint8_t lastBarrel = 0;	 // the last barrel used to fire (for multi-barrel weapons)
+		float temperature = 0;	 // current gun temperature
+		double nextFireTime = 0; // time at which the gun will be ready to fire again
+		// TODO: integrate this with a whole-ship cooling system
+		// Currently uses GunManager::m_coolingBoost
+		// float coolerOverclock = 1.0; // any boost to gun coolers from ship equipment
+		// float powerOverclock = 1.0; // boost to overall firerate (and corresponding increase in heat generation)
+
+		vector3f currentLead;
+		vector3d currentLeadPos;
+
+		WeaponData data;
+		SceneGraph::Model *model; // gun model, currently unused
+	};
+
+	struct WeaponMount {
+		StringName id;
+		SceneGraph::Tag *tag;     // Tag in the parent model that this weapon mount is attached to
+		vector2f gimbalLimitTan;  // tangent of gimbal limits
+		//SceneGraph::Model *model; // model to render for this weapon mount, if any
+		// TODO: enable/disable hardpoint based on ship configuration, i.e. landing/vtol/wings?
+	};
+
+	struct GroupState {
+		WeaponIndexSet weapons;				// Whic weapons are assigned to this group?
+		const Body *target;					// The target for this group, if any
+		bool firing;						// Is the group currently firing
+		bool fireWithoutTargeting;			// Can the group fire without a target/target not in gimbal range?
+		ConnectionTicket onTargetDestroyed;	// Unlock the target once it's been destroyed
+	};
+
+	GunManager() = default;
+
+	void Init(ModelBody *b);
+
+	void SaveToJson(Json &jsonObj, Space *space);
+	void LoadFromJson(const Json &jsonObj, Space *space);
+
+	void StaticUpdate(float deltaTime);
+
+	// ==========================================
+
+	// Add a weapon mount to this gun manager
+	void AddWeaponMount(StringName id, StringName tagName, vector2f gimbalLimitDegrees);
+	// Remove a weapon mount from this gun manager. This will fail if a weapon is still mounted.
+	void RemoveWeaponMount(StringName id);
+
+	// Attach a weapon to a specific mount
+	bool MountWeapon(StringName hardpoint, const WeaponData &data);
+	// Remove the attached weapon from a specific mount
+	void UnmountWeapon(StringName hardpoint);
+	// Check if a weapon is attached to a specific mount
+	bool IsWeaponMounted(StringName hardpoint) const;
+
+	const std::vector<WeaponState> &GetWeapons() const { return m_weapons; }
+
+	uint32_t GetNumWeapons() const { return m_weapons.size(); }
+	const WeaponState *GetWeaponState(uint32_t numWeapon) const { return m_weapons.size() > numWeapon ? &m_weapons[numWeapon] : nullptr; }
+	uint32_t GetWeaponIndexForHardpoint(StringName hardpoint) const;
+
+	// ==========================================
+
+	// For AI/NPC ship usage, combines all weapons into a single group
+	void SetupDefaultGroups();
+	void SetTrackingTarget(const Body *target) { return SetGroupTarget(0, target); }
+	void SetAllGroupsFiring(bool firing = false);
+
+	// For player weapon management
+
+	// Assign a specific weapon to a firing group
+	// Weapons are by default all assigned to group 0
+	void AssignWeaponToGroup(uint32_t numWeapon, uint32_t group);
+	// Remove a weapon group in its entirety
+	void RemoveGroup(uint32_t group);
+
+	const std::vector<GroupState> &GetWeaponGroups() const { return m_groups; }
+
+	const Body *GetGroupTarget(uint32_t group);
+	void SetGroupTarget(uint32_t group, const Body *target);
+	void SetGroupFiring(uint32_t group, bool firing);
+	// Sets whether the group will authorize individual weapons to fire without a target within the gimbal arc of the weapon
+	void SetGroupFireWithoutTargeting(uint32_t group, bool enabled);
+
+	// TEMP: return the average lead position of a firing group - should query individual weapons instead
+	vector3d GetGroupLeadPos(uint32_t group);
+	// TEMP: return the average temperature state of a weapon group in 0..1
+	float GetGroupTemperatureState(uint32_t group);
+
+	// ==========================================
+
+	bool IsFiring() const { return m_isAnyFiring; }
+	bool IsGroupFiring(uint32_t group) const { return m_groups.size() > group ? m_groups[group].firing : false; }
+
+	// TODO: separate this functionality to a ship-wide cooling system
+	void SetCoolingBoost(float boost) { m_coolingBoost = boost; }
+
+	// Semi-hacky, would like to replace with a better eventing system of some sort
+	WeaponIndexSet GetGunsFiredThisFrame() const { return m_firedThisFrame; }
+	WeaponIndexSet GetGunsStoppedThisFrame() const { return m_stoppedThisFrame; }
+
+private:
+
+	// Handle checking and firing a given gun.
+	// Note that this currently does not nicely handle spawning multiple projectiles per timestep - i.e. timewarp or a weapon RPM higher than 3600
+	// Projectile spawns are also "snapped" to the start of a timestep if they are not direct multiples of the timestep duration
+	void Fire(WeaponState *weapon, GroupState *group);
+
+	// Calculate the position a given gun should aim at to hit the current target body
+	// This is effectively the position of the target at T+n
+	void CalcWeaponLead(WeaponState *state, vector3d position, vector3d relativeVelocity);
+
+	const matrix4x4f &GetMountTransform(WeaponState *weapon);
+
+	void RemoveGroupIndex(WeaponIndexSet &group, uint32_t index);
+
+	std::vector<GroupState> m_groups;
+	std::vector<WeaponState> m_weapons;
+	std::map<StringName, WeaponMount> m_mounts;
+
+	WeaponIndexSet m_firedThisFrame;
+	WeaponIndexSet m_stoppedThisFrame;
+	WeaponIndexSet m_stoppedNextFrame;
+
+	ConnectionTicket m_targetDestroyedCallback;
+	ModelBody *m_parent = nullptr;
+	bool m_isAnyFiring = false;
+	float m_coolingBoost = 1.0;
+};
