8325b05e30e62cf7f105bd5e7db82a9e204097b4
Karl F <karlfogel@gmail.com>
2024-12-31 09:42:36 +0100

    Happy New Year!

--- src/graphics/VertexArray.h
+++ src/graphics/VertexArray.h
@@ -1,4 +1,4 @@
-// Copyright Â© 2008-2024 Pioneer Developers. See AUTHORS.txt for details
+// Copyright Â© 2008-2025 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
 #ifndef _VERTEXARRAY_H
