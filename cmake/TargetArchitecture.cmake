34b36bdbeed4922989b9a991e443ec8c67599889
Dmitry Marakasov <474217+AMDmi3@users.noreply.github.com>
2025-02-04 15:37:53 +0300

    Add amd64 architecture
    It's how BSD systems spell x86_64, so it's also PIONEER_TARGET_INTEL.

--- cmake/TargetArchitecture.cmake
+++ cmake/TargetArchitecture.cmake
@@ -1,7 +1,7 @@
 # This script detects supported target architectures and configures test flags
 # accordingly
 
-if (${CMAKE_SYSTEM_PROCESSOR} MATCHES x86|x64)
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES x86|x64|amd64)
 	set(PIONEER_TARGET_INTEL ON)
 endif()
 
