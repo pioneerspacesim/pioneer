329832917c2cde1b2b347becb71721ed81f5a18d
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2025-02-11 15:14:47 +0000

    Increase BaseSphere subdivision to 5 for smoother atmosphere

--- src/BaseSphere.cpp
+++ src/BaseSphere.cpp
@@ -80,8 +80,12 @@ void BaseSphere::DrawAtmosphereSurface(Graphics::Renderer *renderer,
 
 	renderer->SetTransform(matrix4x4f(modelView * matrix4x4d::ScaleMatrix(rad) * invrot));
 
-	if (!m_atmos)
-		m_atmos.reset(new Drawables::Sphere3D(renderer, 4, 1.0f, ATTRIB_POSITION));
+	if (!m_atmos) {
+		// 5 subdivision = 20472 verts, 61440 indices NB: this should be configurable!
+		static constexpr int subdivisions = 5;
+		m_atmos.reset(new Drawables::Sphere3D(renderer, subdivisions, 1.0f, ATTRIB_POSITION));
+	}
+	
 	m_atmos->Draw(renderer, mat.Get());
 
 	renderer->GetStats().AddToStatCount(Graphics::Stats::STAT_ATMOSPHERES, 1);
