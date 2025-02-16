c522967fb0a6d84beaaed171d1c97991b5578629
Armin Kretschmer <armin.kretschmer@nefkom.net>
2025-02-16 19:07:39 +0100

    Show status "pending" if the mission has not yet been fully completed

--- data/modules/Combat/Combat.lua
+++ data/modules/Combat/Combat.lua
@@ -304,6 +304,7 @@ local onShipDestroyed = function (ship, attacker)
 				table.remove(mission.mercenaries, i)
 				if not mission.complete and (#mission.mercenaries == 0 or mission.dedication <= ARMEDRECON) then
 					mission.complete = true
+					mission.status = "PENDING"
 					Comms.ImportantMessage(l.MISSION_COMPLETE)
 				end
 				if attacker and attacker:isa("Ship") and attacker:IsPlayer() then
@@ -320,6 +321,7 @@ local missionTimer = function (mission)
 		if mission.complete or Game.time > mission.due then return true end -- already complete or too late
 		if Game.player.frameBody and Game.player.frameBody.path == mission.location then
 			mission.complete = true
+			mission.status = "PENDING"
 			Comms.ImportantMessage(l.MISSION_COMPLETE)
 			return true
 		else
