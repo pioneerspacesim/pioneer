6c16a8ef3c7e1027a47d0ad4a3dd2398a82f045b
Axtel Sturnclaw <sturnclaw@protonmail.com>
2025-06-07 02:14:10 -0400

    Track player criminal record in PlayerState
    - Simplify CrimeRecord handling with automagic tables.
    - Add compatibility code for v91 saves.

--- data/libs/PlayerState.lua
+++ data/libs/PlayerState.lua
@@ -34,13 +34,52 @@ local utils      = require 'utils'
 
 local PlayerState = {}
 
-local bookmarkIdx = 0
+-- Store player bookmark state:
 ---@type table<BookmarkID, SystemBookmark>
 local bookmarks = {}
+local bookmarkIdx = 0
 
+-- Store player financial status:
 local finances = {
 	cash = 0
 }
+
+-- container for crimes committed
+---@class CrimeRecord
+---@field clone fun(): CrimeRecord
+local CrimeRecord = utils.proto("CrimeRecord")
+
+function CrimeRecord:__clone()
+	-- automagically create empty crimetype records on access
+	-- This would be entirely unnecessary if crimetype records weren't tables with a single value set :(
+	self.crimetype = setmetatable({}, { __index = function(t, crime)
+		local crimetype = { count = 0 }
+		rawset(t, crime, crimetype)
+		return crimetype
+	end})
+	self.fine = 0
+end
+
+-- Append another crime record's list of crimes. Does not append the fine.
+function CrimeRecord:append(other)
+	for crime, v in pairs(other.crimetype) do
+		local record = self.crimetype[crime]
+		record.count = record.count + v.count
+	end
+end
+
+-- Automagically create empty crime records on access
+local automagic_record = {
+	__index = function(t, faction)
+		local record = CrimeRecord:clone()
+		rawset(t, faction, record)
+		return record
+	end
+}
+
+-- Store player legal status:
+local crime_record = setmetatable({}, automagic_record)
+local past_record = setmetatable({}, automagic_record)
 
 --=============================================================================
 
@@ -126,6 +165,174 @@ end
 
 --=============================================================================
 
+--
+-- Function: AddCrime
+--
+-- Add a crime to the player's criminal record
+--
+-- > PlayerState.AddCrime(crime, fine, faction)
+--
+-- Parameters:
+--
+--   crime - a string constant describing the crime
+--
+--   fine - an amount to add to the player's fine
+--
+--   faction - optional argument, defaults to the faction id of the system
+--             the player is currently in
+--
+function PlayerState.AddCrime (crime, fine, faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+	local record = crime_record[forFaction]
+
+	record.crimetype[crime].count = record.crimetype[crime].count + 1
+	record.fine = record.fine + fine
+end
+
+--
+-- Method: ClearCrimeFine
+--
+-- Clear the current record of outstanding fines and crimes for player
+--
+-- > PlayerState.ClearCrimeFine()
+-- > PlayerState.ClearCrimeFine(faction)
+-- > PlayerState.ClearCrimeFine(faction, forget)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the faction id of the system
+--             the player is currently in
+--
+--   forget - optional Boolean argument, defaults to false, resulting in the
+--            cleared fines to still be moved to the player's crime record
+--            over past offences
+--
+function PlayerState.ClearCrimeFine (faction, forget)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+
+	if not forget then
+		past_record[forFaction]:append(crime_record[forFaction])
+	end
+
+	crime_record[forFaction] = nil
+end
+
+--
+-- Method: ClearCrimeRecordHistory
+--
+-- Clear the player's crime record history, excluding current unpayed offences
+--
+-- > PlayerState.ClearCrimeRecordHistory()
+-- > PlayerState.ClearCrimeRecordHistory(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the faction if of the system
+--             the player is currently in
+--
+function PlayerState.ClearCrimeRecordHistory (faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+	-- use rawset so we don't create a garbage CrimeRecord
+	rawset(past_record, forFaction, nil)
+end
+
+--
+-- Function: GetCrimeRecord
+--
+-- Get players past criminal record of and total payed fine for faction
+--
+-- > criminalRecord, fine = PlayerState.GetCrimeRecord()
+-- > criminalRecord, fine = PlayerState.GetCrimeRecord(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the faction id of the system
+--             the player is in
+--
+--
+-- Return:
+--
+--   criminalRecord - a table with key being "crime constant" and "count"
+--                    for each crime committed
+--
+--   fine - the total fine of the player in faction
+--
+function PlayerState.GetCrimeRecord (faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+	local record = rawget(past_record, forFaction)
+
+	if not record then
+		return {}, 0
+	elseif Game.player.flightState == "HYPERSPACE" then
+		return {}, 0
+	else
+		return record.crimetype, record.fine
+	end
+end
+
+-- Function: GetCrimeOutstanding
+--
+-- Get the player's current outstanding criminal record and total unpaid fine for faction
+--
+-- > criminalRecord, fine = PlayerState.GetCrimeOutstanding()
+-- > criminalRecord, fine = PlayerState.GetCrimeOutstanding(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the faction id of the system
+--             the player is currently in
+--
+--
+-- Return:
+--
+--   criminalRecord - a table with key being "crime constant" and "count"
+--                    for each crime committed
+--
+--   fine - the total fine of the player in faction
+--
+function PlayerState.GetCrimeOutstanding (faction)
+	local forFaction = (faction and faction.id) or Game.system.faction.id
+	local record = rawget(crime_record, forFaction)
+
+	if not record then
+		return {}, 0
+	elseif Game.player.flightState == "HYPERSPACE" then
+		return {}, 0
+	else
+		return record.crimetype, record.fine
+	end
+end
+
+--
+-- Function: GetLegalStatus
+--
+-- > local legal_status = PlayerState.GetLegalStatus(faction)
+--
+-- Parameters:
+--
+--   faction - optional argument, defaults to the faction player is in.
+--
+--   legal_status - one of strings "CLEAN", "OFFENDER", "CRIMINAL", "OUTLAW",
+--	                or "FUGITIVE", mapping to translated strings in lang/ui-core
+--
+function PlayerState.GetLegalStatus (faction)
+	local _, fine = PlayerState.GetCrimeOutstanding()
+
+	if fine < 1 then
+		return('CLEAN')
+	elseif fine < 5500 then
+		return('OFFENDER')
+	elseif fine < 20000 then
+		return('CRIMINAL')
+	elseif fine < 100000 then
+		return('OUTLAW')
+	else
+		return('FUGITIVE')
+	end
+end
+
+--=============================================================================
+
 local function onGameStart()
 	local player = Game.player
 
@@ -139,6 +346,9 @@ local function onGameEnd()
 	bookmarkIdx = 0
 	bookmarks = {}
 	finances.cash = 0
+
+	crime_record = setmetatable({}, automagic_record)
+	past_record = setmetatable({}, automagic_record)
 end
 
 ---@return table
@@ -146,7 +356,9 @@ local function serialize()
 	local data = {
 		bookmarkIdx = bookmarkIdx,
 		bookmarks = bookmarks,
-		finances = finances
+		finances = finances,
+		crime_record = crime_record,
+		past_record = past_record,
 	}
 
 	return data
@@ -154,14 +366,37 @@ end
 
 ---@param data table
 local function unserialize(data)
-	bookmarkIdx = data.bookmarkIdx or bookmarkIdx
-	bookmarks = data.bookmarks or bookmarks
-	finances = data.finances or finances
+	-- SAVEBUMP: compatibility with v91 save before PlayerStateDB merge
+	if next(data) == nil then return end
+
+	bookmarkIdx = data.bookmarkIdx
+	bookmarks = data.bookmarks
+	finances = data.finances
+
+	crime_record = setmetatable(data.crime_record, automagic_record)
+	past_record = setmetatable(data.past_record, automagic_record)
 end
 
 Event.Register("onGameStart", onGameStart)
 Event.Register("onGameEnd", onGameEnd)
 
 Serializer:Register("PlayerStateDB", serialize, unserialize)
+Serializer:RegisterClass("CrimeRecord", CrimeRecord)
+
+-- SAVEBUMP: remove when >v91
+if Game.CurrentSaveVersion() == 91 then
+
+	local function unserialize_player(data)
+		if data.record then
+			crime_record = setmetatable(data.record, automagic_record)
+			past_record = setmetatable(data.record_old, automagic_record)
+		end
+	end
+
+	local function _serialize() return nil end
+
+	Serializer:Register("Player", _serialize, unserialize_player)
+
+end
 
 return PlayerState
