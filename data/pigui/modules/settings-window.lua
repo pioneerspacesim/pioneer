pioneerspacesim/pioneer

commit 2a4a802e204034570e9289b2785bc04455e2e435

experimental: Render rayleigh and mie scattering on planets

data/pigui/modules/settings-window.lua
@@ -165,6 +165,9 @@ local function showVideoOptions()
 	local gpuJobs = Engine.GetGpuJobsEnabled()
 	local disableScreenshotInfo = Engine.GetDisableScreenshotInfo()
 
+	local scatteringLabels = { lui.SCATTERING_OLD, lui.RAYLEIGH_FAST, lui.RAYLEIGH_ACCURATE }
+	local realisticScattering = Engine.GetRealisticScattering()
+
 	local cityDetail = keyOf(detailLabels,keyOf(detailLevels, Engine.GetCityDetailLevel()))-1
 	local displayNavTunnels = Engine.GetDisplayNavTunnels()
 	local displaySpeedLines = Engine.GetDisplaySpeedLines()
@@ -185,6 +188,11 @@ local function showVideoOptions()
 	if c then
 		local aa = aaModes[aaLabels[selectedAA+1]]
 		Engine.SetMultisampling(aa)
+	end
+
+	c,scattering = combo(lui.REALISTIC_SCATTERING, realisticScattering, scatteringLabels, lui.REALISTIC_SCATTERING_DESC)
+	if c then
+		Engine.SetRealisticScattering(scattering)
 	end
 
 	ui.columns(2,"video_checkboxes",false)

data/shaders/opengl/basesphere_uniforms.glsl
@@ -13,6 +13,8 @@ layout(std140) uniform BaseSphereData {
 	float geosphereAtmosFogDensity;		// TODO documentation
 	float geosphereAtmosInvScaleHeight; // TODO documentation
 	vec4 atmosColor;
+	vec3 coefficientsR;
+	vec3 coefficientsM;
 
 	// Eclipse data
 	Eclipse eclipse;

data/shaders/opengl/rayleigh.glsl
@@ -1,0 +1,137 @@
+float height(const in vec3 orig, const in vec3 center)
+{
+	vec3 r = orig - center;
+	float height = sqrt(dot(r, r)) - geosphereRadius;
+
+	return height;
+}
+
+void scatter(out float hr, out float hm, const in vec3 orig, const in vec3 center)
+{
+	float Hr = 7994;
+	float Hm = 1200;
+
+	float height = height(orig, center);
+
+	hr = -height / Hr;
+	hm = -height / Hm;
+}
+
+void findClosestHeight(out float h, out float t, const in vec3 orig, const in vec3 dir, const in vec3 center)
+{
+	vec3 radiusVector = center - orig;
+	vec3 tangent = dot(dir, radiusVector) * dir;
+	vec3 normal = radiusVector - tangent;
+	h = sqrt(dot(normal, normal));
+	t = dot(tangent, dir);
+}
+
+// error function
+float erf(const in float x)
+{
+	float a = 0.14001228904;
+	float four_over_pi = 1.27323954;
+
+	float x2 = x*x;
+	float r = -x2 * (four_over_pi + a * x2) / (1 + a * x2);
+	if (x > 0)
+		return  sqrt(1-exp(r)) * 0.5 + 0.5;
+	else
+		return -sqrt(1-exp(r)) * 0.5 + 0.5;
+}
+
+// predict out-scattering density based on coefficients
+float predictDensityOut(const in float atmosphereHeight, const in float height, const in float k, const in float b)
+{
+	if (height > atmosphereHeight)
+		return 0.f;
+
+	if (height < 0)
+		return k;
+
+	return k * exp(-height * b);
+}
+
+// predict in-scattering rate: from 0 to 1
+float predictDensityIn(const in float radius, const in float atmosphereHeight, const in float height, const in float c, const in float t)
+{
+	float minHeight = radius + height;
+	float h = sqrt(minHeight*minHeight + t*t); // height for our position
+
+	if (h > radius + atmosphereHeight) {
+		if (t > 0)
+			return 1.f; // no in-scattering, looking towards atmosphere
+		else
+			return 0.f; // looking from atmosphere
+	} else {
+		return erf(c * t); // erf is monotonic ascending
+	}
+}
+
+vec3 computeIncidentLight(const in vec3 sunDirection, const in vec3 dir, const in vec3 center, in float tmin, in float tmax)
+{
+	vec3 betaR = vec3(3.8e-6f, 13.5e-6f, 33.1e-6f);
+	vec3 betaM = vec3(21e-6f);
+
+	float earthRadius = geosphereRadius,
+	      atmosphereRadius = geosphereRadius * geosphereAtmosTopRad,
+	      atmosphereHeight = atmosphereRadius - earthRadius;
+
+	int numSamples = 16;
+	float segmentLength = (tmax - tmin) / numSamples;
+	float tCurrent = tmin;
+	vec3 sumR = vec3(0.0);
+	vec3 sumM = vec3(0.0); // mie and rayleigh contribution
+	float opticalDepthR = 0, opticalDepthM = 0;
+	float mu = dot(dir, sunDirection); // mu in the paper which is the cosine of the angle between the sun direction and the ray direction
+	float phaseR = 3.f / (16.f * 3.141592) * (1 + mu * mu);
+	float g = 0.76f;
+	float phaseM = 3.f / (8.f * 3.141592) * ((1.f - g * g) * (1.f + mu * mu)) / ((2.f + g * g) * pow(1.f + g * g - 2.f * g * mu, 1.5f));
+
+	float kR, bR, cR;
+	float kM, bM, cM;
+
+	kR = coefficientsR.x;
+	bR = coefficientsR.y;
+	cR = coefficientsR.z;
+
+	kM = coefficientsM.x;
+	bM = coefficientsM.y;
+	cM = coefficientsM.z;
+
+	for (int i = 0; i < numSamples; ++i) {
+		vec3 samplePosition = vec3(tCurrent + segmentLength * 0.5f) * dir;
+		float hr, hm;
+		scatter(hr, hm, samplePosition, center);
+		opticalDepthR += exp(hr) * segmentLength;
+		opticalDepthM += exp(hm) * segmentLength;
+
+		// light optical depth
+		float opticalDepthLightR = 0, opticalDepthLightM = 0;
+		vec3 samplePositionLight = samplePosition;
+
+		float hl, tl;
+		findClosestHeight(hl, tl, samplePositionLight, sunDirection, center);
+		hl -= earthRadius;
+
+		// find out-scattering density
+		opticalDepthLightR = predictDensityOut(atmosphereHeight, hl, kR, bR);
+		opticalDepthLightM = predictDensityOut(atmosphereHeight, hl, kM, bM);
+
+		// apply in-scattering filter
+		opticalDepthLightR *= predictDensityIn(earthRadius, atmosphereHeight, hl, cR, tl);
+		opticalDepthLightM *= predictDensityIn(earthRadius, atmosphereHeight, hl, cM, tl);
+
+		vec3 tau = -(betaR * (opticalDepthR + opticalDepthLightR) + betaM * 1.1f * (opticalDepthM + opticalDepthLightM));
+		vec3 tauR = tau + vec3(hr);
+		vec3 tauM = tau + vec3(hm);
+		vec3 attenuationR = exp(tauR) * segmentLength;
+		vec3 attenuationM = exp(tauM) * segmentLength;
+		sumR += attenuationR;
+		sumM += attenuationM;
+		tCurrent += segmentLength;
+	}
+
+	vec3 ret = (sumR * betaR * phaseR + sumM * betaM * phaseM);
+	return ret;
+}

data/shaders/opengl/rayleigh_accurate.frag
@@ -1,0 +1,49 @@
+// Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "attributes.glsl"
+#include "lib.glsl"
+#include "basesphere_uniforms.glsl"
+#include "rayleigh.glsl"
+
+uniform int NumShadows;
+
+in vec4 varyingEyepos;
+in vec4 vertexColor;
+
+out vec4 frag_color;
+
+void main(void)
+{
+	float skyNear, skyFar;
+	vec3 eyenorm = normalize(varyingEyepos.xyz);
+	vec3 specularHighlight = vec3(0.0);
+
+	sphereEntryExitDist(skyNear, skyFar, geosphereCenter, varyingEyepos.xyz, geosphereRadius * geosphereAtmosTopRad);
+
+	// a&b scaled so length of 1.0 means planet surface.
+	vec3 a = (skyNear * eyenorm - geosphereCenter) * geosphereInvRadius;
+	vec3 b = (skyFar * eyenorm - geosphereCenter) * geosphereInvRadius;
+
+	vec4 atmosDiffuse = vec4(0.0);
+
+#if (NUM_LIGHTS > 0)
+	vec3 surfaceNorm = normalize(skyNear * eyenorm - geosphereCenter);
+	for (int i=0; i<NUM_LIGHTS; ++i) {
+		vec3 lightDir = normalize(vec3(uLight[i].position));
+
+		float uneclipsed = clamp(calcUneclipsedSky(eclipse, NumShadows, a, b, lightDir), 0.0, 1.0);
+
+		float nDotVP =  max(0.0, dot(surfaceNorm, lightDir));
+		float nnDotVP = max(0.0, dot(surfaceNorm, -lightDir));  //need backlight to increase horizon
+		atmosDiffuse +=  uLight[i].diffuse * uneclipsed * 0.5*(nDotVP+0.5*clamp(1.0-nnDotVP*4.0,0.0,1.0) * INV_NUM_LIGHTS);
+
+		specularHighlight += computeIncidentLight(lightDir, eyenorm, geosphereCenter, skyNear, skyFar) * INV_NUM_LIGHTS;
+	}
+#endif
+
+	atmosDiffuse.a = 1.0;
+	frag_color = (atmosDiffuse *
+		vec4(specularHighlight.rgb, 1.0) * 20
+		);
+}

data/shaders/opengl/rayleigh_accurate.shaderdef
@@ -1,0 +1,13 @@
+## Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+## Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+Shader rayleigh_accurate
+
+Buffer LightData binding=0
+Buffer DrawData binding=1
+Buffer BaseSphereData binding=2
+
+PushConstant int NumShadows binding=0
+
+Vertex "rayleigh_accurate.vert"
+Fragment "rayleigh_accurate.frag"

data/shaders/opengl/rayleigh_accurate.vert
@@ -1,0 +1,14 @@
+// Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "attributes.glsl"
+#include "lib.glsl"
+#include "basesphere_uniforms.glsl"
+
+out vec4 varyingEyepos;
+
+void main(void)
+{
+	gl_Position = matrixTransform();
+	varyingEyepos = uViewMatrix * a_vertex;
+}

data/shaders/opengl/rayleigh_fast.frag
@@ -1,0 +1,46 @@
+// Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "attributes.glsl"
+#include "lib.glsl"
+#include "basesphere_uniforms.glsl"
+#include "rayleigh.glsl"
+
+uniform int NumShadows;
+
+in vec4 varyingEyepos;
+in vec4 vertexColor;
+
+out vec4 frag_color;
+
+void main(void)
+{
+	float skyNear, skyFar;
+	vec3 eyenorm = normalize(varyingEyepos.xyz);
+
+	sphereEntryExitDist(skyNear, skyFar, geosphereCenter, varyingEyepos.xyz, geosphereRadius * geosphereAtmosTopRad);
+
+	// a&b scaled so length of 1.0 means planet surface.
+	vec3 a = (skyNear * eyenorm - geosphereCenter) * geosphereInvRadius;
+	vec3 b = (skyFar * eyenorm - geosphereCenter) * geosphereInvRadius;
+
+	vec4 atmosDiffuse = vec4(0.0);
+
+#if (NUM_LIGHTS > 0)
+	vec3 surfaceNorm = normalize(skyNear * eyenorm - geosphereCenter);
+	for (int i=0; i<NUM_LIGHTS; ++i) {
+		vec3 lightDir = normalize(vec3(uLight[i].position));
+
+		float uneclipsed = clamp(calcUneclipsedSky(eclipse, NumShadows, a, b, lightDir), 0.0, 1.0);
+
+		float nDotVP =  max(0.0, dot(surfaceNorm, lightDir));
+		float nnDotVP = max(0.0, dot(surfaceNorm, -lightDir));  //need backlight to increase horizon
+		atmosDiffuse +=  uLight[i].diffuse * uneclipsed * 0.5*(nDotVP+0.5*clamp(1.0-nnDotVP*4.0,0.0,1.0) * INV_NUM_LIGHTS);
+	}
+#endif
+
+	atmosDiffuse.a = 1.0;
+	frag_color = (atmosDiffuse *
+		vec4(vertexColor.rgb, 1.0) * 20
+		);
+}

data/shaders/opengl/rayleigh_fast.shaderdef
@@ -1,0 +1,13 @@
+## Copyright © 2008-2023 Pioneer Developers. See AUTHORS.txt for details
+## Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+Shader rayleigh_fast
+
+Buffer LightData binding=0
+Buffer DrawData binding=1
+Buffer BaseSphereData binding=2
+
+PushConstant int NumShadows binding=0
+
+Vertex "rayleigh_fast.vert"
+Fragment "rayleigh_fast.frag"

commit 2d11e6442034761c2d071436617e40371038edf3

fix(ui): fix variable name for realisticScattering
Presumably a copy-paste error introduced in commit
2a4a802e204034570e9289b2785bc04455e2e435.

data/pigui/modules/settings-window.lua
@@ -186,9 +186,9 @@ local function showVideoOptions()
 		Engine.SetMultisampling(aa)
 	end
 
-	c,scattering = combo(lui.REALISTIC_SCATTERING, realisticScattering, scatteringLabels, lui.REALISTIC_SCATTERING_DESC)
+	c,realisticScattering = combo(lui.REALISTIC_SCATTERING, realisticScattering, scatteringLabels, lui.REALISTIC_SCATTERING_DESC)
 	if c then
-		Engine.SetRealisticScattering(scattering)
+		Engine.SetRealisticScattering(realisticScattering)
 	end
 
 	ui.columns(2,"video_checkboxes",false)