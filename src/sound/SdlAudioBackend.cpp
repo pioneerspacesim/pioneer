c1d025952b0824360da9f35266553e78b9d84fe4
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2026-01-05 22:05:15 +0100

    chore: Update year in copyright messages

--- src/sound/SdlAudioBackend.cpp
+++ src/sound/SdlAudioBackend.cpp
@@ -1,3 +1,6 @@
+// Copyright © 2008-2026 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
 #include "SdlAudioBackend.h"
 #include "MathUtil.h"
 #include "Pi.h"
