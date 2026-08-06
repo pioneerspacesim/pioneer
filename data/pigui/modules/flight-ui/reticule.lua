cbea6f834a99484cde938d3f7eb512669207eeb1
Axtel Sturnclaw <sturnclaw@protonmail.com>
2026-08-06 19:47:23 -0400

    Reduce landing line complexity
    - Render single yaw tick always pointing "down" to the pad. Simple and intuitive when upside down. The yaw tick extents are now 180 degrees instead of 90.
    - Remove horizon line red coloration when inverted, yaw tick already correctly handles this.

--- data/pigui/modules/flight-ui/reticule.lua
+++ data/pigui/modules/flight-ui/reticule.lua
@@ -286,7 +286,11 @@ end
 local function displayReticuleLandingAssist()
 	local target = Game.player:GetNavTarget()
 
-	if target and target:IsStation() and target:GetAssignedBayNumber(Game.player) >= 0 then
+	local displayLandingAsst = target and target:IsStation()
+		and Game.player:GetFlightState() == "FLYING"
+		and target:GetAssignedBayNumber(Game.player) >= 0
+
+	if displayLandingAsst then
 		-- position and { heading, pitch, roll } deviation from the target
 		local _, dev = target:GetAssignedBayNavError(Game.player)
 		assert(dev)
@@ -310,22 +314,17 @@ local function displayReticuleLandingAssist()
 		local pos_a = proj_center - extent
 		local pos_b = proj_center + extent
 
-		ui.addLine(pos_a, pos_b, math.abs(roll) > ui.pi_2 and colors.landingAsstInverted or colors.landingAsstNormal, 1)
+		ui.addLine(pos_a, pos_b, colors.landingAsstHorizon, 1)
 
-		-- Render the yaw error tick below (forwards) or above (rear) the line.
+		-- Render the yaw error tick below the line.
 		-- This tick does not indicate direction to the pad, but rather the ship's
 		-- rotation error relative to the pad's orientation.
-		local heading_line_pos = proj_center - extent * (dev.x / ui.pi_2)
+		local heading_line_pos = proj_center - extent * (dev.x / ui.pi)
 		local heading_line_dir = orient:left()
 
-		local heading = dev.x
-		if math.abs(heading) > ui.pi_2 then
-			local inv = (math.pi - math.abs(heading)) * math.sign(heading)
-			heading_line_pos = proj_center + extent * (inv / ui.pi_2)
-			heading_line_dir = orient:right()
-		end
-
-		ui.addLine(heading_line_pos, heading_line_pos + heading_line_dir * 6, colors.landingAsstYawTick, 1)
+		local yaw_aligned = math.abs(dev.x) < math.deg2rad(10)
+		local yaw_col = yaw_aligned and colors.landingAsstHorizon or colors.landingAsstYawTick
+		ui.addLine(heading_line_pos, heading_line_pos + heading_line_dir * 6, yaw_col, yaw_aligned and 2 or 1)
 
 		-- TODO: render artificial-horizon shaded area helper by deriving angle-on-circle of line end positions
 		-- and manually submitting points on the circle edge between the two angles.
